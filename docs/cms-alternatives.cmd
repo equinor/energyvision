04c36f7de1a53fb4d381375d810a823c1229d58d
Victor Nystad <vnystad@gmail.com>
2019-12-19 11:03:27 +0100

    update doc

--- docs/cms-alternatives.cmd
+++ docs/cms-alternatives.cmd
@@ -4,11 +4,40 @@ CMS alternatives
 Sanity
 ------
 
+- Headless CMS
+- Cloudbased, offers an enterprise subscription model
+- Open source editor
+
+### Trusted by… ###
+
+- Amedia
+- Schibsted
+- Eurostar
+
+### Technical ###
+
+- API: GraphQL (in beta) or GROQ (open source query language invented by Sanity)
+- Cloudbased
+- Open source editor built with React
+
 
 
 Strapi
 ------
 
+- Headless CMS
+- Open source (can be hosted in Azure)
 
+### Trusted by… ###
 
+- IBM
+- Walmart
+- Discovery Channel
 
+### Technical ###
+
+- API: GraphQL or Rest
+- Self hosted
+- Built with Nodejs
+- Supports PostgreSQL, MongoDB, SQLite, MySQL and MariaDB
+- Can use Redis for caching / message broker
