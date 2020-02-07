4915c39b2ce1dcc2d08757c8af6840668d1cdf2a
Sven van Steenis <sven.vsteenis@gmail.com>
2020-02-07 12:42:55 +0100

    add page content type

---
+++ apps/backend-strapi/api/page/controllers/page.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    ctx.query = {
+      ...ctx.query,
+      status: "published"
+    };
+
+    if (ctx.query._q) {
+      entities = await strapi.services.page.search(ctx.query);
+    } else {
+      entities = await strapi.services.page.find(ctx.query);
+    }
+
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.page })
+    );
+  },
+  async findOne(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      status: "published"
+    };
+
+    const entity = await strapi.services.page.find(ctx.query);
+
+    return sanitizeEntity(entity[0], { model: strapi.models.page });
+  }
+};
