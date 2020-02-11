e7ea23f23299abd3d0057f62c201a3e8bc7484b9
Sven van Steenis <sven.van.steenis@no.experis.com>
2020-02-11 13:42:03 +0100

    add ArticleList component

---
+++ apps/frontend-gatsby/src/components/ArticleList.js
@@ -0,0 +1,31 @@
+import Img from 'gatsby-image'
+import React from 'react'
+import { convertDate } from '../helpers'
+import articleListStyle from './articleList.module.css'
+
+export const ArticleList = (props) => {
+  const { articles } = props
+
+  return (
+    <ul className={articleListStyle.list}>
+      {articles.map((document) => {
+        const article = document.node
+        const thumbnail = article.image.childImageSharp.fixed
+
+        return (
+          <li className={articleListStyle.item} key={article.id}>
+            <Img fixed={thumbnail} />
+
+            <div className={articleListStyle.itemBody}>
+              <span className={articleListStyle.date}>
+                {convertDate(article.created_at, 'MMMM d, yyyy')}
+              </span>
+              <h3 className={articleListStyle.title}>{article.title}</h3>
+              <p className={articleListStyle.ingress}>{article.ingress}</p>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
