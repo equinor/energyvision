8cbb6ff11e1596cf4c830c4447b2bfd47e66d921
Sven van Steenis <sven.van.steenis@no.experis.com>
2020-02-11 13:42:42 +0100

    update index page to display list of articles

--- apps/frontend-gatsby/src/pages/index.js
+++ apps/frontend-gatsby/src/pages/index.js
@@ -1,12 +1,44 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { ArticleList } from '../components/ArticleList'
 
-const IndexPage = () => (
+import indexStyle from './index.module.css'
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
+
+    <section className={indexStyle.section}>
+      <h2 className={indexStyle.title}>Latest News</h2>
+      <ArticleList articles={data.allStrapiArticle.edges} />
+    </section>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          title
+          ingress
+          body
+          created_at
+          image {
+            childImageSharp {
+              fixed(width: 350, height: 200) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
