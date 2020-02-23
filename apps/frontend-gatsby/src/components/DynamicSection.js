cfe9b635445300f455ea9b6552ddf1f1d11cb86a
Sven van Steenis <sven.van.steenis@no.experis.com>
2020-02-23 16:06:22 +0100

    fix DynamicSection warnings

--- apps/frontend-gatsby/src/components/DynamicSection.js
+++ apps/frontend-gatsby/src/components/DynamicSection.js
@@ -1,11 +1,13 @@
+import React from 'react'
 import * as Components from './sections'
 
 export const DynamicSection = (props) => {
   const { sections } = props
-  {
-    sections.map((section) => {
-      const Component = Components[section.__typename]
-      return Component ? <Component {...section} key={section.id} /> : null
-    })
-  }
+
+  return sections.map((section) => {
+    const Component = Components[section.__typename]
+    return Component ? (
+      <Component {...section} key={section.__typename + section.id} />
+    ) : null
+  })
 }
