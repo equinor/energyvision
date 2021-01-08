dd120501cf720fded13ad50b4ff856dbb7534c88
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-01-08 15:49:05 +0100

    🔧 Add eslint (#101)
    Add eslint for the web project as a start, and postpone the decision of monorepos and stuff

---
+++ web/.eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y'],
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^React$',
+      },
+    ],
+    'import/newline-after-import': ['error'],
+    'react/no-array-index-key': ['error'],
+    'react/no-typos': ['error'],
+    'react/destructuring-assignment': ['error', 'always'],
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off', // because React 17
+    'react/react-in-jsx-scope': 'off', // because React 17
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'jsx-a11y/accessible-emoji': ['warn'],
+    'jsx-a11y/control-has-associated-label': ['warn'],
+    'jsx-a11y/label-has-associated-control': [
+      'warn',
+      {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: [],
+        assert: 'either',
+        depth: 25,
+      },
+    ],
+  },
+}
