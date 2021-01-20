c6a6fec08f2caff666ad72f4560cd664e089e47c
Victor Nystad <vnys@equinor.com>
2021-01-20 12:58:03 +0100

    ✨ Add storybook and sample components
    Installed the default storybook, but changed some things around a bit.
    Haven’t added things like testing and the addon-a11y yet.

---
+++ components/.storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming'
+
+const theme = create({
+  base: 'light',
+  brandTitle: 'Energy Vision',
+  brandImage: 'https://eds-static.equinor.com/logo/equinor-logo-horizontal.svg#red',
+})
+
+export { theme }
