17aab17c9a1e2003935a70cf4dbaa01ce1190d21
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-08 11:49:00 +0100

    🚧 Shared global config. Simple POC for theme

---
+++ web/styles/elements.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components'
+
+export const elements = css`
+  html {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+  }
+`
---
+++ web/styles/generic.ts
@@ -0,0 +1,9 @@
+import { css } from 'styled-components'
+
+export const generic = css`
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+`
