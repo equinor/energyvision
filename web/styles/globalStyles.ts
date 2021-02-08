8bd0de129863402766bc7403693e1b13844339e3
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-08 11:49:00 +0100

    🚧 Shared global config. Simple POC for theme

---
+++ web/styles/globalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+import { normalize } from 'styled-normalize'
+import { colors } from './settings'
+import { generic } from './generic'
+import { elements } from './elements'
+
+export const GlobalStyle = createGlobalStyle`
+  ${colors}
+  ${normalize}
+  ${generic}
+  ${elements}
+  * {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+`
