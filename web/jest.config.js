0fe459f8ffeb27c703b2cdf1d9911d27e82e7786
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-10 12:22:35 +0100

    ✅ Add testing-library with Jest as test runner (#118)

---
+++ web/jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '@components': '<rootDir>/components/src',
+  },
+}
