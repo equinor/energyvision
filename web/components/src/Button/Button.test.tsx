cc4c64ba0d01cd84133309bc974bed821fba85bc
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-10 12:24:17 +0100

    ✅ Add some simple tests (#118)
    There's a problem with the background color test as the CSS variables are not translated to an actual value. Don't know how this will impact the suggested solution with variables :thinking:

---
+++ web/components/src/Button/Button.test.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
+import styled from 'styled-components'
+import { Button } from './Button'
+
+afterEach(cleanup)
+
+const StyledButton = styled(Button)`
+  clip-path: unset;
+`
+
+describe('Button', () => {
+  it('Has provided label', () => {
+    const labelText = 'The button label'
+    const { queryByText } = render(<Button label={labelText} />)
+    expect(queryByText(labelText)).toBeInTheDocument()
+  })
+  // This test doesn't work
+  /*   it('Is not a primary button by default', () => {
+    const { container } = render(<Button label="My button" />)
+    expect(container.firstChild).toHaveStyleRule('background-color', '#ffe7d6')
+  }) */
+  it('Can extend the css for the component', () => {
+    const { container } = render(<StyledButton label="button-test" />)
+    expect(container.firstChild).toHaveStyleRule('clip-path', 'unset')
+  })
+})
