75b9f019e36993797617c126475fe8034b9249f8
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-15 14:14:01 +0100

    🚧 Husky will be a good idea (#123)

--- web/components/src/Card/Action.tsx
+++ web/components/src/Card/Action.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { forwardRef, HTMLAttributes } from 'react'
 import styled from 'styled-components'
 
-export type ActionProps = {} & HTMLAttributes<HTMLDivElement>
+export type ActionProps = HTMLAttributes<HTMLDivElement>
 
 const StyledAction = styled.div`
   text-transform: uppercase;
--- web/components/src/Card/Content.tsx
+++ web/components/src/Card/Content.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { forwardRef, HTMLAttributes } from 'react'
 import styled from 'styled-components'
 
-export type ContentProps = {} & HTMLAttributes<HTMLDivElement>
+export type ContentProps = HTMLAttributes<HTMLDivElement>
 
 const StyledContent = styled.div``
 
