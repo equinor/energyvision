4ead3aa70fb74ef71b4aa33608502a4ed958d066
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-15 15:42:55 +0100

    ✨ Add header level to title (#123)

--- web/components/src/Card/Title.tsx
+++ web/components/src/Card/Title.tsx
@@ -2,17 +2,22 @@ import * as React from 'react'
 import { forwardRef, HTMLAttributes } from 'react'
 import styled from 'styled-components'
 
-export type TitleProps = HTMLAttributes<HTMLDivElement>
+export type TitleProps = {
+  /** Header level */
+  level?: 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+} & HTMLAttributes<HTMLDivElement>
 
 const StyledTitle = styled.div`
   font-weight: bold;
 `
 
-export const Title = forwardRef<HTMLDivElement, TitleProps>(function CardMedia({ children, ...rest }, ref) {
-  const props = {
-    ...rest,
-    ref,
-  }
-
-  return <StyledTitle {...props}>{children}</StyledTitle>
+export const Title = forwardRef<HTMLDivElement, TitleProps>(function CardMedia(
+  { level = 'h3', children, ...rest },
+  ref,
+) {
+  return (
+    <StyledTitle as={level} ref={ref} {...rest}>
+      {children}
+    </StyledTitle>
+  )
 })
