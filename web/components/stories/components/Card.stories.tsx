b5245331239d0054f1371ad616d3eb0f90db0dbd
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-15 15:52:51 +0100

    📝 Add one more card with real content. Examples on permitted formatting options (#123)

--- web/components/stories/components/Card.stories.tsx
+++ web/components/stories/components/Card.stories.tsx
@@ -50,21 +50,43 @@ export const Default: Story<CardProps> = (args) => <Card>Some default example</C
 Default.storyName = 'Default'
 
 export const NewsCard: Story<CardProps> = (args) => (
-  <Card style={{ maxWidth: '20rem' }}>
-    <Card.Media>
-      <RatioBox>
-        <ImagePlaceholder />
-      </RatioBox>
-    </Card.Media>
-    <Card.Eyebrow>Overline</Card.Eyebrow>
-    <Card.Title>Meet the coolest robots working in energy</Card.Title>
-    <Card.Content>
-      Lorem excepteur cillum exercitation cillum deserunt exercitation non in velit in id veniam. Deserunt culpa
-      proident est fugiat Lorem eu qui eiusmod. Labore aliquip enim est magna eu dolore dolor occaecat dolor laboris
-      laborum. Occaecat cupidatat minim aute esse velit sint. Sunt non cupidatat cillum irure.
-    </Card.Content>
-    <Card.Action>--&gt;</Card.Action>
-  </Card>
+  <>
+    <Card style={{ maxWidth: '20rem' }}>
+      <Card.Media>
+        <RatioBox>
+          <ImagePlaceholder />
+        </RatioBox>
+      </Card.Media>
+      <Card.Eyebrow>Overline</Card.Eyebrow>
+      <Card.Title>Dolor sit amet</Card.Title>
+      <Card.Content>
+        <p>
+          Lorem <strong>excepteur</strong> cillum exercitation cillum deserunt exercitation non in velit in id veniam.
+          Deserunt culpa proident est fugiat Lorem eu qui <i>eiusmod</i>. Labore aliquip enim est magna CO
+          <sub>2</sub> eu dolore dolor occaecat dolor laboris laborum. Occaecat cupidatat minim aute esse velit sint.
+          Sunt non cupidatat cillum irure.
+        </p>
+      </Card.Content>
+      <Card.Action>--&gt;</Card.Action>
+    </Card>
+    <Card style={{ maxWidth: '20rem' }}>
+      <Card.Media>
+        <RatioBox>
+          <ImagePlaceholder />
+        </RatioBox>
+      </Card.Media>
+      <Card.Eyebrow>September 24, 2020</Card.Eyebrow>
+      <Card.Title>Dogger Bank wind farm places record-breaking turbine order boosting local jobs</Card.Title>
+      <Card.Content>
+        <p>
+          Dogger Bank Wind Farm and GE Renewable Energy have today, 22 September, announced contracts confirming the
+          13MW Haliade-X turbine for the Dogger Bank A and Dogger Bank B phases of the world’s largest offshore wind
+          farm.
+        </p>
+      </Card.Content>
+      <Card.Action>--&gt;</Card.Action>
+    </Card>
+  </>
 )
 
 NewsCard.storyName = 'News card'
