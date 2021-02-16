7d844cdf716eb37df0e6b0e5cb341fcbdf1a2a6f
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-16 10:20:32 +0100

    ⚗ For the sake of discussions tomorrow, add EDS Card (#123)

--- web/components/src/Card/Eyebrow.tsx
+++ web/components/src/Card/Eyebrow.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { forwardRef, HTMLAttributes } from 'react'
 import styled from 'styled-components'
 
-export type EyebrowProps = {} & HTMLAttributes<HTMLDivElement>
+export type EyebrowProps = HTMLAttributes<HTMLDivElement>
 
 const StyledEyebrow = styled.div`
   text-transform: uppercase;
