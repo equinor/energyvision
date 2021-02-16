bb1eabb60a78ab8ee008d590b8f204f6df4250e0
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-02-16 10:20:32 +0100

    ⚗ For the sake of discussions tomorrow, add EDS Card (#123)

--- web/components/stories/components/Card.stories.tsx
+++ web/components/stories/components/Card.stories.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react'
 import styled from 'styled-components'
 import { Card, CardProps } from '@components'
+import { Card as EDSCard, CardProps as EDSCardProps, Typography } from '@equinor/eds-core-react'
+
+const { CardHeader, CardHeaderTitle, CardActions, CardMedia } = EDSCard
 
 const ImagePlaceholder = styled.div`
   background-color: hsl(0, 0%, 86%);
@@ -27,6 +30,15 @@ const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
   grid-gap: 2rem;
+`
+
+const StyledEDSCard = styled(EDSCard)`
+  height: 100%;
+  grid-template-rows: auto auto auto 1fr;
+`
+
+const BottomCardActions = styled(CardActions)`
+  align-self: end;
 `
 
 export default {
@@ -106,3 +118,58 @@ NewsCard.parameters = {
     storyDescription: 'News cards should always have a date',
   },
 }
+
+export const EdsNewsCard: Story<EDSCardProps> = () => (
+  <Wrapper>
+    <a href="#">
+      <StyledEDSCard>
+        <CardMedia>
+          <RatioBox>
+            <ImagePlaceholder />
+          </RatioBox>
+        </CardMedia>
+        <CardHeader>
+          <CardHeaderTitle>
+            <Typography variant="overline" as="span">
+              Overline
+            </Typography>
+            <Typography variant="h3">Dolor sit amet</Typography>
+          </CardHeaderTitle>
+        </CardHeader>
+        <p>
+          Lorem <strong>excepteur</strong> cillum exercitation cillum deserunt exercitation non in velit in id veniam.
+          Deserunt culpa proident est fugiat Lorem eu qui <i>eiusmod</i>. Labore aliquip enim est magna CO
+          <sub>2</sub> eu dolore dolor occaecat dolor laboris laborum.
+        </p>
+        <BottomCardActions>--&gt;</BottomCardActions>
+      </StyledEDSCard>
+    </a>
+    <a href="#">
+      <StyledEDSCard>
+        <CardMedia>
+          <RatioBox>
+            <ImagePlaceholder />
+          </RatioBox>
+        </CardMedia>
+        <CardHeader>
+          <CardHeaderTitle>
+            <Typography variant="overline" as="span">
+              September 24, 2020
+            </Typography>
+            <Typography variant="h3">
+              Dogger Bank wind farm places record-breaking turbine order boosting local jobs
+            </Typography>
+          </CardHeaderTitle>
+        </CardHeader>
+        <p>
+          Dogger Bank Wind Farm and GE Renewable Energy have today, 22 September, announced contracts confirming the
+          13MW Haliade-X turbine for the Dogger Bank A and Dogger Bank B phases of the world’s largest offshore wind
+          farm.
+        </p>
+        <BottomCardActions>--&gt;</BottomCardActions>
+      </StyledEDSCard>
+    </a>
+  </Wrapper>
+)
+
+EdsNewsCard.storyName = 'EDS news card'
