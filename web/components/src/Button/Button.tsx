98bca60f664f7eb40a5bea86937d4f061997174b
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-02-26 08:58:24 +0100

    ⚗ use next/link with button if href is passed

--- web/components/src/Button/Button.tsx
+++ web/components/src/Button/Button.tsx
@@ -1,12 +1,25 @@
 import { forwardRef } from 'react'
 import { Button as EdsButton, ButtonProps as EdsButtonProps } from '@equinor/eds-core-react'
+import Link from 'next/link'
 
 export type ButtonProps = EdsButtonProps
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...rest }, ref) => (
-  <EdsButton ref={ref} {...rest}>
-    {children}
-  </EdsButton>
-))
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, href = null, ...rest }, ref) => {
+  if (href) {
+    return (
+      <Link href={href} passHref>
+        <EdsButton ref={ref} {...rest}>
+          {children}
+        </EdsButton>
+      </Link>
+    )
+  }
+
+  return (
+    <EdsButton ref={ref} {...rest}>
+      {children}
+    </EdsButton>
+  )
+})
 
 Button.displayName = 'Button'
