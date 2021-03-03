773aa0c670a6ec2a1889c50eb4b50a7fafacb5cb
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-03 12:25:20 +0100

    ✨ Add a page for showcasing news cards in a group

---
+++ web/components/stories/pages/News/data.ts
@@ -0,0 +1,40 @@
+export const data = [
+  {
+    id: '1',
+    title: 'Dogger Bank wind farm places record-breaking turbine order boosting local jobs',
+    eyebrow: 'September 24, 2020',
+    lead: `Dogger Bank Wind Farm and GE Renewable Energy have today, 22 September, announced contracts confirming the
+    13MW Haliade-X turbine for the Dogger Bank A and Dogger Bank B phases of the world’s largest offshore wind
+    farm.`,
+  },
+  {
+    id: '2',
+    title: 'Production continues at Johan Sverdrup',
+    eyebrow: 'September 24, 2020',
+    lead: `The Johan Sverdrup field on the Norwegian continental shelf is impacted by strike due to a breach of mediation in the negotiations between the employer organization, the Norwegian Oil and Gas Association, and the trade union Lederne.`,
+  },
+  {
+    id: '3',
+    title: 'Equinor partners with BP in US offshore wind to capture value and create platform for growth',
+    eyebrow: 'September 24, 2020',
+    lead: `Equinor (OSE: EQNR, NYSE: EQNR) has entered into an agreement with BP (LSE: BP, NYSE: BP) to sell 50% non-operated interests in the Empire Wind and Beacon Wind assets on the US east coast for a total consideration before adjustments of USD 1.1 billion. Through this transaction, the two companies are also establishing a strategic partnership for further growth within offshore wind in the US.`,
+  },
+  {
+    id: '4',
+    title: 'The Breidablikk field ready for development',
+    eyebrow: 'September 24, 2020',
+    lead: `Equinor and partners Petoro, Vår Energi and ConocoPhillips Skandinavia have decided to develop the Breidablikk field in the North Sea. Today the partnership will submit the plan for development and operation to the Minister of Petroleum and Energy, in addition to awarding contracts for subsea facilities and upgrading of the Grane platform.`,
+  },
+  {
+    id: '5',
+    title: 'Equinor partners with BP in US offshore wind to capture value and create platform for growth',
+    eyebrow: 'September 24, 2020',
+    lead: `Equinor (OSE: EQNR, NYSE: EQNR) has entered into an agreement with BP (LSE: BP, NYSE: BP) to sell 50% non-operated interests in the Empire Wind and Beacon Wind assets on the US east coast for a total consideration before adjustments of USD 1.1 billion. Through this transaction, the two companies are also establishing a strategic partnership for further growth within offshore wind in the US.`,
+  },
+  {
+    id: '6',
+    title: 'Equinor completes world’s first logistics operation with a drone to an offshore installation',
+    eyebrow: 'September 24, 2020',
+    lead: `A drone flew a 3D-printed part for the lifeboat system from the Mongstad base to the Troll A platform in the North Sea. The operation was completed efficiently and according to plan.`,
+  },
+]
