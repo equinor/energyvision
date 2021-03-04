e4d1d3a6923dc5c5bcc668bb40d0f55333de09f3
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-04 15:12:27 +0100

    ✅ add tests for Link component
    missing tests for the different variants still

---
+++ web/components/src/Link/Link.test.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
+import { Link } from './Link'
+
+afterEach(cleanup)
+
+describe('Link', () => {
+  const href = 'https://equinor.com'
+  const label = 'equinor.com'
+
+  it('renders an anchor tag', () => {
+    const { container } = render(<Link href={href}>{label}</Link>)
+    expect(container.querySelector('a')).toBeInTheDocument()
+  })
+
+  it('renders the correct href on the anchor tag', () => {
+    const { container } = render(<Link href={href}>{label}</Link>)
+    expect(container.querySelector('a')?.href).toMatch(href)
+  })
+
+  it('renders the correct label in the anchor tag', () => {
+    const { queryByText } = render(<Link href={href}>{label}</Link>)
+    expect(queryByText(label)).toBeInTheDocument()
+  })
+
+  it('renders the link with an icon if prop external = true', () => {
+    const { container } = render(
+      <Link href={href} external>
+        {label}
+      </Link>,
+    )
+    expect(container.querySelector('svg')).toBeInTheDocument()
+  })
+})
