eef3e60277d86df76e6a1ea9bf29b202204d6300
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-04 15:30:45 +0100

    ✅ add additional tests for Button
    using EDS components so don't have to test
    for everything here. We should however make
    sure the next/link href is passed correctly

--- web/components/src/Button/Button.test.tsx
+++ web/components/src/Button/Button.test.tsx
@@ -11,19 +11,27 @@ const StyledButton = styled(Button)`
   clip-path: unset;
 `
 
+const href = 'https://equinor.com'
+
 describe('Button', () => {
   it('Has provided label', () => {
     const labelText = 'The button label'
     const { queryByText } = render(<Button>{labelText}</Button>)
     expect(queryByText(labelText)).toBeInTheDocument()
   })
-  // This test doesn't work
-  /*   it('Is not a primary button by default', () => {
-    const { container } = render(<Button label="My button" />)
-    expect(container.firstChild).toHaveStyleRule('background-color', '#ffe7d6')
-  }) */
+
   it('Can extend the css for the component', () => {
     const { container } = render(<StyledButton></StyledButton>)
     expect(container.firstChild).toHaveStyleRule('clip-path', 'unset')
+  })
+
+  it('renders an anchor tag as button if href is passed', () => {
+    const { container } = render(<Button href={href}>click me</Button>)
+    expect(container.querySelector('a')).toBeInTheDocument()
+  })
+
+  it('renders the correct href', () => {
+    const { container } = render(<Button href={href}>click me</Button>)
+    expect(container.querySelector('a')?.href).toMatch(href)
   })
 })
