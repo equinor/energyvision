e5f232581b270b5e3795a2884289f7ff4da783e2
Victor Nystad <vnys@equinor.com>
2021-03-08 12:37:28 +0100

    ✨ Add typography variables

--- web/styles/globalStyles.ts
+++ web/styles/globalStyles.ts
@@ -1,16 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
 import { colors } from './settings'
+import { typography } from './settings'
 import { generic } from './generic'
 import { elements } from './elements'
 
 export const GlobalStyle = createGlobalStyle`
   ${colors}
+  ${typography}
   ${normalize}
   ${generic}
   ${elements}
   * {
     font-family: Equinor, 'Open Sans', sans-serif;
   }
-
 `
