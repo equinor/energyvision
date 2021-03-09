5be55880226c88e2193549b7191c1201941fc8ca
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-09 12:45:36 +0100

    🚧 Some colors (#173)

--- web/styles/settings.ts
+++ web/styles/settings.ts
@@ -2,6 +2,61 @@ import { css } from 'styled-components'
 
 export const colors = css`
   :root {
+    /* Primary colours */
+    --moss-green-100: hsl(184, 100%, 24%);
+    --moss-green-90: hsl(184, 31%, 58%);
+    --moss-green-80: hsl(184, 31%, 74%);
+    --moss-green-70: hsl(184, 30%, 84%);
+    --moss-green-60: hsl(184, 31%, 89%);
+    --moss-green-50: hsl(184, 30%, 96%);
+
+    --energy-red-100: hsl(348, 100%, 54%);
+    --energy-red-90: hsl(348, 100%, 67%);
+    --energy-red-80: hsl(347, 100%, 77%);
+    --energy-red-70: hsl(347, 100%, 86%);
+    --energy-red-60: hsl(349, 100%, 91%);
+    --energy-red-50: hsl(347, 100%, 96%);
+
+    --black-100: hsl(0, 0%, 0%);
+    --black-80: hsl(0, 0%, 20%);
+
+    /* Supporting colours */
+    --slate-blue-100: hsl(206, 32%, 21%);
+    --slate-blue-90: hsl(207, 14%, 37%);
+    --slate-blue-80: hsl(207, 8%, 53%);
+    --slate-blue-70: hsl(206, 9%, 68%);
+    --slate-blue-60: hsl(206, 9%, 84%);
+    --slate-blue-50: hsl(210, 8%, 90%);
+
+    --mist-blue-100: hsl(199, 58%, 90%);
+
+    --litchen-green-100: hsl(138, 67%, 94%);
+
+    --spruce-wood-100: hsl(25, 100%, 92%);
+    --spruce-wood-90: hsl(25, 100%, 94%);
+
+    --heritage-red-100: hsl(343, 100%, 25%);
+    --heritage-red-90: hsl(343, 42%, 43%);
+    --heritage-red-80: hsl(344, 32%, 62%);
+    --heritage-red-70: hsl(344, 32%, 77%);
+    --heritage-red-60: hsl(340, 32%, 91%);
+
+    --grey-100: hsl(0, 0%, 9%);
+    --grey-90: hsl(0, 0%, 24%);
+    --grey-80: hsl(0, 0%, 34%);
+    --grey-70: hsl(0, 0%, 40%);
+    --grey-60: hsl(0, 0%, 44%);
+    --grey-50: hsl(0, 0%, 55%);
+    --grey-40: hsl(0, 0%, 75%);
+    --grey-30: hsl(0, 0%, 86%);
+    --grey-20: hsl(0, 0%, 92%);
+    --grey-10: hsl(0, 0%, 97%);
+
+    /* Interactive colours */
+
+    /* EDS themes
+    
+
     /* Background colors */
     --bg-moss-green: #deedee;
     --bg-mist-blue: #d5eaf4;
