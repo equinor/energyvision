173932fcfe8db95a29bdc4643a6858f90a0e1b80
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-10 13:22:06 +0100

    💄 Add a couple of more colors (#173)

--- web/styles/settings.ts
+++ web/styles/settings.ts
@@ -3,7 +3,8 @@ import { css } from 'styled-components'
 export const colors = css`
   :root {
     /* Primary colours */
-    --moss-green-100: hsl(184, 100%, 24%);
+    --moss-green-100: hsl(184, 100%, 17%);
+    --moss-green-95: hsl(184, 100%, 24%);
     --moss-green-90: hsl(184, 31%, 58%);
     --moss-green-80: hsl(184, 31%, 74%);
     --moss-green-70: hsl(184, 30%, 84%);
@@ -20,8 +21,11 @@ export const colors = css`
     --black-100: hsl(0, 0%, 0%);
     --black-80: hsl(0, 0%, 20%);
 
+    --white-100: hsl(0, 0%, 100%);
+
     /* Supporting colours */
-    --slate-blue-100: hsl(206, 32%, 21%);
+    --slate-blue-100: hsl(206, 34%, 14%);
+    --slate-blue-95: hsl(206, 32%, 21%);
     --slate-blue-90: hsl(207, 14%, 37%);
     --slate-blue-80: hsl(207, 8%, 53%);
     --slate-blue-70: hsl(206, 9%, 68%);
@@ -41,6 +45,18 @@ export const colors = css`
     --heritage-red-70: hsl(344, 32%, 77%);
     --heritage-red-60: hsl(340, 32%, 91%);
 
+    --clear-red-100: hsl(0, 100%, 46%);
+    --clear-red-90: hsl(0, 85%, 68%);
+    --clear-red-80: hsl(0, 85%, 78%);
+    --clear-red-70: hsl(0, 86%, 86%);
+    --clear-red-60: hsl(0, 88%, 94%);
+
+    --green-100: hsl(118, 44%, 35%);
+    --green-90: hsl(118, 44%, 50%);
+
+    --orange-100: hsl(34, 100%, 34%);
+    --orange-90: hsl(34, 100%, 50%);
+
     --grey-100: hsl(0, 0%, 9%);
     --grey-90: hsl(0, 0%, 24%);
     --grey-80: hsl(0, 0%, 34%);
@@ -52,7 +68,8 @@ export const colors = css`
     --grey-20: hsl(0, 0%, 92%);
     --grey-10: hsl(0, 0%, 97%);
 
-    /* Interactive colours */
+    --transparent-black: hsla(0, 0%, 0%, 20);
+    --transparent-white: hsla(0, 0%, 100%, 20);
 
     /* EDS themes
     
