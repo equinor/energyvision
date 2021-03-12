e6f42947504e613dd52d04ff068375609915b287
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-12 08:45:42 +0100

    🚧 create menu prototype based on EDS tabs
    very much a work in progress and subject to change

---
+++ web/components/stories/components/mockData/menu.ts
@@ -0,0 +1,16 @@
+export const MockMenuData = [
+  {
+    label: 'Hello there',
+    links: [
+      { label: 'General kenobi', url: 'https://equinor.com' },
+      { label: 'You are a bold one', url: 'https://equinor.com' },
+    ],
+  },
+  {
+    label: 'Do',
+    links: [
+      { label: 'Or do not', url: 'https://equinor.com' },
+      { label: 'There is no try', url: 'https://equinor.com' },
+    ],
+  },
+]
