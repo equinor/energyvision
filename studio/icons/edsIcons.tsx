f32af079168f8741484389206d49e895a397c298
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-15 13:46:08 +0100

    ✨ Add desk structure with a dummy icon (#198)
    We need to figure out what we need for icons, just added a dummy wrapper as a start

---
+++ studio/icons/edsIcons.tsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/default
+import React from 'react'
+import { list } from '@equinor/eds-icons'
+import SvgIcon from './SvgIcon'
+
+export const EdsList = (): JSX.Element => {
+  return <SvgIcon icon={list} />
+}
