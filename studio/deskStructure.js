76c718430dc856ce5c042ab83abb785ebf587306
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-16 13:38:03 +0100

    🚧 add basic news article schema

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,7 +1,24 @@
+/* eslint-disable no-unused-vars */
 import S from '@sanity/desk-tool/structure-builder'
 import { EdsList } from './icons'
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default () =>
-  S.list()
-    .title('Content')
-    .items([S.listItem().icon(EdsList).title('News')])
+export default () => {
+  const listItems = [
+    S.listItem()
+      .icon(EdsList)
+      .title('News')
+      .schemaType('news')
+      .child(
+        S.documentList()
+          .id('news')
+          .title('News articles')
+          .filter('_type == "news"')
+          .canHandleIntent((_name, params, _context) => {
+            return params.type === 'news'
+          }),
+      ),
+  ]
+
+  return S.list().title('Content').items(listItems)
+}
