edcb863b9d209c68aa429eee7d167b850361d0b4
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-17 11:47:16 +0100

    🚧 add basic rich text editor
    schema types from sanity are currently incomplete
    we'll need to either create our own or make do with
    placeholder types with liberal use of `any` for now

---
+++ studio/schemas/editors/blockContentType.tsx
@@ -0,0 +1,162 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react'
+import { ExternalLinkRenderer, SuperScriptRenderer, SubScriptRenderer } from '../components'
+import { link, attach_file, external_link } from '@equinor/eds-icons'
+import { IconSuperScript, IconSubScript, EdsIcon } from '../../icons'
+
+// TODO: write proper types if none become available from @sanity
+export type BlockFieldType = {
+  type: string
+  styles: { title: string; value: string }[]
+  lists: { title: string; value: string }[] | []
+  marks: {
+    decorators: any[]
+    annotations: any[]
+  }
+}
+
+export type BlockContentProps = {
+  h1?: boolean
+  h2?: boolean
+  h3?: boolean
+  h4?: boolean
+  internalLink?: boolean
+  externalLink?: boolean
+  attachment?: boolean
+  lists?: boolean
+}
+
+export const configureBlockContent = (options: BlockContentProps = {}): BlockFieldType => {
+  const {
+    h1 = false,
+    h2 = true,
+    h3 = true,
+    h4 = false,
+    internalLink = true,
+    externalLink = true,
+    attachment = false,
+    lists = true,
+  } = options
+
+  const config: BlockFieldType = {
+    type: 'block',
+    styles: [{ title: 'Normal', value: 'normal' }],
+    lists: lists
+      ? [
+          { title: 'Numbered', value: 'number' },
+          { title: 'Bullet', value: 'bullet' },
+        ]
+      : [],
+    marks: {
+      decorators: [
+        { title: 'Strong', value: 'strong' },
+        { title: 'Emphasis', value: 'em' },
+        {
+          title: 'Sub',
+          value: 'sub',
+          blockEditor: {
+            icon: IconSubScript,
+            render: SubScriptRenderer,
+          },
+        },
+        {
+          title: 'Super',
+          value: 'sup',
+          blockEditor: {
+            icon: IconSuperScript,
+            render: SuperScriptRenderer,
+          },
+        },
+      ],
+      annotations: [],
+    },
+  }
+
+  const h1Config = { title: 'Title (h1)', value: 'h1' }
+  const h2Config = { title: 'Title (h2)', value: 'h2' }
+  const h3Config = { title: 'Subtitle (h3)', value: 'h3' }
+  const h4Config = { title: 'Subsubtitle (h4)', value: 'h4' }
+
+  const externalLinkConfig = {
+    name: 'link',
+    type: 'object',
+    title: 'External link',
+    blockEditor: {
+      render: ExternalLinkRenderer,
+      icon: () => EdsIcon(external_link),
+    },
+    fields: [
+      {
+        name: 'href',
+        type: 'url',
+        validation: (Rule: any) => Rule.uri({ scheme: ['http', 'https', 'tel', 'mailto'] }),
+      },
+    ],
+  }
+
+  const internalLinkConfig = {
+    name: 'internalLink',
+    type: 'object',
+    title: 'Internal link',
+    blockEditor: {
+      icon: () => EdsIcon(link),
+    },
+    fields: [
+      {
+        name: 'reference',
+        type: 'reference',
+        to: [
+          /* objects of allowed page types */
+        ],
+      },
+    ],
+  }
+
+  const attachmentConfig = {
+    name: 'attachment',
+    type: 'object',
+    title: 'Attachment',
+    blockEditor: {
+      icon: () => EdsIcon(attach_file),
+    },
+    fields: [
+      {
+        name: 'reference',
+        type: 'reference',
+        to: [{ type: 'fileUpload' }],
+      },
+    ],
+  }
+
+  if (h1) {
+    config.styles.push(h1Config)
+  }
+
+  if (h2) {
+    config.styles.push(h2Config)
+  }
+
+  if (h3) {
+    config.styles.push(h3Config)
+  }
+
+  if (h4) {
+    config.styles.push(h4Config)
+  }
+
+  if (externalLink) {
+    config.marks.annotations.push(externalLinkConfig)
+  }
+
+  if (internalLink) {
+    config.marks.annotations.push(internalLinkConfig)
+  }
+
+  if (attachment) {
+    config.marks.annotations.push(attachmentConfig)
+  }
+
+  return config
+}
+
+export default configureBlockContent()
