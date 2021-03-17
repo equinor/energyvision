9031e39139ea13105385b125c0a4a5776a63ca23
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-17 14:48:44 +0100

    🚧 basic initial version of BlockQuote component
    still very much a work in progress

---
+++ web/components/src/Quote/BlockQuote.tsx
@@ -0,0 +1,44 @@
+import { forwardRef } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  align-items: row;
+`
+
+const Quote = styled.blockquote`
+  background: none;
+  margin: 0;
+`
+
+const Author = styled.figcaption`
+  text-align: right;
+  margin-top: 1rem;
+  font-style: italic;
+`
+
+export type BlockQuoteProps = {
+  quote: string
+  author: string
+  image?: string | null // url
+}
+
+export const BlockQuote = forwardRef<HTMLDivElement, BlockQuoteProps>(function BlockQuote({
+  quote,
+  author,
+  image = null,
+}: BlockQuoteProps) {
+  return (
+    <Container>
+      <figure>
+        <Quote>{quote}</Quote>
+        <Author>{author}</Author>
+      </figure>
+      {image ? (
+        <div>
+          <img src={image} alt="" />
+        </div>
+      ) : null}
+    </Container>
+  )
+})
