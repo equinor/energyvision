ba90636cc3f8a7fe50f498cd2211d7580650b179
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-17 11:47:16 +0100

    🚧 add basic rich text editor
    schema types from sanity are currently incomplete
    we'll need to either create our own or make do with
    placeholder types with liberal use of `any` for now

---
+++ studio/schemas/components/renderers.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ExternalLinkRenderer: React.FC = ({ children }) => <span>{children}</span>
+
+export const SubScriptRenderer: React.FC = ({ children }) => <sub>{children}</sub>
+
+export const SuperScriptRenderer: React.FC = ({ children }) => <sup>{children}</sup>
