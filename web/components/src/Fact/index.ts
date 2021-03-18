3a4515ef352d017ceb54b49ee50e633fd75a6605
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-18 15:39:44 +0100

    🚧 Add Text component and centered option for title (#205)

--- web/components/src/Fact/index.ts
+++ web/components/src/Fact/index.ts
@@ -1,13 +1,16 @@
 import { Fact as FactWrapper, FactProps } from './Fact'
 import { Title } from './Title'
+import { Text } from './Text'
 
 type FactCompundProps = typeof FactWrapper & {
   Title: typeof Title
+  Text: typeof Text
 }
 
 const Fact = FactWrapper as FactCompundProps
 
 Fact.Title = Title
+Fact.Text = Text
 
 export { Fact }
 export type { FactProps }
