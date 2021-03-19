3a6d7be55aae3bcaa03a2f4ed388c6eea678194d
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-19 12:33:57 +0100

    ♻️ Change the Title API to alignment (#205)

--- web/components/src/Fact/Title.tsx
+++ web/components/src/Fact/Title.tsx
@@ -2,29 +2,26 @@ import { forwardRef, HTMLAttributes } from 'react'
 import { Typography } from '@equinor/eds-core-react'
 import styled from 'styled-components'
 
-type StyledTypographyType = {
-  center: boolean
-}
 /* @TODO: How to use this typography scale with EDS */
-const StyledTypography = styled(Typography)<StyledTypographyType>`
+const StyledTypography = styled(Typography)<TitleProps>`
   font-size: var(--typeScale-2);
-  ${({ center }) =>
-    center && {
+  ${({ alignment }) =>
+    alignment === 'center' && {
       textAlign: 'center',
     }}
 `
 
 export type TitleProps = {
   /* It the title should be centered or not. Default to true */
-  center?: boolean
+  alignment?: 'center' | 'left'
 } & HTMLAttributes<HTMLHeadingElement>
 
 export const Title = forwardRef<HTMLDivElement, TitleProps>(function CardMedia(
-  { center = true, children, ...rest },
+  { alignment = 'center', children, ...rest },
   ref,
 ) {
   return (
-    <StyledTypography center={center} variant="h3" ref={ref} {...rest}>
+    <StyledTypography alignment={alignment} variant="h3" ref={ref} {...rest}>
       {children}
     </StyledTypography>
   )
