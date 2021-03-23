2bf22545136ce0cea5d95ee394ab1bc6c8381de7
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-23 09:43:27 +0100

    🏷️ add placeholder types for validation rules

--- studio/types/schemaTypes.d.ts
+++ studio/types/schemaTypes.d.ts
@@ -1,5 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
 export interface Preview {
   title: string
   subtitle: string
   media: JSX.Element
+}
+
+export type ValidationContext = {
+  document: { [key: string]: any }
+  parent: { [key: string]: any }
+  path: string[]
+}
+
+export type ValidationResult = true | string | Promise<true | string>
+
+export interface ValidationRule {
+  required: () => ValidationRule
+  custom: (value: any, context?: ValidationContext) => ValidationResult
+  min: (arg0: number) => ValidationRule
+  max: (arg0: number) => ValidationRule
+  error: (arg0: string) => ValdiationRule
+  warning: (arg0: string) => ValidationRule
+  [x: string]: any
 }
