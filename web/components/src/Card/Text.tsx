c36c2dae86be0e78967aa8c1c8ee69c76e045993
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-23 12:06:28 +0100

    🚧 Try to use Typography for the lead text. (#211)
    So hard without a proper ts support for the block content. Had to ignore ts error just to be able to move on

--- web/components/src/Card/Text.tsx
+++ web/components/src/Card/Text.tsx
@@ -1,17 +1,18 @@
-import * as React from 'react'
+// This component is just useful if the text is _not_ from a rich text editor
+
 import { forwardRef, HTMLAttributes } from 'react'
-import { Typography } from '@equinor/eds-core-react'
+// import { Typography } from '@equinor/eds-core-react'
 import styled from 'styled-components'
 
-export type CardTextProps = HTMLAttributes<HTMLHeadingElement>
+export type CardTextProps = HTMLAttributes<HTMLDivElement>
 
-const StyledText = styled(Typography)`
+const StyledText = styled.div<CardTextProps>`
   /** Spacing */
   padding: 0 1rem;
 `
-export const Text = forwardRef<HTMLElement, CardTextProps>(function CardMedia({ children, ...rest }, ref) {
+export const Text = forwardRef<HTMLDivElement, CardTextProps>(function CardMedia({ children, ...rest }, ref) {
   return (
-    <StyledText variant="ingress" ref={ref} {...rest}>
+    <StyledText ref={ref} {...rest}>
       {children}
     </StyledText>
   )
