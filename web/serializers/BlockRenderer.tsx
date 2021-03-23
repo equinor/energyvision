b70694ee714896b6e965021cc77364eff7d18edd
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-23 12:06:28 +0100

    🚧 Try to use Typography for the lead text. (#211)
    So hard without a proper ts support for the block content. Had to ignore ts error just to be able to move on

---
+++ web/serializers/BlockRenderer.tsx
@@ -0,0 +1,18 @@
+import BlockContent from '@sanity/block-content-to-react'
+import { Typography } from '@equinor/eds-core-react'
+
+const BlockRenderer = (props: { children: any; node: any }) => {
+  const { children, node } = props
+  const { style = 'normal' } = node
+
+  if (style === 'normal') {
+    return <Typography variant="ingress"> {children}</Typography>
+  }
+
+  // Fall back to default handling
+  // eslint-disable-next-line
+  // @ts-ignore: Ask Sanity about progress for supporting types in block-content
+  return BlockContent.defaultSerializers.types.block(props)
+}
+
+export default BlockRenderer
