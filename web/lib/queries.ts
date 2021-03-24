8a116e0b7a18705103e19c2902d5e4f74e7419c0
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-24 09:49:33 +0100

    🚧 Add a start for news article (#208)

--- web/lib/queries.ts
+++ web/lib/queries.ts
@@ -1,7 +1,24 @@
-export const newsQuery = /* groq */ `
-*[_type == "news"] | order(publishDateTime desc, _updatedAt desc) {
+const newsFields = /* groq */ `
   "id": _id,
- title,
- "slug": slug.current,
- ingress,
+  title,
+  publishDateTime,
+  "slug": slug.current,
+  ingress,
+`
+
+export const allNewsQuery = /* groq */ `
+*[_type == "news"] | order(publishDateTime desc, _updatedAt desc) {
+  ${newsFields}
 }`
+
+export const newsQuery = `
+{
+  "news": *[_type == "news" && slug.current == $slug] | order(_updatedAt desc) | [0] {
+    content,
+    ${newsFields}
+  },
+}`
+
+export const newsSlugsQuery = `
+*[_type == "news" && defined(slug.current)][].slug.current
+`
--- web/lib/sanity.ts
+++ web/lib/sanity.ts
@@ -1,2 +1,5 @@
+import { createPreviewSubscriptionHook } from 'next-sanity'
 // eslint-disable-next-line no-unused-vars
 import { sanityConfig } from './config'
+
+export const usePreviewSubscription = createPreviewSubscriptionHook(sanityConfig)
