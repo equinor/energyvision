acca5c995196facfb5e57754da851c154d4f7b05
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-25 11:05:15 +0100

    ✨ add IntlProvider

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head'
 import { useState, useCallback } from 'react'
+import { IntlProvider } from 'react-intl'
 import { Button, Topbar, Menu } from '@components'
 import { GlobalStyle } from '../styles/globalStyles'
 import { createGlobalStyle } from 'styled-components'
@@ -23,23 +24,26 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     }
   }, [])
 
+  // TODO: get locale from Sanity
   return (
     <>
-      <Head>
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-      </Head>
-      <GlobalStyle />
-      <TopbarOffset />
+      <IntlProvider locale="no" defaultLocale="en">
+        <Head>
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+        </Head>
+        <GlobalStyle />
+        <TopbarOffset />
 
-      <Topbar height={topbarHeight} ref={topbarRef}>
-        <Menu items={MockMenuData} offset={topbarHeight}></Menu>
-        <Button variant="outlined" onClick={toggleTheme}>
-          Toggle theme
-        </Button>
-      </Topbar>
+        <Topbar height={topbarHeight} ref={topbarRef}>
+          <Menu items={MockMenuData} offset={topbarHeight}></Menu>
+          <Button variant="outlined" onClick={toggleTheme}>
+            Toggle theme
+          </Button>
+        </Topbar>
 
-      <Component {...pageProps} />
+        <Component {...pageProps} />
+      </IntlProvider>
     </>
   )
 }
