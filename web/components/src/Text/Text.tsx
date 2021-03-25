a5c9bb32ae61c532da5700d146fc5b509db33c77
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-03-25 12:54:11 +0100

    🚧 Add the first version of a text component (#165)

--- web/components/src/Text/Text.tsx
+++ web/components/src/Text/Text.tsx
@@ -2,21 +2,32 @@ import { forwardRef, HTMLAttributes, CSSProperties } from 'react'
 import { Typography } from '@equinor/eds-core-react'
 import styled from 'styled-components'
 
-const StyledText = styled(Typography)``
+const StyledText = styled(Typography)`
+  font-size: var(--size);
+  line-height: var(--lineHeight-3);
+`
 
 export type TextProps = {
-  size: 'normal' | 'lg'
+  size?: 'regular' | 'md'
 } & HTMLAttributes<HTMLHeadingElement>
 
 /* Should be easy enough to change later on */
+const sizes = {
+  regular: 'var(--typeScale-1)',
+  md: 'var(--typeScale-2)',
+}
 
-export const Text = forwardRef<HTMLDivElement, TextProps>(function Text({ style, children, ...rest }, ref) {
+export const Text = forwardRef<HTMLDivElement, TextProps>(function Text(
+  { size = 'regular', style, children, ...rest },
+  ref,
+) {
   return (
     <StyledText
       ref={ref}
       style={
         {
           ...style,
+          '--size': sizes[size],
         } as CSSProperties
       }
       {...rest}
