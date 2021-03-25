cac84ee54bcb2a307b6d844263367e9dad55455e
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-25 13:25:59 +0100

    💄 add Equinor font (#166)
    revisit this at a later stage to add preloading and such

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -29,6 +29,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <>
       <IntlProvider locale="no" defaultLocale="en">
         <Head>
+          {/* TODO: load the font in a better way */}
+          <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-font.css" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
         </Head>
