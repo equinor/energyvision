88d9fc2659358e2832ca31720b831f47ebfd022f
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-26 09:48:37 +0100

    ♻️ update date/time components for consistency

--- web/components/src/FormattedDateTime/FormattedTime.tsx
+++ web/components/src/FormattedDateTime/FormattedTime.tsx
@@ -1,10 +1,23 @@
 import { FormattedTime as ReactIntlTime } from 'react-intl'
+import { StyledDate, TimeIcon } from './shared'
 
-export type TimeProps = {
+export type FormattedTimeProps = {
   datetime: string
+  withIcon?: boolean
 }
 
-export const FormattedTime = ({ datetime }: TimeProps): JSX.Element => {
+export const FormattedTime = ({ datetime, withIcon = false }: FormattedTimeProps): JSX.Element => {
+  if (withIcon) {
+    return (
+      <StyledDate>
+        <TimeIcon />
+        <time dateTime={datetime}>
+          <ReactIntlTime value={new Date(datetime)} />
+        </time>
+      </StyledDate>
+    )
+  }
+
   return (
     <time dateTime={datetime}>
       <ReactIntlTime value={new Date(datetime)} />
