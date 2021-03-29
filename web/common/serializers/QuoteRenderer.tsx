3394ea2a7a2d902538a5180cbf0a48d0c67580b7
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-03-29 21:58:41 +0200

    🚧 improve structure & styling of pullQuote

--- web/common/serializers/QuoteRenderer.tsx
+++ web/common/serializers/QuoteRenderer.tsx
@@ -2,7 +2,7 @@ import { PullQuote } from '@components'
 
 export const QuoteRenderer = (child: { node: any }): JSX.Element => {
   const { node } = child
-  const { quote, author, image }: { quote: string; author: string; image: any } = node
+  const { quote, author, authorTitle, image }: { quote: string; author: string; authorTitle: string; image: any } = node
 
-  return <PullQuote quote={quote} author={author} image={image} />
+  return <PullQuote quote={quote} author={author} authorTitle={authorTitle} image={image} />
 }
