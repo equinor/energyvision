56823bc53bae3badf3e81d8fd2b0aa50957c0794
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-06 13:16:14 +0200

    ♻️ Use the compsed type for image. Added attribution.

--- studio/schemas/documents/news.js
+++ studio/schemas/documents/news.js
@@ -92,31 +92,7 @@ export default {
     {
       name: 'heroImage',
       title: 'Hero image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-      fields: [
-        {
-          name: 'alt',
-          type: 'string',
-          title: 'Alternative text',
-          description: 'Important for SEO and accessiblity.',
-          options: {
-            isHighlighted: true,
-          },
-        },
-        {
-          name: 'caption',
-          type: 'string',
-          title: 'Caption',
-        },
-        {
-          name: 'attribution',
-          type: 'string',
-          title: 'Attribution',
-        },
-      ],
+      type: 'imageWithAltAndCaption',
     },
     {
       name: 'ingress',
