98bdcda3ecf794f7083c29c04816f11b4918d586
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-04-07 11:50:12 +0200

    🐛 use forwardRef for CharCounterEditor

--- studio/schemas/components/CharCounterEditor.tsx
+++ studio/schemas/components/CharCounterEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 // eslint-disable-next-line import/no-unresolved
 import { BlockEditor } from 'part:@sanity/form-builder'
 import blocksToText from '../../helpers/blocksToText'
@@ -6,7 +6,10 @@ import blocksToText from '../../helpers/blocksToText'
 type CharCounterEditorProps = {
   value: []
 }
-export function CharCounterEditor({ value = [], ...rest }: CharCounterEditorProps): JSX.Element {
+export const CharCounterEditor = forwardRef(function CharCounterEditor({
+  value = [],
+  ...rest
+}: CharCounterEditorProps): JSX.Element {
   const plainText = blocksToText(value)
   return (
     <div>
@@ -14,4 +17,4 @@ export function CharCounterEditor({ value = [], ...rest }: CharCounterEditorProp
       <div style={{ color: 'var(--card-muted-fg-color)', fontSize: '0.8125rem' }}>Characters: {plainText.length}</div>
     </div>
   )
-}
+})
