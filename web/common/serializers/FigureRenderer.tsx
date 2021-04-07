a073ef0d1eedcbaedf1c4acaa7c6cda06e1d35a0
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-04-07 15:52:58 +0200

    🚧 add initial version of FigureRenderer

--- web/common/serializers/FigureRenderer.tsx
+++ web/common/serializers/FigureRenderer.tsx
@@ -1,3 +1,20 @@
-export const FigureRenderer = () => {
-  return <div>TODO: Figure</div>
+import Img from 'next/image'
+import { SanityImageObject } from '@sanity/image-url/lib/types/types'
+import { imageProps } from '../../common/helpers'
+
+type FigureNode = {
+  _key: string
+  _type: 'imageWithAltAndCaption'
+  attribution?: string
+  caption?: string
+  image: { _type: 'imageWithAlt'; alt: string; asset: SanityImageObject }
+}
+
+export const FigureRenderer = (child: { node: FigureNode }) => {
+  const { node } = child
+  const { image = undefined } = node
+
+  if (!image) return false
+
+  return <Img {...imageProps(image.asset, 800)} alt={image.alt} sizes="80rem" layout="intrinsic" />
 }
