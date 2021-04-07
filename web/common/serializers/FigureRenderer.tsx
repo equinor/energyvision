c93e6e0739ca2e12f25c73b3437195674757f597
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-04-07 16:07:23 +0200

    🚧 render figure caption & attribution

--- web/common/serializers/FigureRenderer.tsx
+++ web/common/serializers/FigureRenderer.tsx
@@ -1,6 +1,11 @@
 import Img from 'next/image'
 import { SanityImageObject } from '@sanity/image-url/lib/types/types'
 import { imageProps } from '../../common/helpers'
+import styled from 'styled-components'
+
+const Figure = styled.figure`
+  margin: var(--spacing-medium) 0;
+`
 
 type FigureNode = {
   _key: string
@@ -12,9 +17,19 @@ type FigureNode = {
 
 export const FigureRenderer = (child: { node: FigureNode }) => {
   const { node } = child
-  const { image = undefined } = node
+  const { image = undefined, caption, attribution } = node
 
   if (!image) return false
 
-  return <Img {...imageProps(image.asset, 800)} alt={image.alt} sizes="80rem" layout="intrinsic" />
+  // TODO: add styling for figcaption
+  return (
+    <Figure>
+      <Img {...imageProps(image.asset, 800)} alt={image.alt} sizes="80rem" layout="intrinsic" />
+      {caption || attribution ? (
+        <figcaption>
+          {caption} {attribution}
+        </figcaption>
+      ) : null}
+    </Figure>
+  )
 }
