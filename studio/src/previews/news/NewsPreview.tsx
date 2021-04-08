a713a6a305329e65241a34c76d5a4371502f5f23
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-08 18:44:17 +0200

    🚧 Add preview mode in sanity studio. (#234)
    Not working yet, trying to figure out what is Vercel woo in example

---
+++ studio/src/previews/news/NewsPreview.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './IframePreview.module.css'
+
+export default function NewsPreview(props) {
+  const { document } = props
+  const { displayed } = document
+  console.log('displayed', displayed, props)
+  if (!displayed?.slug?.current) {
+    return <div>The product needs a slug before it can be previewed.</div>
+  }
+  const url =
+    process.env.NODE_ENV === 'production'
+      ? `../../news/${displayed?.slug?.current}?preview`
+      : `http://localhost:3000/news/${displayed?.slug?.current}?preview`
+  return (
+    <div className={styles.componentWrapper}>
+      <div className={styles.iframeContainer}>
+        <iframe src={url} title="preview" frameBorder={'0'} />
+      </div>
+    </div>
+  )
+}
