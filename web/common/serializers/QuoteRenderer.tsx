99375b2c08a7e7d248b92a01faeafbc8bf6baeb3
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-04-08 15:50:23 +0200

    ♻️ WIP: refactor Quote to compound componenton

--- web/common/serializers/QuoteRenderer.tsx
+++ web/common/serializers/QuoteRenderer.tsx
@@ -1,8 +1,21 @@
 import { PullQuote } from '@components'
+import Img from 'next/image'
+import { imageProps } from '../helpers'
 
 export const QuoteRenderer = (child: { node: any }): JSX.Element => {
   const { node } = child
   const { quote, author, authorTitle, image }: { quote: string; author: string; authorTitle: string; image: any } = node
 
-  return <PullQuote quote={quote} author={author} authorTitle={authorTitle} image={image} />
+  return (
+    <PullQuote quote={quote}>
+      <PullQuote.Quote text={quote} />
+      <PullQuote.Author name={author} title={authorTitle} />
+
+      {image ? (
+        <PullQuote.Media>
+          <Img {...imageProps(image.asset, 300, 1)} alt={image.alt} layout="intrinsic" />
+        </PullQuote.Media>
+      ) : null}
+    </PullQuote>
+  )
 }
