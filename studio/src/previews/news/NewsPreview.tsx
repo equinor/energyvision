910528444e3ec76d2866acbccaca45f1438d8cc6
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-09 11:05:18 +0200

    ⚗ Experiments for urls (#234)

--- studio/src/previews/news/NewsPreview.tsx
+++ studio/src/previews/news/NewsPreview.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import styles from './IframePreview.module.css'
 
 export default function NewsPreview(props) {
+  const baseUrl = process.env.SANITY_STUDIO_PROJECT_URL
+  console.log('baseUrl', baseUrl, process.env.NODE_ENV)
+
   const { document } = props
   const { displayed } = document
   if (!displayed?.slug?.current) {
     return <div>The product needs a slug before it can be previewed.</div>
   }
-  const url =
-    process.env.NODE_ENV === 'production'
-      ? `../../news/${displayed?.slug?.current}?preview`
-      : `http://localhost:3000/news/${displayed?.slug?.current}?preview`
+  const url = `${baseUrl}/news/${displayed?.slug?.current}?preview`
+
   return (
     <div className={styles.componentWrapper}>
       <div className={styles.iframeContainer}>
