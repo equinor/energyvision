d900b51977b80e6d10524c381a4f03f0f48954da
petterwildhagen <pewi@equinor.com>
2021-04-09 15:22:33 +0200

    Created E2E skeleton to run tests in parallel for different platforms

---
+++ e2e/platforms.js
@@ -0,0 +1,19 @@
+export const platforms = [{
+  os: 'Windows',
+  os_version: '10',
+  browserName: 'Chrome',
+  browser_version: 'latest',
+},
+{
+  os: 'Windows',
+  os_version: '10',
+  browserName: 'Firefox',
+  browser_version: 'latest',
+},
+{
+  os: 'Windows',
+  os_version: '10',
+  browserName: 'Edge',
+  browser_version: 'latest',
+}
+]
