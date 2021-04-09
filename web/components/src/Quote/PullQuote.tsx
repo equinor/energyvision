2e2b96ba80103eb170bbee9d43ee30fdf4ea283f
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-04-09 11:29:52 +0200

    ♻️ convert named grid lines to grid-template-areas
    much cleaner

--- web/components/src/Quote/PullQuote.tsx
+++ web/components/src/Quote/PullQuote.tsx
@@ -7,42 +7,27 @@ const Container = styled.figure`
   display: grid;
   row-gap: var(--spacing-medium);
   margin: var(--spacing-xLarge) 0;
-  grid-template-rows:
-    [media-start author-start] min-content
-    [media-end author-end quote-start] min-content [quote-end];
-  grid-template-columns:
-    [media-start quote-start] 5rem
-    [media-end spacing-start] var(--spacing-medium)
-    [spacing-end author-start] 1fr [author-end quote-end];
+  grid-template-rows: min-content min-content;
+  grid-template-columns: 5rem var(--spacing-medium) 1fr;
+  grid-template-areas:
+    'media spacing author'
+    'quote quote quote';
 
   // TODO: find better solution for image sizing
   @media (min-width: 800px) {
-    grid-template-rows:
-      [quote-start media-start] min-content
-      [quote-end author-start] min-content [author-end media-end];
-
-    grid-template-columns:
-      [quote-start author-start] 1fr
-      [quote-end author-end spacing-start] var(--spacing-medium)
-      [spacing-end media-start] 8rem [media-end];
+    grid-template-columns: 1fr var(--spacing-medium) 8rem;
+    grid-template-areas:
+      'quote spacing media'
+      'author spacing media';
   }
   @media (min-width: 1000px) {
-    grid-template-columns:
-      [quote-start author-start] 1fr
-      [quote-end author-end spacing-start] var(--spacing-medium)
-      [spacing-end media-start] 10rem [media-end];
+    grid-template-columns: 1fr var(--spacing-medium) 10rem;
   }
   @media (min-width: 1200px) {
-    grid-template-columns:
-      [quote-start author-start] 1fr
-      [quote-end author-end spacing-start] var(--spacing-medium)
-      [spacing-end media-start] 13rem [media-end];
+    grid-template-columns: 1fr var(--spacing-medium) 13rem;
   }
   @media (min-width: 1400px) {
-    grid-template-columns:
-      [quote-start author-start] 1fr
-      [quote-end author-end spacing-start] var(--spacing-medium)
-      [spacing-end media-start] 16rem [media-end];
+    grid-template-columns: 1fr var(--spacing-medium) 16rem;
   }
 `
 
