a24d124a7d38299a7faf7a76acc4b6433c2d9f06
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-09 13:56:59 +0200

    🔥 Disable the read token (#234)

--- web/lib/sanity.server.ts
+++ web/lib/sanity.server.ts
@@ -11,7 +11,7 @@ export const sanityClient = createClient(sanityConfig)
 export const previewClient = createClient({
   ...sanityConfig,
   useCdn: false,
-  token: process.env.SANITY_API_TOKEN,
+  // token: process.env.SANITY_API_TOKEN,
 })
 
 export const getClient = (preview: boolean) => (preview ? previewClient : sanityClient)
