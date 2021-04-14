055b259608ce16006a1fead65244020a63023b56
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-14 15:45:12 +0200

    ♻️ Remove the old image object. Rename the new to Image (#244)

--- studio/schemas/documents/news.js
+++ studio/schemas/documents/news.js
@@ -98,13 +98,7 @@ export default {
       name: 'content',
       title: 'Content',
       type: 'array',
-      of: [
-        blockContentType,
-        { type: 'pullQuote' },
-        { type: 'imageWithAltAndCaption' },
-        { type: 'positionedInlineImage' },
-        { type: 'factbox' },
-      ],
+      of: [blockContentType, { type: 'pullQuote' }, { type: 'positionedInlineImage' }, { type: 'factbox' }],
       validation: (Rule) =>
         Rule.custom((value) => {
           if (!value || value.length === 0) {
