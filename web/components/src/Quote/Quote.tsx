ae5ef100d21b0b1c41b23e3bf087a11c9a3c6805
Sven van Steenis <sven.van.steenis@no.experis.com>
2021-04-14 11:24:38 +0200

    💄 increase font size for short quotes (#251)

--- web/components/src/Quote/Quote.tsx
+++ web/components/src/Quote/Quote.tsx
@@ -1,25 +1,37 @@
 import { forwardRef, HTMLAttributes } from 'react'
 import styled from 'styled-components'
-import { Text } from '@components'
 
 type QuoteProps = {
   text: string
 } & HTMLAttributes<HTMLDivElement>
 
-// TODO: margin from Text is unwanted here?
-const QuoteText = styled((props) => <Text {...props}></Text>)`
+type TextProps = {
+  weight: string
+  size: string
+}
+
+const Text = styled.p`
   grid-area: quote;
   align-self: end;
+  margin: 0;
+  font-style: italic;
+  line-height: var(--lineHeight-3);
+  font-weight: var(${({ weight }: TextProps) => weight}, --fontWeight-regular);
+  font-size: var(${({ size }: TextProps) => size}, --typeScale-1);
 `
 
 const textBoldLimit = 160
+const textSizeLimit = 50
 
 export const Quote = forwardRef<HTMLDivElement, QuoteProps>(function Quote({ text, ...rest }, ref) {
   const quoteText = text.trim()
 
+  const weight = quoteText.length < textBoldLimit ? '--fontWeight-medium' : '--fontWeight-regular'
+  const size = quoteText.length < textSizeLimit ? '--typeScale-5' : '--typeScale-2'
+
   return (
-    <QuoteText size="md" italic bold={quoteText.length < textBoldLimit} ref={ref} {...rest}>
+    <Text size={size} weight={weight} ref={ref} {...rest}>
       {quoteText}
-    </QuoteText>
+    </Text>
   )
 })
