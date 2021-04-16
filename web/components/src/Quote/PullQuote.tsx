56c1511959b9e628aea318c6684dae9034a3e818
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-16 11:30:19 +0200

    💄 Add styles for quote
    Just using regular media queries, the clamp function didn't seem to work very well with grids. k

--- web/components/src/Quote/PullQuote.tsx
+++ web/components/src/Quote/PullQuote.tsx
@@ -7,6 +7,7 @@ const Container = styled.figure`
   display: grid;
   row-gap: var(--spacing-medium);
   margin: 0;
+  position: relative;
   /* margin: var(--spacing-xLarge) 0; */
   grid-template-rows: min-content min-content;
   grid-template-columns: 5rem var(--spacing-medium) 1fr;
@@ -15,10 +16,16 @@ const Container = styled.figure`
     'quote quote quote';
 
   @media (min-width: 800px) {
-    grid-template-columns: 1fr var(--spacing-medium) clamp(8rem, 20vw, 16rem);
+    grid-template-columns: 1fr var(--spacing-medium) 164px;
     grid-template-areas:
       'quote spacing media'
       'author spacing media';
+  }
+  @media (min-width: 1440px) {
+    grid-template-columns: 1fr var(--spacing-medium) 220px;
+  }
+  @media (min-width: 1920px) {
+    grid-template-columns: 1fr var(--spacing-medium) 294px;
   }
 `
 
