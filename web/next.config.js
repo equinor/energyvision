beb26d42306fc04c6c538067992204a8d6f4d048
kalyanvaranasi <kalyan.varanasi@capgemini.com>
2021-04-20 08:51:18 +0200

    Initial commit for archive news pages integration

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,21 @@
+const archiveServerHostname = 'http://localhost:8080'
+
 module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
+  async rewrites() {
+    return [
+      // Archive_Rule: This rule load the archived images from dam
+      {
+        source: '/content/dam/:slug*',
+        destination: `${archiveServerHostname}/content/dam/:slug*`
+      },
+      // Archive_Rule: This rule loads the necessary client libraries for old html content
+      {
+        source: '/etc.clientlibs/:slug*',
+        destination: `${archiveServerHostname}/etc.clientlibs/:slug*`
+      }
+    ]
+  }
 }
