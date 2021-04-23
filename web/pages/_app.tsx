1ea20846ae109665c73c409e4822739b877fc447
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-23 12:46:39 +0200

    ⚡️ Even more performance analysis

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,29 +1,29 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head'
-import { useState, useCallback } from 'react'
+// import { useState, useCallback } from 'react'
 import { IntlProvider } from 'react-intl'
-import { Button, Topbar, Menu } from '@components'
+/* import { Button, Topbar, Menu } from '@components' */
 import { GlobalStyle } from '../styles/globalStyles'
-import { createGlobalStyle } from 'styled-components'
-import { MockMenuData } from '@mockdata'
+/* import { createGlobalStyle } from 'styled-components'
+import { MockMenuData } from '@mockdata' */
 import { DefaultSeo } from 'next-seo'
 
-const toggleTheme = () => {
+/* const toggleTheme = () => {
   document.documentElement.classList.toggle('dark')
 }
-
+ */
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const [topbarHeight, setTopbarHeight] = useState(0)
-  const TopbarOffset = createGlobalStyle`
+  //const [topbarHeight, setTopbarHeight] = useState(0)
+  /*   const TopbarOffset = createGlobalStyle`
     #__next { margin-top: ${topbarHeight}px}
-  `
+  ` */
 
-  const topbarRef = useCallback((node) => {
+  /*   const topbarRef = useCallback((node) => {
     if (node !== null) {
       const height = node.getBoundingClientRect().height
       setTopbarHeight(height)
     }
-  }, [])
+  }, []) */
 
   // TODO: get locale from Sanity
   return (
@@ -37,13 +37,13 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         </Head>
         <GlobalStyle />
         <DefaultSeo dangerouslySetAllPagesToNoIndex={true} dangerouslySetAllPagesToNoFollow={true} />
-        <TopbarOffset />
+        {/*  <TopbarOffset />
         <Topbar height={topbarHeight} ref={topbarRef}>
           <Menu items={MockMenuData} offset={topbarHeight}></Menu>
           <Button variant="outlined" onClick={toggleTheme}>
             Toggle theme
           </Button>
-        </Topbar>
+        </Topbar> */}
 
         <Component {...pageProps} />
       </IntlProvider>
