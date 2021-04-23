a7e3a7b0b204ef2fcb2d422de45f517c6089cb72
Sven van Steenis <sven.vsteenis@gmail.com>
2021-04-23 14:14:32 +0200

    💄update styling of lists in factbox (#246)

--- web/common/serializers/ListRenderer.tsx
+++ web/common/serializers/ListRenderer.tsx
@@ -4,9 +4,7 @@ export const ListRenderer = (child: { type: any; children: any }) => {
   const { type, children } = child
   const bullet = type === 'bullet'
   if (bullet) {
-    // @TODO: Should probably just be doing this if we are inside a fact component?
-    // @TODO: And don't remove padding on smaller devices
-    return <List style={{ padding: 0 }}>{children}</List>
+    return <List>{children}</List>
   }
   return <List variant="numbered">{children}</List>
 }
