561dea77b1d8788f9126070a74a4598c37612854
Victor Nystad <vnys@equinor.com>
2021-04-28 18:02:53 +0200

    ♻️ Update gulpfile
    Updated gulp and gulpfile, and renamed assets → src. Still some tweaking to do, but now everything builds as expected, we just need to change where the location of some of the files.
    
    Related to “Move the CSS from the AEM solution to the envis repository” #265

--- legacy-css/gulpfile.js
+++ legacy-css/gulpfile.js
@@ -1,9 +1,8 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 
-const path = require('path');
+const path = require('path')
 // Include gulp
-const gulp = require('gulp');
-
+const gulp = require('gulp')
 
 // Include Plugins
 const scss = require('gulp-sass'),
@@ -11,107 +10,111 @@ const scss = require('gulp-sass'),
   minifyCSS = require('gulp-minify-css'),
   iconfont = require('gulp-iconfont'),
   consolidate = require('gulp-consolidate'),
-  copyAssets = require('gulp-css-copy-assets').default,
-
+  copyAssets = require('gulp-css-copy-src').default,
   // eslint-disable-next-line no-unused-vars
-  _ = require('lodash');
+  _ = require('lodash')
 
-scss.compiler = require('node-sass');
+scss.compiler = require('node-sass')
 
 // ES6-promise is required by autoprefixer
-require('es6-promise').polyfill();
+require('es6-promise').polyfill()
 
 const copySettings = {
-  srcdirs: [__dirname + '/assets/font'],
-  resolve: '../clientlibs/clientlib/resources/css'
+  srcdirs: [__dirname + '/src/font'],
+  resolve: '../clientlibs/clientlib/resources/css',
 }
 
 /**
  * Will look for .scss|sass files inside the node_modules folder
  */
-let aliases = {};
+let aliases = {}
 
 function npmModule(url, file, done) {
   // check if the path was already found and cached
-  console.log('import', url);
+  console.log('import', url)
   if (aliases[url]) {
-    return done({file: aliases[url]});
+    return done({ file: aliases[url] })
   }
 
   // look for modules installed through npm
   try {
-    const newPath = path.relative('./css', require.resolve(url));
-    aliases[url] = newPath; // cache this request
-    return done({file: newPath});
+    const newPath = path.relative('./css', require.resolve(url))
+    aliases[url] = newPath // cache this request
+    return done({ file: newPath })
   } catch (e) {
     // if your module could not be found, just return the original url
-    aliases[url] = url;
-    return done({file: url});
+    aliases[url] = url
+    return done({ file: url })
   }
 }
 
 const scssSettings = {
-  importer: npmModule
+  importer: npmModule,
 }
 
 //  Gulp functions
 //  ---------------------------------------------------------------------------------------
 console.log(copyAssets)
 // Build SASS
-gulp.task('compass-minify', function () {
-  gulp.src('assets/sass/common.scss')
+gulp.task('compass-minify', function (done) {
+  gulp
+    .src('src/sass/common.scss')
     .pipe(scss(scssSettings))
     .pipe(minifyCSS())
     .pipe(autoprefixer())
     .pipe(copyAssets(copySettings))
     .pipe(gulp.dest('dist/css/minified'))
-});
+  done()
+})
 
 // Build SASS
-gulp.task('compass-unminified', function () {
-  gulp.src('assets/sass/common.scss')
-    .pipe(scss(scssSettings))
-    .pipe(copyAssets(copySettings))
-    .pipe(gulp.dest('dist/css'))
-});
+gulp.task('compass-unminified', function (done) {
+  gulp.src('src/sass/common.scss').pipe(scss(scssSettings)).pipe(copyAssets(copySettings)).pipe(gulp.dest('dist/css'))
+  done()
+})
 
 // Build icons
-const fontName = "equinor_icons";
+const fontName = 'equinor_icons'
 
-gulp.task('Iconfont', function () {
-  gulp.src(['assets/icons/*.svg'])
-    .pipe(iconfont({
-      fontName: fontName,
-      appendUnicode: true,
-      formats: ['ttf', 'eot', 'woff'],
-      normalize: true
-    }))
+gulp.task('Iconfont', function (done) {
+  gulp
+    .src(['src/icons/*.svg'])
+    .pipe(
+      iconfont({
+        fontName: fontName,
+        appendUnicode: true,
+        formats: ['woff'],
+        normalize: true,
+      }),
+    )
     .on('glyphs', function (glyphs, options) {
-      console.log(glyphs, options);
-      gulp.src('assets/sass/icons/equinor-icons.css')
-        .pipe(consolidate('lodash', {
+      console.log(glyphs, options)
+      gulp.src('src/sass/icons/equinor-icons.css').pipe(
+        consolidate('lodash', {
           glyphs: glyphs,
           fontName: fontName,
-          className: 'si'
-        }))
+          className: 'si',
+        }),
+      )
     })
-    .pipe(gulp.dest('assets/font/equinor-icons'))
-});
-
+    .pipe(gulp.dest('src/font/equinor-icons'))
+  done()
+})
 
 //  Gulp Tasks
 //  ---------------------------------------------------------------------------------------
 
 // Default Task
-gulp.task('default', ['compass-minify', 'Iconfont']);
+gulp.task('default', gulp.series('compass-minify', 'Iconfont'))
 
 // Watch Task
-gulp.task('watch', function () {
-  gulp.watch(['assets/sass/*.scss', 'assets/sass/**/*.scss'], ['compass-minify']);
-});
+gulp.task('watch', function (done) {
+  gulp.watch(['src/sass/*.scss', 'src/sass/**/*.scss'], ['compass-minify'])
+  done()
+})
 
 // Autoprefixer
-gulp.task('autoprefixer');
+gulp.task('autoprefixer')
 
 // CSS Lint Task
-gulp.task('css-lint');
+gulp.task('css-lint')
