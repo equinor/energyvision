4434f4d6f3c5ca316a21057a425fc983a28cf385
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-04-29 10:06:48 +0200

    ♻️ Use next/link for internal links (#272)

--- web/tempcomponents/news/RelatedContent.tsx
+++ web/tempcomponents/news/RelatedContent.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from 'react'
 import { Heading, Link, List } from '@components'
 import type { RelatedLinksData, LinkData } from '../../types/types'
+import { default as NextLink } from 'next/link'
 
 const { Item } = List
 
@@ -16,18 +18,35 @@ const RelatedContent = ({ data }: RelatedContentProps) => {
       <List unstyled>
         {data.links.length > 0 &&
           data.links.map((item: LinkData) => {
-            const { id, label, type, link, href, extension = null } = item
+            const { id, label, type, link, href, extension } = item
             if (type === 'internalUrl' && link?.slug === undefined) {
               console.warn('An internal link is missing slug', label)
             }
             // @TODO Internal link beyond news articles
             const url = type === 'internalUrl' ? `/news/${link?.slug}` : href
+            if (!url) {
+              console.warn('Something weird happened. A link is missing href', label)
+              return
+            }
+
             return (
-              <Item key={id}>
-                <Link variant="contentLink" type={type} href={url || '/'}>
-                  {label} {extension && `(${extension.toUpperCase()})`}
-                </Link>
-              </Item>
+              <Fragment key={id}>
+                {type === 'internalUrl' ? (
+                  <Item>
+                    <NextLink href={url} passHref>
+                      <Link variant="contentLink" type={type}>
+                        {label}
+                      </Link>
+                    </NextLink>
+                  </Item>
+                ) : (
+                  <Item>
+                    <Link variant="contentLink" type={type} href={url}>
+                      {label} {extension && `(${extension.toUpperCase()})`}
+                    </Link>
+                  </Item>
+                )}
+              </Fragment>
             )
           })}
       </List>
