98b3731a6ac1e53b864505129541e1d65739bb44
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-05 09:41:02 +0200

    🔊 Better logs on error

--- web/common/serializers/InternalLinkRenderer.tsx
+++ web/common/serializers/InternalLinkRenderer.tsx
@@ -4,6 +4,7 @@ import { default as NextLink } from 'next/link'
 export const InternalLinkRenderer = (child: { mark: any; children: any }) => {
   try {
     const { mark, children } = child
+
     const { id, type } = mark.internalLink
     /** @TODO: More future proof way of handling this when routing is solved */
     switch (type) {
@@ -17,7 +18,7 @@ export const InternalLinkRenderer = (child: { mark: any; children: any }) => {
         return <span>Need to implement internal link</span>
     }
   } catch (e) {
-    console.error('Could not render internal link', e)
+    console.error('Could not render internal link', child, e)
     return null
   }
 }
