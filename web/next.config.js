1f1905aa7528f953328b91c6bb3d832b37b451b4
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-05 10:12:44 +0200

    ⬆️ Use Webpack 5 (#263)

--- web/next.config.js
+++ web/next.config.js
@@ -7,18 +7,21 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
+  future: {
+    webpack5: true,
+  },
   async rewrites() {
     return [
       // Archive_Rule: This rule load the archived images from dam
       {
         source: '/content/dam/:slug*',
-        destination: `${archiveServerHostname}/content/dam/:slug*`
+        destination: `${archiveServerHostname}/content/dam/:slug*`,
       },
       // Archive_Rule: This rule loads the necessary client libraries for old html content
       {
         source: '/etc.clientlibs/:slug*',
-        destination: `${archiveServerHostname}/etc.clientlibs/:slug*`
-      }
+        destination: `${archiveServerHostname}/etc.clientlibs/:slug*`,
+      },
     ]
-  }
+  },
 }
