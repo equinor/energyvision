ee38e119f441f96c9fa541744e5bc26ef500198b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-07 18:55:34 +0200

    🚧 Experiments for careers  in Next  (#280)

--- web/lib/queryFromSlug.ts
+++ web/lib/queryFromSlug.ts
@@ -1,9 +1,15 @@
-import { newsQuery } from './queries'
+import { newsQuery, careersQuery, whatWeDoQuery } from './queries'
 
 const getQuery = (docType: string) => {
+  // @TODO A more sophisticated mapper to avoid to hard code this here
+  // @TODO What to do about the landing pages
   switch (docType) {
     case 'news':
       return newsQuery
+    case 'careers':
+      return careersQuery
+    case 'what-we-do':
+      return whatWeDoQuery
     default:
       return newsQuery
   }
@@ -11,11 +17,15 @@ const getQuery = (docType: string) => {
 
 export const getQueryFromSlug = (slugArray: string[] = []) => {
   const [slugStart] = slugArray
+  console.log(slugArray)
+  // Quick n' dirty
+  const docType = slugStart === 'news' ? 'news' : 'page'
+
   const queryParams = { slug: `/${slugArray.join('/')}` }
 
   return {
     queryParams,
     query: getQuery(slugStart),
-    docType: slugStart,
+    docType: docType,
   }
 }
