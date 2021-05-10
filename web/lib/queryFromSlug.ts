3b78c3e5585f416bc2c691b8af0bdd2d0d3d63be
Sven van Steenis <sven.vsteenis@gmail.com>
2021-05-10 14:45:51 +0200

    ♻️ simplify catch-all page querying code (#280)

--- web/lib/queryFromSlug.ts
+++ web/lib/queryFromSlug.ts
@@ -1,36 +1,27 @@
 import { newsQuery, pageQuery } from './queries'
 
-const getQuery = (docType: string) => {
-  // @TODO A more sophisticated mapper to avoid to hard code this here
-  // @TODO What to do about the landing pages
-  switch (docType) {
-    case 'news':
-      return newsQuery
-    default:
-      return pageQuery
-  }
-}
-
-const getQueryParams = (docType: string, slugArray: string[]) => {
-  const slug = `/${slugArray.join('/')}`
-  switch (docType) {
-    case 'news':
-      return { slug: slug }
-    default:
-      return { slug: slug, pageType: `${docType}_page` }
-  }
-}
-export const getQueryFromSlug = (slugArray: string[] = []) => {
+export const getQueryFromSlug = (slugArray: string[] = ['']) => {
   const [slugStart] = slugArray
-  console.log(slugArray)
-  // Quick n' dirty
-  const docType = slugStart === 'news' ? 'news' : 'page'
+  const slug = `/${slugArray.join('/')}`
 
-  // const queryParams = { slug: `/${slugArray.join('/')}` }
-
-  return {
-    queryParams: getQueryParams(slugStart, slugArray),
-    query: getQuery(slugStart),
-    docType: docType,
+  switch (slugStart) {
+    case '':
+      return {
+        queryParams: { slug: slug },
+        query: '',
+        docType: 'home',
+      }
+    case 'news':
+      return {
+        queryParams: { slug: slug },
+        query: newsQuery,
+        docType: 'news',
+      }
+    default:
+      return {
+        queryParams: { slug: slug, pageType: `${slugStart}_page` },
+        query: pageQuery,
+        docType: 'page',
+      }
   }
 }
