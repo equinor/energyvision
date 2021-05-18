ed53bf118a9a961612540c0b8543c8575ae28386
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-18 10:26:13 +0200

    ✨ Add Media sub component (#310)

--- web/components/stories/components/Teaser.stories.tsx
+++ web/components/stories/components/Teaser.stories.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable */
 import { Story, Meta } from '@storybook/react'
 import { Teaser, TeaserProps } from '@components'
+import { ImagePlaceholder } from './helpers/styles'
 
 export default {
   title: 'Components/Teaser',
   component: Teaser,
-  /*  subcomponents: {
+  subcomponents: {
     Media: Teaser.Media,
-  }, */
+  },
   parameters: {
     docs: {
       description: {
@@ -22,3 +23,18 @@ export default {
 export const Default: Story<TeaserProps> = (args) => <Teaser {...args}></Teaser>
 
 Default.storyName = 'Default'
+
+export const WithMedia: Story<TeaserProps> = ({}) => (
+  <Teaser>
+    <Teaser.Media>
+      <ImagePlaceholder style={{ borderRadius: 0 }} />
+    </Teaser.Media>
+  </Teaser>
+)
+
+WithMedia.storyName = 'With media'
+WithMedia.parameters = {
+  docs: {
+    storyDescription: `A <code>Teaser</code> component with image or video. Select "canvas" mode for this story to see different variations.`,
+  },
+}
