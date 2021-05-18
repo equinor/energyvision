2d8773e0ac69a940b1eefd6862832b46ef31d27c
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-18 11:38:30 +0200

    💄 Add basic layout (#310)

--- web/components/src/Teaser/Teaser.tsx
+++ web/components/src/Teaser/Teaser.tsx
@@ -3,7 +3,20 @@ import styled from 'styled-components'
 
 export type TeaserProps = HTMLAttributes<HTMLElement>
 
-export const StyledCard = styled.article``
+export const StyledCard = styled.article`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: min-content min-content;
+  grid-template-areas:
+    'image'
+    'content';
+  /*     Hardcoded value while waiting for the w3c proposal for environment()
+ */
+  @media (min-width: 500px) {
+    grid-template-columns: repeat(2, 50%);
+    grid-template-rows: min-content;
+  }
+`
 
 export const Teaser = forwardRef<HTMLDivElement, TeaserProps>(function Teaser({ children, ...rest }, ref) {
   return (
