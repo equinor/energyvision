1423a49f86e9961b1044f0da0778ae14fe5c5f22
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-19 14:53:42 +0200

    💄 Less space between the eyebrow title and the title (#310)

--- web/components/src/Teaser/Eyebrow.tsx
+++ web/components/src/Teaser/Eyebrow.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 const StyledTypography = styled(Typography)`
   font-size: var(--typeScale-2);
   line-height: var(--lineHeight-3);
+  margin-bottom: -1rem;
 `
 export type TeaserEyebrowProps = HTMLAttributes<HTMLDivElement>
 
