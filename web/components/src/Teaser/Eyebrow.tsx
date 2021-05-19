6b9903d83ff65775f24ad5585e78c4a81d4fbdb8
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-19 14:55:22 +0200

    💄 Invert the text color for eyebrow title if dark bg (#310)

--- web/components/src/Teaser/Eyebrow.tsx
+++ web/components/src/Teaser/Eyebrow.tsx
@@ -1,11 +1,17 @@
 import { forwardRef, HTMLAttributes } from 'react'
 import { Typography } from '@equinor/eds-core-react'
 import styled from 'styled-components'
+import { StyledTeaser } from './Teaser'
 
 const StyledTypography = styled(Typography)`
   font-size: var(--typeScale-2);
   line-height: var(--lineHeight-3);
   margin-bottom: -1rem;
+
+  /* If the text is used inside a inverted teaser component, the text colour must also be inverted */
+  ${StyledTeaser}.inverted & {
+    color: var(--inverted-text);
+  }
 `
 export type TeaserEyebrowProps = HTMLAttributes<HTMLDivElement>
 
