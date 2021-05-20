ec141b24bfe9c682506447a59a48763026b9c4db
Sven van Steenis <sven.vsteenis@gmail.com>
2021-05-20 13:58:14 +0200

    ✨ add Sanity preview for topic pages (#311)

---
+++ studio/src/previews/page/PagePreview.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  p {
+    padding: 1em;
+  }
+`
+
+const Frame = styled.iframe`
+  border: 0;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`
+
+export default function PagePreview(props: any) {
+  const baseUrl = process.env.SANITY_STUDIO_PROJECT_URL
+  const {
+    document: { displayed },
+  } = props
+
+  if (!displayed?.slug?.current) {
+    return <div>The product needs a slug before it can be previewed.</div>
+  }
+
+  const url = `${baseUrl}${displayed?.slug?.current}?preview`
+
+  return (
+    <Wrapper>
+      <Frame src={url} title="preview" frameBorder={'0'} />
+    </Wrapper>
+  )
+}
