f5b7267f2b5471801e83b6a90aef066e568451a1
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-25 10:19:05 +0200

    ✨ Add basic seo and some fields in Sanity (#343)

--- studio/schemas/documents/page.js
+++ studio/schemas/documents/page.js
@@ -20,8 +20,30 @@ export default (topicSuffix, title) => {
           collapsed: false,
         },
       },
+      {
+        title: 'SEO & metadata',
+        name: 'metadata',
+        description: 'This part is used for meta information when this content is used on the web',
+        options: {
+          collapsible: true,
+          collapsed: true,
+        },
+      },
     ],
     fields: [
+      {
+        name: 'seo',
+        type: 'titleAndMeta',
+        fieldset: 'metadata',
+        title: 'Meta information',
+      },
+      {
+        name: 'openGraphImage',
+        type: 'imageWithAlt',
+        title: 'Open Graph Image',
+        description: 'You can override the hero image as the SoMe image by uploading another image here.',
+        fieldset: 'metadata',
+      },
       {
         name: 'topicSuffix',
         initialValue: `${topicSuffix}`,
