b87bcc507ae6be751a8c096fdb78e13cebe66fff
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-05-27 12:24:56 +0200

    📌 Use v1 for api (#239)

--- web/lib/config.ts
+++ web/lib/config.ts
@@ -4,5 +4,5 @@ export const sanityConfig: ClientConfig = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'h61q9gi9',
   useCdn: process.env.NODE_ENV === 'production',
-  apiVersion: '2021-05-27',
+  apiVersion: 'v1',
 }
