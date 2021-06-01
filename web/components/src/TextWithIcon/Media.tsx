571f278629c6308170699506cbade0bde06376c5
Sven van Steenis <sven.vsteenis@gmail.com>
2021-06-01 15:27:31 +0200

    🚧 initial structure of TextWithIcon component (#279)

---
+++ web/components/src/TextWithIcon/Media.tsx
@@ -0,0 +1,14 @@
+import { HTMLAttributes } from 'react'
+import styled from 'styled-components'
+
+type MediaProps = HTMLAttributes<HTMLDivElement>
+
+const Img = styled.div`
+  align-self: center;
+  grid-area: media;
+  margin-bottom: var(--space-xSmall);
+`
+
+export const Media = ({ children, ...rest }: MediaProps) => {
+  return <Img {...rest}>{children}</Img>
+}
