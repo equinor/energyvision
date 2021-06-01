1d550f20d0a19e23b97ff113f89d87d0eb0b7fe6
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-06-01 13:50:38 +0200

    ✅ Add some tests for my new component babies (#273)

---
+++ web/components/src/Backgrounds/BackgroundContainer.test.tsx
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, cleanup } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import 'jest-styled-components'
+import { BackgroundContainer } from './'
+
+afterEach(cleanup)
+
+describe(`The background container supports different colours from Sanity's colour selector`, () => {
+  it('it can be the default color', () => {
+    const { container } = render(<BackgroundContainer />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--ui-background-default)`)
+  })
+  it('it can be white (default)', () => {
+    const { container } = render(<BackgroundContainer background="White" />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--ui-background-default)`)
+  })
+  it('it can be Moss green ', () => {
+    const { container } = render(<BackgroundContainer background="Moss Green" />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--moss-green-80)`)
+  })
+  it('it can be Lichen green (default)', () => {
+    const { container } = render(<BackgroundContainer background="Lichen Green" />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--lichen-green-100)`)
+  })
+  it('it can be spruce wood (default)', () => {
+    const { container } = render(<BackgroundContainer background="Spruce Wood" />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--spruce-wood-90)`)
+  })
+  it('it can be mist blue (default)', () => {
+    const { container } = render(<BackgroundContainer background="Mist Blue" />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--mist-blue-100)`)
+  })
+  it('it can be slate blue (default)', () => {
+    const { container } = render(<BackgroundContainer background="Slate Blue" />)
+    expect(container.firstChild).toHaveStyle(`--background-color: var(--slate-blue-100)`)
+  })
+  it('Add a class for inverted if the background is so dark that the text must be light for contrast', () => {
+    const { container } = render(<BackgroundContainer background="Slate Blue" />)
+    expect(container.firstChild).toHaveClass('inverted-background')
+  })
+})
---
+++ web/components/src/FigureCaption/FigureCaption.test.tsx
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, cleanup } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import 'jest-styled-components'
+import styled from 'styled-components'
+import { FigureCaption } from '@components'
+
+afterEach(cleanup)
+
+const StyledFigureCaption = styled(FigureCaption)`
+  clip-path: unset;
+`
+
+describe('FigureCaption', () => {
+  it('Can extend the CSS for the Teaser component', () => {
+    const { container } = render(<StyledFigureCaption />)
+    expect(container.firstChild).toHaveStyleRule('clip-path', 'unset')
+  })
+  it('Can render a text', () => {
+    const text = 'My test title'
+    render(<FigureCaption>{text}</FigureCaption>)
+    const inputNode = screen.getByText(text)
+    expect(inputNode).toBeDefined()
+  })
+})
