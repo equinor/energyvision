c5156df0b82535a29fe30bac5fa9a85a799bd51f
Sven van Steenis <sven.vsteenis@gmail.com>
2021-06-01 13:42:29 +0200

    ✨ add background color selector (#279)

--- studio/schemas/objects/textWithIconArray.tsx
+++ studio/schemas/objects/textWithIconArray.tsx
@@ -2,17 +2,40 @@ import React from 'react'
 import blocksToText from '../../helpers/blocksToText'
 import { view_module } from '@equinor/eds-icons'
 import { EdsIcon } from '../../icons'
+import { Colors } from '../../helpers/ColorListValues'
 
 export default {
   type: 'object',
   name: 'textWithIconArray',
   title: 'Group of Text with Icon',
+  fieldsets: [
+    {
+      name: 'design',
+      title: 'Design options',
+    },
+  ],
   fields: [
     {
       type: 'array',
       name: 'group',
       title: 'Group of text with icon',
       of: [{ type: 'textWithIcon' }],
+    },
+    {
+      title: 'Background',
+      description: 'Pick a colour for the background. Default is white.',
+      name: 'background',
+      type: 'colorlist',
+      options: {
+        borderradius: {
+          outer: '100%',
+          inner: '100%',
+        },
+        tooltip: true,
+        list: Colors,
+      },
+      fieldset: 'design',
+      initialValue: Colors[0],
     },
   ],
   preview: {
