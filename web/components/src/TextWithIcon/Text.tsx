56d282359cd4f3c915837e9d2cac14a64d810789
Sven van Steenis <sven.vsteenis@gmail.com>
2021-06-02 13:35:23 +0200

    ♻️ remove use of Text from TextWithIcon (#279)
    this will be used in the renderer instead

--- web/components/src/TextWithIcon/Text.tsx
+++ web/components/src/TextWithIcon/Text.tsx
@@ -1,19 +1,12 @@
 import { HTMLAttributes } from 'react'
-import { Text as TextComponent } from '../Text'
 import styled from 'styled-components'
 
 type TextProps = HTMLAttributes<HTMLDivElement>
 
-const StyledText = styled(TextComponent)`
-  margin: 0;
+const StyledText = styled.div`
   grid-area: text;
-  text-align: center;
 `
 
 export const Text = ({ children, ...rest }: TextProps) => {
-  return (
-    <StyledText size="regular" {...rest}>
-      {children}
-    </StyledText>
-  )
+  return <StyledText {...rest}>{children}</StyledText>
 }
