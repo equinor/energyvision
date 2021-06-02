6d2d5b0381e5c0e938b94e1efc9f43df6fe55cba
Sven van Steenis <sven.vsteenis@gmail.com>
2021-06-02 22:24:46 +0200

    ♻️ use auto-fit instead of set number of cols (#279)

--- web/tempcomponents/topicPages/TextWithIconArray.tsx
+++ web/tempcomponents/topicPages/TextWithIconArray.tsx
@@ -6,10 +6,10 @@ import SimpleBlockContent from '../../common/SimpleBlockContent'
 import { IngressBlockRenderer } from '../../common/serializers'
 import type { TextWithIconArrayData, TextWithIconItem } from '../../types/types'
 
-const Container = styled.div<{ columns: number }>`
+const Container = styled.div`
   display: grid;
   justify-content: center;
-  grid-template-columns: repeat(${({ columns = 1 }) => columns}, minmax(13rem, 18rem));
+  grid-template-columns: repeat(auto-fit, minmax(13rem, 18rem));
   grid-gap: var(--space-xxLarge);
   padding: var(--space-xxLarge);
 `
@@ -23,11 +23,9 @@ type TextWithIconArrayProps = {
 const TextWithIconArray = ({ data }: TextWithIconArrayProps) => {
   const { designOptions, group } = data
 
-  const columns = group.length > 3 ? 3 : group.length
-
   return (
     <BackgroundContainer background={designOptions.background}>
-      <Container columns={columns}>
+      <Container>
         {group.map((item: TextWithIconItem) => (
           <TextWithIcon key={item.id}>
             {item.icon && item.icon.asset && (
