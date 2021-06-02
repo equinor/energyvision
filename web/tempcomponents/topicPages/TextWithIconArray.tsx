329a6536cbb931cd7eb82956d4e2b297f751679a
Sven van Steenis <sven.vsteenis@gmail.com>
2021-06-03 00:19:56 +0200

    ♻️ improve spacing of TextWithIconArray (#279)

--- web/tempcomponents/topicPages/TextWithIconArray.tsx
+++ web/tempcomponents/topicPages/TextWithIconArray.tsx
@@ -1,17 +1,21 @@
 import { TextWithIcon, BackgroundContainer } from '@components'
-import { imageProps } from '../../common/helpers'
+import { urlFor } from '../../common/helpers'
 import Img from 'next/image'
 import styled from 'styled-components'
 import SimpleBlockContent from '../../common/SimpleBlockContent'
 import { IngressBlockRenderer } from '../../common/serializers'
-import type { TextWithIconArrayData, TextWithIconItem } from '../../types/types'
+import type { TextWithIconArrayData, TextWithIconItem, ImageWithAlt } from '../../types/types'
 
 const Container = styled.div`
+  --max-content-width: 1440px;
+
   display: grid;
   justify-content: center;
   grid-template-columns: repeat(auto-fit, minmax(13rem, 18rem));
   grid-gap: var(--space-xxLarge);
-  padding: var(--space-xxLarge);
+  padding: var(--space-xxLarge) 0;
+  margin: auto;
+  max-width: var(--max-content-width);
 `
 
 const { Title, Media, Text } = TextWithIcon
@@ -19,6 +23,8 @@ const { Title, Media, Text } = TextWithIcon
 type TextWithIconArrayProps = {
   data: TextWithIconArrayData
 }
+
+const getImgSrc = (img: ImageWithAlt): string => urlFor(img).size(150, 150).auto('format').toString() || ''
 
 const TextWithIconArray = ({ data }: TextWithIconArrayProps) => {
   const { designOptions, group } = data
@@ -30,7 +36,7 @@ const TextWithIconArray = ({ data }: TextWithIconArrayProps) => {
           <TextWithIcon key={item.id}>
             {item.icon && item.icon.asset && (
               <Media>
-                <Img {...imageProps(item.icon.asset, 400, 1)} alt={item.icon.alt} layout="intrinsic" />
+                <Img src={getImgSrc(item.icon)} width="150" height="150" alt={item.icon.alt} />
               </Media>
             )}
             {item.title && <Title>{item.title}</Title>}
