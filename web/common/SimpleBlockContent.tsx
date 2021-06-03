58b56d34b99f1e3d6b68abec4178db1396ef1c20
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-06-03 11:02:07 +0200

    💄 Add renderer for links in simple block

--- web/common/SimpleBlockContent.tsx
+++ web/common/SimpleBlockContent.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PortableText } from '../lib/sanity'
-import { BlockRenderer, SubRenderer, SupRenderer } from './serializers/'
+import { BlockRenderer, SubRenderer, SupRenderer, ExternalLinkRenderer, InternalLinkRenderer } from './serializers/'
 import { PortableTextProps } from '@sanity/block-content-to-react'
 
 const defaultSerializers = {
   types: { block: BlockRenderer },
-  marks: { sub: SubRenderer, sup: SupRenderer },
+  marks: { sub: SubRenderer, sup: SupRenderer, link: ExternalLinkRenderer, internalLink: InternalLinkRenderer },
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
