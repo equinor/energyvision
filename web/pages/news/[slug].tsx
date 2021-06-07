64a20ff5573a401827f01e4492aee33481657323
Sven van Steenis <sven.vsteenis@gmail.com>
2021-06-07 10:21:40 +0200

    🔥 remove console log

--- web/pages/news/[slug].tsx
+++ web/pages/news/[slug].tsx
@@ -163,8 +163,6 @@ export default function News({ data, preview }: ArticleProps): JSX.Element {
     enabled: preview || router.query.preview !== null,
   })
 
-  console.log('docType in page', slug)
-
   if (!router.isFallback && !slug) {
     return <ErrorPage statusCode={404} />
   }
