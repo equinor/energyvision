010160f93a811e4a1365a3a0a08b34e7e16e3612
kalyanvaranasi <kalyan.varanasi@capgemini.com>
2021-06-11 15:31:19 +0200

    :recycle: Introduced locale variable to fetch correct static pages for blob storage

--- web/pages/news/archive/[...pagePath].tsx
+++ web/pages/news/archive/[...pagePath].tsx
@@ -1,4 +1,3 @@
-//import NewsArchiveHead from './NewsArchiveHead'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import * as fs from 'fs'
 import { NextSeo } from 'next-seo'
@@ -19,6 +18,10 @@ type OldArchivedNewsPageProps = {
   data: {
     news: PageResponseData
   }
+}
+
+const removeHTMLExtension = (path: string): string => {
+  return path.replace('.html', '')
 }
 
 const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element => {
@@ -49,7 +52,7 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
               type: 'article',
               url: fullUrl,
             }}
-          ></NextSeo>
+          />
           <div
             dangerouslySetInnerHTML={{
               __html: data?.news?.content,
@@ -62,18 +65,16 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
   )
 }
 
-//const archiveServerHostname = 'http://localhost:3010'
-
-// @TODO: Language
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
   const pagePathArray = params?.pagePath as string[]
   const pagePath = pagePathArray.join('/')
+  const archiveSeverURL = publicRuntimeConfig.archiveStorageURL
+  /** Check if the required page is old archived AEM page or not
+   * because AEM also has archived pages which has 'archive' the page path */
+  let response
+  if (pagePathArray.length > 1) response = await fetch(`${archiveSeverURL}/${locale}/news/archive/${pagePath}.json`)
+  else response = await fetch(`${archiveSeverURL}/${locale}/news/${pagePath}.json`)
 
-  // @TODO: I don't understand the archive or not part of the url
-  // const response = await fetch(`${archiveServerHostname}/en/news/archive/${pagePath}.json`)
-
-  // const response = await fetch(`${archiveServerHostname}/en/news/${pagePath}.json`)
-  const response = await fetch(`https://envis-legacy.azureedge.net/equinor-archive-content/en/news/${pagePath}.json`)
   let pageData
   try {
     pageData = await response.json()
@@ -82,7 +83,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     pageData = null
   }
 
-  console.log(pageData)
   return {
     props: {
       data: {
@@ -96,10 +96,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }
 }
 
-const removeHTMLExtension = (path: string): string => {
-  return path.replace('.html', '')
-}
-
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths: string[] = []
 
@@ -111,7 +107,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   newsPagesList.map((pagePath: string) => {
     const pageName = removeHTMLExtension(pagePath.substr(pagePath.lastIndexOf('/') + 1))
-
     paths.push(pageName)
   })
 
