80245922ab1d7b2cc27307527d439dd738d670b9
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-06-21 12:42:21 +0200

    ✨ Add the actual content for query by id as well (#375)

--- web/lib/queryFromSlug.ts
+++ web/lib/queryFromSlug.ts
@@ -3,8 +3,7 @@ import { mapLocaleToLang } from './localization'
 
 const isSlugID = (slug: string): boolean => {
   // regex magic to see if string is a UUID
-  const isATranslation = slug.includes('__i18n_nb_NO')
-  console.log('This is a translation', isATranslation)
+
   const regExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi
   return regExp.test(slug.replace('drafts.', '').replace('__i18n_nb_NO', ''))
 }
@@ -13,6 +12,8 @@ export const getQueryFromSlug = (slugArray: string[] = [''], locale = '') => {
   const [slugStart] = slugArray.filter((part: string) => part !== locale)
 
   if (isSlugID(slugStart)) {
+    const isATranslation = slugStart.includes('__i18n_nb_NO')
+    console.log('This is a translation', isATranslation)
     // We are in preview mode for content that has currently no slug (no routes)
     return {
       queryParams: { id: slugStart.replace('drafts.', '') },
