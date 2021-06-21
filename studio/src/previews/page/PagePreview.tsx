a35e347e861c0949746dc6aaca35faf310483748
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-06-21 11:33:07 +0200

    ✨ Add Norwegian and English route preview (#375)

--- studio/src/previews/page/PagePreview.tsx
+++ studio/src/previews/page/PagePreview.tsx
@@ -1,20 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Wrapper = styled.div`
-  p {
-    padding: 1em;
-  }
-`
-
-const Frame = styled.iframe`
-  border: 0;
-  height: 100%;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 100%;
-`
+import { Wrapper, Frame } from './styles/shared'
 
 export default function PagePreview(props: any) {
   const baseUrl = process.env.SANITY_STUDIO_PROJECT_URL
---
+++ studio/src/previews/page/styles/shared.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  p {
+    padding: 1em;
+  }
+`
+
+export const Frame = styled.iframe`
+  border: 0;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`
