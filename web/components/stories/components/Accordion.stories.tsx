da43e94d334886fbfb9efe7f5fa7f4900bee7096
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-07-02 11:35:19 +0200

    📝 Improve storybook story (#350)

--- web/components/stories/components/Accordion.stories.tsx
+++ web/components/stories/components/Accordion.stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Accordion, AccordionProps, BackgroundContainer } from '@components'
+import { Accordion, AccordionProps, BackgroundContainer, Text } from '@components'
 
 export default {
   title: 'Components/Accordion',
@@ -14,14 +14,69 @@ export const Default: Story<AccordionProps> = (args) => (
     <Accordion.Item>
       <Accordion.Header>Produced and processed water</Accordion.Header>
       <Accordion.Panel>
-        Our efforts to continuously improve our management of discharges of large volumes of produced and processed
-        water to the sea continue. The main objective is to minimise the environmental impact from oil and chemicals
-        contained in the discharged water. We continuosly monitor discharges from each of our installations and onshore
-        plants.{' '}
+        <Text>
+          Our efforts to continuously improve our management of discharges of large volumes of produced and processed
+          water to the sea continue. The main objective is to minimise the environmental impact from oil and chemicals
+          contained in the discharged water. We continuosly monitor discharges from each of our installations and
+          onshore plants.
+        </Text>
       </Accordion.Panel>
     </Accordion.Item>
   </Accordion>
 )
+
+export const WithMultipleItems: Story<AccordionProps> = () => (
+  <Accordion>
+    <Accordion.Item>
+      <Accordion.Header>Produced and processed water</Accordion.Header>
+      <Accordion.Panel>
+        <Text>
+          Our efforts to continuously improve our management of discharges of large volumes of produced and processed
+          water to the sea continue. The main objective is to minimise the environmental impact from oil and chemicals
+          contained in the discharged water. We continuosly monitor discharges from each of our installations and
+          onshore plants.
+        </Text>
+      </Accordion.Panel>
+    </Accordion.Item>
+    <Accordion.Item>
+      <Accordion.Header>NOx Emissions</Accordion.Header>
+      <Accordion.Panel>
+        <Text>
+          Our efforts to continuously improve our management of discharges of large volumes of produced and processed
+          water to the sea continue. The main objective is to minimise the environmental impact from oil and chemicals
+          contained in the discharged water. We continuosly monitor discharges from each of our installations and
+          onshore plants.
+        </Text>
+      </Accordion.Panel>
+    </Accordion.Item>
+    <Accordion.Item>
+      <Accordion.Header>
+        Waste handling and a loong title over several lines and even more and more and more
+      </Accordion.Header>
+      <Accordion.Panel>
+        <Text>
+          Our efforts to continuously improve our management of discharges of large volumes of produced and processed
+          water to the sea continue. The main objective is to minimise the environmental impact from oil and chemicals
+          contained in the discharged water. We continuosly monitor discharges from each of our installations and
+          onshore plants.
+        </Text>
+      </Accordion.Panel>
+    </Accordion.Item>
+    <Accordion.Item>
+      <Accordion.Header>Fresh water</Accordion.Header>
+      <Accordion.Panel>
+        <Text>
+          Our efforts to continuously improve our management of discharges of large volumes of produced and processed
+          water to the sea continue. The main objective is to minimise the environmental impact from oil and chemicals
+          contained in the discharged water. We continuosly monitor discharges from each of our installations and
+          onshore plants.
+        </Text>
+      </Accordion.Panel>
+    </Accordion.Item>
+  </Accordion>
+)
+
+WithMultipleItems.storyName = 'With multiple items'
 
 export const WithBackgroundColour: Story<AccordionProps> = () => (
   <BackgroundContainer background="Moss Green">
