a766f726e29f60fe96d3cc640ad81ecdc267b937
Sven van Steenis <sven.vsteenis@gmail.com>
2021-07-06 14:24:40 +0200

    ♻️ use correct padding for page quote (#397)

--- web/tempcomponents/topicPages/PageQuote.tsx
+++ web/tempcomponents/topicPages/PageQuote.tsx
@@ -3,7 +3,7 @@ import type { QuoteData } from '../../types/types'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  padding: var(--space-xLarge) var(--layout-paddingHorizontal-large);
+  padding: var(--space-3xLarge) var(--layout-paddingHorizontal-large);
   max-width: var(--maxViewportWidth);
   margin: auto;
 `
