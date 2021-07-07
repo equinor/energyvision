0ea6917479c6febf4afe1ec216d14871c45daf45
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-07-07 10:02:57 +0200

    ✅ Add some tests for the Accordion component (#350)

--- web/components/src/Accordion/Panel.tsx
+++ web/components/src/Accordion/Panel.tsx
@@ -1,40 +1,13 @@
-import { forwardRef, useRef, useState, useEffect } from 'react'
+import { forwardRef } from 'react'
 import { animated, useSpring } from '@react-spring/web'
-import { usePrefersReducedMotion } from './usePrefersReducedMotion'
+import { usePrefersReducedMotion } from './hooks/usePrefersReducedMotion'
+import { useDivHeight } from './hooks/useDivHeight'
 import {
   AccordionPanel as RAccordionPanel,
   AccordionPanelProps as RAccordionPanelProps,
   useAccordionItemContext,
 } from '@reach/accordion'
 import styled from 'styled-components'
-
-function useDivHeight() {
-  const ref = useRef(null)
-  const [height, setHeight] = useState(0)
-
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver(([entry]) => {
-      requestAnimationFrame(() => {
-        if (!entry) {
-          return
-        }
-        setHeight(entry.target.getBoundingClientRect().height)
-      })
-    })
-
-    if (ref.current) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore: Do you have any suggestions here Sven
-      resizeObserver.observe(ref.current)
-    }
-
-    return () => {
-      resizeObserver.disconnect()
-    }
-  }, [])
-
-  return { ref, height }
-}
 
 export type AccordionPanelProps = {
   animate?: boolean
