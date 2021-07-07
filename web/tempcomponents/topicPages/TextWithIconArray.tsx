3ec094eaf83f1349bd407cc1241e26bb345b2928
Sven van Steenis <sven.vsteenis@gmail.com>
2021-07-07 13:18:02 +0200

    ♻️ improve responsive grid layout (#393)

--- web/tempcomponents/topicPages/TextWithIconArray.tsx
+++ web/tempcomponents/topicPages/TextWithIconArray.tsx
@@ -6,16 +6,27 @@ import SimpleBlockContent from '../../common/SimpleBlockContent'
 import { IngressBlockRenderer } from '../../common/serializers'
 import type { TextWithIconArrayData, TextWithIconItem, ImageWithAlt } from '../../types/types'
 
-const Container = styled.div`
+const Container = styled.div<{ items: number }>`
   --max-content-width: 1440px;
 
   display: grid;
   justify-content: center;
   grid-template-columns: repeat(auto-fit, minmax(13rem, 18rem));
   grid-gap: var(--space-xxLarge);
-  padding: var(--space-xxLarge) 0;
+  padding: var(--space-xxLarge) var(--space-large);
   margin: auto;
   max-width: var(--max-content-width);
+
+  @media (min-width: 750px) {
+    grid-template-columns: ${({ items }) => (items === 2 || items === 4 ? 'repeat(2, 1fr)' : 'repeat(3, 1fr)')};
+  }
+
+  @media (min-width: 1300px) {
+    ${({ items }) =>
+      items === 4 && {
+        gridTemplateColumns: 'repeat(4, 1fr)',
+      }}
+  }
 `
 
 const { Title, Media, Text } = TextWithIcon
@@ -33,7 +44,7 @@ const TextWithIconArray = ({ data }: TextWithIconArrayProps) => {
 
   return (
     <BackgroundContainer background={designOptions?.background}>
-      <Container>
+      <Container items={group.length}>
         {group.map((item: TextWithIconItem) => (
           <TextWithIcon key={item.id}>
             {item.icon && item.icon.asset && (
