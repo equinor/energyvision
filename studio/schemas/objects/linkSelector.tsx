320b6c6e266bda77c577cf945d18f2cfba164ecd
Sven van Steenis <sven.vsteenis@gmail.com>
2021-07-09 17:50:24 +0200

    ✨ add new reusable linkSelector schema object

---
+++ studio/schemas/objects/linkSelector.tsx
@@ -0,0 +1,81 @@
+import { SchemaType } from '../../types'
+
+const validateLink = (value: any, connectedField: any): SchemaType.ValidationResult => {
+  if (value && connectedField) {
+    return 'Can only have a single link. Choose either an internal or external link.'
+  }
+
+  if (!connectedField && !value) {
+    return 'An internal or external link is required.'
+  }
+
+  if (connectedField && !value) {
+    return true
+  }
+
+  return true
+}
+
+export type ReferenceTarget = {
+  type: string
+}
+
+const defaultReferenceTargets: ReferenceTarget[] = [
+  {
+    type: 'news',
+  },
+  { type: 'page' },
+]
+
+const LinkField = {
+  name: 'linkSelector',
+  title: 'Link',
+  type: 'object',
+  description: 'Select either an internal or external URL',
+  fields: [
+    {
+      name: 'label',
+      title: 'Label',
+      description: 'The label that the link/button should have.',
+      type: 'string',
+      validation: (Rule: SchemaType.ValidationRule) => Rule.required(),
+    },
+    {
+      name: 'reference',
+      title: 'Internal link',
+      description: 'Use this field to reference an internal page.',
+      type: 'reference',
+      validation: (Rule: SchemaType.ValidationRule) =>
+        Rule.custom((value: any, context: SchemaType.ValidationContext) => {
+          return validateLink(value, context.parent.url)
+        }),
+      to: defaultReferenceTargets,
+    },
+    {
+      name: 'url',
+      title: 'External URL',
+      description: 'Use this field to link to an external site.',
+      type: 'url',
+      validation: (Rule: SchemaType.ValidationRule) =>
+        Rule.custom((value: any, context: SchemaType.ValidationContext) => {
+          return validateLink(value, context.parent.reference)
+        }),
+    },
+  ],
+}
+
+// Used to generate a linkSelector field with dynamic reference targets
+// Might be a better way of doing this, but doesn't seem like we can pass
+// params to a schema field
+export const FilteredLinkField = (
+  fieldName = 'link',
+  referenceTargets: ReferenceTarget[] = defaultReferenceTargets,
+) => {
+  const FilteredLink = { ...LinkField }
+  FilteredLink.name = fieldName
+  FilteredLink.fields[1].to = referenceTargets
+
+  return FilteredLink
+}
+
+export default LinkField
