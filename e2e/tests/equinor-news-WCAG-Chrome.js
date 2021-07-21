c2c84f38f431fad394fbe80d3228083d0ebc3882
Nils Magne Lunde <nilsml@nilsland.com>
2021-07-21 15:53:44 +0200

    ♻️ Create output dir and refactor e2e test code

--- e2e/tests/equinor-news-WCAG-Chrome.js
+++ e2e/tests/equinor-news-WCAG-Chrome.js
@@ -1,20 +1,22 @@
-import { createRequire } from 'module';
-import {seleniumConfig} from '../seleniumConfig.js'
+import { createRequire } from 'module'
+import { seleniumConfig } from '../seleniumConfig.js'
 import webdriver from 'selenium-webdriver'
-import {envisStartPage} from '../energyVision.js'
-import * as fs from 'fs'
+import { envisStartPage } from '../energyVision.js'
+import fs, { promises as fsAsync } from 'fs'
 import { config } from 'dotenv'
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url)
+
+// TODO: Might want to move generic stuff out of this file and just keep what is needed for one particular test scenario
 
 config()
 
-const axeFile = require.resolve('axe-core/axe.min.js');
+const axeFile = require.resolve('axe-core/axe.min.js')
 
 const project = {
   name: 'Equinor News',
   build: 'Build #1',
-  project: 'Energy Vision'
+  project: 'Energy Vision',
 }
 const platform = {
   os: 'Windows',
@@ -23,32 +25,34 @@ const platform = {
   browser_version: 'latest',
 }
 const authentication = {
-  'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
+  'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER, // Only use when on server. Comment out locally.
   'browserstack.user': process.env.BROWSERSTACK_USERNAME,
-  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
 }
 
-const capabilities = { ...platform, ...project, ...seleniumConfig, ...authentication }
-console.log(capabilities)
+const defaultCapabilities = { ...platform, ...project, ...seleniumConfig, ...authentication }
+console.log(defaultCapabilities)
 
 const browserstackURL = `https://hub-cloud.browserstack.com/wd/hub`
 
-const driver = new webdriver.Builder().usingServer(browserstackURL).withCapabilities(capabilities).build()
-await driver.get(envisStartPage)
+const checkCreateDirectory = (dir) => (fs.existsSync(dir) || fs.mkdirSync(dir)) && dir
 
-const axe = fs.readFileSync(axeFile, 'utf8')
-await driver.executeScript(axe.toString())
-try {
-  const result = (platform.browserName === "Firefox")
-    ? driver.executeScript('let result; axe.run().then((r)=> {result=r}); return result;')
-    : await driver.executeScript('let result; await axe.run().then((r)=> {result=r}); return result;')
-  const reportFile = `output/${platform.browserName}_envis_news_report.json`
-  console.log('report file: ', reportFile)
-  fs.writeFileSync(reportFile, JSON.stringify(result))
-} catch (err) {
-  console.error('An error occured for browser', platform.browserName, err)
+const configureDriver = (browserStackURL) => (capabilities) =>
+  new webdriver.Builder().usingServer(browserStackURL).withCapabilities(capabilities).build()
+
+const storeJsonResult = async (fullPath, jsonResult) => await fsAsync.writeFile(fullPath, JSON.stringify(jsonResult))
+
+const testPage = (browserstackURL) => (capabilities) => async (page) => {
+  const outputDir = 'output'
+  const driver = configureDriver(browserstackURL)(capabilities)
+
+  await driver.get(page)
+  const axe = await fsAsync.readFile(axeFile, 'utf8')
+  await driver.executeScript(axe.toString())
+  await checkCreateDirectory(outputDir)
+  const result = await driver.executeScript('let result; await axe.run().then((r)=> {result=r}); return result;')
+  await storeJsonResult(`${outputDir}/${capabilities.browserName}_envis_news_report.json`, result)
   await driver.quit()
-  throw err
 }
 
-await driver.quit()
+await testPage(browserstackURL)(defaultCapabilities)(envisStartPage)
