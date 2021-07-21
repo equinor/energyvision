49244ccd4079c0dc6e7f961b1dc62b87037c1c07
Sven van Steenis <sven.vsteenis@gmail.com>
2021-07-21 10:45:48 +0200

    ♻️ update CustomArrayFunctions to latest Sanity version

--- studio/components/CustomArrayFunctions.tsx
+++ studio/components/CustomArrayFunctions.tsx
@@ -1,80 +1,89 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable import/no-unresolved */
+// custom implementation of @sanity/form-builder/src/inputs/arrays/common/ArrayFunctions.tsx
+import { ArraySchemaType, isReferenceSchemaType } from '@sanity/types'
+import { AddIcon } from '@sanity/icons'
+import React, { ReactNode, useMemo, useCallback } from 'react'
+import { Button, Grid, Menu, MenuButton, MenuItem } from '@sanity/ui'
+import { useId } from '@reach/auto-id'
 
-// custom implementation of @sanity/form-builder/src/inputs/ArrayInput/ArrayFunctions.tsx
-
-import React from 'react'
-import DropDownButton from 'part:@sanity/components/buttons/dropdown'
-import Button from 'part:@sanity/components/buttons/default'
-import ButtonGrid from 'part:@sanity/components/buttons/button-grid'
-
-type ArrayType = any
-type ItemValue = any
-type PatchEvent = any
-
-type Props = {
-  type: ArrayType
-  children: Node | null
-  value: Array<ItemValue>
+// validation is defined on 'type' but not declared on SchemaType
+type WithValidation = {
+  validation?: any[]
+}
+export interface ArrayFunctionsProps<SchemaType extends ArraySchemaType, MemberType> {
+  className?: string
+  type: SchemaType & WithValidation
+  children?: ReactNode
+  value?: MemberType[]
   readOnly: boolean | null
-  onAppendItem: (itemValue: ItemValue) => void
-  onPrependItem: (itemValue: ItemValue) => void
-  onFocusItem: (item: ItemValue) => void
-  onCreateValue: (type: any) => ItemValue
-  onChange: (event: PatchEvent) => void
+  onAppendItem: (itemValue: MemberType) => void
+  onPrependItem: (itemValue: MemberType) => void
+  onFocusItem: (item: MemberType, index: number) => void
+  onCreateValue: (type: SchemaType) => MemberType
+  onChange: (event: any) => void
 }
 
-const ArrayFunctions = (props: Props) => {
-  const { type, readOnly, children, value, onCreateValue, onAppendItem } = props
+export default function ArrayFunctions<MemberType>(props: ArrayFunctionsProps<ArraySchemaType, MemberType>) {
+  const { type, readOnly, children, onCreateValue, onAppendItem, value } = props
+  const menuButtonId = useId()
 
-  if (readOnly) return null
+  const insertItem = useCallback(
+    (itemType) => {
+      const item = onCreateValue(itemType)
 
-  const maxLength = type.validation[0]._rules.find((rule: any) => rule.flag === 'max')
+      onAppendItem(item)
+    },
+    [onCreateValue, onAppendItem],
+  )
+
+  const handleAddBtnClick = useCallback(() => {
+    insertItem(type.of[0])
+  }, [type, insertItem])
+
+  const popoverProps = useMemo(() => ({ constrainSize: true, portal: true }), [])
+
+  if (readOnly) {
+    return null
+  }
+
+  const maxLength = type?.validation && type.validation[0]._rules.find((rule: any) => rule.flag === 'max')
 
   if (maxLength && value && value.length >= maxLength.constraint) {
     return null
   }
 
-  const handleInsertItem = (type: any) => {
-    const item = onCreateValue(type)
-    onAppendItem(item)
-  }
-
-  const handleDropDownAction = (menuItem: any) => {
-    handleInsertItem(menuItem.type)
-  }
-
-  const handleAddBtnClick = () => {
-    handleInsertItem(props.type.of[0])
-  }
-
-  const renderSelectType = () => {
-    const items = type.of.map((memberDef: any) => ({
-      title: memberDef.title || memberDef.type.name,
-      type: memberDef,
-    }))
-    return (
-      <DropDownButton inverted items={items} onAction={handleDropDownAction}>
-        Add
-      </DropDownButton>
-    )
-  }
-
   return (
-    <div>
-      <ButtonGrid align="start">
-        {type.of.length === 1 ? (
-          <Button inverted onClick={handleAddBtnClick}>
-            Add
-          </Button>
-        ) : (
-          renderSelectType()
-        )}
+    <Grid gap={1} style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(100px, 1fr))' }}>
+      {type.of.length === 1 ? (
+        <Button icon={AddIcon} mode="ghost" onClick={handleAddBtnClick} text="Add item" />
+      ) : (
+        <MenuButton
+          button={<Button icon={AddIcon} mode="ghost" text="Add item…" />}
+          id={menuButtonId || ''}
+          menu={
+            <Menu>
+              {type.of.map((memberDef, i) => {
+                // Use reference icon if reference is to one type only
+                const referenceIcon =
+                  isReferenceSchemaType(memberDef) && (memberDef.to || []).length === 1 && memberDef.to[0].icon
 
-        {children || null}
-      </ButtonGrid>
-    </div>
+                const icon = memberDef.icon || memberDef.type?.icon || referenceIcon
+                return (
+                  <MenuItem
+                    key={memberDef.name}
+                    text={memberDef.title || memberDef.type?.name}
+                    onClick={() => insertItem(memberDef)}
+                    icon={icon}
+                  />
+                )
+              })}
+            </Menu>
+          }
+          popover={popoverProps}
+        />
+      )}
+
+      {children}
+    </Grid>
   )
 }
-
-export default ArrayFunctions
