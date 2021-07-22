4099b003dd8e26c40415c4c8e2aea1b5272ff1c6
Sven van Steenis <sven.vsteenis@gmail.com>
2021-07-22 10:49:32 +0200

    ♻️ use shared ButtonLink component for CallToActions

--- web/tempcomponents/topicPages/CallToActions.tsx
+++ web/tempcomponents/topicPages/CallToActions.tsx
@@ -1,7 +1,8 @@
 import { Fragment } from 'react'
-import { Button, List, Link } from '@components'
+import { List, Link } from '@components'
 import type { LinkData } from '../../types/types'
 import { default as NextLink } from 'next/link'
+import { ButtonLink, getUrl } from '../shared/ButtonLink'
 
 const { Item } = List
 
@@ -9,47 +10,14 @@ type CallToActionsProps = {
   callToActions: LinkData[]
 }
 
-function getUrl(callToAction: LinkData) {
-  const { type, link, href } = callToAction;
-  let url: string
-  if (type === 'internalUrl') {
-    url = link?.type === 'news' ? `/news/${link?.slug}` : link?.slug || ''
-  } else {
-    url = href || ''
-  }
-  return url;
-}
-
-const C2ALinkAsButton = ({ callToAction }: { callToAction: LinkData }) => {
-  const { type, label, extension } = callToAction
-
-  const url = getUrl(callToAction)
-
-  return (
-    <>
-      {type === 'internalUrl' ? (
-        <NextLink passHref href={url}>
-          <Button as="a" variant="outlined" color="secondary">
-            {label}
-          </Button>
-        </NextLink>
-      ) : (
-          <Button as="a" variant="outlined" href={url} color="secondary">
-            {label} {extension && `(${extension.toUpperCase()})`}
-          </Button>
-        )}
-    </>
-  )
-}
-
 const CallToActions = ({ callToActions }: CallToActionsProps) => {
   if (!callToActions) return null
 
-  return (
-    callToActions.length === 1 ?
-      <C2ALinkAsButton callToAction={callToActions[0]} />
-      :
-      <List unstyled>{callToActions.map((callToAction: LinkData) => {
+  return callToActions.length === 1 ? (
+    <ButtonLink action={callToActions[0]} />
+  ) : (
+    <List unstyled>
+      {callToActions.map((callToAction: LinkData) => {
         const { id, type, label, extension } = callToAction
         const url = getUrl(callToAction)
         return (
@@ -63,15 +31,16 @@ const CallToActions = ({ callToActions }: CallToActionsProps) => {
                 </NextLink>
               </Item>
             ) : (
-                <Item>
-                  <Link variant="contentLink" type={type} href={url}>
-                    {label} {extension && `(${extension.toUpperCase()})`}
-                  </Link>
-                </Item>
-              )}
+              <Item>
+                <Link variant="contentLink" type={type} href={url}>
+                  {label} {extension && `(${extension.toUpperCase()})`}
+                </Link>
+              </Item>
+            )}
           </Fragment>
         )
-      })}</List>
+      })}
+    </List>
   )
 }
 
