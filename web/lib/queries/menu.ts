7cb3fc56c78f4ff61c3bf4d616a65ea9adf5be22
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-08-24 11:03:25 +0200

    🚧 1. version of the actual menu query (#436)
    git push

--- web/lib/queries/menu.ts
+++ web/lib/queries/menu.ts
@@ -1,4 +1,14 @@
 export const menuQuery = /* groq */ `
 *[_type == "subMenu"] {
-  ...,
+  "id": _key,
+  label,
+  "group": group[]{
+  	"id": _key,
+  	label,
+ 		"links": links[]{
+       "id": _key,
+      "slug" :route->slug[$lang].current,
+      label,
+    }
+  },
 }`
