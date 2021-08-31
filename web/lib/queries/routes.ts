537939096c910605c3fba9af0f7fc2f214381806
Sven van Steenis <sven.vsteenis@gmail.com>
2021-08-31 14:34:06 +0200

    🚧 update route query for new split language (#445)

--- web/lib/queries/routes.ts
+++ web/lib/queries/routes.ts
@@ -1,37 +1,16 @@
 import pageContentFields from './common/pageContentFields'
 
-// @TODO: This should definitely not be duplicated across languages like this, but we are working with Sanity to figure
-// out why the preview plugin doesn't work with param locale
-export const pageQueryEn = /* groq */ ` 
-   *[_type == "route" && slug.en_GB.current == $slug][0] {
-    "slug": slug.en_GB.current,
-    "allSlugs": slug,
-    "title": content.en_GB->title,
-    "seoAndSome": content.en_GB->{
+export const pageQuery = /* groq */ ` 
+  *[_type == "route_" + $lang && slug.current == $slug][0] {
+    "slug": slug.current,
+    "title": content->title,
+    "seoAndSome": content->{
       "documentTitle": seo.documentTitle,
           "metaDescription": seo.metaDescription,
           openGraphImage,
     },
-    "heroImage": content.en_GB->heroFigure,
-    "content": content.en_GB->content[]{
-        
-      ${pageContentFields}
-    }
-  }
-`
-
-export const pageQueryNo = /* groq */ ` 
-   *[_type == "route" && slug.nb_NO.current == $slug][0] {
-    "slug": slug.nb_NO.current,
-    "allSlugs": slug,
-    "title": content.nb_NO->title,
-    "seoAndSome": content.nb_NO->{
-      "documentTitle": seo.documentTitle,
-          "metaDescription": seo.metaDescription,
-          openGraphImage,
-    },
-    "heroImage": content.nb_NO->heroFigure,
-    "content": content.nb_NO->content[]{
+    "heroImage": content->heroFigure,
+    "content": content->content[]{
         
       ${pageContentFields}
     }
