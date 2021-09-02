99a9982f95a3a09da87f1b970abfa388d7b314fe
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-09-02 09:53:21 +0200

    📝 Add comment on menu preview (#445)

--- web/pages/[[...slug]].tsx
+++ web/pages/[[...slug]].tsx
@@ -78,13 +78,19 @@ export default function Page({ data, preview }: any) {
 
 // eslint-disable-next-line react/display-name
 Page.getLayout = (page: AppProps) => {
-  // This is just an ordinary function
+  /* The getLayout pattern is a way to preserve state in the layout
+  across client side navigation. The downside is that since it's just an
+  ordinary function, we can't use the preview subcscription hook out of the box.
+  As a consequence, preview for the menu data is not available.
+
+  If this is a problem, we need to see if we are able to find another solution  */
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const { props } = page
 
   const { data, preview } = props
+
   const slugs = {
     en_GB: data?.pageData?.allSlugs?.en_GB.current,
     nb_NO: data?.pageData?.allSlugs?.nb_NO.current,
