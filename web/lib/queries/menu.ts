19d4a0dae8318285a7890fae2461ecd822ee3b1d
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-09-03 10:31:52 +0200

    ✨ Use static url in menu query (#139)

--- web/lib/queries/menu.ts
+++ web/lib/queries/menu.ts
@@ -2,10 +2,23 @@ export const menuQuery = /* groq */ `
   *[_type == "siteMenu" && _lang == $lang][0] {
 	"subMenus": group[]{
   	"id": _key,
-  	label,
+    "topLevelLink": {
+      label,
+      "isStatic": coalesce(isStatic, false),
+  		"link": reference-> {
+        "type": _type,
+        "slug": slug.current,
+      },
+      "href": url,
+      "staticUrl": staticUrl,
+    },
     "group": group[]{
       "id": _key,
-      label,
+      "topLevelLink": {
+        label,
+        isStatic,
+      },
+     
       "links": links[]{
          "id": _key,
         "slug": route->slug.current,
