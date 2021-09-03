2bcb4b4147c0b70c1b7440d870bd39392ed59a08
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-09-03 11:53:32 +0200

    ✨ Add static sub level url ins sanity (#139)

--- studio/schemas/objects/menuLink.tsx
+++ studio/schemas/objects/menuLink.tsx
@@ -15,6 +15,14 @@ export default {
       validation: (Rule: SchemaType.ValidationRule): SchemaType.ValidationRule => Rule.required(),
     },
     {
+      name: 'isStatic',
+      title: 'Is static page',
+      description: `While migrating, content can be available as static pages generated from the old CMS. If this is 
+      the case for this menu item, it's important to register the url in the static input field`,
+      type: 'boolean',
+      initialValue: false,
+    },
+    {
       title: 'Route',
       name: 'route',
       description: 'The content you want to appear at this path. Remember that it needs to be published first.',
@@ -26,19 +34,36 @@ export default {
           params: { routeLang: `route_${document._lang}` },
         }),
       },
-      validation: (Rule: SchemaType.ValidationRule): SchemaType.ValidationRule => Rule.required(),
+
+      // @TODO How can we validate only when it's active
+      // validation: (Rule: SchemaType.ValidationRule): SchemaType.ValidationRule => Rule.required(),
+      // eslint-disable-next-line
+      // @ts-ignore: Djeez, typescript
+      hidden: ({ parent }) => parent?.isStatic === true,
+    },
+    {
+      name: 'staticUrl',
+      title: 'Static URL',
+      type: 'string',
+      description: `The URL for the static page. Don't add language information (no/en)`,
+      placeholder: '/careers/experienced-professionals',
+      // eslint-disable-next-line
+      // @ts-ignore: Djeez, typescript
+      hidden: ({ parent }) => parent?.isStatic === false,
     },
   ],
   preview: {
     select: {
       label: 'label',
       route: 'route.slug',
+      isStatic: 'isStatic',
+      staticUrl: 'staticUrl',
     },
     prepare(selection: any) {
-      const { label, route } = selection
+      const { label, route, isStatic, staticUrl } = selection
       return {
         title: label || 'No label added yet',
-        subtitle: route?.current || 'No route selected yet',
+        subtitle: isStatic ? `Static URL: ${staticUrl}` : route?.current || 'No route selected yet',
         media: EdsIcon(link),
       }
     },
