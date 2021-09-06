595d353cf86acb0ffe6f331000dee36c1a064dc1
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-09-06 10:27:26 +0200

    ⚡️ Test preload of font

--- web/styles/globalStyles.ts
+++ web/styles/globalStyles.ts
@@ -5,6 +5,17 @@ import { generic } from './generic'
 import { elements } from './elements'
 
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: Equinor;
+    src: local('ðŸ˜Š'),
+        url(https://eds-static.equinor.com/font/EquinorVariable-VF.woff2) format('woff2-variations'),
+        url(https://eds-static.equinor.com/font/EquinorVariable-VF.woff) format('woff-variations'),
+        url(https://eds-static.equinor.com/font/Equinor-Regular.woff) format('woff');
+    font-style: normal;
+    font-weight: 1 999;
+    font-display: fallback;
+}
+
   ${colors}
   ${typography}
   ${spacings}
@@ -14,6 +25,8 @@ export const GlobalStyle = createGlobalStyle`
   * {
     font-family: Equinor, 'Open Sans', sans-serif;
   }
+
+
 
   body {
     background-color: var(--theme-background-primary);
