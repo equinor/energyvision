0417dfd7eaf27911455bb88f9039bd667bcdbb3c
Sven van Steenis <sven.vsteenis@gmail.com>
2021-09-10 14:04:30 +0200

    ⚗️temp: return of the page routes

--- web/pages/[[...slug]].tsx
+++ web/pages/[[...slug]].tsx
@@ -122,7 +122,7 @@ Page.getLayout = (page: AppProps) => {
 
 export const getStaticProps: GetStaticProps = async ({ params, preview = false, locale = 'en' }) => {
   const { query, queryParams, docType } = getQueryFromSlug(params?.slug as string[], locale)
-
+  // console.log('params', params)
   const pageData = query && (await getClient(preview).fetch(query, queryParams))
   // Let's do it simple stupid and iterate later on
   const menuData = await getClient(preview).fetch(menuQuery, { lang: mapLocaleToLang(locale) })
