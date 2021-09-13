553efd503a0d10c22db39e8e7c8992ed4861965c
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-09-13 13:25:34 +0200

    ➕ Use next-circular-deps (#470)

--- web/next.config.js
+++ web/next.config.js
@@ -4,52 +4,57 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({
-  eslint: {
-    // Warning: Dangerously allow production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  publicRuntimeConfig: {
-    domain: 'https://energyvision.app.radix.equinor.com',
-    archiveStorageURL: archiveServerHostname,
-  },
-  images: {
-    domains: ['cdn.sanity.io'],
-  },
-  i18n: {
-    locales: ['no', 'en'],
-    defaultLocale: 'en',
-    localeDetection: false,
-  },
-  async rewrites() {
-    return [
-      // Archive_Rule: This rule load the archived images from dam
-      {
-        source: '/content/dam/:slug*',
-        destination: `${archiveServerHostname}/content/dam/:slug*`,
-      },
-      // Archive_Rule: This rule loads the necessary client libraries for old html content
-      {
-        source: '/etc.clientlibs/:slug*',
-        destination: `${archiveServerHostname}/etc.clientlibs/:slug*`,
-      },
-    ]
-  },
-  webpack(config, { defaultLoaders }) {
-    config.module.rules.push({
-      test: /\.(css)$/,
-      use: [
-        defaultLoaders.babel,
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextCircularDeps = require('next-circular-dependency')
+
+module.exports = withNextCircularDeps(
+  withBundleAnalyzer({
+    eslint: {
+      // Warning: Dangerously allow production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    publicRuntimeConfig: {
+      domain: 'https://energyvision.app.radix.equinor.com',
+      archiveStorageURL: archiveServerHostname,
+    },
+    images: {
+      domains: ['cdn.sanity.io'],
+    },
+    i18n: {
+      locales: ['no', 'en'],
+      defaultLocale: 'en',
+      localeDetection: false,
+    },
+    async rewrites() {
+      return [
+        // Archive_Rule: This rule load the archived images from dam
         {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires
-          loader: require('styled-jsx/webpack').loader,
-          options: {
-            type: 'global',
-          },
+          source: '/content/dam/:slug*',
+          destination: `${archiveServerHostname}/content/dam/:slug*`,
         },
-      ],
-    })
-    return config
-  },
-})
+        // Archive_Rule: This rule loads the necessary client libraries for old html content
+        {
+          source: '/etc.clientlibs/:slug*',
+          destination: `${archiveServerHostname}/etc.clientlibs/:slug*`,
+        },
+      ]
+    },
+    webpack(config, { defaultLoaders }) {
+      config.module.rules.push({
+        test: /\.(css)$/,
+        use: [
+          defaultLoaders.babel,
+          {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            loader: require('styled-jsx/webpack').loader,
+            options: {
+              type: 'global',
+            },
+          },
+        ],
+      })
+      return config
+    },
+  }),
+)
