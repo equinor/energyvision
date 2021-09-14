f3b001daa8cc276f2d415f94cf599a7634d63c64
Sven van Steenis <sven.vsteenis@gmail.com>
2021-09-14 12:17:13 +0200

    🔥 remove unused code

--- web/common/helpers/staticPageHelpers.tsx
+++ web/common/helpers/staticPageHelpers.tsx
@@ -68,30 +68,3 @@ export const getPagePaths = async (path: string): Promise<string[]> => {
 
   return []
 }
-
-export const getPageLayout = (page: AppProps) => {
-  /* The getLayout pattern is a way to preserve state in the layout
-  across client side navigation. The downside is that since it's just an
-  ordinary function, we can't use the preview subcscription hook out of the box.
-  As a consequence, preview for the menu data is not available.
-
-  If this is a problem, we need to see if we are able to find another solution  */
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const { props } = page
-
-  const { data } = props
-
-  const slugs = {
-    en_GB: data?.pageData?.allSlugs?.en_GB,
-    nb_NO: data?.pageData?.allSlugs?.nb_NO,
-  }
-
-  return (
-    <Layout preview={false}>
-      <Menu slugs={slugs} data={data?.menuData} />
-      {page}
-    </Layout>
-  )
-}
