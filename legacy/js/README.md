e46f6b6d5b827e9b9cc926f96e66421f644e7e3f
Victor Nystad <vnys@equinor.com>
2021-09-20 09:27:45 +0000

    🔧 Link to workspace #477
    Added postinstall script so legacy css and js builds after install.

---
+++ legacy/js/README.md
@@ -0,0 +1,3 @@
+# Energy Vision Legacy JS
+
+This module contains legacy JS for the static pages in Energy Vision
--- legacy/js/gulpfile.js
+++ legacy/js/gulpfile.js
@@ -1,13 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint @typescript-eslint/no-var-requires: "off" */
-var gulp = require('gulp');
-var concat = require('gulp-concat')
-var pipeline = require('readable-stream').pipeline;
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const pipeline = require('readable-stream').pipeline
+const del = require('del')
 
-gulp.task('default', function () {
-  return pipeline(
-        
-        gulp.src(['src/vendor/jquery.js',
+gulp.task('clean', () => del(['./dist/']))
+
+gulp.task(
+  'default',
+  gulp.series('clean', function () {
+    return pipeline(
+      gulp.src(
+        [
+          'src/vendor/jquery.js',
           'src/vendor/jquery-lazy.js',
           'src/vendor/jquery.magnific-popup.js',
           'src/vendor/countUp.js',
@@ -23,42 +29,46 @@ gulp.task('default', function () {
           'src/vendor/ofi.browser.js',
           'src/vendor/list.min.js',
           'src/vendor/moment.js',
-          'src/vendor/moment-timezone-with-data-1970-2030.js'],[
-            'src/animations.js',
-            'src/navigation.js',
-            'src/modals.js',
-            'src/overlay.js',
-            'src/carousels.js',
-            'src/search.js',
-            'src/video.js',
-            'src/magazine.js',
-            'src/cookiealert.js',
-            'src/list.js',
-            'src/language-switch-module.js',
-            'src/progress-button.js',
-            'src/numbers.js',
-            'src/magnific-popup-module.js',
-            'src/scroll-to-top.js',
-            'src/outdated-browser-notification.js',
-            'src/useragent.js',
-            'src/remittable.js',
-            'src/in-page-nav.js',
-            'src/horizontal-bar.js',
-            'src/calendar.js',
-            'src/mailing-list-module.js',
-            'src/service-now-module.js',
-            'src/foundation-module.js',
-            'src/storage-form-date-picker.js',
-            'src/magazine-list.js',
-            'src/career-fair-form.js',
-            'src/buttons.js',
-            'src/visual-list.js',
-            'src/button-click-on-space-bar.js',
-            'src/basicaccordion.js',
-            'src/site-switch.js',
-            'src/init.js' //Leave init.js at the bottom.
-          ]),
-        concat('legacy.minified.js'),
-        gulp.dest('dist/js')
-  );
-});
+          'src/vendor/moment-timezone-with-data-1970-2030.js',
+        ],
+        [
+          'src/animations.js',
+          'src/navigation.js',
+          'src/modals.js',
+          'src/overlay.js',
+          'src/carousels.js',
+          'src/search.js',
+          'src/video.js',
+          'src/magazine.js',
+          'src/cookiealert.js',
+          'src/list.js',
+          'src/language-switch-module.js',
+          'src/progress-button.js',
+          'src/numbers.js',
+          'src/magnific-popup-module.js',
+          'src/scroll-to-top.js',
+          'src/outdated-browser-notification.js',
+          'src/useragent.js',
+          'src/remittable.js',
+          'src/in-page-nav.js',
+          'src/horizontal-bar.js',
+          'src/calendar.js',
+          'src/mailing-list-module.js',
+          'src/service-now-module.js',
+          'src/foundation-module.js',
+          'src/storage-form-date-picker.js',
+          'src/magazine-list.js',
+          'src/career-fair-form.js',
+          'src/buttons.js',
+          'src/visual-list.js',
+          'src/button-click-on-space-bar.js',
+          'src/basicaccordion.js',
+          'src/site-switch.js',
+          'src/init.js', //Leave init.js at the bottom.
+        ],
+      ),
+      concat('legacy.minified.js'),
+      gulp.dest('dist/js'),
+    )
+  }),
+)
