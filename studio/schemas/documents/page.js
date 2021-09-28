2913e6c5e5d02f0fd8b4b5158594dada274cbc9b
Sven van Steenis <sven.vsteenis@gmail.com>
2021-09-28 16:49:06 +0200

    🚧 add schema for iframe object (#425)

--- studio/schemas/documents/page.js
+++ studio/schemas/documents/page.js
@@ -77,6 +77,7 @@ export default {
         { type: 'accordion' },
         { type: 'promoTileArray' },
         { type: 'stockValuesApi' },
+        { type: 'iframe' },
       ],
     },
   ],
---
+++ studio/schemas/objects/iframe.tsx
@@ -0,0 +1,37 @@
+import { SchemaType } from '../../types'
+
+export default {
+  title: 'IFrame',
+  name: 'iframe',
+  type: 'object',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+      description: 'The title of the iframe. This value is not visible on the page but is required for accessibility.',
+      validation: (Rule: SchemaType.ValidationRule) => Rule.required(),
+    },
+    {
+      name: 'url',
+      type: 'url',
+      title: 'URL',
+      description: 'Link to the content to be loaded inside the iframe.',
+      validation: (Rule: SchemaType.ValidationRule) => Rule.required(),
+    },
+    {
+      name: 'aspectRatio',
+      type: 'string',
+      title: 'Aspect ratio',
+      options: {
+        list: [
+          { title: '16:9', value: '16:9' },
+          { title: '4:3', value: '4:3' },
+          { title: '1:1', value: '1:1' },
+        ],
+        layout: 'dropdown',
+      },
+      initialValue: '16:9',
+    },
+  ],
+}
