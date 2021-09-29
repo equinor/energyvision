63c8c5edb1c4912458ffc54ec6d931ac56e008c8
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-09-29 09:21:25 +0200

    ♿️ Use aria-label instead of srOnly (#510)

--- web/pageComponents/shared/Header.tsx
+++ web/pageComponents/shared/Header.tsx
@@ -12,20 +12,6 @@ const TopbarOffset = createGlobalStyle<{ topbarHeight: number }>`
   }
 `
 
-const SrText = styled.span`
-  border: 0;
-  padding: 0;
-  margin: 0;
-  position: absolute !important;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(1px 1px 1px 1px); /* IE6, IE7 - a 0 height clip, off to the bottom right of the visible 1px box */
-  clip: rect(1px, 1px, 1px, 1px); /*maybe deprecated but we need to support legacy browsers */
-  clip-path: inset(50%); /*modern browsers, clip-path works inwards from each corner*/
-  white-space: nowrap; /* added line to stop words getting smushed together (as they go onto seperate lines and some screen readers do not understand line feeds as a space */
-`
-
 const TempHideSmall = styled.div`
   display: none;
   @media (min-width: 1440px) {
@@ -63,9 +49,8 @@ const Header = ({ slugs, data }: HeaderProps) => {
       <Topbar height={topbarHeight} ref={topbarRef}>
         {/* @TODO: Localize strings */}
         <Link href="/" passHref>
-          <a>
+          <a aria-label="Equinor home page">
             <Logo />
-            <SrText>Equinor home page</SrText>
           </a>
         </Link>
         <TempHideLarge>
