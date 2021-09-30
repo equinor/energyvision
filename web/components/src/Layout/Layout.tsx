bf862b11fcc9953297616738cb23a7044f1e73ad
Ådne Forslund <adne.forslund@gmail.com>
2021-09-30 16:05:22 +0200

    :rocket: implement footer in layout

--- web/components/src/Layout/Layout.tsx
+++ web/components/src/Layout/Layout.tsx
@@ -1,12 +1,14 @@
-import styled from 'styled-components'
+import { Footer } from '@components'
 
+/*
 const Footer = styled.footer`
   background-color: var(--slate-blue-95);
   min-height: var(--space-4xLarge);
-  /* Because the static AEM content have several float: left styles added to the wrapper containers  */
+  
   clear: both;
-`
-
+` 
+*/
+/* Because the static AEM content have several float: left styles added to the wrapper containers  */
 export type LayoutProps = {
   /* Prewiew or not */
   preview?: boolean
