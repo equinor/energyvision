83a01439b47941a54f9d5ab2a3675f4d25636be0
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-01 08:23:36 +0200

    💄 Highlight the open menu items (#510)

--- web/components/src/Menu/SubMenuPanel.tsx
+++ web/components/src/Menu/SubMenuPanel.tsx
@@ -1,18 +1,35 @@
-import { forwardRef } from 'react'
+import { forwardRef, HTMLAttributes, CSSProperties } from 'react'
 import styled from 'styled-components'
-import { AccordionPanel as RAccordionPanel, AccordionPanelProps as RAccordionPanelProps } from '@reach/accordion'
+import {
+  AccordionPanel as RAccordionPanel,
+  AccordionPanelProps as RAccordionPanelProps,
+  useAccordionItemContext,
+} from '@reach/accordion'
 
 const StyledPanel = styled(RAccordionPanel)`
   border-bottom: 1px solid var(--grey-40);
+  background-color: var(--background-color);
 `
-export type SubMenuPanelProps = RAccordionPanelProps
+export type SubMenuPanelProps = RAccordionPanelProps & HTMLAttributes<HTMLDivElement>
 
-export const SubMenuPanel = forwardRef<HTMLDivElement, RAccordionPanelProps>(function SubMenuPanel(
-  { children, ...rest },
+export const SubMenuPanel = forwardRef<HTMLDivElement, SubMenuPanelProps>(function SubMenuPanel(
+  { children, style, ...rest },
   ref,
 ) {
+  const context = useAccordionItemContext()
+  const isExpanded = context.isExpanded
+
   return (
-    <StyledPanel ref={ref} {...rest}>
+    <StyledPanel
+      style={
+        {
+          ...style,
+          '--background-color': isExpanded ? 'var(--grey-10)' : 'var(--ui-background-default)',
+        } as CSSProperties
+      }
+      ref={ref}
+      {...rest}
+    >
       {children}
     </StyledPanel>
   )
