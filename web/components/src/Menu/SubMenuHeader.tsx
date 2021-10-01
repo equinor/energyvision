f9dd261b575a92a4f97cf950a0d09d6d1c53d794
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-01 08:23:36 +0200

    💄 Highlight the open menu items (#510)

--- web/components/src/Menu/SubMenuHeader.tsx
+++ web/components/src/Menu/SubMenuHeader.tsx
@@ -1,11 +1,11 @@
-import { forwardRef } from 'react'
+import { forwardRef, CSSProperties } from 'react'
 import styled from 'styled-components'
 import {
   AccordionButton as RAccordionButton,
   AccordionButtonProps as RAccordionButtonProps,
   useAccordionItemContext,
 } from '@reach/accordion'
-import { Icon, Typography } from '@equinor/eds-core-react'
+import { Icon, Typography, TypographyProps } from '@equinor/eds-core-react'
 import { add, minimize } from '@equinor/eds-icons'
 import { outlineTemplate, Tokens } from '@utils'
 
@@ -28,20 +28,41 @@ const StyledButton = styled(RAccordionButton)`
 
 const StyledHeader = styled(Typography)`
   padding: 0 var(--space-large);
+  background-color: var(--background-color);
 `
-const StyledTypography = styled(Typography)``
-export type SubMenuHeaderProps = RAccordionButtonProps
+const StyledTypography = styled(Typography)`
+  font-weight: var(--font-weight);
+`
+
+export type SubMenuHeaderProps = TypographyProps & RAccordionButtonProps
 
 export const SubMenuHeader = forwardRef<HTMLButtonElement, SubMenuHeaderProps>(function SubMenuHeader(
-  { children, ...rest },
+  { children, style, ...rest },
   ref,
 ) {
   const context = useAccordionItemContext()
   const isExpanded = context.isExpanded
   return (
-    <StyledHeader forwardedAs="h3">
+    <StyledHeader
+      forwardedAs="h3"
+      style={
+        {
+          ...style,
+          '--background-color': isExpanded ? 'var(--grey-10)' : 'var(--ui-background-default)',
+        } as CSSProperties
+      }
+    >
       <StyledButton ref={ref} {...rest}>
-        <StyledTypography forwardedAs="span">{children}</StyledTypography>
+        <StyledTypography
+          forwardedAs="span"
+          style={
+            {
+              '--font-weight': isExpanded ? '700' : '400',
+            } as CSSProperties
+          }
+        >
+          {children}
+        </StyledTypography>
         {isExpanded ? <Icon data={minimize} /> : <Icon data={add} />}
       </StyledButton>
     </StyledHeader>
