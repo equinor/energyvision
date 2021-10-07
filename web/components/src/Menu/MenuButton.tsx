109b74cc8e3b4f9e4c9bdbe0d468736c35788715
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-07 21:26:40 +0200

    ♻️ Refactor the menu button and reuse (#510)

--- web/components/src/Menu/MenuButton.tsx
+++ web/components/src/Menu/MenuButton.tsx
@@ -4,46 +4,32 @@ import { Icon, Button, ButtonProps } from '@equinor/eds-core-react'
 import { clear, menu } from '@equinor/eds-icons'
 
 export type MenuButtonProps = Omit<ButtonProps, 'color' | 'variant'> & {
-  /** Do we want to make it explicit like this? Or just aria-expanded from button */
-  ariaExpanded: boolean
+  expanded?: boolean
   /** The text for the menu button, visually hidden on small screens */
   title: string
 }
 
-const StyledIcon = styled(Icon)`
-  fill: var(--default-text);
-`
+const StyledIcon = styled(Icon)``
 
-const Title = styled.span`
-  color: var(--default-text);
-  @media (max-width: 800px) {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0, 0, 0, 0);
-    white-space: nowrap;
-    border: 0;
-  }
-`
+const Title = styled.span``
 
 const StyledMenuButton = styled(Button)`
+  color: var(--default-text);
+  fill: var(--default-text);
   @media (min-width: 1300px) {
     display: none;
   }
 `
 
 export const MenuButton = forwardRef<HTMLButtonElement, MenuButtonProps>(function MenuButton(
-  { ariaExpanded, title, ...rest },
+  { expanded = false, title, ...rest },
   ref,
 ) {
   const iconSize = 24
   return (
-    <StyledMenuButton variant="ghost" aria-expanded={ariaExpanded} ref={ref} {...rest}>
+    <StyledMenuButton variant="ghost" ref={ref} {...rest}>
       <Title>{title}</Title>
-      {ariaExpanded ? <StyledIcon size={iconSize} data={clear} /> : <StyledIcon size={iconSize} data={menu} />}
+      {expanded ? <StyledIcon size={iconSize} data={clear} /> : <StyledIcon size={iconSize} data={menu} />}
     </StyledMenuButton>
   )
 })
