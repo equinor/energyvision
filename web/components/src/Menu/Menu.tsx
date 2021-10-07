26d2e2a24a120cca7879e1e398c3dd7d0b1329a8
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-07 22:14:27 +0200

    🚧 Try to render menu as list (#510)

--- web/components/src/Menu/Menu.tsx
+++ web/components/src/Menu/Menu.tsx
@@ -5,14 +5,20 @@ import { Accordion as RAccordion, AccordionProps as RAccordionProps } from '@rea
 export type MenuProps = RAccordionProps
 
 const StyledAccordion = styled(RAccordion)`
+  /*   margin: 0;
+  padding: 0;
+  list-style: none; */
   @media (min-width: 1300px) {
     display: flex;
   }
 `
 
-export const Menu = forwardRef<HTMLDivElement, MenuProps>(function Menu({ children, ...rest }, ref) {
+export const Menu = forwardRef</* HTMLUListElement */ HTMLDivElement, MenuProps>(function Menu(
+  { children, ...rest },
+  ref,
+) {
   return (
-    <StyledAccordion ref={ref} {...rest}>
+    <StyledAccordion /* as="ul" */ ref={ref} {...rest}>
       {children}
     </StyledAccordion>
   )
