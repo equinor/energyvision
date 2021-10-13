14086d1b85383be5aba7ff6d19ba84664be9380a
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-13 13:55:24 +0200

    ✨ Add uppercase and underline to Link and Header (#525)

--- web/components/stories/components/Menu.stories.tsx
+++ web/components/stories/components/Menu.stories.tsx
@@ -1,6 +1,8 @@
 import { Story, Meta } from '@storybook/react'
-import { Menu, MenuProps } from '@components'
+import { Menu, MenuProps, Heading, Link, List } from '@components'
 import styled from 'styled-components'
+
+const { Item } = List
 
 export default {
   title: 'Components/Menu/Menu',
@@ -31,13 +33,49 @@ export const Default: Story<MenuProps> = (args) => (
       <Menu.SubMenu>
         <Menu.SubMenuHeader>Menu item 1</Menu.SubMenuHeader>
         <Menu.SubMenuPanel>
-          <Menu.SubMenuGroups>Menu content 1</Menu.SubMenuGroups>
+          <Menu.SubMenuGroups>
+            <div>
+              <Heading level="h3" size="sm" uppercase>
+                Some column title
+              </Heading>
+              <List unstyled>
+                <Item>
+                  <Link underline={false} href="/">
+                    Some menu item
+                  </Link>
+                </Item>
+                <Item>
+                  <Link underline={false} href="/">
+                    Some menu item
+                  </Link>
+                </Item>
+              </List>
+            </div>
+          </Menu.SubMenuGroups>
         </Menu.SubMenuPanel>
       </Menu.SubMenu>
       <Menu.SubMenu>
         <Menu.SubMenuHeader>Menu item 2</Menu.SubMenuHeader>
         <Menu.SubMenuPanel>
-          <Menu.SubMenuGroups>Menu content 2</Menu.SubMenuGroups>
+          <Menu.SubMenuGroups>
+            <div>
+              <Heading level="h3" size="sm" uppercase>
+                Some other column title
+              </Heading>
+              <List unstyled>
+                <Item>
+                  <Link underline={false} href="/">
+                    Some other menu item
+                  </Link>
+                </Item>
+                <Item>
+                  <Link underline={false} href="/">
+                    Some other menu item
+                  </Link>
+                </Item>
+              </List>
+            </div>
+          </Menu.SubMenuGroups>
         </Menu.SubMenuPanel>
       </Menu.SubMenu>
     </Menu>
