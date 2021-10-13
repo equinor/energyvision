931f5c365f256d4068e18da852b5fce69e30c5b5
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-13 15:14:54 +0200

    ✅ Add some basic tests for menu (#525)

---
+++ web/components/src/Menu/Menu.test.tsx
@@ -0,0 +1,63 @@
+/**
+ * @jest-environment jsdom
+ */
+/* eslint-disable no-undef */
+import { render, cleanup } from '@testing-library/react'
+import 'jest-styled-components'
+import styled from 'styled-components'
+import { Menu } from './index'
+
+const { SubMenu, SubMenuHeader, SubMenuPanel, SubMenuGroups } = Menu
+
+afterEach(cleanup)
+
+const StyledMenu = styled(Menu)`
+  clip-path: unset;
+`
+
+describe('Menu', () => {
+  const header = 'Header'
+  const content = 'Content'
+
+  it('Can extend the css for the component', () => {
+    const { container } = render(
+      <StyledMenu>
+        <SubMenu>
+          <SubMenuHeader>{header}</SubMenuHeader>
+          <SubMenuPanel>
+            <SubMenuGroups>{content}</SubMenuGroups>
+          </SubMenuPanel>
+        </SubMenu>
+      </StyledMenu>,
+    )
+    expect(container.firstChild).toHaveStyleRule('clip-path', 'unset')
+  })
+  it('Can have a top level title', () => {
+    const { getByText } = render(
+      <StyledMenu>
+        <SubMenu>
+          <SubMenuHeader>{header}</SubMenuHeader>
+          <SubMenuPanel>
+            <SubMenuGroups>{content}</SubMenuGroups>
+          </SubMenuPanel>
+        </SubMenu>
+      </StyledMenu>,
+    )
+    const headerNode = getByText(header)
+    expect(headerNode).toBeDefined()
+  })
+  it('Can have content for the sub menu panels', () => {
+    const { getByText } = render(
+      <StyledMenu>
+        <SubMenu>
+          <SubMenuHeader>{header}</SubMenuHeader>
+          <SubMenuPanel>
+            <SubMenuGroups>{content}</SubMenuGroups>
+          </SubMenuPanel>
+        </SubMenu>
+      </StyledMenu>,
+    )
+    const contentNode = getByText(content)
+    expect(contentNode).toBeDefined()
+  })
+})
