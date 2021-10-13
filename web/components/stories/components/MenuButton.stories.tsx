432c76ae5d613bdd998bf5a5e888aa05758bdd59
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-13 09:06:58 +0200

    📝 Improve menu button story (#525)

---
+++ web/components/stories/components/MenuButton.stories.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import { Story, Meta } from '@storybook/react'
+import { MenuButton, MenuButtonProps } from '@components'
+
+export default {
+  title: 'Components/Menu/MenuButton',
+  component: MenuButton,
+
+  parameters: {
+    docs: {
+      description: {
+        component: `Main site navigation.
+        `,
+      },
+    },
+  },
+} as Meta
+
+export const Default: Story<MenuButtonProps> = (args) => <MenuButton {...args} title="Menu"></MenuButton>
+
+Default.storyName = 'Default'
+
+export const Open: Story<MenuButtonProps> = () => <MenuButton expanded title="Menu"></MenuButton>
+
+Open.storyName = 'Open menu'
+
+export const WithAria: Story<MenuButtonProps> = () => (
+  <>
+    <MenuButton aria-expanded={false} title="Menu"></MenuButton>
+    <MenuButton aria-expanded={true} expanded title="Menu"></MenuButton>
+  </>
+)
+
+WithAria.storyName = 'With aria-expanded'
+WithAria.parameters = {
+  docs: {
+    storyDescription: `It's important to use the aria-expanded prop for menu status`,
+  },
+}
