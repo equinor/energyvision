658efacaed3a21ffb745617de8ac7ac08d752676
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-14 15:59:15 +0200

    💄 Adjust arrow (#187)

--- web/pageComponents/news/NewsCard.tsx
+++ web/pageComponents/news/NewsCard.tsx
@@ -1,16 +1,23 @@
+import { CSSProperties } from 'react'
 import { Card, FormattedDate } from '@components'
 import { default as NextLink } from 'next/link'
 import SimpleBlockContent from '../../common/SimpleBlockContent'
+import styled from 'styled-components'
 import type { NewsCardData } from '../../types/types'
 import Image from '../shared/Image'
 
 const { Title, Header, Action, Arrow, Media, CardLink, Text, Eyebrow } = Card
 
+const StyledCard = styled(Card)`
+  height: var(--height);
+`
+
 type NewsCardProp = {
   data: NewsCardData
+  fitToContent?: boolean
 }
 
-const NewsCard = ({ data }: NewsCardProp) => {
+const NewsCard = ({ data, fitToContent = false }: NewsCardProp) => {
   const { slug, title, ingress, publishDateTime, heroImage } = data
   if (!heroImage) return null
 
@@ -18,7 +25,13 @@ const NewsCard = ({ data }: NewsCardProp) => {
     <NextLink href={`/news/${slug}`} passHref>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <CardLink>
-        <Card>
+        <StyledCard
+          style={
+            {
+              '--height': fitToContent ? 'auto' : '100%',
+            } as CSSProperties
+          }
+        >
           <Media>
             {heroImage && <Image image={heroImage.image} maxWidth={400} aspectRatio={0.56} layout="responsive" />}
           </Media>
@@ -34,7 +47,7 @@ const NewsCard = ({ data }: NewsCardProp) => {
           <Action>
             <Arrow />
           </Action>
-        </Card>
+        </StyledCard>
       </CardLink>
     </NextLink>
   )
