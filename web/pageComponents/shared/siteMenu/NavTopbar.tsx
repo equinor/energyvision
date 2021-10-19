81cba204e0211f44538fb92dcb727caedd384f95
Sven van Steenis <sven.vsteenis@gmail.com>
2021-10-19 11:12:53 +0200

    💄 fix spacing/outlining of inner topbar (#521)

--- web/pageComponents/shared/siteMenu/NavTopbar.tsx
+++ web/pageComponents/shared/siteMenu/NavTopbar.tsx
@@ -3,11 +3,10 @@ import styled from 'styled-components'
 
 const StyledNavTopbar = styled.div`
   height: var(--topbar-height);
-  padding: var(--space-small) var(--space-medium);
+  width: 100%;
+  padding: var(--space-small) var(--layout-paddingHorizontal-small);
   display: flex;
   align-items: center;
-  max-width: calc(var(--topbar-innerMaxWidth) + (var(--space-medium) * 2));
-  margin: auto;
   border-bottom: solid 1px transparent;
 `
 
