e2bb6437daf8b09649eb80f3060a08286a5b4d79
Sven van Steenis <sven.vsteenis@gmail.com>
2021-10-20 11:06:57 +0200

    💄 add max width to menu topbar (#521)

--- web/pageComponents/shared/siteMenu/NavTopbar.tsx
+++ web/pageComponents/shared/siteMenu/NavTopbar.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 const StyledNavTopbar = styled.div`
   height: var(--topbar-height);
   width: 100%;
+  max-width: var(--maxViewportWidth);
+  margin: auto;
   padding: var(--space-small) var(--layout-paddingHorizontal-small);
   display: flex;
   align-items: center;
