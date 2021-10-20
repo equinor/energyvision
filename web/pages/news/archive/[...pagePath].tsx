fa108eb320ae3e1bfb1f8b948ad93fbac9d3e320
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-20 10:18:43 +0200

    ✨ Load menu data for archive news (#517)

--- web/pages/news/archive/[...pagePath].tsx
+++ web/pages/news/archive/[...pagePath].tsx
@@ -1,13 +1,21 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import * as fs from 'fs'
 import { NextSeo } from 'next-seo'
+import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import getConfig from 'next/config'
+import { Layout } from '@components'
+import { menuQuery } from '../../../lib/queries/menu'
+import { getClient } from '../../../lib/sanity.server'
 import { removeHTMLExtension } from '../../../lib/archive/archiveUtils'
+import { mapLocaleToLang } from '../../../lib/localization'
 import Header from '../../../pageComponents/shared/Header'
+
+import type { MenuData } from '../../../types/types'
+
 import { anchorClick } from '../../../common/helpers/staticPageHelpers'
-import { useEffect} from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -29,21 +37,22 @@ type PageResponseData = {
 type OldArchivedNewsPageProps = {
   data: {
     news: PageResponseData
+    menuData: MenuData
   }
 }
 
 const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element => {
-  const [isArchivePage, setIsArchivePage] = useState(true);
+  const [isArchivePage, setIsArchivePage] = useState(true)
   const router = useRouter()
   const { pathname } = router
   useEffect(() => {
-    if (isArchivePage && document.getElementById("legacyScript") == null) {
-    const scriptTag = document.createElement('script')
-    scriptTag.src = "/legacy/legacy.minified.js";
-    scriptTag.id = "legacyScript"
-    document.body.appendChild(scriptTag)
+    if (isArchivePage && document.getElementById('legacyScript') == null) {
+      const scriptTag = document.createElement('script')
+      scriptTag.src = '/legacy/legacy.minified.js'
+      scriptTag.id = 'legacyScript'
+      document.body.appendChild(scriptTag)
     }
-});
+  })
 
   if (!router.isFallback && !data.news) {
     setIsArchivePage(false)
@@ -51,8 +60,8 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
   }
   const fullUrlDyn = pathname.indexOf('http') === -1 ? `${publicRuntimeConfig.domain}${pathname}` : pathname
   const fullUrl = fullUrlDyn.replace('[...pagePath]', data?.news?.slug)
-  const onLinkClicked = (e : any) => {
-    anchorClick(e,router)
+  const onLinkClicked = (e: any) => {
+    anchorClick(e, router)
   }
   return (
     <>
@@ -65,10 +74,15 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
           <style jsx global>
             {archivedStyles}
           </style>
-          <Head> <script src="https://consent.cookiebot.com/uc.js"
-       id="Cookiebot" data-cbid="f1327b03-7951-45da-a2fd-9181babc783f"/>
-      </Head>
-          <Header />
+          <Head>
+            {' '}
+            <script
+              src="https://consent.cookiebot.com/uc.js"
+              id="Cookiebot"
+              data-cbid="f1327b03-7951-45da-a2fd-9181babc783f"
+            />
+          </Head>
+
           <NextSeo
             title={data?.news?.title}
             description={data?.news?.description}
@@ -78,14 +92,14 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
               type: 'article',
               url: fullUrl,
             }}
-          />  
+          />
           <SkipNavContent />
           {/* The <div> element has a child <button> element that allows keyboard interaction */}
-          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}   
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
           <div
             className="legacyStyles"
-            onClick={ onLinkClicked }
-            onKeyDown = {onLinkClicked}
+            onClick={onLinkClicked}
+            onKeyDown={onLinkClicked}
             dangerouslySetInnerHTML={{
               __html: data?.news?.content,
             }}
@@ -97,7 +111,35 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
   )
 }
 
-export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+// eslint-disable-next-line react/display-name
+OldArchivedNewsPage.getLayout = (page: AppProps) => {
+  /* The getLayout pattern is a way to preserve state in the layout
+  across client side navigation. The downside is that since it's just an
+  ordinary function, we can't use the preview subcscription hook out of the box.
+  As a consequence, preview for the menu data is not available.
+
+  If this is a problem, we need to see if we are able to find another solution  */
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const { props } = page
+
+  const { data } = props
+  const slugs = {
+    en_GB: '/en/news/archive',
+    nb_NO: '/no/nyheter/arkiv',
+  }
+  return (
+    <Layout>
+      <Header slugs={slugs} data={data?.menuData} />
+
+      <SkipNavContent />
+      {page}
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ preview = false, params, locale = 'en' }) => {
   const pagePathArray = params?.pagePath as string[]
   const pagePath = pagePathArray.join('/')
   const archiveSeverURL = publicRuntimeConfig.archiveStorageURL
@@ -115,9 +157,11 @@ export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
     pageData = null
   }
 
+  const menuData = await getClient(preview).fetch(menuQuery, { lang: mapLocaleToLang(locale) })
   return {
     props: {
       data: {
+        menuData,
         news: {
           ...pageData,
           slug: pagePath,
