f4ba93fe15ab29e303ef199e0338c2c239878b69
Ådne Forslund <adne.forslund@gmail.com>
2021-10-21 01:27:40 +0200

    :construction: #485 modify query

--- web/lib/queries/footer.ts
+++ web/lib/queries/footer.ts
@@ -1,8 +1,18 @@
 export const footerQuery = /* groq */ `
   *[_type == "footer" && _lang == $lang][0] {
-	...
-  	
-   
+	footerColumns[]{
+        _key,
+        "header": columnHeader, 
+        "linkList": columnLinks[]{ 
+        label,
+        "isStatic": coalesce(isStatic, false),
+  	    "link": reference-> {
+        "type": _type,
+        "slug": slug.current,
+      },
+       url,
+      "staticUrl": staticUrl,} 
+      }
    
 	}
 
