2382c1466616a3b58cebea0060a287650d0415e2
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-20 09:45:33 +0200

    ✨ Add the menu data to archive summary page (#517)

--- web/pages/news/archive/index.tsx
+++ web/pages/news/archive/index.tsx
@@ -1,12 +1,17 @@
 import * as fs from 'fs'
 import { NextSeo } from 'next-seo'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { Heading, Link, List, Layout } from '@components'
 import { GetStaticProps } from 'next'
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { SkipNavContent } from '@reach/skip-nav'
+import { Heading, Link, List, Layout } from '@components'
+import { menuQuery } from '../../../lib/queries/menu'
+import { getClient } from '../../../lib/sanity.server'
+import { mapLocaleToLang } from '../../../lib/localization'
 import styled from 'styled-components'
 import Header from '../../../pageComponents/shared/Header'
-import { SkipNavContent } from '@reach/skip-nav'
+import type { MenuData } from '../../../types/types'
 
 import { default as NextLink } from 'next/link'
 
@@ -17,46 +22,78 @@ const Container = styled.div`
   margin-right: auto;
 `
 
-export default function AllArchivedNews({ newsList }: any) {
+type AllArchivedNewsProps = {
+  data: {
+    newsList: any
+    menuData: MenuData
+  }
+}
+
+export default function AllArchivedNews({ data }: AllArchivedNewsProps) {
   const router = useRouter()
   const { locale } = router
+
+  const { newsList } = data
+  // @TODO: Norwegian version
+  //   @TODO: Avoid duplicate description, write one for the archived pages
+
   return (
     <>
-      {/* @TODO: Norwegian version */}
-      {/*       @TODO: Avoid duplicate description, write one for the archived pages
-       */}{' '}
       <NextSeo
         title="Archived news"
         description="Keep updated on news from Equinor. Find all our general news, stock market announcements and notifiable tradings here in our news archive."
       ></NextSeo>
-      <Layout>
-        <Head>
-          <title>Archived news</title>
-        </Head>
-        <Header />
-
-        <SkipNavContent />
-        <Container>
-          <Heading level="h1" size="2xl" style={{ margin: '1rem 0' }}>
-            {/* @TODO Language strings */}
-            {locale === 'no' ? 'Arkiverte nyheter fra 2016 til 2018' : '2016 to 2018 archived news page list'}
-          </Heading>
-          {newsList && (
-            <List unstyled>
-              {newsList.map((value: string) => {
-                return (
-                  <List.Item key={value}>
-                    <NextLink href={value} passHref>
-                      <Link>{value}</Link>
-                    </NextLink>
-                  </List.Item>
-                )
-              })}
-            </List>
-          )}
-        </Container>
-      </Layout>
+      <Head>
+        <title>Archived news</title>
+      </Head>
+      <Container>
+        <Heading level="h1" size="2xl" style={{ margin: '1rem 0' }}>
+          {/* @TODO Language strings */}
+          {locale === 'no' ? 'Arkiverte nyheter fra 2016 til 2018' : '2016 to 2018 archived news page list'}
+        </Heading>
+        {newsList && (
+          <List unstyled>
+            {newsList.map((value: string) => {
+              return (
+                <List.Item key={value}>
+                  <NextLink href={value} passHref>
+                    <Link>{value}</Link>
+                  </NextLink>
+                </List.Item>
+              )
+            })}
+          </List>
+        )}
+      </Container>
     </>
+  )
+}
+
+// eslint-disable-next-line react/display-name
+AllArchivedNews.getLayout = (page: AppProps) => {
+  /* The getLayout pattern is a way to preserve state in the layout
+  across client side navigation. The downside is that since it's just an
+  ordinary function, we can't use the preview subcscription hook out of the box.
+  As a consequence, preview for the menu data is not available.
+
+  If this is a problem, we need to see if we are able to find another solution  */
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const { props } = page
+
+  const { data } = props
+  const slugs = {
+    en_GB: '/en/news/archive',
+    nb_NO: '/no/nyheter/arkiv',
+  }
+  return (
+    <Layout>
+      <Header slugs={slugs} data={data?.menuData} />
+
+      <SkipNavContent />
+      {page}
+    </Layout>
   )
 }
 
@@ -70,7 +107,7 @@ const removeHTMLExtension = (path: string): string => {
   return path.replace('.html', '')
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
+export const getStaticProps: GetStaticProps = async ({ preview = false, locale = 'en' }) => {
   const newsPaths = fs
     .readFileSync(process.cwd() + `/lib/archive/news2016To2018.txt`)
     .toString()
@@ -81,10 +118,10 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
     // @TODO: Revisit with i18n
     .filter((pagePath) => pagePath.startsWith(`/${locale}`))
     .map((pagePath) => sanitizeNewsURL(pagePath))
-
+  const menuData = await getClient(preview).fetch(menuQuery, { lang: mapLocaleToLang(locale) })
   return {
     props: {
-      newsList: newsList,
+      data: { newsList, menuData },
     },
   }
 }
