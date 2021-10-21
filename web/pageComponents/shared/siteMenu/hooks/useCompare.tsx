ed61cc9cc535e95152987e81ee3687de28fa312e
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-21 13:32:58 +0200

    ✨ Only close accordion when the window changes (#538)

---
+++ web/pageComponents/shared/siteMenu/hooks/useCompare.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react'
+
+export function useCompare(value: number) {
+  const prevVal = usePreviousValue(value)
+  return prevVal !== value
+}
+
+// Helper hook
+function usePreviousValue(value: number) {
+  const ref = useRef<number>()
+  useEffect(() => {
+    ref.current = value
+  })
+  return ref.current
+}
