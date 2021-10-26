c435d0c8cec9708a427911093f286ced34cf51da
Sven van Steenis <sven.vsteenis@gmail.com>
2021-10-26 09:47:40 +0200

    ✨ make reusable groq query for block editor marks (#551)

---
+++ web/lib/queries/common/blockEditorMarks.ts
@@ -0,0 +1,12 @@
+import slugReference from './slugReference'
+
+export default `
+  _type == "internalLink" => {
+    "internalLink": reference->{
+      name,
+      "id": ${slugReference},
+      "type": _type,
+      "lang": coalesce(_lang, 'en_GB'),
+    },
+  },
+`
