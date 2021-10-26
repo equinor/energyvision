a7dca0cd19cbb46533ba193f28456d5cc0c51fea
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-26 14:16:09 +0200

    ♻️ Reuse query for links (#552)

---
+++ web/lib/queries/common/downloadableFileFields.ts
@@ -0,0 +1,11 @@
+const linkSelectorFields = /* groq */ `
+_type == "downloadableFile" => {
+  "id": _key,
+  "type": _type,
+  "label": filename,
+  "href": file.asset-> url,
+  "extension": file.asset-> extension 
+}
+`
+
+export default linkSelectorFields
