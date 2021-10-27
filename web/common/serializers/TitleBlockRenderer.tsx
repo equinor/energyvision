9feb99f2310c944f0ab10689840ffe055ede4b79
Sven van Steenis <sven.vsteenis@gmail.com>
2021-10-27 16:51:44 +0200

    ♻️ allow for customizable TitleBlockRenderer (#550)

--- web/common/serializers/TitleBlockRenderer.tsx
+++ web/common/serializers/TitleBlockRenderer.tsx
@@ -1,17 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import BlockContent from '@sanity/block-content-to-react'
 import type { BlockNode } from '@sanity/block-content-to-react'
-import { Heading } from '@components'
+import { Heading, HeadingProps } from '@components'
 
 type TitleBlockProps = {
   children: string[]
   node: BlockNode
-}
+} & HeadingProps
 
 export const TitleBlockRenderer = (props: TitleBlockProps) => {
-  const { children } = props
+  const { children, node, size = 'xl', level = 'h2', ...rest } = props
 
   return (
-    <Heading size="xl" level="h2">
+    <Heading size={size} level={level} {...rest}>
       {children}
     </Heading>
   )
