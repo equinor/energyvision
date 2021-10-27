7ec785ed739b64d10823a03cf0f88d0836a77a16
Ådne Forslund <adne.forslund@gmail.com>
2021-10-27 14:14:38 +0200

    :construction: #526 add type to schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -14,6 +14,7 @@ import route from './documents/route'
 import subMenu from './documents/subMenu'
 import siteMenu from './documents/siteMenu'
 import footer from './documents/footer'
+import tag from './documents/tag'
 
 // Objects
 import imageWithAlt from './objects/imageWithAlt'
@@ -91,5 +92,6 @@ export default createSchema({
     iframe,
     footer,
     remitTable,
+    tag
   ]),
 })
