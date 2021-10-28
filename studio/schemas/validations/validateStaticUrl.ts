d57e3699fe01a29ed39c6e7aa173616045282f9b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-28 08:33:36 +0200

    🚸 No punctuations in url (#552)

--- studio/schemas/validations/validateStaticUrl.ts
+++ studio/schemas/validations/validateStaticUrl.ts
@@ -3,17 +3,16 @@ import { SchemaType } from '../../types'
 export const validateStaticUrl = (value: string, context: SchemaType.ValidationContext) => {
   // This is not a static link
   if (!context.parent?.isStatic) return true
+
   if (context.parent?.isStatic && value === undefined) {
     return 'A link is required'
-  }
-  if (value.startsWith('/no/') || value.startsWith('/en/')) {
+  } else if (value.startsWith('/no/') || value.startsWith('/en/')) {
     return `Please don't add the language information`
-  }
-  if (value.endsWith('.html')) {
+  } else if (value.endsWith('.html')) {
     return `Please remove .html`
-  }
-  if (!value.startsWith('/')) {
+  } else if (!value.startsWith('/')) {
     return `The link must start with a forward slash (/)`
-  }
-  return true
+  } else if (value.match(/([.])/g)) {
+    return 'Illegal character (.)'
+  } else return true
 }
