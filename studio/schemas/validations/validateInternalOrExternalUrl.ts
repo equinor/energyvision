61ee8ca8c3b566a491bc172c384ca80630942d50
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-10-28 10:06:16 +0200

    ♻️ Share the validation function for internal/external url

---
+++ studio/schemas/validations/validateInternalOrExternalUrl.ts
@@ -0,0 +1,17 @@
+export const validateInternalOrExternalUrl = (isStatic: boolean, value: string, connectedField: string) => {
+  // This is not a static link
+  if (isStatic) return true
+  if (value && connectedField) {
+    return 'Can only have a single link. Choose either an internal or external link.'
+  }
+
+  if (!value && !connectedField) {
+    return 'You must provide either an internal or external link.'
+  }
+
+  if (connectedField && !value) {
+    return true
+  }
+
+  return true
+}
