a0d2b1b729d0ce844df9f5a2e795f12a33612a04
Sven van Steenis <sven.vsteenis@gmail.com>
2021-11-02 12:45:04 +0100

    ♻️ remove commented code & fix type issues in _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,51 +1,52 @@
-import type { AppProps /*, AppContext */ } from 'next/app'
+import type { AppProps } from 'next/app'
+import type { NextPage } from 'next'
+import type { ReactNode } from 'react'
 import { useRouter } from 'next/router'
 import { IntlProvider } from 'react-intl'
 import { GlobalStyle, GlobalFontStyle } from '../styles/globalStyles'
 import { DefaultSeo } from 'next-seo'
 import { SkipNavLink } from '@reach/skip-nav'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// TODO fix the eslint issues
-/* import archivedStyles from '@equinor/energyvision-legacy-css' */
-/* import MenuProvider from '../tempcomponents/shared/menu/MenuProvider' */
-/* 
-import { AppInsightsContext, AppInsightsErrorBoundary } from '@microsoft/applicationinsights-react-js'
-import { reactPlugin } from '../common' */
+// import archivedStyles from '@equinor/energyvision-legacy-css'
+// import { AppInsightsContext, AppInsightsErrorBoundary } from '@microsoft/applicationinsights-react-js'
+// import { reactPlugin } from '../common'
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+/**
+ * TODO:
+ * - Figure out a way of not rendering legacyStyles on Sanity pages, See: isArchivePage at'../lib/archive/archiveUtils'
+ * - Figure out how to add/use AppInsights without performance hit
+ * - Get locale data from Sanity
+ */
+
+// eslint-disable-next-line no-unused-vars
+type GetLayout = (page: ReactNode) => ReactNode
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Page<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: GetLayout
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type CustomAppProps<P = {}> = AppProps<P> & {
+  Component: Page<P>
+}
+
+function MyApp({ Component, pageProps }: CustomAppProps): JSX.Element {
   const router = useRouter()
   const defaultLocale = router.defaultLocale || 'en'
   const locale = router.locale || defaultLocale
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const getLayout = Component.getLayout || ((page) => page)
-  // TODO: get locale from Sanity
+  const getLayout = Component.getLayout || ((page: ReactNode): ReactNode => page)
+
   return (
     <>
-      {/*   <AppInsightsErrorBoundary onError={() => <h1>I believe something went wrong</h1>} appInsights={reactPlugin}>
-        <AppInsightsContext.Provider value={reactPlugin}> */}
       <IntlProvider locale={locale} defaultLocale={defaultLocale}>
-        {/*       <MenuProvider> */}
-
-        {/* 
-                @TODO: Figure out a way of not rendering this on Sanity pages
-                See: isArchivePage at'../lib/archive/archiveUtils'
-              */}
-
         <GlobalStyle />
         <GlobalFontStyle />
-        {/*   <style jsx global>
-          {archivedStyles}
-        </style> */}
-        <DefaultSeo dangerouslySetAllPagesToNoIndex={true} dangerouslySetAllPagesToNoFollow={true} />
-        <SkipNavLink />
 
+        <DefaultSeo dangerouslySetAllPagesToNoIndex={true} dangerouslySetAllPagesToNoFollow={true} />
+
+        <SkipNavLink />
         {getLayout(<Component {...pageProps} />)}
-        {/*      </MenuProvider> */}
       </IntlProvider>
-      {/*      </AppInsightsContext.Provider>
-      </AppInsightsErrorBoundary> */}
     </>
   )
 }
