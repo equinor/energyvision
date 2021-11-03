286d97c04384ddf0a3888d830c4b193bc2f3adcf
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-11-03 13:16:22 +0100

    🐛 Make Card images great again (#568)

--- web/components/src/Card/Media.tsx
+++ web/components/src/Card/Media.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react'
 import { forwardRef, HTMLAttributes } from 'react'
-import { Card } from '@equinor/eds-core-react'
-
-const { Media: EdsMedia } = Card
 
 export type CardMediaProps = HTMLAttributes<HTMLDivElement>
 
 export const Media = forwardRef<HTMLDivElement, CardMediaProps>(function Media({ children, ...rest }, ref) {
   return (
-    <EdsMedia ref={ref} {...rest}>
+    <div ref={ref} {...rest}>
       {children}
-    </EdsMedia>
+    </div>
   )
 })
