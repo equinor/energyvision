6f437005392ca40e675ee302bea0b83d0ced2682
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-11-09 08:58:50 +0100

    🚸 Move the title and ingress to the first dialog (#574)

--- studio/schemas/objects/promotion/promotion.tsx
+++ studio/schemas/objects/promotion/promotion.tsx
@@ -3,6 +3,9 @@ import blocksToText from '../../../helpers/blocksToText'
 
 import type { Rule, Block } from '@sanity/types'
 import type { ColorListValue } from 'sanity-plugin-color-list'
+import { configureBlockContent, configureTitleBlockContent } from '../../editors'
+import CompactBlockEditor from '../../components/CompactBlockEditor'
+import CharCounterEditor from '../../components/CharCounterEditor'
 
 export type Promotion = {
   _type: 'promotion'
@@ -12,12 +15,37 @@ export type Promotion = {
   background?: ColorListValue
 }
 
+const titleContentType = configureTitleBlockContent()
+
+const ingressContentType = configureBlockContent({
+  h1: false,
+  h2: false,
+  h3: false,
+  h4: false,
+  attachment: false,
+})
+
 export default {
   title: 'Promotion',
   name: 'promotion',
   type: 'object',
 
   fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'array',
+      inputComponent: CompactBlockEditor,
+      of: [titleContentType],
+      validation: (Rule: Rule) => Rule.required().warning('In most cases you should add a title'),
+    },
+    {
+      name: 'ingress',
+      title: 'Ingress',
+      type: 'array',
+      inputComponent: CharCounterEditor,
+      of: [ingressContentType],
+    },
     {
       type: 'array',
       name: 'promotion',
