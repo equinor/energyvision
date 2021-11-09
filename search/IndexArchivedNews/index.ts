48ec51449bdca5fd131ff555a5be4048ec4e982a
Nils Magne Lunde <nilsml@nilsland.com>
2021-11-09 20:50:21 +0000

    :sparkles: Set up basic scaffolding for azure function for search

---
+++ search/IndexArchivedNews/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context } from '@azure/functions'
+
+const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
+  const timeStamp = new Date().toISOString()
+
+  if (myTimer.isPastDue) {
+    context.log('Timer function is running late!')
+  }
+  context.log('Timer trigger function ran!', timeStamp)
+}
+
+export default timerTrigger
