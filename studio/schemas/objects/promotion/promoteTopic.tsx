8a157ecc4d4eb3f8518990003f666e57fbcd8805
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-11-11 10:04:30 +0100

    ✨  Add in ingress for the topic page card (#574)

--- studio/schemas/objects/promotion/promoteTopic.tsx
+++ studio/schemas/objects/promotion/promoteTopic.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { configureBlockContent } from '../../editors/blockContentType'
+import CharCounterEditor from '../../components/CharCounterEditor'
 import blocksToText from '../../../helpers/blocksToText'
 import type { Block, Rule } from '@sanity/types'
 import type { ColorListValue } from 'sanity-plugin-color-list'
@@ -10,6 +11,34 @@ export type Promotion = {
   title?: Block[]
   ingress?: Block[]
   background?: ColorListValue
+}
+
+const introBlockContentType = configureBlockContent({
+  h1: false,
+  h2: false,
+  h3: false,
+  h4: false,
+  internalLink: false,
+  externalLink: false,
+  attachment: false,
+  lists: false,
+})
+
+const validateIngress = (value: any) => {
+  if (!value || value.length === 0) {
+    return 'Required'
+  }
+
+  const count = value[0].children.reduce(
+    (total: any, current: { text: string | any[] }) => total + current.text.length,
+    0,
+  )
+
+  if (count > 215) {
+    return `The introduction should be no longer than 215 characters. Currently ${count} characters long.`
+  }
+
+  return true
 }
 
 export default {
@@ -23,16 +52,33 @@ export default {
       type: 'array',
       of: [
         {
-          title: 'Topics to be promoted',
-          description: 'Select the topics you want to promote',
-          type: 'reference',
-          to: [{ type: 'route_en_GB' }, { type: 'route_nb_NO' }],
-          options: {
-            filter: ({ document }: { document: any }) => ({
-              filter: `_type == $routeLang `,
-              params: { routeLang: `route_${document._lang}` },
-            }),
-          },
+          type: 'object',
+          name: 'topics',
+          title: 'Topic',
+          fields: [
+            {
+              title: 'Topic to be promoted',
+              name: 'reference',
+              description: 'Select the topic you want to promote',
+              type: 'reference',
+              to: [{ type: 'route_en_GB' }, { type: 'route_nb_NO' }],
+              options: {
+                filter: ({ document }: { document: any }) => ({
+                  filter: `_type == $routeLang `,
+                  params: { routeLang: `route_${document._lang}` },
+                }),
+              },
+            },
+            {
+              name: 'ingress',
+              title: 'Ingress',
+              description: 'A short and catchy introduction text for this card (max. 215 chars)',
+              type: 'array',
+              inputComponent: CharCounterEditor,
+              of: [introBlockContentType],
+              validation: (Rule: Rule) => Rule.custom((value: any) => validateIngress(value)),
+            },
+          ],
         },
       ],
       validation: (Rule: Rule) => Rule.unique().min(3).max(3),
@@ -40,9 +86,9 @@ export default {
   ],
   preview: {
     select: {
-      reference1: 'references.0.content.title',
-      reference2: 'references.1.content.title',
-      reference3: 'references.2.content.title',
+      reference1: 'references.0.reference.content.title',
+      reference2: 'references.1.reference.content.title',
+      reference3: 'references.2.reference.content.title',
     },
     prepare({ reference1, reference2, reference3 }: { reference1: Block[]; reference2: Block[]; reference3: Block[] }) {
       const plainTitle1 = reference1 ? blocksToText(reference1) : undefined
