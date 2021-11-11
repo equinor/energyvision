1c13457fb7a53d95fa0bbe9d226122c2a0828028
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-11-11 14:42:38 +0100

    ✨ Create the landing page template (#562)

---
+++ studio/schemas/documents/landingPage.ts
@@ -0,0 +1,57 @@
+import React from 'react'
+import { i18n } from '../documentTranslation'
+import { configureBlockContent } from '../editors/blockContentType'
+import { configureTitleBlockContent } from '../editors'
+import CharCounterEditor from '../components/CharCounterEditor'
+import CompactBlockEditor from '../components/CompactBlockEditor'
+import type { Rule } from '@sanity/types'
+
+const titleContentType = configureTitleBlockContent()
+const ingressContentType = configureBlockContent({
+  h1: false,
+  h2: false,
+  h3: false,
+  h4: false,
+  attachment: false,
+})
+
+export default {
+  type: 'document',
+  title: `Landing page`,
+  name: `landingPage`,
+  i18n,
+  fieldsets: [
+    {
+      title: 'SEO & metadata',
+      name: 'metadata',
+      description: 'This part is used for meta information when this content is used on the web',
+      options: {
+        collapsible: true,
+        collapsed: true,
+      },
+    },
+  ],
+  fields: [
+    {
+      title: 'Meta information',
+      name: 'seo',
+      type: 'titleAndMeta',
+      fieldset: 'metadata',
+    },
+    {
+      name: 'title',
+      type: 'array',
+      title: 'Title',
+      inputComponent: CompactBlockEditor,
+      of: [titleContentType],
+      validation: (Rule: Rule) => Rule.required(),
+    },
+    {
+      title: 'Ingress',
+      name: 'ingress',
+      type: 'array',
+      inputComponent: CharCounterEditor,
+      of: [ingressContentType],
+    },
+  ],
+}
