caed2dffd4ca45ce3cf80d4d5f23970352fbb63d
Sven van Steenis <sven.vsteenis@gmail.com>
2021-11-11 11:00:57 +0100

    🚧 more work on Promotion rendering (#583)

--- web/pageComponents/topicPages/Promotion.tsx
+++ web/pageComponents/topicPages/Promotion.tsx
@@ -1,29 +1,76 @@
 import styled from 'styled-components'
 import { BackgroundContainer } from '@components'
 import NewsCard from '../news/NewsCard'
-import type { PromotionData } from '../../types/types'
+import SimpleBlockContent from '../../common/SimpleBlockContent'
+import { TitleBlockRenderer, IngressBlockRenderer } from '../../common/serializers'
+import type { PromotionData, CardData, TopicCardData } from '../../types/types'
 
 const Wrapper = styled.div`
-  padding: var(--space-xxLarge) var(--space-large);
+  padding: var(--space-3xLarge) var(--layout-paddingHorizontal-small);
+  max-width: 1920px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const StyledHeading = styled(TitleBlockRenderer)`
+  text-align: center;
+  margin-bottom: var(--space-xLarge);
+`
+
+const CardsWrapper = styled.div`
+  margin-top: var(--space-xLarge);
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
-  grid-row-gap: 4rem;
-  grid-column-gap: 2rem;
-  /** @TODO: Clamp? */
-  @media (min-width: 1300px) {
-    grid-column-gap: 4rem;
-  }
-  @media (min-width: 1900px) {
-    grid-column-gap: var(--space-4xLarge);
-  }
+  grid-gap: var(--space-large);
 `
+
+const getCards = (articles: CardData[], pages: TopicCardData[]) => {
+  if (articles && articles.length > 0) {
+    return articles.map((article) => <NewsCard data={article} key={article.id} />)
+  }
+
+  if (pages && pages.length > 0) {
+    // todo: refactor NewsCard to make it more generic
+    return false
+  }
+
+  return false
+}
 
 const Promotion = ({ data }: { data: PromotionData }) => {
   console.log(data)
-  const { articles, pages } = data.promotion
+  const { title, ingress } = data
+  const { articles = [], pages = [] } = data.promotion
+
+  const cards = getCards(articles, pages)
+
+  if (!cards) return null
+
   return (
     <BackgroundContainer background={data.designOptions.background}>
-      <Wrapper>{articles && articles.map((article) => <NewsCard data={article} key={article.id} />)}</Wrapper>
+      <Wrapper>
+        {title && (
+          <SimpleBlockContent
+            blocks={title}
+            serializers={{
+              types: {
+                block: (props) => <StyledHeading level="h2" size="xl" {...props} />,
+              },
+            }}
+          />
+        )}
+        {ingress && (
+          <SimpleBlockContent
+            blocks={ingress}
+            serializers={{
+              types: {
+                block: (props) => <IngressBlockRenderer centered={true} {...props} />,
+              },
+            }}
+          ></SimpleBlockContent>
+        )}
+        {cards && <CardsWrapper>{cards}</CardsWrapper>}
+      </Wrapper>
     </BackgroundContainer>
   )
 }
