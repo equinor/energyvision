e5dc505da3fff1521ae9668463ce6f06682c85d1
Nils Magne Lunde <nilsml@nilsland.com>
2021-11-19 16:45:44 +0100

    :construction: Add ENV to process.ENV

--- search/IndexArchivedNews/index.ts
+++ search/IndexArchivedNews/index.ts
@@ -1,7 +1,11 @@
 import { AzureFunction, Context } from '@azure/functions'
+// eslint-disable-next-line import/no-named-as-default
+import DotenvAzure from 'dotenv-azure'
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
   const timeStamp = new Date().toISOString()
+
+  await new DotenvAzure().config()
 
   if (myTimer.isPastDue) {
     context.log('Timer function is running late!')
