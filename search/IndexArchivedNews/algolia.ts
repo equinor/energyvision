ce4ef85a898719984f033839a3c389be83b53c6b
Nils Magne Lunde <nilsml@nilsland.com>
2021-11-23 10:13:07 +0100

    :recycle: Simplification. Geve up on mocking algoliasearch 🤯

--- search/IndexArchivedNews/algolia.ts
+++ search/IndexArchivedNews/algolia.ts
@@ -6,12 +6,10 @@ import { flow, pipe } from 'fp-ts/lib/function'
 const algoliaSearchCurried = (appId: string) => (apiKey: string) => algoliasearch(appId, apiKey)
 
 type GetProcessEnvType = IO.IO<E.Either<string, string>>
-const getAlgoliaAppId: GetProcessEnvType = () =>
-  pipe(E.fromNullable('Unable to find app id'), (fromNullable) => fromNullable(process.env.ALGOLIA_APP_ID))
-const getAlgoliaApiKey: GetProcessEnvType = () =>
-  pipe(E.fromNullable('Un2able to find API key'), (fromNullable) => fromNullable(process.env.ALGOLIA_API_KEY))
+const getAlgoliaAppId: GetProcessEnvType = () => E.fromNullable('Unable to find app id')(process.env.ALGOLIA_APP_ID)
+const getAlgoliaApiKey: GetProcessEnvType = () => E.fromNullable('Un2able to find API key')(process.env.ALGOLIA_API_KEY)
 const getAlgoliaIndexName: GetProcessEnvType = () =>
-  pipe(E.fromNullable('Unable to find index name'), (fromNullable) => fromNullable(process.env.ALGOLIA_INDEX_NAME))
+  E.fromNullable('Unable to find index name')(process.env.ALGOLIA_INDEX_NAME)
 
 type InitFuncType = IO.IO<E.Either<string, SearchIndex>>
 export const initFunc: InitFuncType = flow(
@@ -21,8 +19,6 @@ export const initFunc: InitFuncType = flow(
   E.chain((client) => pipe(getAlgoliaIndexName(), E.map(client.initIndex))),
 )
 
-//export const initOrDie = () => E.getOrElse(initFunc(), throw new Error())
-
 // Push to Algolia index
-type UpdateType = (indexName: string, value: string) => boolean
-export const update: UpdateType = (indexName, value) => !!(indexName && value)
+type UpdateType = (index: SearchIndex) => (data: readonly Readonly<Record<string, never>>[]) => void
+export const update: UpdateType = (index) => (data) => index.saveObjects(data).then((res) => console.log(res))
