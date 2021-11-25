61368a87ba4fb1b8e8f1d67e5ce1f698630577c2
Sven van Steenis <SvSven@users.noreply.github.com>
2021-11-25 12:36:39 +0100

    ✨ add localized slugs to event query (#601)

--- web/lib/queries/event.ts
+++ web/lib/queries/event.ts
@@ -3,9 +3,29 @@ import linkSelectorFields from './common/actions/linkSelectorFields'
 import downloadableFileFields from './common/actions/downloadableFileFields'
 import downloadableImageFields from './common/actions/downloadableImageFields'
 
+const localizedSlugsFromEnglish = /* groq */ `
+  "allSlugs": {
+    "en_GB": slug.current,
+    "nb_NO": *[_type == "event" && _id == ^._id + "__i18n_nb_NO"][0].slug.current,
+  }
+`
+
+const localizedSlugsFromNorwegian = /* groq */ `
+  "allSlugs": {
+    "en_GB": *[_type == "event" && ^._id in _langRefs[]._key][0].slug.current,
+    "nb_NO": slug.current,
+  }
+`
+
 export const eventFields = /* groq */ `
   "id": _id,
   "slug": slug.current,
+  $lang == "en_GB" => {
+    ${localizedSlugsFromEnglish}
+  },
+  $lang == "nb_NO" => {
+    ${localizedSlugsFromNorwegian}
+  },
   title[]{
     ...,
     ${markDefs},
