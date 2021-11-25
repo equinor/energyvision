2ea4d7792ccbdd87540cdbd3fb225229eb656550
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-11-25 10:29:46 +0100

    ⚗ Experiments with lint-staged with tsc for commit hook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
   '**/*.ts?(x)': () => 'pnpm web type-check',
+  // '**/*.ts?(x)': () => 'tsc -p tsconfig.base.json --noEmit',
   // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `pnpm . ${filenames.join(' ')}`,
+  '**/*.(ts|js)?(x)': (filenames) => `pnpm lint . ${filenames.join(' ')}`,
 }
