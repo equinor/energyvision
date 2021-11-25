ae9f9bd7f666767ca1fd5446af07114d3656c1a5
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-11-25 11:12:25 +0100

    ⚗ Remove lint-staged, didn't make Husky faster anyways

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'pnpm web type-check',
-  // '**/*.ts?(x)': () => 'tsc -p tsconfig.base.json --noEmit',
-  // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `pnpm lint . ${filenames.join(' ')}`,
-}
