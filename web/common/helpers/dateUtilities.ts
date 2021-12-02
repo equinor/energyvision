51b1acdbdba9e3d4071cabb04bd795c48c7dc4d1
Sven van Steenis <SvSven@users.noreply.github.com>
2021-12-02 09:31:39 +0100

    ✨ add date utility helpers

---
+++ web/common/helpers/dateUtilities.ts
@@ -0,0 +1,23 @@
+import { getYear, getMonth, getDate, getHours, getMinutes, getSeconds } from 'date-fns'
+
+export const toDateParts = (datetime: Date): number[] => {
+  return [
+    getYear(datetime),
+    getMonth(datetime),
+    getDate(datetime),
+    getHours(datetime),
+    getMinutes(datetime),
+    getSeconds(datetime),
+  ]
+}
+
+export const toUTCDate = (datetime: Date): Date => {
+  return new Date(
+    datetime.getUTCFullYear(),
+    datetime.getUTCMonth(),
+    datetime.getUTCDate(),
+    datetime.getUTCHours(),
+    datetime.getUTCMinutes(),
+    datetime.getUTCSeconds(),
+  )
+}
