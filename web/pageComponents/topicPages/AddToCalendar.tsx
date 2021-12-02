a54e43ab9ad61e0eae2405a5cd593d5ceb5a8a2d
Sven van Steenis <SvSven@users.noreply.github.com>
2021-12-02 13:33:10 +0100

    🚧 improve structure, only render if event is upcomming (#484)

--- web/pageComponents/topicPages/AddToCalendar.tsx
+++ web/pageComponents/topicPages/AddToCalendar.tsx
@@ -3,8 +3,9 @@ import { Button } from '@components'
 import { blocksToText } from '../../common/helpers'
 import { Icon } from '@equinor/eds-core-react'
 import { add } from '@equinor/eds-icons'
-import { getYear, getMonth, getDate, getHours, getMinutes } from 'date-fns'
+import { isAfter } from 'date-fns'
 import { zonedTimeToUtc } from 'date-fns-tz'
+import { toUTCDateParts } from '../../common/helpers/dateUtilities'
 import type { EventSchema } from '../../types/types'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -14,49 +15,71 @@ type AddToCalendarProps = {
   event: EventSchema
 }
 
-const dateToParts = (datetime: Date): number[] => {
-  return [getYear(datetime), getMonth(datetime), getDate(datetime), getHours(datetime), getMinutes(datetime)]
+type ICSProps = {
+  start: number[]
+  startInputType: string
+  end: number[]
+  endInputType: string
+  title: string
+  location?: string
+}
+
+// Because in 2021 we still have a zero-index month
+// which means Jan = 0, Dec = 11, etc
+const padMonth = (dateParts: number[]): number[] => {
+  dateParts[1] = dateParts[1] + 1
+  return dateParts
+}
+
+const isUpcomming = (eventDate: Date): boolean => {
+  if (isAfter(eventDate, new Date())) {
+    return true
+  }
+  return false
+}
+
+const createICS = (eventData: ICSProps): string | boolean => {
+  return ics.createEvent(eventData, (error: any, value: string) => {
+    if (error) {
+      console.error('An error occured while generating ICS file.', error)
+      return false
+    }
+
+    const file = new Blob([value], { type: 'text/calendar' })
+    return URL.createObjectURL(file)
+  })
 }
 
 const AddToCalendar = ({ event }: AddToCalendarProps) => {
-  const [fileData, setFileData] = useState('')
+  const [fileData, setFileData] = useState<string | boolean>(false)
 
   useEffect(() => {
-    // TODO: use date, time, and timezone from event once implemented
-    // Date used here is just placeholder
-    const date = new Date()
-    const start = zonedTimeToUtc(date, 'Europe/Oslo')
-    const end = zonedTimeToUtc(date.setHours(date.getHours() + 1), 'Europe/Oslo')
+    const date = new Date() // placeholder
+    const tz = 'Europe/Oslo' // TODO: use timezone passed by event
+    const start = zonedTimeToUtc(date, tz) // TODO: use start date passed by event
+    const end = zonedTimeToUtc(date.setHours(date.getHours() + 1), tz) // TODO: use end date passed by event
 
-    const eventData = {
-      start: dateToParts(start),
-      startInputType: 'utc',
-      end: dateToParts(end),
-      endInputType: 'utc',
-      title: blocksToText(event.title),
-      location: event.content.location,
-    }
-
-    ics.createEvent(eventData, (error: any, value: string) => {
-      if (error) {
-        console.error('An error occured while generating ICS file.', error)
-        return
+    if (isUpcomming(end)) {
+      const eventData = {
+        start: padMonth(toUTCDateParts(start)),
+        startInputType: 'utc',
+        end: padMonth(toUTCDateParts(end)),
+        endInputType: 'utc',
+        title: blocksToText(event.title),
+        location: event.content.location,
       }
 
-      const file = new Blob([value], { type: 'text/calendar' })
-      setFileData(URL.createObjectURL(file))
-    })
+      setFileData(createICS(eventData))
+    }
   }, [event])
 
   if (!fileData) return null
 
   return (
-    <>
-      <Button {...{ href: fileData, download: 'event.ics' }}>
-        <Icon data={add} />
-        Add to Calendar
-      </Button>
-    </>
+    <Button {...(fileData && { href: fileData as string, download: 'event.ics' })}>
+      <Icon data={add} />
+      Add to Calendar
+    </Button>
   )
 }
 
