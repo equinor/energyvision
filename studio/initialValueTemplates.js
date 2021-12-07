94f6447dd8665b8fd3172283b7a0b0568bf52bcd
Fernando Lucchesi <fernando@lucalto.com>
2021-12-07 16:56:36 -0300

    :sparkles: Pre-populate child routes with parent id

--- studio/initialValueTemplates.js
+++ studio/initialValueTemplates.js
@@ -1,4 +1,15 @@
 import T from '@sanity/base/initial-value-template-builder'
+import { languages } from './schemas/languages'
+
+const ParentRoutesTemplates = languages.map(({ name, title }) =>
+  T.template({
+    id: `parent-route-${name}`,
+    title: `Parent route - ${title}`,
+    schemaType: `route_${name}`,
+    parameters: [{ name: 'parentId', type: 'string' }],
+    value: (params) => ({ parent: { _type: 'reference', _ref: params.parentId } }),
+  }),
+)
 
 export default [
   ...T.defaults(),
@@ -20,4 +31,5 @@ export default [
       _lang: params.isoCode,
     }),
   }),
+  ...ParentRoutesTemplates,
 ]
