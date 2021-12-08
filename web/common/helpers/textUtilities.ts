76e6924a4f1d2211a52d32415d4ca090211109da
Ådne Forslund <adne.forslund@gmail.com>
2021-12-08 10:36:26 +0100

    add whitespace remover helper

---
+++ web/common/helpers/textUtilities.ts
@@ -0,0 +1,9 @@
+const removeWhiteSpace = (input: string) => {
+  if (typeof input == 'undefined' || input == null) {
+    return null
+  } else {
+    return input.replace(/\s/g, '')
+  }
+}
+
+export default removeWhiteSpace
