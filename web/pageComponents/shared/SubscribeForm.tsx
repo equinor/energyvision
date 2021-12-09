ac5c706469fd4187f6dd1f84cbf184706969ea8e
Padmaja <52911293+padms@users.noreply.github.com>
2021-12-09 19:26:39 +0530

    💄 Adjusted padding below button and field errors (#590)

--- web/pageComponents/shared/SubscribeForm.tsx
+++ web/pageComponents/shared/SubscribeForm.tsx
@@ -1,13 +1,11 @@
-import type { SubscribeFormData } from '../../types/types'
-//import type {SubscribeFormParmeters} from '../../types/types'
+import type { SubscribeFormParmeters, SubscribeFormData } from '../../types/types'
 import styled from 'styled-components'
 import SimpleBlockContent from '../../common/SimpleBlockContent'
 import { TitleBlockRenderer } from '../../common/serializers'
 import { Button, TextField, Checkbox, Icon } from '@equinor/eds-core-react'
 import { useForm, Controller } from 'react-hook-form'
 import { error_filled } from '@equinor/eds-icons'
-//import { useRouter } from 'next/router'
-//import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const StyledHeading = styled(TitleBlockRenderer)`
   padding: 0 0 var(--space-large) 0;
@@ -19,11 +17,15 @@ const Container = styled.div`
 `
 
 const ButtonWrapper = styled.div`
-  float: right;
+  display: flex;
+  justify-content: right;
   padding: var(--space-medium) 0px var(--space-medium) 0px;
 `
 const TextFieldWrapper = styled.div`
   padding: var(--space-small) 0px var(--space-medium) 0px;
+  p {
+    color: var(--clear-red-100);
+  }
 `
 
 const StyledFieldset = styled.fieldset`
@@ -42,13 +44,10 @@ const StyledLegend = styled.legend`
   padding: 0 0 var(--space-small) 0;
 `
 const UnstyledList = styled.ul`
-  margin: 0;
+  margin: -12px;
   padding: var(--space-medium) 0 var(--space-large) 0px;
   list-style-type: none;
-  column-count: 3;
-  @media (max-width: 1184px) {
-    column-count: 2;
-  }
+  column-count: 2;
   @media (max-width: 920px) {
     column-count: 1;
   }
@@ -56,11 +55,6 @@ const UnstyledList = styled.ul`
 const StyledButton = styled(Button)`
   padding-left: var(--space-xLarge);
   padding-right: var(--space-xLarge);
-`
-
-const StyledSpan = styled.span`
-  font-weight: var(--fontWeight-regular);
-  font-size: var(--typeScale-1);
 `
 const ErrorStyledDiv = styled.div`
   font-weight: var(--fontWeight-regular);
@@ -73,6 +67,10 @@ const StyledIcon = styled(Icon)`
   margin-left: var(--space-small);
 `
 
+const StyledCheckbox = styled(Checkbox)`
+  margin: 8px 0;
+`
+
 type FormValues = {
   firstName: string
   email: string
@@ -80,24 +78,28 @@ type FormValues = {
 }
 
 const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
-  //const router = useRouter()
-  //  const locale = router.locale == 'no-nb' ? 'no' : 'en'
-  //const [submitSuccess, setSubmitSuccess] = useState(false)
-  const onSubmit = (data: FormValues) => {
-    //   data.languageCode = locale
-    /*const res = await fetch('/api/subscribe-form', {
-      body: JSON.stringify(data),
+  const router = useRouter()
+  const onSubmit = async (data: FormValues) => {
+    const subscribeFormParamers: SubscribeFormParmeters = {
+      firstName: data.firstName,
+      email: data.email,
+      crudeOilAssays: data.categories.includes('crudeOilAssays'),
+      generalNews: data.categories.includes('generalNews'),
+      stockMarketAnnouncements: data.categories.includes('stockMarketAnnouncements'),
+      magazineStories: data.categories.includes('magazineStories'),
+      loopStories: data.categories.includes('loopStories'),
+      languageCode: router.locale == 'no-nb' ? 'no' : 'en',
+    }
+
+    const res = await fetch('/api/subscribe-form', {
+      body: JSON.stringify(subscribeFormParamers),
       headers: {
         'Content-Type': 'application/json',
       },
       method: 'POST',
-    })*/
-    // const result = await res.json()
-    //setSubmitSuccess(result.statusCode == 200)
-    console.log(data)
-    // setSubmitSuccess(true)
-
-    console.log(data)
+    })
+    const result = await res.json()
+    if (result.statusCode != 200) console.error(result)
     reset()
   }
 
@@ -107,12 +109,7 @@ const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
     register,
     reset,
     formState: { errors },
-  } = useForm<FormValues>()
-
-  /* useEffect(() => {
-    if (!submitSuccess) console.error('Failed to submit form')
-    reset()
-  }, [submitSuccess, reset])*/
+  } = useForm()
 
   return (
     <>
@@ -130,30 +127,29 @@ const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
         {/* @TODO Norwegian translations for labels and button text*/}
         <form onSubmit={handleSubmit(onSubmit)}>
           <StyledFieldset>
-            <StyledLegend> Categories </StyledLegend>
-            {!errors.categories && <StyledSpan>Please select the category you want to subscribe to</StyledSpan>}
+            <StyledLegend>Let us know what you’re interested in</StyledLegend>
 
             {errors.categories && (
               <ErrorStyledDiv role="alert" id="atleast-one-category-required">
-                <span>Please select the category you want to subscribe to</span>
+                <span>Please select at least one category</span>
                 <StyledIcon data={error_filled} aria-hidden="true" />
               </ErrorStyledDiv>
             )}
 
             <UnstyledList>
               <li>
-                <Checkbox
+                <StyledCheckbox
                   label="General news"
                   value="generalNews"
-                  aria-invalid={errors.categories ? 'true' : 'false'}
                   aria-describedby="atleast-one-category-required"
                   {...register('categories', {
                     validate: (values) => values.length > 0,
                   })}
+                  aria-invalid={errors.categories ? 'true' : 'false'}
                 />
               </li>
               <li>
-                <Checkbox
+                <StyledCheckbox
                   label="Magazine stories"
                   aria-invalid={errors.categories ? 'true' : 'false'}
                   aria-describedby="atleast-one-category-required"
@@ -162,8 +158,8 @@ const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
                 />
               </li>
               <li>
-                <Checkbox
-                  label="Stock Market Announcements"
+                <StyledCheckbox
+                  label="Stock market announcements"
                   value="stockMarketAnnouncements"
                   aria-invalid={errors.categories ? 'true' : 'false'}
                   aria-describedby="atleast-one-category-required"
@@ -171,8 +167,8 @@ const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
                 />
               </li>
               <li>
-                <Checkbox
-                  label="Crude Oil Assays"
+                <StyledCheckbox
+                  label="Crude oil assays"
                   aria-invalid={errors.categories ? 'true' : 'false'}
                   aria-describedby="atleast-one-category-required"
                   value="crudeOilAssays"
@@ -180,7 +176,7 @@ const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
                 />
               </li>
               <li>
-                <Checkbox
+                <StyledCheckbox
                   label="Loop stories"
                   aria-invalid={errors.categories ? 'true' : 'false'}
                   aria-describedby="atleast-one-category-required"
@@ -188,60 +184,63 @@ const SubscribeForm = ({ data: { title } }: { data: SubscribeFormData }) => {
                   {...register('categories')}
                 />
               </li>
+              <li>
+                <StyledCheckbox
+                  label="All"
+                  aria-invalid={errors.categories ? 'true' : 'false'}
+                  aria-describedby="atleast-one-category-required"
+                  value="all"
+                  {...register('categories')}
+                />
+              </li>
             </UnstyledList>
           </StyledFieldset>
-
-          <StyledFieldset>
-            <StyledLegend> Contact information </StyledLegend>
-            <Controller
-              name="firstName"
-              control={control}
-              rules={{
-                required: 'Required',
-              }}
-              render={({ field: { ref, ...props }, fieldState: { invalid, error } }) => (
-                <TextFieldWrapper>
-                  <TextField
-                    {...props}
-                    id={props.name}
-                    label="First Name"
-                    inputRef={ref}
-                    inputIcon={invalid ? <Icon data={error_filled} title="error" /> : undefined}
-                    helperText={error?.message}
-                    helperIcon={<Icon data={error_filled} title="error" />}
-                    variant={invalid ? 'error' : 'default'}
-                    aria-invalid={invalid ? 'true' : 'false'}
-                  />
-                </TextFieldWrapper>
-              )}
-            />
-            <Controller
-              name="email"
-              control={control}
-              rules={{
-                required: 'Required',
-                pattern: {
-                  value: /^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$/g,
-                  message: 'Not a valid email',
-                },
-              }}
-              render={({ field: { ref, ...props }, fieldState: { invalid, error } }) => (
-                <TextFieldWrapper>
-                  <TextField
-                    {...props}
-                    id={props.name}
-                    label="Email"
-                    inputRef={ref}
-                    inputIcon={invalid ? <Icon data={error_filled} title="error" /> : undefined}
-                    helperText={error?.message}
-                    helperIcon={<Icon data={error_filled} title="error" />}
-                    variant={invalid ? 'error' : 'default'}
-                    aria-invalid={invalid ? 'true' : 'false'}
-                  />
-                </TextFieldWrapper>
-              )}
-            />
-          </StyledFieldset>
+          <Controller
+            name="firstName"
+            control={control}
+            rules={{
+              required: 'Please fill out your name',
+            }}
+            render={({ field: { ref, ...props }, fieldState: { invalid, error } }) => (
+              <TextFieldWrapper>
+                <TextField
+                  {...props}
+                  id={props.name}
+                  label="First Name"
+                  inputRef={ref}
+                  inputIcon={invalid ? <Icon data={error_filled} title="error" /> : undefined}
+                  helperText={error?.message}
+                  variant={invalid ? 'error' : 'default'}
+                  aria-invalid={invalid ? 'true' : 'false'}
+                />
+              </TextFieldWrapper>
+            )}
+          />
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: 'Please fill out a valid email address',
+              pattern: {
+                value: /^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$/g,
+                message: 'Please fill out a valid email address',
+              },
+            }}
+            render={({ field: { ref, ...props }, fieldState: { invalid, error } }) => (
+              <TextFieldWrapper>
+                <TextField
+                  {...props}
+                  id={props.name}
+                  label="Email"
+                  inputRef={ref}
+                  inputIcon={invalid ? <Icon data={error_filled} title="error" /> : undefined}
+                  helperText={error?.message}
+                  variant={invalid ? 'error' : 'default'}
+                  aria-invalid={invalid ? 'true' : 'false'}
+                />
+              </TextFieldWrapper>
+            )}
+          />
           <ButtonWrapper>
             <StyledButton type="submit">Subscribe</StyledButton>
           </ButtonWrapper>
