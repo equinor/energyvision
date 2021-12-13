f754dd7537b18b657dcd6249847767044896b9f1
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2021-12-13 15:30:48 +0100

    ⚗ POC for current dataset inside the route preview (#649)

--- studio/src/previews/page/RoutePreview.tsx
+++ studio/src/previews/page/RoutePreview.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Wrapper, Frame } from './styles/shared'
+// eslint-disable-next-line import/no-unresolved
+import client from 'part:@sanity/base/client'
 
 export default function RoutePreview(props: any) {
   const baseUrl = process.env.SANITY_STUDIO_PROJECT_URL
@@ -10,6 +12,10 @@ export default function RoutePreview(props: any) {
   if (!displayed?.slug?.current) {
     return <div>The route needs a slug before it can be previewed.</div>
   }
+
+  // Get the selected dataset
+  // @TODO: We need a way for the web to preview the right dataset
+  console.log('dataset', client.clientConfig.dataset)
 
   const slug = `${displayed?.slug?.current}?preview`
   const locale = displayed._type == 'route_nb_NO' ? '/no' : ''
