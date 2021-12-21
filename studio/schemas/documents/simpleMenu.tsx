e49adcc2fa5eb277780a2e31f55622ece15d0971
Ådne Forslund <adne.forslund@gmail.com>
2021-12-21 13:58:32 +0100

    :construction: #675 new menu

---
+++ studio/schemas/documents/simpleMenu.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default {
+  type: 'document',
+  title: `Site menu(simple)`,
+  name: `simpleMenu`,
+  __experimental_actions: ['update', 'publish'],
+
+  fields: [
+    {
+      title: 'Placeholder',
+      name: 'placeholder',
+      type: 'string',
+    },
+  ],
+}
