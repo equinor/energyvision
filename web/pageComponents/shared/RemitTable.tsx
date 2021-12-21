413085c1d58e06bf8f7d87cb0c0dd0f4f4a69410
Padmaja <52911293+padms@users.noreply.github.com>
2021-12-21 13:42:08 +0530

    Used envis Table instead of eds and updated source of remit json( #558)

--- web/pageComponents/shared/RemitTable.tsx
+++ web/pageComponents/shared/RemitTable.tsx
@@ -1,94 +1,88 @@
-import { Table } from "@equinor/eds-core-react"
-import { useEffect, useState } from "react"
-import {Remit} from '../../types/types'
+import { Table, BackgroundContainer } from '@components'
+import { useEffect, useState } from 'react'
+import { Remit, Message } from '../../types/types'
+import styled from 'styled-components'
+import getConfig from 'next/config'
 
+const { publicRuntimeConfig } = getConfig()
 
-const RemitTable = () =>{
-  
-  const [remitData,setRemitData] = useState<Remit>()
+const Container = styled.div`
+  padding: var(--space-3xLarge) var(--layout-paddingHorizontal-large);
+  max-width: var(--maxViewportWidth);
+  margin: auto;
+`
+
+const RemitTable = () => {
+  const [remitData, setRemitData] = useState<Remit>()
+  const fetchData = async () => {
+    try {
+      const url = `${publicRuntimeConfig.archiveStorageURL}/remit.json`
+      const response = await fetch(url)
+      const json: Remit = await response.json()
+      setRemitData(json)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
   useEffect(() => {
-    const url = "https://www.equinor.com/services/remit";
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch(url);
-            const json:Remit = await response.json();
-            setRemitData(json);
-        } catch (error) {
-            console.log("error", error);
-        }
-    };
     fetchData()
-  },[]);
-  
+  }, [])
 
+  const rowItem = (message: Message) => {
+    return (
+      <Table.Row key={message.ID}>
+        <Table.Cell>{message.ID}</Table.Cell>
+        <Table.Cell>
+          {message.MessagePublishedDateLocal}
+          <br></br>
+          {message.MessagePublishedTimeOfDayLocal}
+        </Table.Cell>
+        <Table.Cell>
+          {message.EventStartDateLocal}
+          <br></br>
+          {message.EventStartTimeOfDayLocal}
+        </Table.Cell>
+        <Table.Cell>
+          {message.ExpectedEventEndDateLocal}
+          <br></br>
+          {message.ExpectedEventEndTimeOfDayLocal}
+        </Table.Cell>
+        <Table.Cell>
+          {message.ActualEventEndDateLocal}
+          <br></br>
+          {message.ActualEventEndTimeOfDayLocal}
+        </Table.Cell>
+        <Table.Cell>{message.Volume}</Table.Cell> <Table.Cell>{message.Comments}</Table.Cell>
+      </Table.Row>
+    )
+  }
   return (
-<Table>
-  <Table.Caption>
-      REMIT Table
-  </Table.Caption>
-  <Table.Head>
-    <Table.Row>
-      <Table.Cell>
-        EVENT ID
-      </Table.Cell>
-      <Table.Cell>
-        PUBLISHED
-      </Table.Cell>
-      <Table.Cell>
-        EVENT START
-      </Table.Cell>
-      <Table.Cell>
-        EXPECTED END
-      </Table.Cell>
-      <Table.Cell>
-        EVENT END
-      </Table.Cell>
-      <Table.Cell>
-        VOLUME IMPACT
-      </Table.Cell>
-      <Table.Cell>
-        COMMENTS
-      </Table.Cell>
-    </Table.Row>
-  </Table.Head>
-  <Table.Body>
-     {
-        remitData?.Data?.List?.map(it => 
-            <Table.Row key={it.CurrentMessage.ID}>
-              <Table.Cell>
-                {it.CurrentMessage.ID}
-              </Table.Cell>
-              <Table.Cell>
-                {it.CurrentMessage.MessagePublishedDateLocal}
-                <br></br>
-                {it.CurrentMessage.MessagePublishedTimeOfDayLocal}
-              </Table.Cell>
-              <Table.Cell>
-              {it.CurrentMessage.EventStartDateLocal}
-                <br></br>
-                {it.CurrentMessage.EventStartTimeOfDayLocal}
-              </Table.Cell> <Table.Cell>
-              {it.CurrentMessage.ExpectedEventEndDateLocal}
-                <br></br>
-                {it.CurrentMessage.ExpectedEventEndTimeOfDayLocal}
-              </Table.Cell> <Table.Cell>
-              {it.CurrentMessage.ActualEventEndDateLocal}
-                <br></br>
-                {it.CurrentMessage.ActualEventEndTimeOfDayLocal}
-              </Table.Cell> <Table.Cell>
-                {it.CurrentMessage.Volume}
-              </Table.Cell> <Table.Cell>
-                {it.CurrentMessage.Comments}
-              </Table.Cell>
+    <BackgroundContainer>
+      <Container>
+        <Table>
+          <Table.Head>
+            <Table.Row>
+              <Table.Cell>EVENT ID</Table.Cell>
+              <Table.Cell>PUBLISHED</Table.Cell>
+              <Table.Cell>EVENT START</Table.Cell>
+              <Table.Cell>EXPECTED END</Table.Cell>
+              <Table.Cell>EVENT END</Table.Cell>
+              <Table.Cell>VOLUME IMPACT</Table.Cell>
+              <Table.Cell>COMMENTS</Table.Cell>
             </Table.Row>
-        )
-     }
-  </Table.Body>
-</Table>
-
+          </Table.Head>
+          <Table.Body>
+            {remitData?.Data?.List?.map((it) => (
+              <>
+                {rowItem(it.CurrentMessage)}
+                {it.MessageHistory && <>{it.MessageHistory.map((item) => (item ? rowItem(item) : ''))}</>}
+              </>
+            ))}
+          </Table.Body>
+        </Table>
+      </Container>
+    </BackgroundContainer>
   )
-
 }
 
 export default RemitTable
