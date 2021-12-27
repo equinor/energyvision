82be747fb524ed78566ac10b4f6cd27370404c53
Padmaja <52911293+padms@users.noreply.github.com>
2021-12-27 20:29:46 +0530

    Add locale in news distribution params ( #604)

--- web/pages/api/news-letter-distribution.ts
+++ web/pages/api/news-letter-distribution.ts
@@ -1,12 +1,24 @@
 import { distribute } from './subscription'
+import languages from '../../languages'
+import { NewsDistributionParameters } from '../../types/types'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const data = req.body
-  await distribute(data).then((isSuccessful) => {
+  const locale = languages.find((lang)=> lang.name == data.languageCode)?.locale || "en"
+
+  const newsDistributionParameters : NewsDistributionParameters ={
+    timeStamp: data.timeStamp,
+    title: data.title,
+    ingress: data.ingress,
+    link: data.link,
+    newsType: data.newsType,
+    languageCode: locale
+  }
+  await distribute(newsDistributionParameters).then((isSuccessful) => {
     if (!isSuccessful) {
-      return res.status(500).json({ msg: 'Subscribe failed' })
+      return res.status(500).json({ msg: `Subscribe failed ${newsDistributionParameters}` })
     }
-    res.status(200).json({ msg: `Successfully distributed. ${data}` })
+    res.status(200).json({ msg: `Successfully distributed. ${newsDistributionParameters}` })
   })
 }
