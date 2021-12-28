afdc0d43e0f1e328044e1aae2c8d8f413d91bd14
Nils Magne Lunde <nilsml@nilsland.com>
2021-12-28 09:11:52 +0100

    ♻️ Use const instead of var

--- studio/server.js
+++ studio/server.js
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 'use strict'
 
-var path = require('path')
-var express = require('express')
-var fallback = require('express-history-api-fallback')
+const path = require('path')
+const express = require('express')
+const fallback = require('express-history-api-fallback')
 
-var port = process.env.PORT || 3333
-var rootDir = process.env.ROOT_DIR || path.join(__dirname, 'dist')
+const port = process.env.PORT || 3333
+const rootDir = process.env.ROOT_DIR || path.join(__dirname, 'dist')
 
-var app = express()
+const app = express()
 app.use(express.static(rootDir))
 app.use(fallback('index.html', { root: rootDir }))
 
-var server = app.listen(port, function () {
+const server = app.listen(port, function () {
   console.log('Sanity studio listening on http://localhost:' + port)
 })
 
