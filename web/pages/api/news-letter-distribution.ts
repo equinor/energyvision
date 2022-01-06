7d878da4dc343082b272e5727b9802cdb3f98d69
Padmaja <52911293+padms@users.noreply.github.com>
2022-01-06 19:26:51 +0530

    App Insight to track news letter failure

--- web/pages/api/news-letter-distribution.ts
+++ web/pages/api/news-letter-distribution.ts
@@ -3,9 +3,10 @@ import languages from '../../languages'
 import { NewsDistributionParameters } from '../../types/types'
 import { NextApiRequest, NextApiResponse } from 'next'
 import getConfig from 'next/config'
-import { useAppInsightsContext } from '@microsoft/applicationinsights-react-js'
+import {  appInsights } from '../../common'
 
 const { publicRuntimeConfig } = getConfig()
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const data = req.body
   const locale = languages.find((lang)=> lang.name == data.languageCode)?.locale || "en"
@@ -19,12 +20,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
   await distribute(newsDistributionParameters).then((isSuccessful) => {
     if (!isSuccessful) {
-      const appInsights = useAppInsightsContext()
       appInsights.trackEvent({name:"News Distribution Failure"})
       return res.status(500).json({ msg: `Distribution failed ${newsDistributionParameters.link}` })
     }
-
-    const appInsights = useAppInsightsContext()
     appInsights.trackEvent({name:"News Distribution Success"})
     res.status(200).json({ msg: `Successfully distributed ${newsDistributionParameters.link}` })
   })
