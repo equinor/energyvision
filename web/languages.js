4dc1c2156047c5b27fdfc7606e5dcc31681893da
Fernando Lucchesi <fernando@lucalto.com>
2022-01-07 06:39:56 -0300

    Update languages.js

--- web/languages.js
+++ web/languages.js
@@ -28,7 +28,7 @@ let languages = [
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 if (dataset === 'production' || dataset === 'global') {
-  languages = languages.filter((lang) => lang.locale === 'en' || lang.locale === 'no' || lang.locale === 'pt')
+  languages = languages.filter((lang) => lang.locale === 'en' || lang.locale === 'no')
 } else if (dataset === 'brazil') {
   languages = languages.filter((lang) => lang.locale === 'pt')
 } else if (dataset === 'germany') {
