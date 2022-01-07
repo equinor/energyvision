680bceb60c7ca83f794b4d57ba0ae95f5aeaa403
Ådne Forslund <adne.forslund@gmail.com>
2022-01-07 17:16:42 +0100

    :construction: #675 switch template based on dataset in studio, probably a better way to do this :alembic:

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -19,9 +19,12 @@ import * as I18nS from 'sanity-plugin-intl-input/lib/structure'
 import { i18n } from './schemas/documentTranslation'
 import DocumentsPane from 'sanity-plugin-documents-pane'
 import languages from './languages'
+// eslint-disable-next-line import/no-unresolved
+import client from 'part:@sanity/base/client'
 // import Iframe from 'sanity-plugin-iframe-pane'
 
 // import resolveProductionUrl from './resolveProductionUrl'
+const dataSet = client.clientConfig.dataset
 
 const menus = languages.map((lang) =>
   S.listItem({
@@ -29,7 +32,9 @@ const menus = languages.map((lang) =>
     id: `menu-${lang.id}`,
     icon: lang.flag,
     child: () =>
-      S.documentWithInitialValueTemplate('menu-with-locale', { isoCode: `${lang.name}` })
+      S.documentWithInitialValueTemplate(dataSet === 'global' ? 'menu-with-locale' : 'simple-menu-with-locale', {
+        isoCode: `${lang.name}`,
+      })
         .id(`${lang.id}-menu`)
         .title(`${lang.title} site menu`),
   }),
