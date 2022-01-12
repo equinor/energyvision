5acba5ed89d2c003a7309b9d4f993e362386c8ee
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-01-12 09:00:14 +0100

    💄 Use Envis grey for background colours in table headers (#687)

--- web/components/src/Table/Cell.tsx
+++ web/components/src/Table/Cell.tsx
@@ -1,11 +1,14 @@
 import { forwardRef, CSSProperties } from 'react'
 import { Table as EdsTable, CellProps } from '@equinor/eds-core-react'
 import styled from 'styled-components'
+import { StyledHeadItem } from './Head'
 
 const { Cell: EdsCell } = EdsTable
 
 const StyledCellItem = styled(EdsCell)`
-  /*  font-size: var(--typeScale-0); */
+  ${StyledHeadItem} & {
+    background-color: var(--grey-20);
+  }
 `
 
 export type TableCellProps = CellProps
