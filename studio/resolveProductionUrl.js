bb287624dc48e60e9886732e044c83a9d2b68d25
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-01-12 20:17:20 +0100

    ⚗ Try to understand the env variables (#713)

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,6 +1,8 @@
 // Any random string, must match SANITY_PREVIEW_SECRET in the Next.js .env.local file
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET
 
+// No worries, these are not "real" secrets
+const mySecret = process.env.TEST_SECRET
 const dataset = process.env.SANITY_STUDIO_API_DATASET
 
 const remoteUrl =
