deecae3d174b1537a9721572d99d3ec66815a433
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-01-12 20:28:15 +0100

    ⚗ zomg, save ftw

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -3,6 +3,8 @@ const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET
 
 // No worries, these are not "real" secrets
 const mySecret = process.env.TEST_SECRET
+
+console.log(previewSecret, mySecret)
 const dataset = process.env.SANITY_STUDIO_API_DATASET
 
 const remoteUrl =
