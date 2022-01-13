8388b129129bebb3189d002d5a31f15225501f90
Victor Nystad <vnys@equinor.com>
2022-01-13 12:11:25 +0100

    🎨 Prevent runtime error
    Use optional chaining to prevent runtime error

--- web/common/helpers/checkCookieConsent.ts
+++ web/common/helpers/checkCookieConsent.ts
@@ -2,7 +2,7 @@ export const checkCookieConsent = (cookiePolicy: string): boolean => {
   if (cookiePolicy === 'none') return true
 
   if (typeof window !== 'undefined') {
-    const consent = window.Cookiebot.consent
+    const consent = window.Cookiebot?.consent
 
     if (consent) {
       return cookiePolicy in consent && consent[cookiePolicy]
