214acb857497bed33fde5377d3cff7e455c1df17
Ådne Forslund <adne.forslund@gmail.com>
2022-01-19 13:27:06 +0100

    #675 :construction: add type to query

--- web/lib/queries/simpleMenu.ts
+++ web/lib/queries/simpleMenu.ts
@@ -2,7 +2,8 @@ export const simpleMenuQuery = /* groq */ `
   *[_type == "simpleMenu" && _lang == $lang][0] {
 	"groups": group[]{
       "id": _key,
-      label,   
+      label,
+      "type": _type,
       "links": links[]{
           label,
          "id": _key,
