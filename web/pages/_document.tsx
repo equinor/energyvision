69e723009efbc400d7fdc6ec6ca2db91dd19cc8f
Victor Nystad <vnystad@gmail.com>
2022-01-19 12:43:28 +0000

    🚩 Add test links in header (#717)

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,4 +1,4 @@
- /* eslint-disable */
+/* eslint-disable */
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
@@ -28,19 +28,43 @@ export default class MyDocument extends Document {
     }
   }
   render() {
-
     type FeatureFlagsProps = {
-      'data-dynamic-typography-version': 'v1' | 'v2',
-      'data-eds-flexible-height'? : Boolean
+      'data-dynamic-typography-version': 'v1' | 'v2'
+      'data-eds-flexible-height'?: Boolean
+      'data-dataset'?: String
     }
 
     const featureFlags: FeatureFlagsProps = {
       'data-dynamic-typography-version': process.env.NEXT_PUBLIC_VNYS_717_IMPROVED_TYPOGRAPHY === 'true' ? 'v2' : 'v1',
+      'data-dataset': process.env.NEXT_PUBLIC_SANITY_DATASET,
     }
 
     if (process.env.NEXT_PUBLIC_VNYS_653_EDS_FLEXIBLE_HEIGHT === 'true') {
       featureFlags['data-eds-flexible-height'] = true
     }
+
+    const linkList: Array<{ title: string; url: URL }> = [
+      {
+        title: 'Natural Gas',
+        url: new URL(`${process.env.NEXT_PUBLIC_LOCALHOST}/energy/natural-gas`),
+      },
+      {
+        title: 'Renewables and low carbon',
+        url: new URL(`${process.env.NEXT_PUBLIC_LOCALHOST}/energy/renewables-and-low-carbon`),
+      },
+      {
+        title: 'Sustainability',
+        url: new URL(`${process.env.NEXT_PUBLIC_LOCALHOST}/sustainability`),
+      },
+      {
+        title: 'Our Approach',
+        url: new URL(`${process.env.NEXT_PUBLIC_LOCALHOST}/sustainability/our-approach`),
+      },
+      {
+        title: 'Professionals',
+        url: new URL(`${process.env.NEXT_PUBLIC_LOCALHOST}/careers/professionals`),
+      },
+    ]
 
     return (
       <Html {...featureFlags}>
@@ -50,6 +74,31 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
         </Head>
         <body>
+          {process.env.NEXT_PUBLIC_VNYS_717_IMPROVED_TYPOGRAPHY === 'true' && (
+            <ul
+              style={{
+                position: 'fixed',
+                top: '0',
+                display: 'flex',
+                listStyleType: 'none',
+                gap: '1em',
+                zIndex: '11',
+                margin: '0',
+                padding: '.2em 1em',
+                background: 'silver',
+                fontSize: '0.875rem',
+                width: '100vw',
+              }}
+            >
+              {linkList.map(({ title, url }) => (
+                <li key={url.href}>
+                  <a style={{ textDecoration: 'none', color: '#333' }} href={url.href}>
+                    {title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
           <Main />
           <NextScript />
         </body>
