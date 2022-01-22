c0b624aeea682f971d0cdd4575882280de8c1e37
Victor Nystad <vnystad@gmail.com>
2022-01-22 22:29:47 +0000

    🚩 Add dynamic height flag to Storybook (#717)

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -16,12 +16,17 @@ module.exports = {
     config.resolve.modules.push(path.resolve(__dirname, '../components'))
     config.resolve.alias['@components'] = path.resolve(__dirname, '../components/src')
     config.resolve.alias['@utils'] = path.resolve(__dirname, '../components/utils')
-    config.plugins.push(
-      new webpack.EnvironmentPlugin({
-        STORYBOOK_BUILT_AT: Date.now(),
-        VNYS_717_IMPROVED_TYPOGRAPHY: process.env.NEXT_PUBLIC_VNYS_717_IMPROVED_TYPOGRAPHY === 'true' ? 'v2' : 'v1',
-      }),
-    )
+
+    const envVars = {
+      STORYBOOK_BUILT_AT: Date.now(),
+      VNYS_717_IMPROVED_TYPOGRAPHY: process.env.NEXT_PUBLIC_VNYS_717_IMPROVED_TYPOGRAPHY === 'true' ? 'v2' : 'v1',
+    }
+
+    if (process.env.NEXT_PUBLIC_VNYS_653_EDS_FLEXIBLE_HEIGHT === 'true') {
+      envVars['VNYS_653_EDS_FLEXIBLE_HEIGHT'] = true
+    }
+
+    config.plugins.push(new webpack.EnvironmentPlugin(envVars))
     return config
   },
 }
