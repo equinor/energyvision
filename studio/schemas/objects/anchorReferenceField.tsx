ed747a1a18db5dc891b4a66f4508f4e1e5d683f7
Sven van Steenis <SvSven@users.noreply.github.com>
2022-02-01 12:37:43 +0100

    ✨ update field description (#715)

--- studio/schemas/objects/anchorReferenceField.tsx
+++ studio/schemas/objects/anchorReferenceField.tsx
@@ -1,11 +1,26 @@
+import React from 'react'
 import { validateAnchorReference } from '../validations/validateAnchorReference'
+import styled from 'styled-components'
 import { Rule } from '@sanity/types'
+
+const StyledSpan = styled.span`
+  display: block;
+  margin-top: 0.3em;
+`
+
+const description = () => (
+  <>
+    <StyledSpan>Add an optional anchor reference that can be used to link directly to this component.</StyledSpan>
+    <StyledSpan>
+      Allowed characters are: letters, numbers, hyphens, and underscores. The # symbol is not needed.
+    </StyledSpan>
+  </>
+)
 
 export default {
   name: 'anchorReferenceField',
   title: 'Anchor reference',
-  description:
-    'Add an optional anchor reference that can be used to link directly to this component. Allowed characters are: letters, numbers, hyphens, and underscores.',
+  description: description(),
   type: 'string',
   validation: (Rule: Rule) => Rule.custom((value: string) => validateAnchorReference(value)),
 }
