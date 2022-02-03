ae53f1f13155a8822333df08aa1e260a32a49419
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-03 08:21:00 +0100

    🚧 Poor man's debugging (#818)

--- web/lib/config.ts
+++ web/lib/config.ts
@@ -11,3 +11,5 @@ export const algolia = {
   applicationId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
   searchApiKey: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
 }
+
+console.log('Application id', algolia.applicationId)
