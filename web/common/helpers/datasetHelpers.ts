d5b6af621fe93bb3ca457f10dfd7dc98f6825a9d
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-03 09:28:52 +0100

    🐛 Include the global test datasets for news

--- web/common/helpers/datasetHelpers.ts
+++ web/common/helpers/datasetHelpers.ts
@@ -2,11 +2,12 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
 // What is the dataset name for global production site
 const EQUINOR_COM_NAME = 'global'
+const EQUINOR_COM_DEV = 'production' || 'global_development'
 
 export const getDataset = () => dataset
 
-export const isGlobal = dataset === EQUINOR_COM_NAME || dataset === 'production'
+export const isGlobal = dataset === EQUINOR_COM_NAME || dataset === EQUINOR_COM_DEV
 
-export const hasNews = dataset === EQUINOR_COM_NAME || dataset === 'germany'
+export const hasNews = dataset === EQUINOR_COM_NAME || dataset === EQUINOR_COM_DEV || dataset === 'germany'
 
-export const hasArchivedNews = dataset === EQUINOR_COM_NAME
+export const hasArchivedNews = dataset === EQUINOR_COM_NAME || dataset === EQUINOR_COM_DEV
