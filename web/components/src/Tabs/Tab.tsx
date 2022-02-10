c2748b626e7866b15a9a8fa627e3170347d3028c
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-10 13:10:33 +0100

    💄 Add some styles for the tab (#827)

--- web/components/src/Tabs/Tab.tsx
+++ web/components/src/Tabs/Tab.tsx
@@ -1,13 +1,34 @@
-import { forwardRef } from 'react'
-
+import { forwardRef, CSSProperties } from 'react'
 import { Tab as RTab, TabProps as RTabProps } from '@reach/tabs'
+import styled from 'styled-components'
 
-export type TabProps = RTabProps
+const StyledTab = styled(RTab)`
+  color: var(--font-color);
+  background: transparent;
+  border: none;
+  border-bottom: 2px solid;
+  padding: var(--space-xSmall) 0;
+  :not(:first-child) {
+    margin-left: var(--space-small);
+  }
+`
 
-export const Tab = forwardRef<HTMLButtonElement, TabProps>(function Tab({ children, ...rest }, ref) {
+export type TabProps = RTabProps & {
+  inverted?: boolean
+}
+
+export const Tab = forwardRef<HTMLButtonElement, TabProps>(function Tab({ inverted = false, children, ...rest }, ref) {
   return (
-    <RTab ref={ref} {...rest}>
+    <StyledTab
+      ref={ref}
+      {...rest}
+      style={
+        {
+          '--font-color': inverted ? 'var(--inverted-text)' : 'var(--default-text)',
+        } as CSSProperties
+      }
+    >
       {children}
-    </RTab>
+    </StyledTab>
   )
 })
