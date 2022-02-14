b7fa8d62318ea078e3a1fb694ff1a2564d1fc9c8
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-14 09:40:39 +0100

    🚧 Add a temp. link for a hit (#825)

--- web/pageComponents/search/hooksLib/Hit.tsx
+++ web/pageComponents/search/hooksLib/Hit.tsx
@@ -1,6 +1,12 @@
 import { Highlight } from './Highlight'
 import { Heading, Text } from '@components'
 import type { Hit as AlgoliaHit } from '@algolia/client-search'
+import { default as NextLink } from 'next/link'
+import styled from 'styled-components'
+
+const TempLink = styled.a`
+  text-decoration: none;
+`
 
 type SearchResultHit = {
   ingress: string
@@ -16,15 +22,22 @@ type HitProps = {
 }
 
 const Hit = ({ hit }: HitProps) => {
+  // Shouldn't be empty string, but this is work in progress
+  const { slug = '' } = hit
+
   return (
     <article>
-      <Heading inverted>
-        <Highlight hit={hit} path="title" />
-      </Heading>
+      <NextLink href={slug} passHref>
+        <TempLink>
+          <Heading inverted>
+            <Highlight hit={hit} path="title" />
+          </Heading>
 
-      <Text inverted>
-        <Highlight hit={hit} path="ingress" />
-      </Text>
+          <Text inverted>
+            <Highlight hit={hit} path="ingress" />
+          </Text>
+        </TempLink>
+      </NextLink>
     </article>
   )
 }
