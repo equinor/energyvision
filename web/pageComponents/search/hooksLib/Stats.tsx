b40dc095acc9735665c39736689530672ee3943b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-14 20:36:16 +0100

    📝 Add remember about fixing the number of hits (#825)

--- web/pageComponents/search/hooksLib/Stats.tsx
+++ web/pageComponents/search/hooksLib/Stats.tsx
@@ -2,7 +2,8 @@ import { useHits } from 'react-instantsearch-hooks'
 
 const Stats = () => {
   const { hits } = useHits()
-
+  // @TODO: Figure out how to find the number of hits,
+  // this number is maximum the number of hits per page from the <Configure />
   return <span>({hits.length || 0})</span>
 }
 
