1d06001e52c5c68bebafa9ad65851448e6682ccf
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-15 09:26:49 +0100

    🚸 Remove the original search field (#825)
    We still want to see how this is done, so just disabling it atm

--- web/pageComponents/search/hooksLib/SearchOverlay.tsx
+++ web/pageComponents/search/hooksLib/SearchOverlay.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import styled from 'styled-components'
-import { useRouter, NextRouter } from 'next/router'
+/* import { useRouter, NextRouter } from 'next/router' */
 import { RemoveScroll } from 'react-remove-scroll'
 import FocusLock from 'react-focus-lock'
 import { Button } from '@components'
 import { Icon } from '@equinor/eds-core-react'
 import { search, close } from '@equinor/eds-icons'
-import Search from '../Search'
+/* import Search from '../Search' */
 import NewSearch from './Search'
 
 /** @TODO Move and refactor */
@@ -35,7 +35,8 @@ const SearchContainer = styled.div`
   margin: 0 auto;
 `
 // Temp.! Refactor, rethink, whatever
-const getSearchState = (router: NextRouter) => {
+// We will probably need this at some point, so I didn't delete it
+/* const getSearchState = (router: NextRouter) => {
   const { query: routerQuery } = router
   const { query, page } = routerQuery
 
@@ -47,22 +48,22 @@ const getSearchState = (router: NextRouter) => {
     page,
     query,
   }
-}
+} */
 
 const SiteMenu = () => {
-  const router = useRouter()
+  /* const router = useRouter() */
   const [isOpen, setIsOpen] = useState(false)
 
-  const searchState = getSearchState(router)
+  /* const searchState = getSearchState(router) */
 
   function onCloseButtonClick() {
     setIsOpen(!isOpen)
   }
 
   // @TODO Lot's of todo
-  const setSearchState = (/* newSearchState */) => {
+  /* const setSearchState = ( newSearchState ) => {
     //_setSearchState(newSearchState, router);
-  }
+  } */
   return (
     <>
       <StyledButton variant="ghost_icon" aria-expanded={isOpen} onClick={onCloseButtonClick} aria-label="Search">
@@ -84,7 +85,7 @@ const SiteMenu = () => {
             </NavTopbar>
             <SearchContainer>
               <NewSearch setIsOpen={setIsOpen} />
-              <Search setSearchState={setSearchState} searchState={searchState} />
+              {/*  <Search setSearchState={setSearchState} searchState={searchState} /> */}
             </SearchContainer>
           </DarkTopbarDropdown>
         </RemoveScroll>
