67c141486360d84c52ca75b5f41891bb8687f3c0
Nils Magne Lunde <nilsml@nilsland.com>
2022-02-15 15:38:36 +0100

    Lazy getting env variables for events

--- search/IndexSanityContent/events/index.ts
+++ search/IndexSanityContent/events/index.ts
@@ -1,4 +1,4 @@
-import { pipe } from 'fp-ts/lib/function'
+import { flow, pipe } from 'fp-ts/lib/function'
 import * as E from 'fp-ts/lib/Either'
 import * as T from 'fp-ts/lib/Task'
 import * as TE from 'fp-ts/lib/TaskEither'
@@ -18,12 +18,12 @@ const indexIdentifier = 'EVENTS'
 // TODO: From where to get language?
 const language = pipe(languageFromIso('en-GB'), languageOrDefault)
 
-const indexName = pipe(getEnvironment(), E.map(generateIndexName(indexIdentifier)(language.isoCode)))
-const updateAlgolia = update(E.getOrElse(() => indexIdentifier)(indexName))(indexSettings)
+const indexName = flow(getEnvironment, E.map(generateIndexName(indexIdentifier)(language.isoCode)))
+const updateAlgolia = () => update(E.getOrElse(() => indexIdentifier)(indexName()))(indexSettings)
 
 export const indexEvents = pipe(
   fetchData(sanityClient)(language),
   TE.map((events) => events.map(mapData)),
-  TE.chain(updateAlgolia),
+  TE.chain(updateAlgolia()),
   T.map(E.fold(console.error, console.log)),
 )
