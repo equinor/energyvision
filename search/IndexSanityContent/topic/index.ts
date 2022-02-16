23455acf7d305f32cc4cbbc9f67f4aa5aa0c6619
Nils Magne Lunde <nilsml@nilsland.com>
2022-02-16 10:20:04 +0100

    🔊 Add logging to sanity indexer

--- search/IndexSanityContent/topic/index.ts
+++ search/IndexSanityContent/topic/index.ts
@@ -4,7 +4,7 @@ import * as E from 'fp-ts/lib/Either'
 import * as T from 'fp-ts/lib/Task'
 import * as TE from 'fp-ts/lib/TaskEither'
 import { update, sanityClient, generateIndexName, getEnvironment, languageFromIso, languageOrDefault } from '../../common'
-import { fetchData } from './sanity'
+import { fetchData, TopicPage } from './sanity'
 import { indexSettings } from './algolia'
 import { mapData } from './mapper'
 
@@ -15,9 +15,21 @@ const language = pipe(languageFromIso('en-GB'), languageOrDefault)
 const indexName = pipe(getEnvironment(), E.map(generateIndexName(indexIdentifier)(language.isoCode)))
 const updateAlgolia = update(E.getOrElse(() => indexIdentifier)(indexName))(indexSettings)
 
+const logSanity = (items: TopicPage[]) => {
+  console.log('items from Sanity: ', items)
+  return items
+}
+
+const logMapper = (items: Readonly<Record<string, string>>[]) => {
+  console.log('items from Sanity: ', items)
+  return items
+}
+
 export const indexTopic = pipe(
   fetchData(sanityClient)(language),
+  TE.map(logSanity),  // Log what we got from Sanity
   TE.map((pages) => pipe(pages.map(mapData), flatten)),
+  TE.map(logMapper),  // Log what we got from Mapper
   TE.chain(updateAlgolia),
   T.map(E.fold(console.error, console.log)),
 )
