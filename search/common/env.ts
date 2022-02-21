7b61c308e5b241f02a42edd3c96c34a8c3ec5d52
Nils Magne Lunde <nilsml@nilsland.com>
2022-02-21 08:29:04 +0100

    🔇 Remove logging statement (#832)

--- search/common/env.ts
+++ search/common/env.ts
@@ -3,7 +3,4 @@ import { GetProcessEnvType } from './types'
 
 export const getAlgoliaAppId: GetProcessEnvType = () => E.fromNullable('Unable to find app id')(process.env.ALGOLIA_APP_ID)
 export const getAlgoliaApiKey: GetProcessEnvType = () => E.fromNullable('Unable to find API key')(process.env.ALGOLIA_API_KEY)
-export const getEnvironment: GetProcessEnvType = () => {
-  console.log('Should not run early')
-  return E.fromNullable('Unable to find environment')(process.env.ENV)
-}
+export const getEnvironment: GetProcessEnvType = () => E.fromNullable('Unable to find environment')(process.env.ENV)
