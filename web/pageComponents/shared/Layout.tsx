a0145d6a8d6f1a4455a4304ba1e9269c2709876a
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-23 16:22:01 +0100

    🐛 Add iso code for react-intl (#608)

--- web/pageComponents/shared/Layout.tsx
+++ web/pageComponents/shared/Layout.tsx
@@ -2,6 +2,7 @@ import Footer from './Footer'
 import type { FooterColumns } from '../../types/types'
 import { IntlProvider } from 'react-intl'
 import { defaultLanguage } from '../../lib/localization'
+import { getIsoFromLocale } from '../../lib/localization'
 
 export type LayoutProps = {
   /* Prewiew or not */
@@ -18,8 +19,14 @@ export type LayoutProps = {
 
 export const Layout = ({ children, footerData, intl }: LayoutProps): JSX.Element => {
   const defaultLocale = defaultLanguage.locale
+  const locale = intl?.locale || defaultLocale
+
   return (
-    <IntlProvider locale={intl?.locale || defaultLocale} defaultLocale={defaultLocale} messages={intl?.messages}>
+    <IntlProvider
+      locale={getIsoFromLocale(locale)}
+      defaultLocale={getIsoFromLocale(defaultLocale)}
+      messages={intl?.messages}
+    >
       <div>
         {children}
         <Footer footerData={footerData} />
