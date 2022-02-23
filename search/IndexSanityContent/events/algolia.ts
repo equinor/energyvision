8a92777e5840fc33802d671643635b88092c9765
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-02-23 11:02:11 +0100

    📝 Add a comment for number of words for snippeting (#824)

--- search/IndexSanityContent/events/algolia.ts
+++ search/IndexSanityContent/events/algolia.ts
@@ -12,7 +12,7 @@ export type EventIndex = {
 
 export const indexSettings: Settings = {
   searchableAttributes: ['title', 'ingress', 'eventDescription'], // date should not be searchable?
-  attributesToSnippet: ['ingress', 'eventDescription'], // I don't know if this is relevant with the hooks lib?
+  attributesToSnippet: ['ingress', 'eventDescription'], // 10 words are default. eventDescription:30 for 30 words
   attributeForDistinct: 'slug',
   distinct: 1,
   // This is the default ranking. Does it make sense to sort on date somehow? I think we need to
