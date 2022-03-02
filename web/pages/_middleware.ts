525eb04eb1c9f0a7cdf9480d65b524996c54c4ce
Fernando Lucchesi <fernando@lucalto.com>
2022-03-02 10:26:47 +0100

    ✨ Permanent redirect .html paths #609

---
+++ web/pages/_middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+const PERMANENT_REDIRECT = 301
+const PUBLIC_FILE = /\.(.*)$/
+const DOT_HTML = '.html'
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const isDotHtml = pathname.slice(-5) === DOT_HTML
+
+  // Check if pathname is valid
+  if ((PUBLIC_FILE.test(pathname) && !isDotHtml) || pathname.includes('/api/')) {
+    return undefined
+  }
+
+  // Check if pathname ends with .html
+  if (isDotHtml) {
+    return NextResponse.redirect(pathname.replace(DOT_HTML, ''), PERMANENT_REDIRECT)
+  }
+}
