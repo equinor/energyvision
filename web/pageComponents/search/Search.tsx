3352543ba14bb598038841cffff45dea38e1b88d
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-03-03 11:36:54 +0100

    🚧 Add the topic index as main index (#896)
    In the near future, this will be the all index

--- web/pageComponents/search/Search.tsx
+++ web/pageComponents/search/Search.tsx
@@ -1,22 +1,32 @@
 import { InstantSearch, Configure } from 'react-instantsearch-hooks'
 import { searchClient } from '../../lib/algolia'
+import { useRouter } from 'next/router'
+import { isGlobalProduction } from '../../common/helpers/datasetHelpers'
+
 import SearchBox from './SearchBox'
 import SearchResults from './SearchResults'
+import { getIsoFromLocale } from '../../lib/localization'
 
 type SearchProps = {
   setIsOpen: (arg0: boolean) => void
 }
 
 const Search = ({ setIsOpen }: SearchProps) => {
+  const router = useRouter()
   // @TODO: Don't hard code it like this
   if (searchClient.appId === '') {
     console.warn('You need to add an app id for Algolia search')
     return null
   }
-  const testIndex = `dev_EVENTS_en-GB`
+
+  const envPrefix = isGlobalProduction ? 'prod' : 'dev'
+  const isoCode = getIsoFromLocale(router.locale)
+
+  // The main index will be "all" at some point
+  const mainIndex = `${envPrefix}_TOPICS_${isoCode}`
 
   return (
-    <InstantSearch searchClient={searchClient} indexName={testIndex}>
+    <InstantSearch searchClient={searchClient} indexName={mainIndex}>
       <Configure hitsPerPage={5} snippetEllipsisText="..." />
 
       <SearchBox />
