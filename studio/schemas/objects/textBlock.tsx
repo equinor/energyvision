3a2447fa86e69c482e6ac9a07169085e2209936f
Ådne Forslund <adne.forslund@gmail.com>
2022-03-03 17:34:10 +0100

    :construction: exclude small text from ingress in textBlock compo #655

--- studio/schemas/objects/textBlock.tsx
+++ studio/schemas/objects/textBlock.tsx
@@ -23,6 +23,7 @@ const ingressContentType = configureBlockContent({
   h3: false,
   h4: false,
   attachment: false,
+  smallText: false,
 })
 const titleContentType = configureTitleBlockContent()
 
