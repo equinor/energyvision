614fd2d6aceb46332695240880f2bec0b7c52ad2
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-03-15 10:24:33 +0100

    🐛 Prevent runtime error. React is actually needed by Sanity

--- studio/schemas/editors/blockContentType.tsx
+++ studio/schemas/editors/blockContentType.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { ExternalLinkRenderer, SuperScriptRenderer, SubScriptRenderer } from '../components'
 import { link, attach_file, external_link } from '@equinor/eds-icons'
 import { IconSuperScript, IconSubScript, EdsIcon } from '../../icons'
