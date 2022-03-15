ed03bcbbc91ed93415402a32fb2609388dbeff1e
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-03-15 07:58:45 +0100

    🐛 Remove html from page
    This should already have been done, don't know why it's still here

--- web/pages/news/archive/[...pagePath].tsx
+++ web/pages/news/archive/[...pagePath].tsx
@@ -89,8 +89,6 @@ const OldArchivedNewsPage = ({ data }: OldArchivedNewsPageProps): JSX.Element =>
             />
             {/*  eslint-disable-next-line @next/next/no-css-tags */}
             <link rel="stylesheet" href="/styles/legacy.minified.css" />
-            {/* eslint-disable jsx-a11y/html-has-lang */}
-            <html data-template="news-legacy" />
           </Head>
           <NextSeo
             title={data?.news?.title}
--- web/pages/news/archive/index.tsx
+++ web/pages/news/archive/index.tsx
@@ -56,8 +56,6 @@ export default function AllArchivedNews({ data }: AllArchivedNewsProps) {
       ></NextSeo>
       <Head>
         <title>Archived news</title>
-        {/* eslint-disable jsx-a11y/html-has-lang */}
-        <html data-template="news-archived" />
       </Head>
       <Container>
         <Heading level="h1" size="2xl" style={{ margin: 'var(--space-medium) 0' }}>
