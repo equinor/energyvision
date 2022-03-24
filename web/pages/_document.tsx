dbcdf6e01ff8648cb82a7c1fcf9c6ec30a609967
Fernando Lucchesi <fernando@lucalto.com>
2022-03-24 18:35:37 +0100

    🔥 Remove GTM set directly on header #908

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,19 +1,6 @@
 /* eslint-disable */
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-import { GTM_ID } from '../lib/gtm'
-
-const GoogleTagManager = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');`,
-    }}
-  ></script>
-)
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -47,7 +34,6 @@ export default class MyDocument extends Document {
           {/* <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-uprights-vf.css" /> */}
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-          <GoogleTagManager />
         </Head>
         <body>
           {/*  <noscript>
