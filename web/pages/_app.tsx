4fb00f7557e12a751e2b3dedd11c8294b14a66be
Fernando Lucchesi <fernando@lucalto.com>
2022-03-24 18:20:52 +0100

    ✨ Add GTM #908

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -10,6 +10,7 @@ import Script from 'next/script'
 import { useEffect } from 'react'
 import { GTM_ID, pageview } from '../lib/gtm'
 import { isGlobal } from '../common/helpers/datasetHelpers'
+import TagManager from 'react-gtm-module'
 
 // import archivedStyles from '@equinor/energyvision-legacy-css'
 // import { AppInsightsContext, AppInsightsErrorBoundary } from '@microsoft/applicationinsights-react-js'
@@ -46,6 +47,10 @@ function MyApp({ Component, pageProps }: CustomAppProps): JSX.Element {
   const router = useRouter()
   const getLayout = Component.getLayout || ((page: ReactNode): ReactNode => page)
   const isLocalhost = !!process.env.NEXT_PUBLIC_LOCALHOST
+
+  useEffect(() => {
+    GTM_ID && TagManager.initialize({ gtmId: GTM_ID })
+  }, [])
 
   useEffect(() => {
     if (!GTM_ID) return
@@ -91,21 +96,6 @@ function MyApp({ Component, pageProps }: CustomAppProps): JSX.Element {
           strategy="beforeInteractive"
           data-blockingmode="auto"
           data-culture={router.locale == 'no' ? 'nb' : router.locale}
-        />
-      )}
-      {GTM_ID && (
-        <Script
-          id="GTM"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
-          `,
-          }}
         />
       )}
       {getLayout(<Component {...pageProps} />)}
--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -2,6 +2,28 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { GTM_ID } from '../lib/gtm'
+
+const GoogleTagManagerHead = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');`,
+    }}
+  ></script>
+)
+
+const GoogleTagManagerBody = () => (
+  <noscript
+    dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+    }}
+  ></noscript>
+)
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -34,6 +56,7 @@ export default class MyDocument extends Document {
           {/* <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-uprights-vf.css" /> */}
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+          <GoogleTagManagerHead />
         </Head>
         <body>
           {/*  <noscript>
@@ -44,6 +67,7 @@ export default class MyDocument extends Document {
               style={{ display: 'none', visibility: 'hidden' }}
             />
        </noscript> */}
+          <GoogleTagManagerBody />
           <Main />
           <NextScript />
         </body>
