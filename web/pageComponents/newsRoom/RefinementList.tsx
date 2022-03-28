a39ae60a5271057308092550556d45fcaec2afe5
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-03-28 15:16:56 +0200

    🚸 Add an example for making it explicit in case of no facets (#949)

--- web/pageComponents/newsRoom/RefinementList.tsx
+++ web/pageComponents/newsRoom/RefinementList.tsx
@@ -1,27 +1,37 @@
 import { useRefinementList, UseRefinementListProps } from 'react-instantsearch-hooks'
 import { List, Checkbox } from '@components'
+import styled from 'styled-components'
 
 export type RefinementListProps = React.ComponentProps<'div'> & UseRefinementListProps
 
 const { Item } = List
+
+const NoRelevant = styled.span`
+  padding: var(--space-small) 0;
+  display: inline-block;
+`
 
 export function RefinementList(props: RefinementListProps) {
   const { items, refine } = useRefinementList(props)
 
   return (
     <div>
-      <List unstyled>
-        {items.map((item) => (
-          <Item key={item.value}>
-            <Checkbox
-              value={item.value}
-              label={`${item.label} (${item.count})`}
-              checked={item.isRefined}
-              onChange={() => refine(item.value)}
-            ></Checkbox>
-          </Item>
-        ))}
-      </List>
+      {items.length > 0 ? (
+        <List unstyled>
+          {items.map((item) => (
+            <Item key={item.value}>
+              <Checkbox
+                value={item.value}
+                label={`${item.label} (${item.count})`}
+                checked={item.isRefined}
+                onChange={() => refine(item.value)}
+              ></Checkbox>
+            </Item>
+          ))}
+        </List>
+      ) : (
+        <NoRelevant>No content is satisfying this filter</NoRelevant>
+      )}
     </div>
   )
 }
