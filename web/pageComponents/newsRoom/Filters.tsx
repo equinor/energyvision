f6d9bb4b2b3324c6bb64a1ab71fa18b0a24cffbd
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-03-28 14:17:48 +0200

    ♻️ Refactor to use new accordion header (#949)

--- web/pageComponents/newsRoom/Filters.tsx
+++ web/pageComponents/newsRoom/Filters.tsx
@@ -1,26 +1,40 @@
-import { Accordion, AccordionItem, AccordionButton, AccordionPanel } from '@reach/accordion'
+import { useState } from 'react'
+import { Accordion, AccordionItem, AccordionPanel } from '@reach/accordion'
+
+import FilterHeader from '././FilterHeader'
 import { RefinementList } from './RefinementList'
 import SearchBox from './SearchBox'
 
 const Filters = ({ ...rest }) => {
+  const [indices, setIndices] = useState([0, 1, 2])
+
+  function toggleItem(toggledIndex: number) {
+    console.log(toggledIndex)
+    if (indices.includes(toggledIndex)) {
+      setIndices(indices.filter((currentIndex) => currentIndex !== toggledIndex))
+    } else {
+      setIndices([...indices, toggledIndex].sort())
+    }
+  }
+
   return (
     <div {...rest}>
       <SearchBox />
-      <Accordion id="filters" multiple collapsible>
+      <Accordion id="filters" index={indices} onChange={toggleItem}>
         <AccordionItem>
-          <AccordionButton>Year</AccordionButton>
+          <FilterHeader label="Year"></FilterHeader>
           <AccordionPanel>
             <RefinementList sortBy={['name:desc']} attribute="year" />
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem>
-          <AccordionButton>Country</AccordionButton>
+          <FilterHeader label="Country" />
           <AccordionPanel>
             <RefinementList sortBy={['name:asc']} attribute="countryTags" />
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem>
-          <AccordionButton>Topic</AccordionButton>
+          <FilterHeader label="Topic" />
           <AccordionPanel>
             <RefinementList sortBy={['name:asc']} limit={20} attribute="topicTags" />
           </AccordionPanel>
