a1bc22ec1fd3a5ad1f9e87b20533fd55d7b8a303
Nils Magne Lunde <nilsml@nilsland.com>
2022-03-29 10:26:39 +0200

    🚧 Rewritten python script to work for preprod (#927)

--- .github/ci/modifyTag.py
+++ .github/ci/modifyTag.py
@@ -1,25 +1,9 @@
 import sys
 from ruamel.yaml import YAML
 
-prodEnvironment = 'prod'
-prodBranch = 'main'
-
-# Gets component index from component name
-
-
-def getComponentIndex(components, name):
-    componentIndex = 0
-    for component in components:
-        if component['name'] == name:
-            return componentIndex
-
-        componentIndex += 1
-
-    return -1
+environment = 'preprod'
 
 # Gets environment index from environment name
-
-
 def getEnvironmentIndex(environments, name):
     environmentIndex = 0
     for environment in environments:
@@ -32,23 +16,20 @@ def getEnvironmentIndex(environments, name):
 
 
 # Main
-component = str(sys.argv[1])
-branch = str(sys.argv[2])
-newTag = str(sys.argv[3])
+prefix = str(sys.argv[1])
+newTag = str(sys.argv[2])
 
 inp_radixconfig = open("radixconfig.yaml").read()
 yaml = YAML()
 content = yaml.load(inp_radixconfig)
 
-# Only prod tag will be dynamic
-if branch == prodBranch:
-    componentIndex = getComponentIndex(
-        content['spec']['components'], component)
-    environmentIndex = getEnvironmentIndex(
-        content['spec']['components'][componentIndex]['environmentConfig'], prodEnvironment)
+# Set newTag for selected prefix (web or studio)
+for component in content['spec']['components']:
+    if component['name'].startswith(prefix):
+        environmentIndex = getEnvironmentIndex(content['spec']['components'][component['name']]['environmentConfig'], environment)
+        if environmentIndex > -1:
+            content['spec']['components'][component['name']]['environmentConfig'][environmentIndex]['imageTagname'] = newTag
 
-    content['spec']['components'][componentIndex]['environmentConfig'][environmentIndex]['imageTagName'] = newTag
-
-    outp_radixconfig = open("radixconfig.yaml", "w")
-    yaml.dump(content, outp_radixconfig)
-    outp_radixconfig.close()
+outp_radixconfig = open("radixconfig.yaml", "w")
+yaml.dump(content, outp_radixconfig)
+outp_radixconfig.close()
