58fbb5c654ddfa6547db80fa875cfacf571a4f8e
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-03-30 10:38:46 +0200

    🚧 Add the newsroom to misc folder (#947)

--- studio/src/lib/structure/items/Misc.js
+++ studio/src/lib/structure/items/Misc.js
@@ -1,7 +1,9 @@
 import S from '@sanity/desk-tool/structure-builder'
 import { FileIcon, TopicDocuments } from '../../../../icons'
+import { HAS_NEWSROOM } from '../../datasetHelpers'
+import { EmptyItem } from './EmptyItem'
 
-const errorPages = [
+const miscPages = [
   S.listItem()
     .title('404 - Page not found')
     .icon(FileIcon)
@@ -56,9 +58,38 @@ const errorPages = [
           },
         ]),
     ),
+  HAS_NEWSROOM
+    ? S.listItem()
+        .title('Newsroom')
+        .icon(FileIcon)
+        .child(
+          S.documentList()
+            .id('newsroom')
+            .title('Newsroom')
+            .schemaType('newsroom')
+            .filter('(_id match "*" + $id) && _type == $type')
+            .params({
+              id: 'newsroom',
+              type: 'newsroom',
+            })
+            .menuItems([
+              {
+                title: 'Create new',
+                intent: {
+                  type: 'create',
+                  params: {
+                    id: 'newsroom',
+                    type: 'newsroom',
+                    template: 'newsroom',
+                  },
+                },
+              },
+            ]),
+        )
+    : EmptyItem,
 ]
 
 export const Misc = S.listItem()
   .title('Misc')
   .icon(TopicDocuments)
-  .child(S.list('misc').id('misc').title('Misc').items(errorPages))
+  .child(S.list('misc').id('misc').title('Misc').items(miscPages))
