176c3d04c50920c7783decb581b83de72b4aa4c9
Nils Magne Lunde <nilsml@nilsland.com>
2022-04-07 11:57:20 +0200

    🔥 Remove unused code

--- search/IndexSanityContent/news/sanity.ts
+++ search/IndexSanityContent/news/sanity.ts
@@ -1,51 +1,9 @@
-//import * as A from 'fp-ts/lib/Array'
-//import { nonEmptyArray as A } from 'fp-ts'
-//import { NonEmptyArray } from 'fp-ts/lib/NonEmptyArray'
 import * as E from 'fp-ts/lib/Either'
 import * as TE from 'fp-ts/lib/TaskEither'
 import { pipe } from 'fp-ts/lib/function'
 import { SanityClient } from '@sanity/client'
 import { Language } from '../../common'
 
-/*
-const defaults = { nonTextBehavior: 'remove' };
-const blocksToArray = (blocks, opts = {}) => {
-  const options = Object.assign({}, defaults, opts);
-  if (blocks.length === 0) return undefined;
-  return blocks
-    .map(block => {
-      if (block._type !== 'block' || !block.children) {
-        return options.nonTextBehavior === 'remove' ? '' : `[${block._type} block]`;
-      }
-      return block.children.map(child => child.text).join('');
-    })
-    .filter(block => {
-      return !!block.trim();
-    });
-};
-*/
-
-// Move to mapper or create decoder
-/* const getBlockText = (blocks: NonEmptyArray<BlockNode>) =>
-  pipe(
-    blocks,
-    A.map((block) =>
-      pipe(
-        block,
-        E.fromPredicate(
-          (block) => block.children.length > 0,
-          () => block,
-        ),
-        E.fold(
-          () => '',
-          (nodeWithChildren) => nodeWithChildren.children.map((child) => child.text).join(' '),
-        ),
-      ),
-    ),
-  ) */
-
-// NM: Didn't feel good to take this from the web folder since the search
-//is so isolated :/ Copied it
 const publishDateTimeQuery = /* groq */ `
   select(
     customPublicationDate == true =>
