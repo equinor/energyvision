2720b3f55769c4639972720ee13422585406412b
Padmaja <52911293+padms@users.noreply.github.com>
2022-04-11 15:26:51 +0530

    Fix cookie declaration (#962)

--- web/pageComponents/topicPages/CookieDeclaration.tsx
+++ web/pageComponents/topicPages/CookieDeclaration.tsx
@@ -13,18 +13,33 @@ const Container = styled.div`
   padding: var(--space-3xLarge) var(--layout-paddingHorizontal-large);
   max-width: var(--maxViewportWidth);
   margin: auto;
+  overflow-x: auto;
 `
 
 const CookieDeclaration = () => {
   const router = useRouter()
+  const language = router.locale == 'no' ? 'nb' : router.locale
   useEffect(() => {
     if (window.CookieDeclaration != undefined && document.getElementsByClassName('CookieDeclaration') == undefined) {
       window.CookieDeclaration.init()
+    } else {
+      const script = document.createElement('script')
+      script.setAttribute('id', 'CookieDeclaration')
+      script.setAttribute('src', 'https://consent.cookiebot.com/f1327b03-7951-45da-a2fd-9181babc783f/cd.js')
+      script.setAttribute('type', 'text/javascript')
+      if (language) {
+        script.setAttribute('data-culture', language)
+      }
+      script.onload = () => {
+        window.CookieDeclaration.init()
+      }
+      const cookieDeclarationWrapper = document.querySelector('cookie-declaration-wrapper')
+      cookieDeclarationWrapper?.appendChild(script)
     }
-  })
+  }, [router.asPath, language])
   return (
     <BackgroundContainer background="White">
-      <Container>
+      <Container id="cookie-declaration-wrapper">
         {
           <script
             id="CookieDeclaration"
