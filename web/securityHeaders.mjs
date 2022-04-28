f24c400ebabca44081ac6b2a6ddc1f7844e00ea8
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2022-04-28 08:06:48 +0200

    🔒 Tweak security headers (#1030)

--- web/securityHeaders.mjs
+++ web/securityHeaders.mjs
@@ -4,23 +4,29 @@
  */
 import { dataset } from './languages.js'
 
-/*
-//We probably need to whitelist Eds cdn too?
-const ContentSecurityPolicy = `
-default-src 'self' cdn.sanity.io;
-img-src 'self' cdn.sanity.io;
-script-src 'self';
-child-src 'self';
-style-src 'self';
-font-src 'self';
-*/
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const envs = ['dev', 'preprod', 'prod']
-const localUrl = process.env.NODE_ENV === 'development' ? 'localhost:3333' : ''
+const localUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : ''
 const globalUrl = dataset === 'global' ? 'https://equinor.sanity.studio' : ''
 const secretUrl = dataset === 'secret' ? 'https://equinor-restricted.sanity.studio' : ''
 const studioUrls = envs.map((env) => `https://studio-${dataset}-energyvision-${env}.radix.equinor.com/`)
 const xFrameUrls = [localUrl, ...studioUrls, globalUrl, secretUrl].filter((e) => e).join(' ')
+
+
+ const ContentSecurityPolicy = `
+  default-src 'self' cdn.sanity.io;
+  style-src 'report-sample' 'self' 'unsafe-inline';
+  ${isProduction ? "script-src 'report-sample' 'self' 'unsafe-inline' https://www.googletagmanager.com": "script-src 'report-sample' 'self' 'unsafe-inline' https://www.googletagmanager.com 'unsafe-eval'"};
+  img-src 'self' data: cdn.sanity.io https://*.siteimproveanalytics.io www.googletagmanager.com;
+  connect-src 'self' https://tools.eurolandir.com https://*.algolia.net https://*.algolianet.com;
+  script-src-elem 'self' https://siteimproveanalytics.com https://consent.cookiebot.com https://consentcdn.cookiebot.com;
+  frame-src 'self' https://consentcdn.cookiebot.com https://www.youtube.com https://vimeo.com;
+  frame-ancestors ${xFrameUrls};
+  font-src 'self';
+` 
+//console.log(ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim())
 
 export default [
   {
@@ -35,11 +41,11 @@ export default [
     key: 'X-XSS-Protection',
     value: '1; mode=block',
   },
-  {
+ /*  {
     //This blocks preview from working, unless we whitelist all studio urls
     key: 'X-Frame-Options',
-    value: `frame-ancestors ${xFrameUrls}`,
-  },
+    value: 'SAMEORIGIN',
+  }, */
   {
     //https://github.com/w3c/webappsec-permissions-policy/issues/189
     key: 'Permissions-Policy',
@@ -54,10 +60,11 @@ export default [
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
-  /*
+  
   {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+   // key: 'Content-Security-Policy',
+   key: 'Content-Security-Policy-Report-Only',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
   },
-  */
+  
 ]
