ed774e2ae1235e7b53b8e3dc9bffb31faeec411a
Sven van Steenis <SvSven@users.noreply.github.com>
2022-05-04 10:08:06 +0200

    🐛 fix getFullUrl to handle [...pagePath] catch-all (#1056)

--- web/common/helpers/getFullUrl.ts
+++ web/common/helpers/getFullUrl.ts
@@ -7,7 +7,10 @@ export const getFullUrl = (pathname: string, slug: string, locale = ''): string 
   const intl = locale === defaultLanguage?.locale ? '' : `/${locale}`
 
   const fullUrlDyn = pathname.indexOf('http') === -1 ? `${publicRuntimeConfig.domain}${intl}${pathname}` : pathname
-  const fullUrl = fullUrlDyn.replace('/[[...slug]]', slug)
 
-  return fullUrl
+  if (fullUrlDyn.includes('[...pagePath]')) {
+    return fullUrlDyn.replace('[...pagePath]', slug)
+  }
+
+  return fullUrlDyn.replace('/[[...slug]]', slug)
 }
