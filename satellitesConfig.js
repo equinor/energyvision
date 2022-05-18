f81024262e0c15bb2e7067a2d214f7390faa9278
Fernando Lucchesi <fernando@lucalto.com>
2022-05-18 13:40:41 +0200

    🐛 Add type check #950

--- satellitesConfig.js
+++ satellitesConfig.js
@@ -120,7 +120,7 @@ const getDomain = (dataset) => websiteDomains[dataset] || 'Domain not set'
  * @param {string} dataset
  */
 const getMetaTitleSuffix = (dataset) => {
-  return dataset == 'global' ? 'Equinor' : (websiteDomains[dataset] || '').replace('https://', '').replace('www.', '')
+  return dataset === 'global' ? 'Equinor' : (websiteDomains[dataset] || '').replace('https://', '').replace('www.', '')
 }
 
 module.exports = {
