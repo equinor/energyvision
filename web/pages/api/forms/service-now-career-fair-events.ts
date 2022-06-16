c2fa82bf3be57f6197c489e17e3aab2185ee7889
Nils Magne Lunde <nilsml@nilsland.com>
2022-06-16 17:00:44 +0200

    🎨 Format documents

--- web/pages/api/forms/service-now-career-fair-events.ts
+++ web/pages/api/forms/service-now-career-fair-events.ts
@@ -3,45 +3,67 @@ import { sendRequestToServiceNow } from './service-now-base'
 import { validateCaptcha } from './validateCaptcha'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const catalogIdentifier = "848f447ddb692600ff6272dabf961948"
-    const frcCaptchaSolution = req.body.frcCaptchaSolution
-    const data = req.body.data
-    const email = encodeURI(data.email)
-    const groupOrganisation = encodeURI(data.organisation)
-    const contactPerson = encodeURI(data.contactPerson)
-    const phone = encodeURI(data.phone)
-    const event = encodeURI(data.event)
-    const eventDescription = encodeURI(data.eventDescription)
-    const website = encodeURI(data.website)
-    const sendSupportingDocuments = data.supportingDocuments!==""? "Yes":"No"
-    const preferredLang = data.preferredLang
-    
-    const urlString = process.env.SERVICE_NOW_FORM_URL
-    + "/api/stasa/statoildotcomproject/CareerFairs/" + process.env.SERVICE_NOW_FORM_CATALOG_ITEM 
-        + "/" + catalogIdentifier
-    + "?SchoolOrganisation="+groupOrganisation+"&ContactPerson="+contactPerson+"&PhoneNumber="+phone+"&Email="+email+"&Event="+event+"&DescriptionOfEvent="+eventDescription+"&LinkToWebsite="+website+"&SupportingDocuments="+sendSupportingDocuments+"&PreferredLang="+preferredLang
+  const catalogIdentifier = '848f447ddb692600ff6272dabf961948'
+  const frcCaptchaSolution = req.body.frcCaptchaSolution
+  const data = req.body.data
+  const email = encodeURI(data.email)
+  const groupOrganisation = encodeURI(data.organisation)
+  const contactPerson = encodeURI(data.contactPerson)
+  const phone = encodeURI(data.phone)
+  const event = encodeURI(data.event)
+  const eventDescription = encodeURI(data.eventDescription)
+  const website = encodeURI(data.website)
+  const sendSupportingDocuments = data.supportingDocuments !== '' ? 'Yes' : 'No'
+  const preferredLang = data.preferredLang
 
-    if (!frcCaptchaSolution) {
-        return res.status(500).json({ msg: 'Anti-robot check solution was not present' })
-    } 
-    try {
-        const { accept, errorCode } = await validateCaptcha(frcCaptchaSolution)
-        if (!accept) {
-          console.log(`Anti-robot check failed [code=${errorCode}] for career fair and events form`)
-          return res.status(400).json({ msg: `Anti-robot check failed [code=${errorCode}], please try again.` })
-        }
-      } catch (err) {
-        console.error(err)
-        return res.status(502).json({ msg: 'failed to validate captcha' })
-      }
- await sendRequestToServiceNow(urlString).then((response)=>{
-     if(JSON.parse(response).status == 'failure' || JSON.parse(response).Status?.includes("Failure")){
-        console.log("Failed to create ticket in service-now")
+  const urlString =
+    process.env.SERVICE_NOW_FORM_URL +
+    '/api/stasa/statoildotcomproject/CareerFairs/' +
+    process.env.SERVICE_NOW_FORM_CATALOG_ITEM +
+    '/' +
+    catalogIdentifier +
+    '?SchoolOrganisation=' +
+    groupOrganisation +
+    '&ContactPerson=' +
+    contactPerson +
+    '&PhoneNumber=' +
+    phone +
+    '&Email=' +
+    email +
+    '&Event=' +
+    event +
+    '&DescriptionOfEvent=' +
+    eventDescription +
+    '&LinkToWebsite=' +
+    website +
+    '&SupportingDocuments=' +
+    sendSupportingDocuments +
+    '&PreferredLang=' +
+    preferredLang
+
+  if (!frcCaptchaSolution) {
+    return res.status(500).json({ msg: 'Anti-robot check solution was not present' })
+  }
+  try {
+    const { accept, errorCode } = await validateCaptcha(frcCaptchaSolution)
+    if (!accept) {
+      console.log(`Anti-robot check failed [code=${errorCode}] for career fair and events form`)
+      return res.status(400).json({ msg: `Anti-robot check failed [code=${errorCode}], please try again.` })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.status(502).json({ msg: 'failed to validate captcha' })
+  }
+  await sendRequestToServiceNow(urlString)
+    .then((response) => {
+      if (JSON.parse(response).status == 'failure' || JSON.parse(response).Status?.includes('Failure')) {
+        console.log('Failed to create ticket in service-now')
         res.status(500).end()
-     }
-     res.status(200).end()
- }).catch(error =>{
-     console.log(error)
-     res.status(500).end()
- })
+      }
+      res.status(200).end()
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).end()
+    })
 }
--- web/pages/api/forms/service-now-order-reports.ts
+++ web/pages/api/forms/service-now-order-reports.ts
@@ -2,55 +2,75 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { sendRequestToServiceNow } from './service-now-base'
 import { validateCaptcha } from './validateCaptcha'
 
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const catalogIdentifier = "d1872741db26ea40977079e9bf961949"
-   
-    const frcCaptchaSolution = req.body.frcCaptchaSolution
-    const data = req.body.data
-    const email = encodeURI(data.email)
-    const name = encodeURI(data.name)
-    const address = encodeURI(data.address)
-    const zipcode = encodeURI(data.zipcode)
-    const city = encodeURI(data.city)
-    const country = encodeURI(data.country)
-    const company = encodeURI(data.company)
-    const annualReport = data.reports.includes("annualReport")? "Yes":"No"
-    const prospectusReport = data.reports.includes("prospectusReport")? "Yes":"No"
-    const statutoryReport = data.reports.includes("statutoryReport")? "Yes":"No"
-    
-    const urlString = process.env.SERVICE_NOW_FORM_URL
-    + "/api/stasa/statoildotcomproject/OrderReports/" + process.env.SERVICE_NOW_FORM_CATALOG_ITEM + "/"
-    + catalogIdentifier + "?Company=" + company + "&City=" + city + "&Address=" + address
-    + "&Name=" + name + "&ZIP=" + zipcode + "&StatutoryReport=" + statutoryReport + "&AnnualReport="
-    + annualReport + "&Country=" + country + "&Email=" + email + "&ProspectusReport="
-    + prospectusReport;
-    
-    
-    if (!frcCaptchaSolution) {
-      return res.status(500).json({ msg: 'Anti-robot check solution was not present' })
-    }
-  
-    try {
-        const { accept, errorCode } = await validateCaptcha(frcCaptchaSolution)
-        if (!accept) {
-          console.log(`Anti-robot check failed [code=${errorCode}] for order annual reports form`)
-          return res.status(400).json({ msg: `Anti-robot check failed [code=${errorCode}], please try again.` })
-        }
-      } catch (err) {
-        console.error(err)
-        return res.status(502).json({ msg: 'failed to validate captcha' })
-      }
+  const catalogIdentifier = 'd1872741db26ea40977079e9bf961949'
 
- await sendRequestToServiceNow(urlString).then((response)=>{
-    console.log("red "+response)
-    if(JSON.parse(response).status == 'failure' || JSON.parse(response).Status?.includes("Failure")){
-        console.log("Failed to create ticket in service-now")
-        res.status(500).end()  
-     }
-     res.status(200).end()
- }).catch(error =>{
-     console.log(error)
-     res.status(500).end()
- })
+  const frcCaptchaSolution = req.body.frcCaptchaSolution
+  const data = req.body.data
+  const email = encodeURI(data.email)
+  const name = encodeURI(data.name)
+  const address = encodeURI(data.address)
+  const zipcode = encodeURI(data.zipcode)
+  const city = encodeURI(data.city)
+  const country = encodeURI(data.country)
+  const company = encodeURI(data.company)
+  const annualReport = data.reports.includes('annualReport') ? 'Yes' : 'No'
+  const prospectusReport = data.reports.includes('prospectusReport') ? 'Yes' : 'No'
+  const statutoryReport = data.reports.includes('statutoryReport') ? 'Yes' : 'No'
+
+  const urlString =
+    process.env.SERVICE_NOW_FORM_URL +
+    '/api/stasa/statoildotcomproject/OrderReports/' +
+    process.env.SERVICE_NOW_FORM_CATALOG_ITEM +
+    '/' +
+    catalogIdentifier +
+    '?Company=' +
+    company +
+    '&City=' +
+    city +
+    '&Address=' +
+    address +
+    '&Name=' +
+    name +
+    '&ZIP=' +
+    zipcode +
+    '&StatutoryReport=' +
+    statutoryReport +
+    '&AnnualReport=' +
+    annualReport +
+    '&Country=' +
+    country +
+    '&Email=' +
+    email +
+    '&ProspectusReport=' +
+    prospectusReport
+
+  if (!frcCaptchaSolution) {
+    return res.status(500).json({ msg: 'Anti-robot check solution was not present' })
+  }
+
+  try {
+    const { accept, errorCode } = await validateCaptcha(frcCaptchaSolution)
+    if (!accept) {
+      console.log(`Anti-robot check failed [code=${errorCode}] for order annual reports form`)
+      return res.status(400).json({ msg: `Anti-robot check failed [code=${errorCode}], please try again.` })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.status(502).json({ msg: 'failed to validate captcha' })
+  }
+
+  await sendRequestToServiceNow(urlString)
+    .then((response) => {
+      console.log('red ' + response)
+      if (JSON.parse(response).status == 'failure' || JSON.parse(response).Status?.includes('Failure')) {
+        console.log('Failed to create ticket in service-now')
+        res.status(500).end()
+      }
+      res.status(200).end()
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).end()
+    })
 }
