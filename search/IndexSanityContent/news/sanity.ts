06208b2b81ff9e3fa950a7fea831da7165ac310e
Padmaja <52911293+padms@users.noreply.github.com>
2022-07-12 16:05:58 +0530

    ♻️ Filter out crude-oi-assays tags from indexing (#1049)

--- search/IndexSanityContent/news/sanity.ts
+++ search/IndexSanityContent/news/sanity.ts
@@ -12,7 +12,8 @@ const publishDateTimeQuery = /* groq */ `
   )
 `
 
-export const query = /* groq */ `*[_type == "news" && _lang == $lang && !(_id in path("drafts.**")) && excludeFromSearch != true && subscriptionType != "Crude"] {
+export const query = /* groq */ `*[_type == "news" && _lang == $lang && !(_id in path("drafts.**")) && excludeFromSearch != true && 
+(subscriptionType != "Crude" && ((count(tags)==1 && tags[0]->.key.current !="crude-oil-assays") || (count(tags) >1))) ] {
   "slug": slug.current,
   _id,
   "title": title,
