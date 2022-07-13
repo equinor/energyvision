577afe0d396cbdece9ced12cff19cce5c214581d
Padmaja <52911293+padms@users.noreply.github.com>
2022-07-13 12:33:31 +0530

    🔊 Logs to newsletter distribution #1186

--- web/pages/api/news-letter-distribution.ts
+++ web/pages/api/news-letter-distribution.ts
@@ -9,6 +9,15 @@ const SANITY_API_TOKEN = process.env.SANITY_API_TOKEN || ''
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!isValidRequest(req, SANITY_API_TOKEN)) {
+    console.log('**************News Letter Authorization Failure**********\n')
+    console.log('Headers -----------------------\n')
+    console.log('Idempotency-key: ' + req.headers['idempotency-key'] + '\n')
+    console.log('content-length: ' + req.headers['content-length'] + '\n')
+    console.log('user-agent: ' + req.headers['user-agent'] + '\n')
+    console.log('host: ' + req.headers['host'] + '\n')
+    console.log('Headers -----------------------\n')
+    console.log(JSON.stringify(req.body))
+    console.log('**************News Letter Authorization Failure**********\n')
     return res.status(401).json({ success: false, msg: 'Unauthorized!' })
   }
   const { publicRuntimeConfig } = getConfig()
