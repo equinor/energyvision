0e470d6118efad19ad3adc638096314cad50f964
Fernando Lucchesi <fernando@lucalto.com>
2022-07-14 04:57:05 -0300

    🚑️ .xml and .txt extensions not working

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -14,7 +14,7 @@ const withBundle = withBundleAnalyzer({
 
 const locales = languages.map((lang) => lang.locale)
 
-const extensions = ['tsx', 'ts', 'txt.ts', 'xml.ts', 'js', 'jsx']
+const extensions = ['tsx', 'ts', 'js', 'jsx']
 const globalOnlyExtensions = ['global.tsx', 'global.ts']
 const isGlobal = dataset === 'global' || dataset === 'global-development' || dataset === 'global-test'
 const pageExtensions = isGlobal ? [...extensions, ...globalOnlyExtensions] : extensions
