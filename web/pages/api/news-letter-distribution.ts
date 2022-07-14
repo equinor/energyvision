2b44270371ec3ee7dab34f169fdefc6e0aa11f89
Fernando Lucchesi <fernando@lucalto.com>
2022-07-14 09:05:43 -0300

    🔊 Cleanup code for logging #1186

--- web/pages/api/news-letter-distribution.ts
+++ web/pages/api/news-letter-distribution.ts
@@ -7,17 +7,18 @@ import getConfig from 'next/config'
 
 const SANITY_API_TOKEN = process.env.SANITY_API_TOKEN || ''
 
+const logRequest = (req: NextApiRequest, title: string) => {
+  console.log('\n\n')
+  console.log(title)
+  console.log('Datetime: ' + new Date())
+  console.log('Headers:\n', req.headers)
+  console.log('Body:\n', req.body)
+  console.log('\n\n')
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!isValidRequest(req, SANITY_API_TOKEN)) {
-    console.log('**************News Letter Authorization Failure**********\n')
-    console.log('Headers -----------------------\n')
-    console.log('Idempotency-key: ' + req.headers['idempotency-key'] + '\n')
-    console.log('content-length: ' + req.headers['content-length'] + '\n')
-    console.log('user-agent: ' + req.headers['user-agent'] + '\n')
-    console.log('host: ' + req.headers['host'] + '\n')
-    console.log('Headers -----------------------\n')
-    console.log(JSON.stringify(req.body))
-    console.log('**************News Letter Authorization Failure**********\n')
+    logRequest(req, 'Unauthorized request: Newsletter Distribution Endpoint')
     return res.status(401).json({ success: false, msg: 'Unauthorized!' })
   }
   const { publicRuntimeConfig } = getConfig()
