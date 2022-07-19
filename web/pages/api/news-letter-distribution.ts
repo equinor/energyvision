fabb33c44f6915c1f84570211f7937430bd94653
Fernando Lucchesi <fernando@lucalto.com>
2022-07-19 08:37:31 -0300

    ⬆️ Upgrade sanity webhook #1202

--- web/pages/api/news-letter-distribution.ts
+++ web/pages/api/news-letter-distribution.ts
@@ -2,10 +2,26 @@ import { distribute } from './subscription'
 import { languages } from '../../languages'
 import { NewsDistributionParameters } from '../../types/types'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { isValidRequest } from '@sanity/webhook'
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import getConfig from 'next/config'
 
 const SANITY_API_TOKEN = process.env.SANITY_API_TOKEN || ''
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+// https://nextjs.org/docs/api-routes/api-middlewares#custom-config
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+async function readBody(readable: NextApiRequest) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString('utf8')
+}
 
 const logRequest = (req: NextApiRequest, title: string) => {
   console.log('\n\n')
@@ -17,7 +33,10 @@ const logRequest = (req: NextApiRequest, title: string) => {
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!isValidRequest(req, SANITY_API_TOKEN)) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string
+  const body = await readBody(req)
+  console.log('Sending newsletter...')
+  if (!isValidSignature(body, signature, SANITY_API_TOKEN)) {
     logRequest(req, 'Unauthorized request: Newsletter Distribution Endpoint')
     return res.status(401).json({ success: false, msg: 'Unauthorized!' })
   }
@@ -36,6 +55,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!isSuccessful) {
       return res.status(400).json({ msg: `Distribution failed ${newsDistributionParameters.link}` })
     }
+    console.log('Newsletter sent successfully!')
     res.status(200).json({ msg: `Successfully distributed ${newsDistributionParameters.link}` })
   })
 }
