3a99134646dcfed6dd9653b25be8878417160eef
Padmaja <52911293+padms@users.noreply.github.com>
2022-07-22 17:14:50 +0530

    Revert "🔥 Remove unused import"
    This reverts commit c18aaf1124279093f43481d9caa956a3b47f2051.

--- web/pageComponents/topicPages/Video.tsx
+++ web/pageComponents/topicPages/Video.tsx
@@ -3,6 +3,7 @@ import { BackgroundContainer } from '@components'
 import styled from 'styled-components'
 import IngressText from '../../pageComponents/shared/portableText/IngressText'
 import TitleText from '../../pageComponents/shared/portableText/TitleText'
+import { useEffect, useRef } from 'react'
 import MuxVideo from '@mux/mux-video-react';
 
 type VideoProps = {
