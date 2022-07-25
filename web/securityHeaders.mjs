5431a0fa7c2e44d2d510185e8a94e9b94decf251
Padmaja <52911293+padms@users.noreply.github.com>
2022-07-25 19:12:11 +0530

    🚧 Video not playing in mobile (#1100)

--- web/securityHeaders.mjs
+++ web/securityHeaders.mjs
@@ -30,7 +30,7 @@ const ContentSecurityPolicy = `
    frame-src 'self' https://consentcdn.cookiebot.com https://lt.morningstar.com https://www.youtube.com https://vimeo.com https://sds-maintenance.com https://tools.eurolandir.com/ https://platform.twitter.com/;
    frame-ancestors ${xFrameUrls};
    font-src 'self' https://eds-static.equinor.com;
-   media-src 'self' blob:;
+   media-src 'self' blob: https://stream.mux.com/;
 
  `
 
