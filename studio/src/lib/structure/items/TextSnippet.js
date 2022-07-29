5c969356a72b48dc762c197799a3cfc53e06446b
Padmaja <52911293+padms@users.noreply.github.com>
2022-07-29 19:32:26 +0530

    ♻️ Group text snippets (#1135)

--- studio/src/lib/structure/items/TextSnippet.js
+++ studio/src/lib/structure/items/TextSnippet.js
@@ -2,9 +2,37 @@ import S from '@sanity/desk-tool/structure-builder'
 import { EdsIcon } from '../../../../icons'
 import { text_field } from '@equinor/eds-icons'
 import textSnippets from '../../../../schemas/textSnippets'
+import { groups } from '../../../../schemas/textSnippets'
 
-const textSnippetItems = Object.keys(textSnippets).map((key) =>
-  S.listItem({
+const ungroupedTextSnippets = Object.keys(textSnippets)
+  .filter((key) => !textSnippets[key].group && !textSnippets[key].hidden)
+  .map((key) => S.listItem(createTextSnippetItem(key)))
+
+const textSnippetGroups = Object.keys(groups)
+  .filter((it) => !groups[it].hidden)
+  .map((it) => S.listItem(createTextSnippetGroupItem(groups[it])))
+
+const textSnippetItems = [...ungroupedTextSnippets, S.divider(), ...textSnippetGroups]
+
+function createTextSnippetGroupItem(group) {
+  return {
+    title: group.title,
+    icon: () => EdsIcon(text_field),
+    id: `snippet-group-${group.title.split(' ').join('-')}`,
+    child: () =>
+      S.list('textSnippet')
+        .id('textSnippet')
+        .title(group.title)
+        .items(
+          Object.keys(textSnippets)
+            .filter((it) => textSnippets[it].group === group)
+            .map((it) => S.listItem(createTextSnippetItem(it))),
+        ),
+  }
+}
+
+function createTextSnippetItem(key) {
+  return {
     title: textSnippets[key].title,
     displayOptions: {
       showIcon: false,
@@ -17,8 +45,8 @@ const textSnippetItems = Object.keys(textSnippets).map((key) =>
         .documentId(`text_snippet_${key}`)
         .title(`Text Snippet: ${key} `)
         .schemaType('textSnippet'),
-  }),
-)
+  }
+}
 
 export const TextSnippet = S.listItem()
   .icon(() => EdsIcon(text_field))
