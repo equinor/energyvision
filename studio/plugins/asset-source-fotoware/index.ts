8991635df2d0409c72c9a329eac6e5c7a009e74d
Fernando Lucchesi <fernando@lucalto.com>
2022-08-15 08:39:42 -0300

    🚩 Enable fotoware to production #1116

--- studio/plugins/asset-source-fotoware/index.ts
+++ studio/plugins/asset-source-fotoware/index.ts
@@ -1,11 +1,9 @@
 import FotowarePlugin from './src/FotowareAssetSource'
-import PlacholderAssetSource from './src/Placeholder'
 import Icon from './src/Icon'
-import { HAS_FOTOWARE } from '../../src/lib/datasetHelpers'
 
 export default {
   name: 'fotoware',
   title: 'Fotoware',
-  component: HAS_FOTOWARE ? FotowarePlugin : PlacholderAssetSource,
+  component: FotowarePlugin,
   icon: Icon,
 }
