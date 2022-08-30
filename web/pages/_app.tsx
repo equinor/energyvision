d51d70580da6bacda5cb2503e8fc3b159ddc501c
Fernando Lucchesi <fernando@lucalto.com>
2022-08-30 06:20:35 -0300

    ⚗️ Refactor installation of GTM #1224

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -54,6 +54,43 @@ const CookieBot = ({ locale }: { locale: string | undefined }) => (
   />
 )
 
+const GoogleConsentMode = () => (
+  <Script
+    id="gtag-consent"
+    data-cookieconsent="ignore"
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("consent", "default", {
+            ad_storage: "denied",
+            analytics_storage: "denied",
+            functionality_storage: "denied",
+            personalization_storage: "denied",
+            security_storage: "granted",
+            wait_for_update: 500,
+        });
+        gtag("set", "ads_data_redaction", true);
+      `,
+    }}
+  />
+)
+
+const GoogleTagManager = () => (
+  <Script
+    id="gtag-base"
+    dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');`,
+    }}
+  />
+)
+
 // Log errors to relevant services here
 const HandleBoundaryError = (error: Error, info: { componentStack: string }) => {
   console.error('ErrorBoundary caught error: ', error, info)
@@ -109,6 +146,12 @@ function MyApp({ Component, pageProps }: CustomAppProps): JSX.Element {
         <GlobalStyle />
         <SkipNavLink />
         {IS_LIVE && <CookieBot locale={router.locale} />}
+        {GTM_ID && (
+          <>
+            <GoogleConsentMode />
+            <GoogleTagManager />
+          </>
+        )}
         {getLayout(<Component {...pageProps} />)}
       </>
     </ErrorBoundary>
--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -4,48 +4,15 @@ import React from 'react'
 import { ServerStyleSheet } from 'styled-components'
 import { GTM_ID } from '../lib/gtm'
 
-const GoogleConsentMode = () => (
-  <script
-    data-cookieconsent="ignore"
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag("consent", "default", {
-            ad_storage: "denied",
-            analytics_storage: "denied",
-            functionality_storage: "denied",
-            personalization_storage: "denied",
-            security_storage: "granted",
-            wait_for_update: 500,
-        });
-        gtag("set", "ads_data_redaction", true);
-      `,
-    }}
-  ></script>
-)
-
-const GoogleTagManagerHead = () => (
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');`,
-    }}
-  ></script>
-)
-
 const GoogleTagManagerBody = () => (
-  <noscript
-    dangerouslySetInnerHTML={{
-      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-    }}
-  ></noscript>
+  <noscript>
+    <iframe
+      src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+      height="0"
+      width="0"
+      style={{ display: 'none', visibility: 'hidden' }}
+    />
+  </noscript>
 )
 // We need this to support server-side rendering for styled components. It does create some issues with React 18 types
 // See https://github.com/vercel/next.js/issues/36008#issuecomment-1094073787 for the added fix
@@ -81,12 +48,6 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://eds-static.equinor.com/font/equinor-uprights-vf.css" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-          {GTM_ID && (
-            <>
-              <GoogleConsentMode />
-              <GoogleTagManagerHead />
-            </>
-          )}
         </Head>
         <body>
           {GTM_ID && <GoogleTagManagerBody />}
