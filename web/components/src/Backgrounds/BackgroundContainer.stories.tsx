8bb2df2bc2e04f79e57ec3056eee3368777868ce
Sven van Steenis <SvSven@users.noreply.github.com>
2022-09-06 09:27:18 +0200

    🎨 improve BackgroundContainer story (#1158)

--- web/components/src/Backgrounds/BackgroundContainer.stories.tsx
+++ web/components/src/Backgrounds/BackgroundContainer.stories.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable */
 import { Story, Meta } from '@storybook/react'
-import { BackgroundContainer, BackgroundContainerProps } from '@components'
+import { BackgroundContainer, BackgroundContainerProps, Text, Heading } from '@components'
+import styled from 'styled-components'
+
+const Content = styled.div`
+  padding: 2em;
+`
 
 export default {
   title: 'Components/BackgroundContainer',
@@ -9,15 +14,23 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: `🚧 &nbsp; Work in progress to avoid duplicating the background colour styles. 
-        `,
+        component: `The background container is meant to be used for coloured backgrounds. The purpose is to consolidate the usage of background colours and avoid duplication.`,
       },
     },
   },
 } as Meta
 
 export const Default: Story<BackgroundContainerProps> = (args) => (
-  <BackgroundContainer {...args}>Content</BackgroundContainer>
+  <BackgroundContainer {...args}>
+    <Content>
+      <Heading>Content</Heading>
+      <Text>This is content inside a background container.</Text>
+      <Text>
+        Content that uses the <code>Text</code> and <code>Heading</code> components will automatically invert their text
+        colour if "Slate Blue" is selected as background colour.
+      </Text>
+    </Content>
+  </BackgroundContainer>
 )
 
 Default.storyName = 'Default'
