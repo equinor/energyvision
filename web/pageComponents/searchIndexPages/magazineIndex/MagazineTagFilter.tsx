eee700150f5f0de4195c9de09277ca37eb90af96
Padmaja <52911293+padms@users.noreply.github.com>
2022-10-03 14:22:02 +0530

    🐛 Wrong highlight on click (#1138)

--- web/pageComponents/searchIndexPages/magazineIndex/MagazineTagFilter.tsx
+++ web/pageComponents/searchIndexPages/magazineIndex/MagazineTagFilter.tsx
@@ -8,21 +8,25 @@ export type RefinementListProps = { tags: string[]; initiallyActive?: string } &
 export function MagazineTagFilter(props: RefinementListProps) {
   const { items, refine } = useMenu(props)
   const { refine: clear } = useClearRefinements()
+  const [active, setActive] = useState('')
   const { tags, initiallyActive } = props
   const tagLinks = tags.map((e) => ({
     href: '#',
     label: e,
-    active: e === items.find((it) => it.isRefined)?.value || initiallyActive === e,
+    active: e === items.find((it) => it.isRefined)?.value || initiallyActive === e || active === e,
   }))
+  console.log(items)
+  console.log(initiallyActive)
   return (
     <MagazineTagBar
       href="#"
       tags={tagLinks}
-      defaultActive={items.find((it) => it.isRefined) === undefined && !initiallyActive}
+      defaultActive={items.find((it) => it.isRefined) === undefined && !initiallyActive && active === 'ALL'}
       onClick={(value: string) => {
+        setActive(value)
         if (value === 'ALL') {
           clear()
-        } else {
+        } else if (value !== active) {
           refine(value)
         }
       }}
