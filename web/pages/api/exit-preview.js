9a727ce35c3daceb7301abfbbb2e8f0f8d9f231c
Fernando Lucchesi <fernando@lucalto.com>
2022-10-24 08:27:35 -0300

    🐛 Refactor preview redirect url

--- web/pages/api/exit-preview.js
+++ web/pages/api/exit-preview.js
@@ -1,5 +1,5 @@
 export default function exit(req, res) {
   res.clearPreviewData()
 
-  res.writeHead(307, { Location: req?.query?.slug ?? `/` })
+  res.writeHead(307, { Location: `/${req?.query?.slug ?? ''}` })
 }
