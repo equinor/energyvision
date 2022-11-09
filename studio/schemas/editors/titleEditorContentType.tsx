5d20d0a6a72ab3338887e58d63c18d2da5fb4c58
Padmaja <52911293+padms@users.noreply.github.com>
2022-11-09 11:32:16 +0530

    🚩 Remove feature flag (#741)

--- studio/schemas/editors/titleEditorContentType.tsx
+++ studio/schemas/editors/titleEditorContentType.tsx
@@ -3,7 +3,6 @@ import { SuperScriptRenderer, SubScriptRenderer, StrikeThroughRenderer } from '.
 import { IconSuperScript, IconSubScript, EdsIcon } from '../../icons'
 import type { BlockFieldType } from '../../types/schemaTypes'
 import { format_strikethrough } from '@equinor/eds-icons'
-import { Flags } from '../../src/lib/datasetHelpers'
 
 // TODO: Add relevant styles for titles (i.e. highlighted text)
 export const configureTitleBlockContent = (): BlockFieldType => {
@@ -14,18 +13,16 @@ export const configureTitleBlockContent = (): BlockFieldType => {
     lists: [],
     marks: {
       decorators: [
-        Flags.IS_DEV ? { title: 'Strong', value: 'strong' } : undefined,
-        Flags.IS_DEV ? { title: 'Emphasis', value: 'em' } : undefined,
-        Flags.IS_DEV
-          ? {
-              title: 'Strikethrough',
-              value: 's',
-              blockEditor: {
-                icon: EdsIcon(format_strikethrough),
-                render: StrikeThroughRenderer,
-              },
-            }
-          : undefined,
+        { title: 'Strong', value: 'strong' },
+        { title: 'Emphasis', value: 'em' },
+        {
+          title: 'Strikethrough',
+          value: 's',
+          blockEditor: {
+            icon: EdsIcon(format_strikethrough),
+            render: StrikeThroughRenderer,
+          },
+        },
         {
           title: 'Sub',
           value: 'sub',
@@ -42,7 +39,7 @@ export const configureTitleBlockContent = (): BlockFieldType => {
             render: SuperScriptRenderer,
           },
         },
-      ].filter((e) => e),
+      ],
       annotations: [],
     },
   }
