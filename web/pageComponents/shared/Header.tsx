4e343cee75ebcd7dbfc7fa2d51bf92e42fd7d19f
Malin J <millianapia@gmail.com>
2022-11-15 12:51:56 +0100

    🚩Remove featureflag #1366

--- web/pageComponents/shared/Header.tsx
+++ web/pageComponents/shared/Header.tsx
@@ -173,7 +173,7 @@ const Header = ({ slugs, menuData }: HeaderProps) => {
               <ControlChild>
                 <NextLink href="/search" passHref legacyBehavior>
                   <StyledSearchButton variant="ghost_icon" aria-expanded="true" aria-label="Search">
-                    <Icon size={Flags.IS_DEV ? 24 : 32} data={search} />
+                    <Icon size={24} data={search} />
                   </StyledSearchButton>
                 </NextLink>
               </ControlChild>
