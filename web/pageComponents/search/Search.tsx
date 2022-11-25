e274d0ea2b536cccdc99200c18b09b7033e362d8
Sven van Steenis <SvSven@users.noreply.github.com>
2022-11-25 11:08:53 +0100

    🔥 remove commented out code (#1006)

--- web/pageComponents/search/Search.tsx
+++ web/pageComponents/search/Search.tsx
@@ -1,12 +1,10 @@
 import { InstantSearch, Configure } from 'react-instantsearch-hooks-web'
 import { searchClient } from '../../lib/algolia'
 import { useRouter } from 'next/router'
-//import { history } from 'instantsearch.js/es/lib/routers/index.js'
 import dynamic from 'next/dynamic'
 import { Flags } from '../../common/helpers/datasetHelpers'
 
 import { SearchBox } from './SearchBox'
-//import SearchResults from './SearchResults'
 import { getIsoFromLocale } from '../../lib/localization'
 import { SearchContextProvider } from './SearchContext'
 
@@ -24,51 +22,11 @@ const Search = () => {
   const envPrefix = Flags.IS_GLOBAL_PROD ? 'prod' : 'dev'
   const isoCode = getIsoFromLocale(router.locale)
 
-  // Don't know if this is 100% correct
-  //const url = router.asPath
-
   // The main index will be "all" at some point
   const mainIndex = `${envPrefix}_TOPICS_${isoCode}`
 
   return (
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={mainIndex}
-      /*  routing={{
-        // @TODO If this is enabled, the app will freeze with browser back
-        router: history({
-          getLocation() {
-            if (typeof window === 'undefined') {
-              return new URL(url!) as unknown as Location
-            }
-
-            return window.location
-          },
-        }),
-
-        stateMapping: {
-          // eslint-disable-next-line
-          // @ts-ignore: @TODO: The types are not correct
-          stateToRoute(uiState) {
-            const indexUiState = uiState[mainIndex]
-            return {
-              query: indexUiState.query,
-              tab: activeTabIndex,
-            }
-          },
-          // eslint-disable-next-line
-          // @ts-ignore: @TODO: The types are not correct
-          routeToState(routeState) {
-            return {
-              [mainIndex]: {
-                query: routeState.query,
-                tab: activeTabIndex,
-              },
-            }
-          },
-        },
-      }} */
-    >
+    <InstantSearch searchClient={searchClient} indexName={mainIndex}>
       <Configure hitsPerPage={5} snippetEllipsisText="..." />
       <SearchContextProvider>
         <SearchBox />
