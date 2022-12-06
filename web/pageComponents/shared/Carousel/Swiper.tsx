e2cf98374a3345246051d9c5bcc138532f3e9c3b
Sven van Steenis <SvSven@users.noreply.github.com>
2022-12-06 13:00:46 +0100

    🚧 add new Carousel web component (#1373, #1339)

---
+++ web/pageComponents/shared/Carousel/Swiper.tsx
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable import/no-unresolved */
+import { Children, ReactNode } from 'react'
+import { Swiper, SwiperSlide, type SwiperProps } from 'swiper/react'
+import { Pagination } from 'swiper'
+import styled, { css } from 'styled-components'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+const CardCarouselStyles = css`
+  max-width: calc(var(--card-maxWidth) * ${({ items }: any) => items || '3'} + var(--space-large) * 4);
+  padding: 0 var(--space-large);
+`
+
+// more to be added
+export type CarouselTypes = 'card'
+
+export type StyledSwiperTypes = {
+  $items?: number
+  $carouselType?: CarouselTypes
+} & SwiperProps
+
+const StyledSwiper = styled(Swiper)<StyledSwiperTypes>`
+  ${({ $carouselType }: any) => ($carouselType === 'card' ? CardCarouselStyles : '')}
+
+  .swiper-wrapper {
+    padding-bottom: var(--space-3xLarge);
+  }
+
+  .swiper-pagination {
+    bottom: 0;
+  }
+`
+
+const StyledSwiperSlide = styled(SwiperSlide)`
+  display: flex;
+  width: auto;
+  height: auto;
+  margin: 0 var(--space-large);
+
+  &:first-of-type {
+    margin: 0;
+  }
+
+  &:last-of-type {
+    margin: 0;
+  }
+`
+
+export type CarouselProps = {
+  slidesPerView?: number | 'auto'
+  type?: CarouselTypes
+  children: any
+}
+
+export const Carousel = ({ slidesPerView = 'auto', type = 'card', children, ...rest }: CarouselProps) => {
+  const numberOfItems = Children.toArray(children).length
+
+  return (
+    <StyledSwiper
+      pagination={{
+        dynamicBullets: true,
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      slidesPerView={slidesPerView}
+      $items={numberOfItems}
+      $carouselType={type}
+      {...rest}
+    >
+      {children}
+    </StyledSwiper>
+  )
+}
+
+export type CarouselItemProps = {
+  children: ReactNode
+}
+
+export const CarouselItem = ({ children, ...rest }: CarouselItemProps) => {
+  return <StyledSwiperSlide {...rest}>{children}</StyledSwiperSlide>
+}
+// This is required in order for the Swiper library to correctly render
+CarouselItem.displayName = 'SwiperSlide'
