ce359fc99c2d1355bcf3868d25e75adb0194e978
Sven van Steenis <SvSven@users.noreply.github.com>
2022-12-09 09:44:02 +0100

    💡 add comment about Carousel location in code (#1373, #1339)

--- web/pageComponents/shared/Carousel/index.ts
+++ web/pageComponents/shared/Carousel/index.ts
@@ -1,1 +1,10 @@
+/**
+ * This should ideally be in web/components/src/Carousel instead of in web/pageComponents/Carousel
+ * However, it would seem that the Swiper.js library and Jest do not get along nicely
+ * See: https://github.com/nolimits4web/swiper/discussions/4969
+ * I have tried some of the suggestions in that discussion without much success. Due to the structure of our code, using
+ * the transformIgnorePatterns causes other issues, since we are exporting all components from web/components/src/index.ts
+ * and we can't selectively ignore just the export line for the Carousel there
+ */
+
 export * from './Swiper'
