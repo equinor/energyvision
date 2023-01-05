50216b179a4ba0e0ac6a4c887a5ea9a8e9c68a6b
Sven van Steenis <SvSven@users.noreply.github.com>
2023-01-05 13:39:44 +0100

    ✨ add horizontal scroll to latest news section (#1373)

--- web/pageComponents/news/LatestNews.tsx
+++ web/pageComponents/news/LatestNews.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 import NewsCard from '../cards/NewsCard'
 import type { CardData } from '../../types/types'
 import { FormattedMessage } from 'react-intl'
+import { HorizontalScroll, HorizontalScrollItem } from '../shared/Carousel'
+import useWindowSize from '../../lib/hooks/useWindowSize'
+import { Flags } from '../../common/helpers/datasetHelpers'
 
 const Wrapper = styled.div`
   display: grid;
@@ -18,8 +21,24 @@ const Wrapper = styled.div`
   }
 `
 
+const DevWrapper = styled.div`
+  --card-maxWidth: 400px;
+
+  display: flex;
+  gap: var(--space-large);
+  justify-content: center;
+  align-content: center;
+`
+
 const StyledHeading = styled(Heading)`
   margin: var(--space-xLarge);
+`
+
+const StyledNewsCard = styled(NewsCard)`
+  min-width: var(--card-minWidth);
+  max-width: var(--card-maxWidth);
+  flex-basis: 0;
+  flex-grow: 1;
 `
 
 type LatestNewsProp = {
@@ -27,11 +46,41 @@ type LatestNewsProp = {
 }
 
 const LatestNews = ({ data }: LatestNewsProp) => {
+  const { width } = useWindowSize()
+  const renderScroll = Boolean(width && width <= 800)
+
+  if (Flags.IS_DEV) {
+    return (
+      <>
+        <StyledHeading size="xl" level="h2" center>
+          <FormattedMessage id="latest_news" defaultMessage="Latest News" />
+        </StyledHeading>
+
+        {renderScroll ? (
+          <HorizontalScroll type="card">
+            {data.map((newsItem: CardData) => (
+              <HorizontalScrollItem key={newsItem.id}>
+                <StyledNewsCard data={newsItem} key={newsItem.id} />
+              </HorizontalScrollItem>
+            ))}
+          </HorizontalScroll>
+        ) : (
+          <DevWrapper>
+            {data.map((newsItem: CardData) => {
+              return <StyledNewsCard data={newsItem} key={newsItem.id} />
+            })}
+          </DevWrapper>
+        )}
+      </>
+    )
+  }
+
   return (
     <>
       <StyledHeading size="xl" level="h2" center>
         <FormattedMessage id="latest_news" defaultMessage="Latest News" />
       </StyledHeading>
+
       <Wrapper>
         {data.map((newsItem: CardData) => {
           return <NewsCard data={newsItem} key={newsItem.id} />
