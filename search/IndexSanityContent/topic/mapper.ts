711dbb8d0f68aca47914c8dbb08633b3240262fd
Fernando Lucchesi <fernando@lucalto.com>
2023-01-16 08:48:57 -0300

    ✏️ Fix typo in search index for topic id

--- search/IndexSanityContent/topic/mapper.ts
+++ search/IndexSanityContent/topic/mapper.ts
@@ -1,9 +1,9 @@
+import * as A from 'fp-ts/lib/Array'
 import { pipe } from 'fp-ts/lib/function'
 import { ap } from 'fp-ts/lib/Identity'
-import * as A from 'fp-ts/lib/Array'
 import * as O from 'fp-ts/lib/Option'
-import { TopicPage } from './sanity'
 import { TopicIndex } from '../../common'
+import { TopicPage } from './sanity'
 
 type MappableObjectType = {
   _key: string
@@ -17,7 +17,7 @@ const mapperFunction: MapperFunctionType =
   (page) =>
   ({ _key, title, ingress, text }) => ({
     slug: page.slug,
-    objectID: `${page._id}- ${_key}`,
+    objectID: `${page._id}-${_key}`,
     type: 'page',
     pageTitle: page.title,
     title,
@@ -27,22 +27,19 @@ const mapperFunction: MapperFunctionType =
 
 type MapDataType = (page: TopicPage) => TopicIndex[]
 export const mapData: MapDataType = (page) =>
-  pipe(
-    mapperFunction,
-    ap(page),
-    (fn) =>
+  pipe(mapperFunction, ap(page), (fn) =>
+    pipe(
       pipe(
+        O.fromNullable(page.textBlocks),
+        O.map(A.map(fn)),
+        O.getOrElse(() => [] as TopicIndex[]),
+      ),
+      A.concat(
         pipe(
-          O.fromNullable(page.textBlocks),
+          O.fromNullable(page.accordions),
           O.map(A.map(fn)),
           O.getOrElse(() => [] as TopicIndex[]),
         ),
-        A.concat(
-          pipe(
-            O.fromNullable(page.accordions),
-            O.map(A.map(fn)),
-            O.getOrElse(() => [] as TopicIndex[]),
-          ),
-        ),
       ),
+    ),
   )
