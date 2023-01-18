d5cc67d8494689483681a6ad7b61b094cb371470
Fernando Lucchesi <fernando@lucalto.com>
2023-01-18 07:55:20 -0300

    🚧 Setup sanity v3 repo with SSO login #1403

---
+++ sanityv3/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'h61q9gi9',
+    dataset: 'global-development'
+  }
+})
---
+++ sanityv3/sanity.config.ts
@@ -0,0 +1,33 @@
+import {visionTool} from '@sanity/vision'
+import {createAuthStore, defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Equinor',
+
+  projectId: 'h61q9gi9',
+  dataset: 'global-development',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+
+  auth: createAuthStore({
+    projectId: 'h61q9gi9',
+    dataset: 'global-development',
+    mode: 'replace',
+    redirectOnSingle: true,
+    providers: [
+      {
+        name: 'saml',
+        title: 'Equinor SSO',
+        url: 'https://api.sanity.io/v2021-10-01/auth/saml/login/55ba173c',
+        logo: '/static/favicon.ico',
+      },
+    ],
+  }),
+})
