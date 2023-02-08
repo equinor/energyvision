3ffc78d67a0dc58f710c4298ed455f33431d870b
Padmaja <52911293+padms@users.noreply.github.com>
2023-02-08 16:17:33 +0530

    ♻️ Convert timezoneinput to v3 (#1504)

--- sanityv3/schemas/components/TimezoneInput.tsx
+++ sanityv3/schemas/components/TimezoneInput.tsx
@@ -1,57 +1,26 @@
 import React, { forwardRef } from 'react'
 
 import { useId } from '@reach/auto-id'
-import { Select, TextInput, useForwardedRef } from '@sanity/ui'
-import { FormField } from 'sanity'
+import { Select } from '@sanity/ui'
 import timezones from '../../helpers/timezones'
 // eslint-disable-next-line import/no-unresolved
-import { PatchEvent, set, unset } from 'sanity'
+import { set, unset, StringInputProps } from 'sanity'
 
-type Props = {
-  value?: string
-  markers: any[]
-  presence: any
-  onChange: (event: any) => void
-  readOnly?: boolean
-  type: {
-    name: string
-    title: string
-    description?: string
-    placeholder?: string
-  }
-}
-
-const TimezoneInput = forwardRef((props: Props, forwardedRef: React.ForwardedRef<HTMLSelectElement>) => {
-  const { presence, type, readOnly, markers, value, onChange } = props
-  const ref = useForwardedRef(forwardedRef)
+const TimezoneInput = forwardRef((props: StringInputProps) => {
+  const { value = '', onChange, elementProps } = props
   const id = useId()
-
   const handleChange = (event: React.FormEvent<HTMLSelectElement>) => {
-    onChange(createPatchFrom(event.currentTarget.value))
+    onChange(event.currentTarget.value === '' ? unset() : set(event.currentTarget.value))
   }
-
-  const createPatchFrom = (value: string) => PatchEvent.from(value === '' ? unset() : set(value))
 
   return (
-    <FormField
-      __unstable_markers={markers}
-      __unstable_presence={presence}
-      inputId={id}
-      title={type.title}
-      description={type.description}
-    >
-      {readOnly ? (
-        <TextInput readOnly value={value} />
-      ) : (
-        <Select id={id} value={value} ref={ref} onChange={handleChange}>
-          {timezones.map((tz) => (
-            <option key={tz} value={tz}>
-              {tz.replace(/_/g, ' ')}
-            </option>
-          ))}
-        </Select>
-      )}
-    </FormField>
+    <Select {...elementProps} id={id} value={value} onChange={handleChange}>
+      {timezones.map((tz) => (
+        <option key={tz} value={tz}>
+          {tz.replace(/_/g, ' ')}
+        </option>
+      ))}
+    </Select>
   )
 })
 
