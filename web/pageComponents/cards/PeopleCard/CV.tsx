cb65e03b0ef3ef10030c3840e8666de061c02c5d
Malin J <millianapia@gmail.com>
2023-02-20 11:20:29 +0100

    🚩 Remove feature flag #1434

--- web/pageComponents/cards/PeopleCard/CV.tsx
+++ web/pageComponents/cards/PeopleCard/CV.tsx
@@ -3,7 +3,6 @@ import { ButtonLink } from '@components'
 import type { LinkData } from '../../../types/types'
 import { getUrlFromAction } from '../../../common/helpers/getUrlFromAction'
 import { getLocaleFromName } from '../../../lib/localization'
-import { Flags } from '../../../common/helpers/datasetHelpers'
 
 type CVProps = {
   data: LinkData
@@ -17,7 +16,7 @@ const CV = ({ data }: CVProps) => {
   return (
     <>
       {type === 'internalUrl' ? (
-        <NextLink href={url || ''} locale={Flags.IS_DEV ? locale : undefined} passHref legacyBehavior>
+        <NextLink href={url || ''} locale={locale} passHref legacyBehavior>
           <ButtonLink aria-label={ariaLabel}>{label}</ButtonLink>
         </NextLink>
       ) : (
