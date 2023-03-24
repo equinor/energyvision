2106b8e104921ac259ac2ae8ec872c829a64fb1f
Fernando Lucchesi <fernando@lucalto.com>
2023-03-24 17:14:34 -0300

    🔥 Remove code that was used for experiment #1510

--- studio/plugins/asset-source-mediabank/index.ts
+++
@@ -1,9 +0,0 @@
-import Brandmaster from './src/BrandmasterAssetSource'
-import Icon from './src/Icon'
-
-export default {
-  name: 'mediabank',
-  title: 'Media Bank',
-  component: Brandmaster,
-  icon: Icon,
-}
