11d93bce54e14ddab43f40120670e274763cb66d
Sven van Steenis <SvSven@users.noreply.github.com>
2023-03-24 14:14:27 +0100

    🚧 attempt custom validation for breadcrumbs (#1501)

--- studio/schemas/objects/breadcrumbs.tsx
+++ studio/schemas/objects/breadcrumbs.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import sanityClient from 'part:@sanity/base/client'
-import { SanityDocument } from '@sanity/types'
+import { CustomValidator, SanityDocument, Rule, SlugParent, SlugSchemaType } from '@sanity/types'
 import slugify from 'slugify'
 import SlugInput from '../components/SlugInput'
 
@@ -37,5 +37,36 @@ export function breadcrumbs(source = `breadcrumbsInput`, fieldset: string) {
       slugify: formatBreadcrumbs,
       isUnique: validateIsUniqueWithinLocale,
     },
+    validation: (Rule: Rule) => Rule.custom((value, context) => slugValidator(value, context)),
   }
+}
+
+export const slugValidator: CustomValidator = async (value, context) => {
+  if (!value) {
+    return true
+  }
+  if (typeof value !== 'object') {
+    return 'Breadcrumbs must be an object'
+  }
+
+  const slugValue = (value as { current?: string }).current
+  if (!slugValue) {
+    return 'Breadcrumbs must have a value'
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const slugContext: SlugValidationContext = {
+    ...context,
+    parent: context.parent as SlugParent,
+    type: context.type as SlugSchemaType,
+    defaultIsUnique: validateIsUniqueWithinLocale,
+  }
+
+  const wasUnique = await validateIsUniqueWithinLocale(slugValue, slugContext)
+  if (wasUnique) {
+    return true
+  }
+
+  return 'Breadcrumbs already in use'
 }
