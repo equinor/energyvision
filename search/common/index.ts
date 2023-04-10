9f4bd3a251335f3c6efd0619896de70658163423
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-10 17:31:50 +0800

    Delete search directory

--- search/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { update, generateIndexName, remove } from './algolia'
-export { getSanityClient } from './sanity'
-export { getEnvironment } from './env'
-export { Language, languageFromInternalCode, languageFromIso, languageOrDefault } from './language'
-export { BlockNode, EventIndex, TopicIndex, NewsIndex, MagazineIndex, IndexType, Page } from './types'
