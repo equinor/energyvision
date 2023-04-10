a4964900c51a331f2a1f4269ab93d01616ffb744
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-10 14:32:13 +0800

    Delete e2e directory

--- e2e/energyVision.js
+++
@@ -1,3 +0,0 @@
-export const envisStartPage = 'http://localhost:3000'
-export const careers = 'http://localhost:3000/careers'
-export const pettersTestPage = 'http://localhost:3000/news/petters-test-slug'
--- e2e/seleniumConfig.js
+++
@@ -1,5 +0,0 @@
-export const seleniumConfig = {
-  "browserstack.selenium_version" : "4.0.0-beta-1",
-  "browserstack.networkLogs" : true,
-  "browserstack.console" : "errors"
-}
