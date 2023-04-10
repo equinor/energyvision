e8dc7d774e285edbfcb1ca5ccf6ebc5703e8e8fb
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-10 17:31:30 +0800

    Delete sanityv3 directory

--- sanityv3/sanity.client.ts
+++
@@ -1,8 +0,0 @@
-import client from '@sanity/client'
-
-export const sanityClient = client({
-  projectId: 'h61q9gi9',
-  dataset: 'global-development',
-  apiVersion: '2023-01-01',
-  useCdn: false,
-})
--- sanityv3/src/lib/datasetHelpers.ts
+++
@@ -1,6 +0,0 @@
-import FeatureFlags from '../../../FeatureFlags'
-
-export const projectId = 'h61q9gi9'
-export const dataset = 'global-development'
-export const IS_SECRET = false //projectId === 'w3b4om14'
-export const Flags = FeatureFlags(dataset)
