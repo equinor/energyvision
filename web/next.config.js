3a91d71a186ff6d297724ec83f0fdaa679216d18
Padmaja <52911293+padms@users.noreply.github.com>
2023-04-12 18:12:55 +0530

    🚩 Switch to new storage account (#1536)

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,4 @@
-const archiveServerHostname =
-  process.env.ARCHIVE_CONTENT_LINK || 'https://envis-legacy.azureedge.net/equinor-archive-content'
+const archiveServerHostname = process.env.NEXT_PUBLIC_ARCHIVE_CONTENT_LINK
 /* eslint-disable @typescript-eslint/no-var-requires */
 import withBundleAnalyzer from '@next/bundle-analyzer'
 import nextTranspileModules from 'next-transpile-modules'
