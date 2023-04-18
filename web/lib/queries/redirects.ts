3b01fdcffa4840bc6cedcac06ff9a7c3d6712416
Fernando Lucchesi <fernando@lucalto.com>
2023-04-18 10:07:51 -0300

    🐛 Fix redirects ending with .html #1599

--- web/lib/queries/redirects.ts
+++ web/lib/queries/redirects.ts
@@ -9,15 +9,17 @@ export type ExternalRedirectsType = {
   to: string
 } | null
 
+const from = /* groq */ `(from == $slug || from == $slug + '.html' || from == $slugWithLocale || from == $slugWithLocale + '.html')`
+
 export const redirects = /* groq */ `
-  *[_type == "redirect" && (from == $slug || from == $slugWithLocale) && ${noDrafts}][0]{
+  *[_type == "redirect" && ${from} && ${noDrafts}][0]{
     "lang": _lang,
     "to": to->slug.current
   }
 `
 
 export const externalRedirects = /* groq */ `
-  *[_type == "externalRedirect" && (from == $slug || from == $slugWithLocale) && ${noDrafts}][0]{
+  *[_type == "externalRedirect" && ${from} && ${noDrafts}][0]{
     to
   }
 `
