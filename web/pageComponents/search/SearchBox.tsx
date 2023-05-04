b344994cc7bcd0f4520623e2334a83a28a3237b9
Padmaja <52911293+padms@users.noreply.github.com>
2023-05-04 11:29:33 +0530

    ♻️ Use queryHook prop to debounce (#1616)

--- web/pageComponents/search/SearchBox.tsx
+++ web/pageComponents/search/SearchBox.tsx
@@ -2,42 +2,43 @@ import { useEffect, useRef, useState, ChangeEvent, ComponentProps, useContext } 
 import { useSearchBox, UseSearchBoxProps, UseSortByProps } from 'react-instantsearch-hooks-web'
 import ControlledSearchBox from './ControlledSearchBox'
 import { SearchContext } from './SearchContext'
-import useDebounce from '../../lib/hooks/useDebounce'
 
 const DEBOUNCE_TIME = 300
 
 export type SearchBoxProps = ComponentProps<'div'> & UseSearchBoxProps
 
+let timerId: any = undefined
+
+const queryHook: UseSearchBoxProps['queryHook'] = (query, search) => {
+  if (timerId) {
+    clearTimeout(timerId)
+  }
+  timerId = setTimeout(() => search(query), DEBOUNCE_TIME)
+}
+
 export function SearchBox(props: SearchBoxProps) {
   // I don't think we need iSearchStalled when we don't have a manual reset button and/or loading
   // spinner if search is slow? Do we need a spinner if this happens?
-  const { query, refine /* isSearchStalled */, clear } = useSearchBox(props)
+  const { query, refine /* isSearchStalled */, clear } = useSearchBox({ ...props, queryHook })
   const [value, setValue] = useState(query)
-  const debouncedValue = useDebounce<string>(value, DEBOUNCE_TIME)
   const { userTyped, setUserTyped } = useContext(SearchContext)
   const inputRef = useRef<HTMLInputElement>(null)
 
   function onReset() {
     setValue('')
+    clear()
     setUserTyped(false)
   }
 
   function onChange(event: ChangeEvent<HTMLInputElement>) {
     setUserTyped(true)
     setValue(event.currentTarget.value)
+    refine(event.currentTarget.value)
   }
 
   useEffect(() => {
-    if (query !== debouncedValue) {
-      if (debouncedValue.length === 0) {
-        if (userTyped) clear()
-        else refine(query)
-      } else refine(debouncedValue)
-    }
-    // We want to track when the value coming from the React state changes
-    // to update the InstantSearch.js query, so we don't need to track the
-    // InstantSearch.js query.
-  }, [debouncedValue, query])
+    setValue(query)
+  }, [query])
 
   return (
     <ControlledSearchBox
