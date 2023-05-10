621357cf312c180a5572cd2e1c8cc2c40b3e66ad
Sven van Steenis <SvSven@users.noreply.github.com>
2023-05-10 15:46:12 +0200

    ✨ add Brandmaster plugin to v3 (#1633)

---
+++ sanityv3/plugins/asset-source-brandmaster/index.ts
@@ -0,0 +1,9 @@
+import Brandmaster from './src/BrandmasterAssetSource'
+import Icon from './src/Icon'
+
+export default {
+  name: 'brandmaster',
+  title: 'Brandmaster',
+  component: Brandmaster,
+  icon: Icon,
+}
