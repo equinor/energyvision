3424f773f1f6643d74f3958bcaf28057249859d6
Padmaja <52911293+padms@users.noreply.github.com>
2023-05-19 20:27:31 +0530

    ⚡️ Optimize thumbnail for video (#1643)

--- web/pageComponents/shared/Hero/LoopingVideo.tsx
+++ web/pageComponents/shared/Hero/LoopingVideo.tsx
@@ -1,7 +1,11 @@
 import { HLSPlayer } from '@components'
+import { Flags } from '../../../common/helpers/datasetHelpers'
+import { useSanityLoader } from '../../../lib/hooks/useSanityLoader'
 import styled from 'styled-components'
 import { urlFor } from '../../../common/helpers'
 import { LoopingVideoData, LoopingVideoRatio } from '../../../types'
+
+const DEFAULT_MAX_WIDTH = 1440
 
 const StyledFigure = styled.figure<{ ratio: LoopingVideoRatio }>`
   justify-content: center;
@@ -21,9 +25,18 @@ export const LoopingVideo = ({ video }: { video: LoopingVideoData }) => {
     width: '100%',
     objectFit: ratio === 'narrow' ? 'cover' : undefined,
   }
+  const thumbnailURL = useSanityLoader(thumbnail, DEFAULT_MAX_WIDTH, undefined)
   return (
     <StyledFigure ratio={ratio}>
-      <HLSPlayer loop muted autoPlay title={title} poster={urlFor(thumbnail).url()} src={url} style={style} />
+      <HLSPlayer
+        loop
+        muted
+        autoPlay
+        title={title}
+        poster={Flags.IS_DEV ? thumbnailURL.src : urlFor(thumbnail).url()}
+        src={url}
+        style={style}
+      />
     </StyledFigure>
   )
 }
--- web/pageComponents/shared/SanityImage.tsx
+++ web/pageComponents/shared/SanityImage.tsx
@@ -1,8 +1,6 @@
-import { useNextSanityImage } from 'next-sanity-image'
+import { useSanityLoader } from '../../lib/hooks/useSanityLoader'
 import Img, { ImageProps } from 'next/image'
-
 import type { ImageWithAlt } from 'types'
-import { sanityClientWithEquinorCDN } from '../../lib/sanity.server'
 
 type Props = Omit<ImageProps, 'src' | 'alt'> & {
   image: ImageWithAlt
@@ -23,20 +21,6 @@ export enum Ratios {
 
 const DEFAULT_SIZES = '(max-width: 800px) 100vw, 800px'
 const DEFAULT_MAX_WIDTH = 1440
-
-const useSanityLoader = (image: ImageWithAlt, maxWidth: number, aspectRatio: number | undefined) =>
-  useNextSanityImage(sanityClientWithEquinorCDN, image, {
-    imageBuilder: (imageUrlBuilder, options) => {
-      const { width: imageWidth, croppedImageDimensions: cropped } = options
-      // We do not want to allow gigantic images to exist due to performance
-      const width = Math.round(imageWidth || Math.min(maxWidth, cropped.width))
-      const height = aspectRatio
-        ? Math.round(width * aspectRatio)
-        : Math.round(width * (cropped.height / cropped.width))
-
-      return imageUrlBuilder.width(width).height(height).auto('format').quality(70)
-    },
-  })
 
 const Image = ({
   image,
