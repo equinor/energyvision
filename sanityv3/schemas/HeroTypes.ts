96354eec05436cd5a83dd7d4ba53f44159d23bbd
Malin J <millianapia@gmail.com>
2023-06-14 16:08:13 +0200

    🚧  Full width video #1649

--- sanityv3/schemas/HeroTypes.ts
+++ sanityv3/schemas/HeroTypes.ts
@@ -2,5 +2,7 @@ export enum HeroTypes {
   DEFAULT = 'default',
   FIFTY_FIFTY = 'fiftyFifty',
   FULL_WIDTH_IMAGE = 'fullWidthImage',
+  FULL_WIDTH_VIDEO = 'fullWidthImage',
+
   LOOPING_VIDEO = 'loopingVideo',
 }
