3a956b053d9d7b36e088119afaf5fb811db817b0
Fernando Lucchesi <fernando@lucalto.com>
2023-06-28 10:23:46 -0300

    ✨ Properly set video ratios #1649

--- web/pageComponents/topicPages/FullWidthVideo.tsx
+++ web/pageComponents/topicPages/FullWidthVideo.tsx
@@ -2,33 +2,46 @@ import { FullWidthVideoData, FullWidthVideoRatio } from '../../types/types'
 import styled from 'styled-components'
 import { BackgroundContainer, HLSPlayer } from '@components'
 
-const Container = styled.div`
-  max-width: var(--iframe-maxWidth, var(--maxViewportWidth));
-  margin: auto;
+const Container = styled.div<{ $aspectRatio: FullWidthVideoRatio }>`
+  position: relative;
+  width: 100%;
+  ${({ $aspectRatio }) => {
+    switch ($aspectRatio) {
+      case 'fullScreen':
+        return 'height: 100vh;'
+      case 'narrow':
+        return `
+          padding-bottom: 75%;
+          @media(min-width: 750px) {
+            padding-bottom: 30%;
+          }
+        `
+      case '2:1':
+        return 'padding-bottom: 50%;'
+      default:
+        return 'padding-bottom: 50%;'
+    }
+  }}
 `
 
 const StyledFigure = styled.figure`
   justify-content: center;
   display: flex;
   margin: 0;
-  background-color: transparent;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
 `
 
-const StyledHLSPlayer = styled(HLSPlayer)<{ $aspectRatio?: FullWidthVideoRatio }>`
-  object-fit: cover;
+const StyledHLSPlayer = styled(HLSPlayer)`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
-  height: ${({ $aspectRatio }) => {
-    switch ($aspectRatio) {
-      case 'narrow':
-        return '30vh'
-      case 'fullScreen':
-        return '100vh'
-      case '2:1':
-        return '50%'
-      default:
-        return '50%'
-    }
-  }};
+  height: 100%;
+  object-fit: cover;
 `
 
 const FullWidthVideo = ({ anchor, data }: { data: FullWidthVideoData; anchor?: string }) => {
@@ -36,17 +49,9 @@ const FullWidthVideo = ({ anchor, data }: { data: FullWidthVideoData; anchor?: s
   const { background, aspectRatio } = designOptions
   return (
     <BackgroundContainer background={background} id={anchor}>
-      <Container style={spacing ? { marginTop: '50px', marginBottom: '50px' } : {}}>
+      <Container $aspectRatio={aspectRatio} style={spacing ? { marginTop: '50px', marginBottom: '50px' } : {}}>
         <StyledFigure>
-          <StyledHLSPlayer
-            $aspectRatio={aspectRatio}
-            src={video.url}
-            title={video.title}
-            playsInline
-            autoPlay
-            muted
-            loop
-          />
+          <StyledHLSPlayer src={video.url} title={video.title} playsInline autoPlay muted loop />
         </StyledFigure>
       </Container>
     </BackgroundContainer>
