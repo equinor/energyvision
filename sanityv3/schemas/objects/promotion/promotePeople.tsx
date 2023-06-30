0b8ad985382d4ab640eb98dc69c2a291837cd855
Padmaja <52911293+padms@users.noreply.github.com>
2023-06-30 16:10:00 +0530

    🐛 Typo #1270

--- sanityv3/schemas/objects/promotion/promotePeople.tsx
+++ sanityv3/schemas/objects/promotion/promotePeople.tsx
@@ -37,7 +37,7 @@ export default {
           type: 'object',
           name: 'people',
           title: 'People',
-          fieldset: [
+          fieldsets: [
             {
               name: 'seo',
               title: 'SEO',
