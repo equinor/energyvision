e8ca13491f739eee69f5a782ecca468f8e1b56e8
Malin J <millianapia@gmail.com>
2023-11-07 13:55:57 +0100

    ♻️ New icon for x #1959

--- web/pageComponents/icons/Twitter.tsx
+++ web/pageComponents/icons/Twitter.tsx
@@ -1,26 +1,25 @@
-const Twitter = ({ width = 42, height = 33 }: { width?: number; height?: number }) => {
+const Twitter = ({ width = 42, height = 42 }: { width?: number; height?: number }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 144.233 144.233">
-      <g id="Twitter_X_Icon" data-name="Twitter X Icon" transform="translate(-352.45 -128.77)">
+    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 963.6 963.6">
+      <g id="Twitter_X_Icon-equinor" data-name="Twitter X Icon-equinor" transform="translate(-352.5 -128.8)">
         <circle
           id="Ellipse_1"
           data-name="Ellipse 1"
-          cx="71.616"
-          cy="71.616"
-          r="71.616"
-          transform="translate(352.95 129.27)"
+          cx="481.3"
+          cy="481.3"
+          r="481.3"
+          transform="translate(353 129.3)"
+          fill="#fff"
           stroke="#fff"
           strokeMiterlimit="10"
           strokeWidth="1"
         />
-        <g id="layer1" transform="translate(380.235 159.394)">
-          <path
-            id="path1009"
-            d="M484.156,356.79l34.232,45.77L483.94,439.774h7.753l30.159-32.582,24.367,32.582H572.6l-36.159-48.344,32.064-34.639h-7.753L532.981,386.8,510.539,356.79Zm11.4,5.711h12.12L561.2,434.063H549.08Z"
-            transform="translate(-483.94 -356.79)"
-            fill="black"
-          />
-        </g>
+        <path
+          id="Path_1"
+          data-name="Path 1"
+          d="M902.9,565.2l224.9-243h-78L868.1,518.6,721.2,322.2h-201L756.9,638.7,516,899h78L791.8,685.3,951.5,899h201ZM692.5,379.6l345.5,462H980.3l-345.5-462h57.7Z"
+          fill="#243847"
+        />
       </g>
     </svg>
   )
