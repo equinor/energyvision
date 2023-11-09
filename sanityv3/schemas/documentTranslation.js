782f45169c573e3236d68dca68d9d729aca10b4b
Padmaja <52911293+padms@users.noreply.github.com>
2023-11-09 14:05:03 +0530

    🐛 Correct filter #1868

--- sanityv3/schemas/documentTranslation.js
+++ sanityv3/schemas/documentTranslation.js
@@ -3,11 +3,13 @@ import { Flags } from '../src/lib/datasetHelpers'
 import { documentsWithI18n } from './documents'
 
 const datasetSpecificSchemas = [
-  Flags.HAS_NEWS && documentsWithI18n.news.name,
-  Flags.HAS_LOCAL_NEWS && documentsWithI18n.localNews.name,
-  Flags.HAS_EVENT && documentsWithI18n.event.name,
-  Flags.HAS_MAGAZINE && documentsWithI18n.magazine.name,
-  Flags.HAS_MAGAZINE_INDEX && documentsWithI18n.magazineIndex.name,
+  !Flags.HAS_NEWS && documentsWithI18n.news.name,
+  !Flags.HAS_LOCAL_NEWS && documentsWithI18n.localNews.name,
+  !Flags.HAS_EVENT && documentsWithI18n.event.name,
+  !Flags.HAS_MAGAZINE && documentsWithI18n.magazine.name,
+  !Flags.HAS_MAGAZINE_INDEX && documentsWithI18n.magazineIndex.name,
+  !Flags.HAS_NEWSROOM && documentsWithI18n.newsroom.name,
+  !Flags.HAS_LANDING_PAGE && documentsWithI18n.landingPage.name,
 ].filter((e) => e)
 export const i18n = {
   supportedLanguages: languages.map((it) => {
@@ -19,6 +21,7 @@ export const i18n = {
   referenceBehavior: 'weak',
   languageField: 'lang',
   schemaTypes: Object.keys(documentsWithI18n)
-    .filter((it) => datasetSpecificSchemas.includes(documentsWithI18n[it].name))
+    .filter((it) => !datasetSpecificSchemas.includes(documentsWithI18n[it].name))
     .map((it) => documentsWithI18n[it].name),
+  withTranslationsMaintenance: true,
 }
