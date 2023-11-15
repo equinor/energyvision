443760acec81c95fdc88e5df90ba7d67f8d7554f
Fernando Lucchesi <fernando@lucalto.com>
2023-11-15 12:35:46 +0100

    ✨ Whitelist iframe url #1979

--- web/securityHeaders.js
+++ web/securityHeaders.js
@@ -26,6 +26,7 @@ const iframeSrcs = [
   'https://vds.issgovernance.com',
   'https://eac.plaii.no',
   'https://livestream.com',
+  'https://pixel.as/equinor',
   dataset === 'global-development' && 'https://equinor-gms1.wd3.myworkdayjobs-impl.com',
   dataset === 'global-development' && 'https://careers.peopleclick.eu.com',
   'https://h61q9gi9.api.sanity.io',
