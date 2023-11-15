5f46bac6cf4e204dd6393676fb7cc2b7b61afaeb
Malin J <millianapia@gmail.com>
2023-11-15 11:05:05 +0100

    ♻️ Small refactor for double linkreferences #1889

--- web/lib/queries/common/pageContentFields.ts
+++ web/lib/queries/common/pageContentFields.ts
@@ -4,7 +4,6 @@ import { videoPlayerFields } from '../videoPlayerFields'
 import downloadableFileFields from './actions/downloadableFileFields'
 import downloadableImageFields from './actions/downloadableImageFields'
 import linkSelectorFields, { linkReferenceFields } from './actions/linkSelectorFields'
-import { promoLinkReferenceFields } from './actions/promoLinkReferenceFields'
 import markDefs from './blockEditorMarks'
 import { eventPromotionFields, futureEventsQuery, pastEventsQuery } from './eventPromotion'
 import { imageCarouselFields } from './imageCarouselFields'
@@ -189,7 +188,7 @@ const pageContentFields = /* groq */ `
       "id": _key,
       ingress[]{..., ${markDefs}},
       linkLabelAsTitle,
-      "action": ${promoLinkReferenceFields},
+      "action": ${linkSelectorFields},
       "designOptions": {
         "background": coalesce(background.title, 'none'),
       },
