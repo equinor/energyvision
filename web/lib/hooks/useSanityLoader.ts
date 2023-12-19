1f9c6d1f179a6ec98abce91bb169bbac3051879b
Padmaja <52911293+padms@users.noreply.github.com>
2023-12-19 13:32:30 +0530

    Revert "⚡️Use dpr #1941"
    This reverts commit 99e5ee453abc9c298bfa0194525d785975121434.

--- web/lib/hooks/useSanityLoader.ts
+++ web/lib/hooks/useSanityLoader.ts
@@ -1,20 +1,9 @@
 import { useNextSanityImage } from 'next-sanity-image'
 import type { ImageWithAlt } from 'types'
 import { sanityClientWithEquinorCDN } from '../../lib/sanity.server'
-import { useState, useEffect } from 'react'
 
-export const useSanityLoader = (image: ImageWithAlt, maxWidth: number, aspectRatio: number | undefined) => {
-  const [devicePixelRatio, setDevicePixelRatio] = useState(1)
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // Access the device pixel ratio when the component mounts
-      const ratio = window.devicePixelRatio
-      setDevicePixelRatio(ratio)
-    }
-  }, [])
-
-  return useNextSanityImage(sanityClientWithEquinorCDN, image, {
+export const useSanityLoader = (image: ImageWithAlt, maxWidth: number, aspectRatio: number | undefined) =>
+  useNextSanityImage(sanityClientWithEquinorCDN, image, {
     imageBuilder: (imageUrlBuilder, options) => {
       const { width: imageWidth, croppedImageDimensions: cropped } = options
       // We do not want to allow gigantic images to exist due to performance
@@ -23,7 +12,6 @@ export const useSanityLoader = (image: ImageWithAlt, maxWidth: number, aspectRat
         ? Math.round(width * aspectRatio)
         : Math.round(width * (cropped.height / cropped.width))
 
-      return imageUrlBuilder.width(width).height(height).auto('format').quality(70).dpr(devicePixelRatio)
+      return imageUrlBuilder.width(width).height(height).auto('format').quality(70)
     },
   })
-}
