a0c3f94afeced2bb6d6f5c3de10f8d5313ac0a0e
Fernando Lucchesi <fernando@lucalto.com>
2023-12-22 17:24:33 +0100

    🐛 Move GTM setup to _app #1924

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -81,12 +81,7 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://cdn.eds.equinor.com/font/equinor-uprights-vf.css" />
           <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
           <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-          {GTM_ID && (
-            <>
-              <GoogleConsentMode />
-              <GoogleTagManagerHead />
-            </>
-          )}
+          {GTM_ID && <GoogleConsentMode />}
         </Head>
         <body>
           {GTM_ID && <GoogleTagManagerBody />}
