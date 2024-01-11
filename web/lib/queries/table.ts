b6fa9ef4304355b708183185689c8cf2317eef6b
Fernando Lucchesi <fernando@lucalto.com>
2024-01-11 13:47:11 +0100

    🎨 Move table query to its file #1832

--- web/lib/queries/table.ts
+++ web/lib/queries/table.ts
@@ -4,48 +4,47 @@ import { linkReferenceFields } from './common/actions/linkSelectorFields'
 import markDefs from './common/blockEditorMarks'
 
 export const tableFields = /* groq */ `
- "type": _type,
+  "type": _type,
+  "id": _key,
+  title[]{
+    ...,
+    ${markDefs},
+  },
+  ingress[]{
+    ...,
+    ${markDefs},
+  },
+  tableHeaders[]{
     "id": _key,
-    title[]{
+    headerCell[]{
       ...,
       ${markDefs},
-    },
-    ingress[]{
-      ...,
-      ${markDefs},
-    },
-    tableHeaders[]{
+    }
+  },
+  tableRows[]{
+    "id": _key,
+    row[] {
+      "type": _type,
       "id": _key,
-      headerCell[]{
+      "text": tableRichText[] {
         ...,
-        ${markDefs},
-      }
-    },
-    tableRows[]{
-      "id": _key,
-      row[] {
-        "type": _type,
-        "id": _key,
-        "text": tableRichText[] {
-          ...,
-          ${markDefs}
-        }
-        label,
-        "link": select(
-          linkToOtherLanguage == true =>
-            referenceToOtherLanguage->${linkReferenceFields},
-            reference->${linkReferenceFields},
-        ),
-        "href": url,
-        ${downloadableFileFields},
-        ${downloadableImageFields},
-        ...
+        ${markDefs}
       },
+      label,
+      "link": select(
+        linkToOtherLanguage == true =>
+          referenceToOtherLanguage->${linkReferenceFields},
+          reference->${linkReferenceFields},
+      ),
+      "href": url,
+      ${downloadableFileFields},
+      ${downloadableImageFields},
+      ...
     },
-    "designOptions": {
-      "aspectRatio": coalesce(aspectRatio, '16:9'),
-      "background": coalesce(background.title, 'none'),
-      height,
-    },
-
+  },
+  "designOptions": {
+    "aspectRatio": coalesce(aspectRatio, '16:9'),
+    "background": coalesce(background.title, 'none'),
+    height,
+  }
 `
