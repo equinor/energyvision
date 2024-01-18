b69e03e07a811d3f7e48f83ecd7c253bdfc4c8a0
Malin J <millianapia@gmail.com>
2024-01-18 15:53:26 +0100

    🐛 Remove spacing on child element of list #2064

--- web/pageComponents/shared/portableText/IngressText.tsx
+++ web/pageComponents/shared/portableText/IngressText.tsx
@@ -11,6 +11,10 @@ const { Item } = List
 const StyledList = styled(List)`
   font-size: var(--typeScale-2);
   margin-bottom: var(--space-medium);
+
+  li > ul {
+    margin-bottom: 0;
+  }
 `
 
 const defaultComponents = (centered: boolean) => {
--- web/pageComponents/shared/portableText/components/BulletList.tsx
+++ web/pageComponents/shared/portableText/components/BulletList.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 
 const StyledList = styled(List)`
   margin-bottom: var(--space-medium);
+
+  li > ul {
+    margin-bottom: 0;
+  }
 `
 
 export const BulletList = ({ children }: { children?: React.ReactNode }) => {
