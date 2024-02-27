fa663a0291bb27b59df0ad90eaeccdfccca1150a
Malin J <millianapia@gmail.com>
2024-02-27 15:09:25 +0100

    🐛 Remove spacing on child element of list #2064 (#2065)
    * 🐛 Remove spacing on child element of list #2064
    
    * ♻️ Refactor to use not css prop #2064

--- web/pageComponents/shared/portableText/IngressText.tsx
+++ web/pageComponents/shared/portableText/IngressText.tsx
@@ -10,7 +10,10 @@ const { Item } = List
 
 const StyledList = styled(List)`
   font-size: var(--typeScale-2);
-  margin-bottom: var(--space-medium);
+
+  &:not(li > ul) {
+    margin-bottom: var(--space-medium);
+  }
 `
 
 const defaultComponents = (centered: boolean) => {
--- web/pageComponents/shared/portableText/components/NumberedList.tsx
+++ web/pageComponents/shared/portableText/components/NumberedList.tsx
@@ -2,7 +2,9 @@ import { List } from '@components'
 import styled from 'styled-components'
 
 const StyledList = styled(List)`
-  margin-bottom: var(--space-medium);
+  &:not(li > ul) {
+    margin-bottom: var(--space-medium);
+  }
 `
 export const NumberedList = ({ children }: { children?: React.ReactNode }) => {
   return <StyledList variant="numbered">{children}</StyledList>
