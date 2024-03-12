c29143eebd673cf9d233abedd1f49c42c261c996
Padmaja <52911293+padms@users.noreply.github.com>
2024-03-12 13:33:03 +0530

    ✨ Add link option to text in 404 #2081

--- sanityv3/schemas/documents/404.ts
+++ sanityv3/schemas/documents/404.ts
@@ -14,7 +14,7 @@ const textContentType = configureBlockContent({
   h3: false,
   h4: false,
   externalLink: false,
-  internalLink: false,
+  internalLink: true,
   lists: false,
   attachment: false,
   smallText: false,
