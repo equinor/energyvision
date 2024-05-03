7b6f9530fd33097cb533b030e4a0a499111df2d6
Malin J <millianapia@gmail.com>
2024-05-03 12:35:37 +0200

    🐛 increase padding #2255

--- web/pageComponents/topicPages/promotions/MultiplePromotions.tsx
+++ web/pageComponents/topicPages/promotions/MultiplePromotions.tsx
@@ -108,7 +108,7 @@ const TWCard = ({ slug, title, ingress, publishDateTime, heroImage }: CardData) 
             eyebrow: <FormattedDate datetime={publishDateTime} uppercase />,
           })}
         />
-        {ingress && <Blocks value={ingress} className="line-clamp-5 grow pb-[0.14em]" />}
+        {ingress && <Blocks value={ingress} className="line-clamp-5 grow pb-[0.5em]" />}
       </Card.Content>
     </Card>
   )
