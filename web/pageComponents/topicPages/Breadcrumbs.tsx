368e80f8499b866b95e632aaf5ce0956881b299a
Borghild Selle <104756130+BorghildSelle@users.noreply.github.com>
2024-05-06 10:53:07 +0200

    :art: take back old breadcrumbs and update styling #2272 (#2273)
    * :art: take back old breadcrumbs and update styling
    
    * :art: update old breadcrumb logic
    
    * :art: updates to breadcrumbs
    
    * :art: refactor
    
    * :art: use variable

--- web/pageComponents/topicPages/Breadcrumbs.tsx
+++ web/pageComponents/topicPages/Breadcrumbs.tsx
@@ -1,45 +1,20 @@
-import styled from 'styled-components'
 import { default as NextLink } from 'next/link'
 import { BackgroundContainer, BackgroundContainerProps, BreadcrumbsList } from '@components'
 import { BreadcrumbJsonLd } from 'next-seo'
 import { useRouter } from 'next/router'
 import type { NextRouter } from 'next/router'
 import { getFullUrl } from '../../common/helpers/getFullUrl'
-import { BackgroundColours, Breadcrumb } from '../../types'
+import { Breadcrumb } from '../../types'
+import { twMerge } from 'tailwind-merge'
 
 const { BreadcrumbsListItem } = BreadcrumbsList
-
-type ContainerStyles = {
-  hasTopMargin?: boolean
-}
-
-const Container = styled(BackgroundContainer)<{ $containerStyles?: ContainerStyles }>`
-  max-width: var(--maxViewportWidth);
-  margin-left: auto;
-  margin-right: auto;
-  color: var(--color-on-background);
-`
-const StyledBreadcrumbList = styled(BreadcrumbsList)`
-  padding: var(--space-xLarge) var(--layout-paddingHorizontal-large);
-`
-
-const StyledBreadcrumbsListItem = styled(BreadcrumbsListItem)<{ $bgColor?: BackgroundColours }>`
-  &:last-child {
-    color: var(--breadcrumbs-inactive-color);
-  }
-`
-
-const StyledNextLink = styled(NextLink)<{ $bgColor?: BackgroundColours }>`
-  text-decoration: none;
-  color: var(--color-on-background);
-`
 
 type BreadcrumbsProps = {
   slug: string
   useCustomBreadcrumbs: boolean
   defaultBreadcrumbs: Breadcrumb[]
   customBreadcrumbs: Breadcrumb[]
-  containerStyles: ContainerStyles
+  className?: string
 } & BackgroundContainerProps
 
 const buildJsonLdElements = (crumbs: Breadcrumb[], router: NextRouter) => {
@@ -56,52 +31,49 @@ const buildJsonLdElements = (crumbs: Breadcrumb[], router: NextRouter) => {
   })
 }
 
-const parseBreadcrumbs = (crumbs: Breadcrumb[], custom = false) => {
-  return crumbs
-    .filter((item) => item.slug && item.label)
-    .map((item) => ({
-      ...item,
-      // @TODO: the item.type check can be removed once all existing custom breadcrumbs have been updated to use the segment type
-      label: capitalize(custom && item.type == 'segment' ? item.label : item.label.replaceAll('-', ' ')),
-    }))
+const parseSlug = (item: string): string => {
+  const parts = item?.split('/').at(-1) || item
+  return parts?.[0] + parts?.slice(1)
 }
-
-const capitalize = (string: string): string => string[0].toUpperCase() + string.slice(1)
 
 export const Breadcrumbs = ({
   slug,
   useCustomBreadcrumbs,
   defaultBreadcrumbs,
   customBreadcrumbs,
-  containerStyles,
   background,
+  className = '',
 }: BreadcrumbsProps) => {
   const router = useRouter()
 
   const crumbs =
-    useCustomBreadcrumbs && customBreadcrumbs && customBreadcrumbs.length >= 3
-      ? parseBreadcrumbs(customBreadcrumbs, true)
-      : parseBreadcrumbs(defaultBreadcrumbs)
+    useCustomBreadcrumbs && customBreadcrumbs && customBreadcrumbs.length >= 3 ? customBreadcrumbs : defaultBreadcrumbs
 
   if (crumbs.length < 2) return null
 
   return (
-    <Container $containerStyles={containerStyles} background={background}>
-      <StyledBreadcrumbList>
-        {crumbs.map((item: Breadcrumb) => {
-          if (item.slug === slug) {
-            return <StyledBreadcrumbsListItem key={item.slug}>{item.label}</StyledBreadcrumbsListItem>
-          }
-          return (
-            <BreadcrumbsListItem key={item.slug}>
-              <StyledNextLink href={item.slug} prefetch={false}>
-                {item.label}
-              </StyledNextLink>
-            </BreadcrumbsListItem>
-          )
-        })}
-      </StyledBreadcrumbList>
+    <BackgroundContainer background={background} renderFragmentWhenPossible className="mx-auto max-w-viewport">
+      <BreadcrumbsList className={twMerge(`py-10 px-layout-lg`, className)}>
+        {crumbs
+          .filter((item) => item.slug && item.label)
+          .map((item: Breadcrumb) => {
+            const label = item.label ?? parseSlug(item.slug)?.replaceAll('-', ' ')
+            const shouldLink = item.slug !== slug
+
+            return (
+              <BreadcrumbsListItem key={item.slug}>
+                {shouldLink ? (
+                  <NextLink href={item.slug} prefetch={false} className="hover:underline no-underline">
+                    {label}
+                  </NextLink>
+                ) : (
+                  <>{label}</>
+                )}
+              </BreadcrumbsListItem>
+            )
+          })}
+      </BreadcrumbsList>
       <BreadcrumbJsonLd itemListElements={buildJsonLdElements(crumbs, router)} />
-    </Container>
+    </BackgroundContainer>
   )
 }
