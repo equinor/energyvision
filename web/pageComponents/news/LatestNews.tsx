f7942207a668535c78063de34a68bb844c202943
Borghild <borghild.vikoyr@miles.no>
2024-06-05 12:42:06 +0200

    :art: more adjustments

--- web/pageComponents/news/LatestNews.tsx
+++ web/pageComponents/news/LatestNews.tsx
@@ -27,7 +27,7 @@ const LatestNews = ({ data }: LatestNewsProp) => {
       <Typography variant="xl" as="h2" className="mb-10">
         <FormattedMessage id="latest_news" defaultMessage="Latest News" />
       </Typography>
-      <ul className="grid auto-rows-fr gap-x-6 gap-y-3 xl:grid-cols-3">
+      <ul className="max-lg:w-full grid grid-cols-1 auto-rows-fr gap-x-6 gap-y-3 xl:grid-cols-3">
         {data.map((newsItem: CardData) => {
           return (
             <li key={newsItem.id} className="">
