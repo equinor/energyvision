6071230ab03fe275f4920b22a9502d8034f2191f
Malin J. <millianapia@gmail.com>
2024-06-17 13:50:13 +0200

    ♿️  Rewrite footer to TW and increase lineheight for accessibility #2379

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -33,6 +33,11 @@ module.exports = {
     extend: {
       screens: {
         '3xl': '1600px',
+        'max-2xl': { max: '1535px' },
+        'max-xl': { max: '1279px' },
+        'max-lg': { max: '1023px' },
+        'max-md': { max: '750px' },
+        'max-sm': { max: '639px' },
       },
       colors: ({ theme }) => ({
         current: 'currentColor',
