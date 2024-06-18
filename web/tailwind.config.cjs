50ca8a3aea7d90a56b14ecde9ca073967cf00e7f
Malin J. <millianapia@gmail.com>
2024-06-18 10:59:04 +0200

    🎨 remove max defaults, change md default to 750px #2379

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -32,12 +32,8 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+        md: '750px',
         '3xl': '1600px',
-        'max-2xl': { max: '1535px' },
-        'max-xl': { max: '1279px' },
-        'max-lg': { max: '1023px' },
-        'max-md': { max: '750px' },
-        'max-sm': { max: '639px' },
       },
       colors: ({ theme }) => ({
         current: 'currentColor',
