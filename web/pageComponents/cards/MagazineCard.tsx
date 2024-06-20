0bafd0f896bbda359803015b52b1e1e7a5336e94
Malin J. <millianapia@gmail.com>
2024-06-20 11:22:31 +0200

    ✨Add arrow on overflowing magazine tags#1574

--- web/pageComponents/cards/MagazineCard.tsx
+++ web/pageComponents/cards/MagazineCard.tsx
@@ -1,8 +1,10 @@
 import { Card } from '@components'
-import { CSSProperties } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Image, { Ratios } from '../shared/SanityImage'
 import { MagazineCardData } from '../../types/types'
+import { Icon } from '@equinor/eds-core-react'
+import { chevron_right } from '@equinor/eds-icons'
 
 const { Title, Header, Action, Arrow, Media, CardLink } = Card
 
@@ -11,31 +13,16 @@ const StyledCard = styled(Card)`
   --card-gap: var(--space-large);
 `
 
-const StyledLink = styled(CardLink)`
-  display: inline-block;
-`
-
 const TagsContainer = styled.div`
-  overflow-x: scroll;
-  white-space: nowrap;
-  flex-wrap: nowrap;
-  gap: var(--space-medium);
-  display: flex;
   padding: 0 var(--space-medium) var(--space-medium);
+  scrollbar-width: none; 
+  -ms-overflow-style: none;
 
   ::-webkit-scrollbar {
-    display: none;
-  }
-  @media (min-width: 1024px) {
-    flex-wrap: wrap;
-    overflow: overlay;
+    display: none; 
   }
 `
-const Tag = styled.span`
-  color: var(--moss-green-100);
-  font-weight: var(--fontWeight-medium);
-  font-size: var(--typeScale-0);
-`
+
 type MagazineCardProp = {
   data: MagazineCardData
   fitToContent?: boolean
@@ -59,10 +46,30 @@ const getThumbnail = (data: MagazineCardData) => {
 const MagazineCard = ({ data, fitToContent = false, ...rest }: MagazineCardProp) => {
   const { slug, title, tags } = data
   const thumbnail = getThumbnail(data)
+  const tagsContainerRef = useRef<HTMLDivElement>(null)
+  const [isOverflowing, setIsOverflowing] = useState(false)
+
+  useEffect(() => {
+    const tagsContainer = tagsContainerRef.current
+    if (tagsContainer) {
+      const checkOverflow = () => {
+        const isAtEnd = tagsContainer.scrollWidth - tagsContainer.scrollLeft <= tagsContainer.clientWidth
+        setIsOverflowing(!isAtEnd)
+      }
+
+      checkOverflow()
+      tagsContainer.addEventListener('scroll', checkOverflow)
+
+      return () => {
+        tagsContainer.removeEventListener('scroll', checkOverflow)
+      }
+    }
+  }, [tags])
+
   if (!thumbnail) return null
 
   return (
-    <StyledLink href={slug} prefetch={false} {...rest}>
+    <CardLink className="inline-block" href={slug} prefetch={false} {...rest}>
       <StyledCard
         style={
           {
@@ -80,14 +87,25 @@ const MagazineCard = ({ data, fitToContent = false, ...rest }: MagazineCardProp)
           <Arrow />
         </Action>
         {tags && tags.length > 0 && (
-          <TagsContainer>
-            {tags.map((tag: string) => (
-              <Tag key={tag}>{tag}</Tag>
-            ))}
-          </TagsContainer>
+          <div className="relative flex items-center">
+            <TagsContainer className='overflow-x-auto flex whitespace-nowrap gap-x-4' ref={tagsContainerRef}>
+              {tags.map((tag: string) => (
+                <span className="text-moss-green-100 font-medium text-xs" key={tag}>
+                  {tag}
+                </span>
+              ))}
+            </TagsContainer>
+            <div
+              className={`${
+                isOverflowing ? 'opacity-100' : 'opacity-0'
+              } shrink-0 flex items-center pr-2 transition-opacity pt-0 pb-4`}
+            >
+              <Icon className="sm:min-h-6 sm:min-w-6 h-6 w-6 text-moss-green-100" data={chevron_right} />
+            </div>
+          </div>
         )}
       </StyledCard>
-    </StyledLink>
+    </CardLink>
   )
 }
 
