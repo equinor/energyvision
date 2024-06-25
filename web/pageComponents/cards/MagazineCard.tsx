f0a2299c9579b8534d94311186259020dbbc00dc
Malin J. <millianapia@gmail.com>
2024-06-25 13:37:40 +0200

    🎨  Change tag overflow design #1574

--- web/pageComponents/cards/MagazineCard.tsx
+++ web/pageComponents/cards/MagazineCard.tsx
@@ -1,26 +1,14 @@
 import { Card } from '@components'
-import { CSSProperties, useEffect, useRef, useState } from 'react'
+import { CSSProperties } from 'react'
 import styled from 'styled-components'
 import Image, { Ratios } from '../shared/SanityImage'
 import { MagazineCardData } from '../../types/types'
-import { Icon } from '@equinor/eds-core-react'
-import { chevron_right } from '@equinor/eds-icons'
 
 const { Title, Header, Action, Arrow, Media, CardLink } = Card
 
 const StyledCard = styled(Card)`
   height: var(--height);
   --card-gap: var(--space-large);
-`
-
-const TagsContainer = styled.div`
-  padding: 0 var(--space-medium) var(--space-medium);
-  scrollbar-width: none; 
-  -ms-overflow-style: none;
-
-  ::-webkit-scrollbar {
-    display: none; 
-  }
 `
 
 type MagazineCardProp = {
@@ -46,25 +34,6 @@ const getThumbnail = (data: MagazineCardData) => {
 const MagazineCard = ({ data, fitToContent = false, ...rest }: MagazineCardProp) => {
   const { slug, title, tags } = data
   const thumbnail = getThumbnail(data)
-  const tagsContainerRef = useRef<HTMLDivElement>(null)
-  const [isOverflowing, setIsOverflowing] = useState(false)
-
-  useEffect(() => {
-    const tagsContainer = tagsContainerRef.current
-    if (tagsContainer) {
-      const checkOverflow = () => {
-        const isAtEnd = tagsContainer.scrollWidth - tagsContainer.scrollLeft <= tagsContainer.clientWidth
-        setIsOverflowing(!isAtEnd)
-      }
-
-      checkOverflow()
-      tagsContainer.addEventListener('scroll', checkOverflow)
-
-      return () => {
-        tagsContainer.removeEventListener('scroll', checkOverflow)
-      }
-    }
-  }, [tags])
 
   if (!thumbnail) return null
 
@@ -88,19 +57,13 @@ const MagazineCard = ({ data, fitToContent = false, ...rest }: MagazineCardProp)
         </Action>
         {tags && tags.length > 0 && (
           <div className="relative flex items-center">
-            <TagsContainer className='overflow-x-auto flex whitespace-nowrap gap-x-4' ref={tagsContainerRef}>
-              {tags.map((tag: string) => (
+            <div className="gap-x-1 px-4 pt-0 pb-4">
+              {tags.map((tag: string, index: number) => (
                 <span className="text-moss-green-100 font-medium text-xs" key={tag}>
                   {tag}
+                  {index < tags.length - 1 && ', '}
                 </span>
               ))}
-            </TagsContainer>
-            <div
-              className={`${
-                isOverflowing ? 'opacity-100' : 'opacity-0'
-              } shrink-0 flex items-center pr-2 transition-opacity pt-0 pb-4`}
-            >
-              <Icon className="sm:min-h-6 sm:min-w-6 h-6 w-6 text-moss-green-100" data={chevron_right} />
             </div>
           </div>
         )}
