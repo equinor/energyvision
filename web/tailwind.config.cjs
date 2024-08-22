54380d34d49b37cef78f678ba44a55207817766c
Borghild <borghild.vikoyr@miles.no>
2024-08-22 19:40:16 +0200

    :art: fix build

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -461,7 +461,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    plugin(function ({ matchVariant, addUtilities, theme }) {
+    plugin(function ({ addVariant, matchVariant, addUtilities, theme }) {
       matchVariant(
         'nth',
         (value) => {
