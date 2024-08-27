9ecd26bebae8a0028e392407fcc35bfdae77896b
Borghild <borghild.vikoyr@miles.no>
2024-08-27 09:31:42 +0200

    :art: make algolia search box to core comp

--- web/templates/newsroom/QuickSearch/QuickSearch.tsx
+++ web/templates/newsroom/QuickSearch/QuickSearch.tsx
@@ -1,4 +1,4 @@
-import { SearchBox } from '../../../pageComponents/search/SearchBox'
+import { SearchBox } from '../../../core/AlgoliaSearchBox/SearchBox'
 import { forwardRef } from 'react'
 import { SearchBoxProps } from 'react-instantsearch'
 import { useIntl } from 'react-intl'
@@ -18,24 +18,6 @@ const QuickSearch = forwardRef<HTMLDivElement, QuickSearchProps>(function QuickS
           defaultMessage: 'Search among Equinor corporate-level news releases',
         })}
         placeholder={intl.formatMessage({ id: 'search_quick_search', defaultMessage: 'Quick search' })}
-        className="
-        bg-white-100 
-        border 
-        border-slate-blue-95 
-        px-6 
-        py-3 
-        focus-visible:envis-outline 
-        dark:focus-visible:envis-outline-invert"
-        submitClassName="bg-norwegian-woods-70
-          text-slate-blue-95
-          hover:text-slate-blue-95
-          hover:bg-norwegian-woods-60
-          focus:outline-none
-          focus-visible:envis-outline
-          dark:focus-visible:envis-outline-invert
-          border
-          border-slate-blue-95
-          border-l-0"
       />
     </div>
   )
