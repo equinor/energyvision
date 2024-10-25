5d53fbb05d526fce87622dd61b1f44dbd7faa94a
Malin J. <millianapia@gmail.com>
2024-10-25 13:40:53 +0200

    ♻️ Rewrite and move breadcrumbs #2604

---
+++ web/core/Breadcrumbs/BreadcrumbListItem.tsx
@@ -0,0 +1,19 @@
+import { forwardRef, HTMLAttributes } from 'react'
+import { List } from '@core/List'
+
+export type BreadcrumbsListItemProps = HTMLAttributes<HTMLLIElement>
+
+export const BreadcrumbsListItem = forwardRef<HTMLLIElement, BreadcrumbsListItemProps>(function Breadcrumbs(
+  { children, ...rest },
+  forwardedRef,
+) {
+  return (
+    <List.Item
+      ref={forwardedRef}
+      {...rest}
+      className="inline-block pr-2 font-bold text-grey-90 after:content-['>'] after:pl-2 last:font-medium last:text-slate-blue-90 last:after:content-[''] first-letter:capitalize"
+    >
+      {children}
+    </List.Item>
+  )
+})
