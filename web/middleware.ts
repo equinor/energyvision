4871bf952b9de2525a55715e4cedfada64be60dc
Malin J. <millianapia@gmail.com>
2024-10-31 13:57:27 +0100

    Add localhost #2619

--- web/middleware.ts
+++ web/middleware.ts
@@ -57,14 +57,17 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(dnsRedirect, PERMANENT_REDIRECT)
   }
 
-  // Skip WWW redirect for Radix URLs
-  if (host !== process.env.NEXT_PUBLIC_RADIX_URL || host !== process.env.RADIX_CANONICAL_DOMAIN_NAME) {
+  // Skip WWW redirect for Radix URLs and localhost
+  if (
+    (host !== process.env.RADIX_PUBLIC_DOMAIN_NAME && host !== process.env.RADIX_CANONICAL_DOMAIN_NAME) ||
+    (host !== 'localhost:3000' && !host.includes('www'))
+  ) {
     const wwwRedirect = getWWWRedirect(host, pathname)
     if (wwwRedirect) {
       return NextResponse.redirect(wwwRedirect, PERMANENT_REDIRECT)
     }
   }
-  
+
   // Redirect external links to news which is now archived if link doesn't exist in Sanity
   if (Flags.HAS_ARCHIVED_NEWS && pathname.startsWith('/news') && !pathname.startsWith('/news/archive')) {
     const existsInSanity = await pathExistsInSanity(pathname, isPreview)
