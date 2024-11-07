7acdbd7f68298d825f42f1864635d6afd099fe4c
Borghild Selle <104756130+BorghildSelle@users.noreply.github.com>
2024-11-07 14:53:12 +0100

    :art: update (#2662)

--- web/pages/api/news/next.ts
+++ web/pages/api/news/next.ts
@@ -1,16 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getNameFromLocale } from '../../../lib/localization'
 import { sanityClient } from '../../../lib/sanity.server'
 import { getNewsArticlesByPage } from '../../../lib/queries/newsroom'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const lang = req.query.lang === 'no' ? getNameFromLocale('no') : getNameFromLocale('en') // Defaults to 'en' if the lang parameter is not 'no'
-
+  console.log('get next on lang', req.query.lang)
   console.log('get next from req.query.lastId', req.query.lastId)
   console.log('get next from req.query.lastPublishedAt', req.query.lastPublishedAt)
   try {
     const news = await sanityClient.fetch(getNewsArticlesByPage(false, true), {
-      lang,
+      lang: req.query.lang,
       lastId: req.query.lastId,
       lastPublishedAt: req.query.lastPublishedAt,
     })
