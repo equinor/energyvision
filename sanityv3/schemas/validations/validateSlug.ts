d677f1e55d5f3cf79dc7f2f0d501001565b0a8dd
Borghild Selle <104756130+BorghildSelle@users.noreply.github.com>
2024-11-08 11:01:38 +0100

    :art: add warning for http and not valid slug #2496 (#2503)
    * :art: add warning for http and not valid slug
    
    * 🐛 Refactor after comments #2496
    
    * 🚨 Lint #2496
    
    * 💚 🐛 Fix import bug #2496
    
    * 🐛 Fix error with validation #2496
    
    * 🐛 Check lowercase excluding queryparam #2496
    
    * 🐛 Allow http to avoid error msg that confuses #2496
    
    ---------
    
    Co-authored-by: Padmaja <52911293+padms@users.noreply.github.com>

--- sanityv3/schemas/validations/validateSlug.ts
+++ sanityv3/schemas/validations/validateSlug.ts
@@ -1,6 +1,7 @@
 import { ValidationContext } from 'sanity'
 import { apiVersion } from '../../sanity.client'
 import { Flags } from '../../src/lib/datasetHelpers'
+import { isEquinorUrl } from './checkEquinorUrls'
 
 const validateIsUniqueWithinLocale = async (slug: string, context: ValidationContext) => {
   const { document, getClient } = context
@@ -40,4 +41,29 @@ export const withSlugValidation = (options: any) => {
         isUnique: validateIsUniqueWithinLocale,
       }
     : options
+}
+
+const stringIsSlug =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+const httpRegex = /^http:\/\/[^\s]+$/
+export const warnHttpOrNotValidSlugExternal = (slug: string) => {
+  if (!slug) return true
+  if (!slug.startsWith('http')) return true // ignore mailto , tel
+  const isHttp = httpRegex.test(slug)
+  const validSlug = stringIsSlug.test(slug)
+  const slugWithOutQueryParam = slug.split('?')[0]
+  const isInvalidEquinorUrl = slugWithOutQueryParam !== slugWithOutQueryParam.toLowerCase() && isEquinorUrl(slug)
+
+  let message = ''
+  if (isHttp) {
+    message = 'Use https in url. '
+  }
+  if (!validSlug) {
+    message = message.concat(`Not a valid url.`)
+  }
+
+  if (isInvalidEquinorUrl) {
+    message = message.concat('Equinor urls should contain lowercase letters only.')
+  }
+  return isHttp || !validSlug || isInvalidEquinorUrl ? message : true
 }
