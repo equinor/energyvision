26618a7cf27afad6b53ad3d734a548f18c3f1259
Borghild <borghild.vikoyr@miles.no>
2024-11-21 10:01:40 +0100

    :art: rewrite issue was elsewhere

--- web/pageComponents/topicPages/Form/careersContactForm/CareersContactForm.tsx
+++ web/pageComponents/topicPages/Form/careersContactForm/CareersContactForm.tsx
@@ -48,8 +48,6 @@ const CareersContactForm = () => {
       candidateType: '',
       supportingDocuments: '',
     },
-    mode: 'onBlur',
-    reValidateMode: 'onBlur',
   })
 
   const watchCategory = useWatch({
