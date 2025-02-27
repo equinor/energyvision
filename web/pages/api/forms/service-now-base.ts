f14fcdeac9d5f209259fb5e2efe4379b49110c83
Padmaja <52911293+padms@users.noreply.github.com>
2025-02-27 14:08:30 +0530

    Log the service now request in staging

--- web/pages/api/forms/service-now-base.ts
+++ web/pages/api/forms/service-now-base.ts
@@ -1,4 +1,7 @@
+import { Flags } from '../../../common/helpers/datasetHelpers'
+
 export const sendRequestToServiceNow = async (url: string) => {
+  if (Flags.IS_DEV) console.log('Request to service now \n' + url)
   const headersList = {
     Accept: '*/*',
     Authorization: 'Basic ' + process.env.SERVICE_NOW_CREDENTIALS,
