8ede86efb64683605f2d3ede1fbe5c4ea8be5c48
Padmaja <52911293+padms@users.noreply.github.com>
2025-03-05 15:37:59 +0530

    Include meta as well

--- sanityv3/schemas/components/VideoSelector.tsx
+++ sanityv3/schemas/components/VideoSelector.tsx
@@ -36,7 +36,7 @@ const VideoSelector = forwardRef(function VideoSelector(
     if (value?.id) {
       setIsOpen(true)
       const videoId = value.id
-      const endpoint = `${baseUrl}/api/screen9/${SCREEN9_ACCOUNT_ID}/videos/${videoId}/streams?ssl=true`
+      const endpoint = `${baseUrl}/api/screen9/${SCREEN9_ACCOUNT_ID}/videos/${videoId}/streams,meta?ssl=true`
 
       const data = await fetch(endpoint, {
         headers: {
@@ -59,7 +59,7 @@ const VideoSelector = forwardRef(function VideoSelector(
       if (data && !data.error) {
         const video = {
           id: videoId,
-          title: 'Try only with streams ', //data.meta.title,
+          title: data.meta.title,
           url: data.streams.hls,
         }
         // save to document
