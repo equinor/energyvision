3d221a8a777f6b7ab40626e46f1aed9c088ff2e5
Malin J. <millianapia@gmail.com>
2025-03-07 00:19:55 +0100

    WIP: add logging

--- web/pages/api/rss/index.global.ts
+++ web/pages/api/rss/index.global.ts
@@ -70,7 +70,12 @@ const generateRssFeed = async () => {
   }
 }
 
-export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Generating RSS feed...')
+  console.log('Datetime: ' + new Date())
+  console.log('\n')
+  console.log(res)
+  console.log('req', req)
   const rss = await generateRssFeed()
   res.setHeader('Content-Type', 'text/xml')
   res.write(rss)
