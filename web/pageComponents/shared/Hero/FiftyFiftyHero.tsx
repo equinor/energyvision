a597a6354a7d520b18195e76c4452757377935e8
VarunVAshrit <vaash@equinor.com>
2025-05-07 12:54:56 +0530

    Some minor changes

--- web/pageComponents/shared/Hero/FiftyFiftyHero.tsx
+++ web/pageComponents/shared/Hero/FiftyFiftyHero.tsx
@@ -21,39 +21,29 @@ export const FiftyFiftyHero = ({
     <BackgroundContainer background={{ backgroundColor: background }}>
       <div className="mx-auto grid max-w-[1440px] min-h-[350px] md:grid-cols-2">
         {/* Image Section */}
-        <div className="relative min-h-[350px] md:order-2">
-          {figure && (
-            <Image
-              maxWidth={4096}
-              sizes="(max-width: 800px) 100vw, 800px"
-              image={figure.image}
-              fill
-              priority
-            />
-          )}
-        </div>
+        {figure && (
+          <div className="relative min-h-[350px] md:order-2">
+            <Image maxWidth={4096} sizes="(max-width: 800px) 100vw, 800px" image={figure.image} fill priority />
+          </div>
+        )}
 
         {/* Content Section */}
         <div className="flex flex-col justify-center gap-8 px-4 py-24 max-w-full md:min-h-[450px] md:justify-self-end md:px-16 xl:px-4 xl:pr-32">
           {title && (
             <Heading
               value={title}
-              variant={isBigTitle ? '5xl' : 'h1'}
+              variant={isBigTitle ? '2xl' : 'xl'}
               className={`max-w-[1186px] ${isBigTitle ? 'font-normal' : 'font-medium'}`}
             />
           )}
 
-          {ingress && !isBigTitle && (
-            <div className="hidden md:block">
-              <Blocks value={ingress} />
-            </div>
-          )}
+          {ingress && !isBigTitle && <Blocks value={ingress} className="hidden md:block" />}
 
-          {action && !isBigTitle && url && (
+          {action && !isBigTitle && (
             <ResourceLink
-            href={url as string}
-            {...(action.link?.lang && { locale: getLocaleFromName(action.link?.lang) })}
-            type={action.type}
+              href={url as string}
+              {...(action.link?.lang && { locale: getLocaleFromName(action.link?.lang) })}
+              type={action.type}
             >
               {`${action.label} ${action.extension ? `(${action.extension.toUpperCase()})` : ''}`}
             </ResourceLink>
