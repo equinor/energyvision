3c9e478a14a00abcb1f674625b8cdbc320e52edd
Borghild Selle <borghild.vikoyr@miles.no>
2025-05-22 09:50:42 +0200

    :art: format rss feed

--- web/pages/api/rss/index.global.ts
+++ web/pages/api/rss/index.global.ts
@@ -1,6 +1,6 @@
 import { sanityClient } from '../../../lib/sanity.server'
 import { LatestNewsType, latestMagazine, latestNews } from './groq.global'
-import { defaultComponents, toHTML } from '@portabletext/to-html'
+
 import { urlFor } from '../../../common/helpers/urlFor'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { format } from 'date-fns'
@@ -21,44 +21,36 @@ const generateRssFeed = async (locale: 'en_GB' | 'nb_NO') => {
     const articles: LatestNewsType[] = [...newsArticles, ...magazineArticles].sort(
       (a, b) => new Date(b.publishDateTime).getTime() - new Date(a.publishDateTime).getTime(),
     )
+    const newsLink = locale === 'en_GB' ? 'https://www.equinor.com/news/' : 'https://www.equinor.com/nyheter/'
+    const newsRSSLink =
+      locale === 'en_GB' ? 'https://www.equinor.com/api/rss?lang=en' : 'https://www.equinor.com/api/rss?lang=no'
 
     let rss = `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:nl="http://www.w3.org">
+    <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:media="http://search.yahoo.com/mrss/" version="2.0" >
       <channel>
-      <title>Equinor news and magazine stories</title>
-      <description>Latest news and magazine stories</description>
-      <link>https://www.equinor.com</link>`
-
-    const serializers = {
-      ...defaultComponents,
-      marks: {
-        ...defaultComponents.marks,
-        sub: ({ children }: { children: string }) => `<sub>${children}</sub>`,
-        sup: ({ children }: { children: string }) => `<sup>${children}</sup>`,
-      },
-    }
+        <title>${
+          locale === 'en_GB' ? 'Latest news and magazine stories - Equinor' : 'Siste nyheter og magasinsaker - Equinor'
+        }</title>
+        <link>${newsLink}</link>
+        <description />
+        <language>${locale === 'en_GB' ? 'en-gb' : 'no'}</language>
+        <atom:link href="${newsLink}" type="text/html" rel="alternate"/>
+        <atom:link href="${newsRSSLink}" type="application/rss+xml" rel="self"/>
+    `
 
     articles.forEach((article) => {
       const langPath = article.lang === 'nb_NO' ? '/no' : ''
       const locale = article.lang === 'nb_NO' ? 'no' : 'en'
-      const descriptionHtml = toHTML(article.ingress, {
-        components: serializers,
-        onMissingComponent: (e) => String(e),
-      })
 
       const hero = article.hero
       const bannerImageUrl = hero?.image?.asset ? urlFor(hero.image).size(560, 280).auto('format').toString() : ''
-      const imageAlt = hero?.image?.alt ? ` alt="${hero.image.alt}"` : ''
 
-      const publishDate = new Date(article.publishDateTime)
+      const publishDate = new Date(article.publishDateTime).toUTCString()
+      console.log('publishDate', publishDate)
 
       // Format the main pubDate
-      const formattedPubDate = format(new TZDate(publishDate, 'Europe/Oslo'), 'EEE, dd MMM yyyy HH:mm:ss xxxx')
+      //const formattedPubDate =  //format(new TZDate(publishDate, 'Europe/Oslo'), 'EEE, dd MMM yyyy HH:mm:ss xxxx')
 
-      // Format the extra field date as dd.MM.yyyy
-      /*       const extraFormattedDate = format(utcToZonedTime(publishDate, 'Europe/Oslo'), 'dd.MM.yyyy', {
-        timeZone: 'Europe/Oslo',
-      }) */
       const categoryTag = article.type === 'magazine' ? 'magazineStories' : article.subscriptionType
       const title = article.type === 'magazine' ? toPlainText(article.title as PortableTextBlock[]) : article.title
 
@@ -67,13 +59,20 @@ const generateRssFeed = async (locale: 'en_GB' | 'nb_NO') => {
           <title>${title}</title>
           <link>https://equinor.com${langPath}${article.slug}</link>
           <guid>https://equinor.com${langPath}${article.slug}</guid>
-          <pubDate>${formattedPubDate}</pubDate>
-          <description><![CDATA[<img src="${bannerImageUrl}"${imageAlt}/><br/>${descriptionHtml}]]></description>
-          <language>${article.lang}</language>
+          <pubDate>${publishDate}</pubDate>
+          <description>${toPlainText(article.ingress)}</description>
           ${categoryTag ? `<category>${mapCategoryToId(categoryTag, locale)}</category>` : ''}
           <nl:extra1>${format(new TZDate(publishDate, 'Europe/Oslo'), "d.MMMM yyyy hh:mm ('CEST')", {
             locale: article.lang === 'nb_NO' ? nb : enGB,
           })}</nl:extra1>
+          <media:content medium="image" type="image/jpeg" url="${bannerImageUrl}">
+            ${
+              article.hero?.attribution
+                ? `<media:credit role="photographer" scheme="urn:ebu">${article.hero?.attribution}</media:credit>`
+                : ''
+            }
+            ${article.hero?.image?.alt ? `<media:title>${article.hero?.image?.alt}</media:title>` : ''}
+          </media:content>
         </item>`
     })
 
