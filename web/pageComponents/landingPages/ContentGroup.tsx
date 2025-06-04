d9171f89b2453461af2554e66f0f0ed17000094e
Malin J <millianapia@gmail.com>
2025-06-04 16:59:36 +0200

    ♻️ Small refactoring #3054

--- web/pageComponents/landingPages/ContentGroup.tsx
+++ web/pageComponents/landingPages/ContentGroup.tsx
@@ -1,41 +1,57 @@
-import type { SubMenuGroupData, PromoTileData } from '../../types/index'
 import { Typography } from '@core/Typography'
 import { PromoTile } from '@sections/PromoTiles/PromoTile'
 
-type ContentGroupType = {
-  group: SubMenuGroupData
+export type ContentGroupData = {
+  id: string
+  label?: string
+  links: Array<{
+    id: string
+    label: string
+    image: any
+    href?: string | null
+    link?: {
+      type: string
+      slug: string
+      lang?: string
+    }
+  }>
 }
 
-const ContentGroup = ({ group }: ContentGroupType) => {
-  const { label, id, links } = group
+type ContentGroupProps = {
+  group: {
+    id: string
+    label?: string
+    links: any[]
+  }
+}
 
-  const transformedLinks: PromoTileData[] = links
-    .filter((link) => link.id)
-    .map((link) => ({
-      id: link.id!,
-      title: [],
-      image: link.image,
-      linkLabelAsTitle: true,
-      action: {
-        label: link.label,
-        href: link.href ?? undefined,
-        type: link.link ? 'internalUrl' : 'externalUrl',
-        link: link.link
-          ? {
-              ...link.link,
-              lang: 'en',
-            }
-          : undefined,
-        anchorReference: undefined,
-        ariaLabel: undefined,
-      },
-      designOptions: {
-        background: { value: 'white-100', dark: false },
-      },
-    }))
+const ContentGroup = ({ group }: ContentGroupProps) => {
+  const { links, label, id } = group
+
+  const promoTiles = Array.isArray(links)
+    ? links.map((tile) => ({
+        ...tile,
+        action: {
+          label: tile.label,
+          href: tile.href ?? tile.link?.slug ?? '/',
+          type: tile.link ? 'internalUrl' : 'externalUrl',
+          link: tile.link ?? null,
+          anchorReference: null,
+          ariaLabel: null,
+        },
+        title: [],
+        designOptions: {
+          background: {
+            value: 'white-100',
+            dark: false,
+          },
+        },
+        linkLabelAsTitle: true,
+      }))
+    : []
 
   return (
-    <section className="mx-0 my-16 md:my-32">
+    <section className="mx-0 my-16 md:my-32" id={id}>
       {label && (
         <div className="mx-auto max-w-viewport pt-0 pb-xl px-layout-md">
           <Typography variant="xl" as="h2">
@@ -46,19 +62,21 @@ const ContentGroup = ({ group }: ContentGroupType) => {
 
       <ul
         className={`
-                px-layout-sm
-                mx-auto
-                max-w-viewport
-                grid
-                grid-cols-3
-                gap-x-6
-                list-none`}
+          px-layout-sm
+          mx-auto
+          max-w-viewport
+          grid
+          grid-cols-3
+          gap-x-6
+          list-none`}
       >
-        {transformedLinks.map((tile) => (
-          <li key={tile.id} className="h-full w-full">
-            <PromoTile {...tile} hasSectionTitle={!!label} variant="primary" />
-          </li>
-        ))}
+        {promoTiles.map((tile) => {
+          return (
+            <li key={tile.id} className="h-full w-full">
+              <PromoTile {...tile} hasSectionTitle={!!label} variant="primary" />
+            </li>
+          )
+        })}
       </ul>
     </section>
   )
