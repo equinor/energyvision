d377d5b85a46657406462c2a5548840878650d83
Borghild Selle <borghild.vikoyr@miles.no>
2025-06-06 11:14:19 +0200

    :art: add color

--- web/sections/HomePageBanner/HomePageBanner.tsx
+++ web/sections/HomePageBanner/HomePageBanner.tsx
@@ -51,50 +51,67 @@ type HomePageBannerProps = {
   image: ImageWithAlt
   attribution: string
   ctaCards: any[]
-  colorTheme?: {
-    title: string
-    value: number
+  designOptions: {
+    colorTheme?: {
+      title: string
+      value: number
+    }
+    backgroundType?: number
   }
-  backgroundType?: number
   anchor?: string
 }
 
 export const HomePageBanner = forwardRef<HTMLDivElement, HomePageBannerProps>(function HomePageBanner(
-  { anchor, title, image, attribution, ctaCards, backgroundType, colorTheme },
+  { anchor, title, image, attribution, ctaCards, designOptions },
   ref,
 ) {
   const desktopUrl = useSanityLoader(image, 2560, Ratios.ONE_TO_TWO)
+  const { backgroundType, colorTheme } = designOptions
+  const { foreground, background } = getColorForHomepageBannerTheme(colorTheme?.value ?? 0)
+  const useImage = backgroundType == 1
+  console.log('backgroundType', backgroundType)
+  console.log('useImage', useImage)
 
   return (
-    <div ref={ref} id={anchor} className={`relative px-layout-md`}>
-      <picture className="absolute inset-0">
-        <source srcSet={desktopUrl?.src} media="(min-width: 1250px)" />
-        <Image maxWidth={810} aspectRatio={3.33} image={image} sizes="100vw" fill priority className="" />
-      </picture>
-      <div className="grid grid-cols-1 grid-rows-[max-content_min-content] gap-12 pt-44 pb-16 max-w-viewport">
+    <div ref={ref} id={anchor} className={`relative px-layout-md ${!useImage ? background : ''}`}>
+      {useImage && (
+        <picture className="absolute inset-0">
+          <source srcSet={desktopUrl?.src} media="(min-width: 1250px)" />
+          <Image maxWidth={810} aspectRatio={3.33} image={image} sizes="100vw" fill priority className="" />
+        </picture>
+      )}
+      <div className="grid grid-cols-1 grid-rows-[max-content_min-content] gap-0 lg:gap-12 pt-16 lg:pt-32 pb-20 max-w-viewport">
         {title && (
           <Heading
             as="h1"
-            variant="h2"
+            variant="h1"
             value={title}
-            className="[text-shadow:rgba(255,255,255,0.45)_1px_0px_1px] text-balance z-10 mx-4 max-w-text"
+            className={`
+            text-4xl
+            tracking-tighter
+            text-balance
+            [text-shadow:rgba(255,255,255,0.45)_1px_0px_1px]
+            ${useImage ? 'z-10' : ''} 
+            lg:ml-20
+            max-w-text
+            `}
           />
         )}
         {ctaCards?.length && (
-          <ul className="flex flex-col lg:flex-row gap-4 z-10">
+          <ul className={`flex flex-col lg:flex-row gap-4 ${useImage ? 'z-10' : ''}`}>
             {ctaCards?.map((ctaCard) => {
               const { id, link, overline } = ctaCard
               const url = getUrlFromAction(link)
               return (
                 <li key={id}>
                   <BaseLink
-                    className="group h-full bg-white-100 lg:max-w-[30vw] rounded-md px-4 py-6 flex flex-col gap-2"
+                    className={`${foreground} group h-full lg:max-w-[30vw] rounded-md px-4 py-6 flex flex-col gap-2`}
                     type={link?.type}
                     href={url}
                   >
                     {overline && <div className="text-xs font-medium h-max mb-1">{overline}</div>}
                     <div className="group-hover:underline text-md max-w-[310px] h-max mb-2">{link.label}</div>
-                    <div className="mt-auto flex justify-start">{getArrowElement(link?.type, 'xl:ml-0', 'xl:m-0')}</div>
+                    <div className="mt-auto flex justify-start">{getArrowElement(link?.type, '', 'ml-0 xl:ml-0')}</div>
                   </BaseLink>
                 </li>
               )
@@ -102,7 +119,7 @@ export const HomePageBanner = forwardRef<HTMLDivElement, HomePageBannerProps>(fu
           </ul>
         )}
       </div>
-      {attribution && <div>{attribution}</div>}
+      {useImage && attribution && <div>{attribution}</div>}
     </div>
   )
 })
