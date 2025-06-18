4e31f83f35f2eb0619ade871af9db9b3f15cdf65
Malin J. <millianapia@gmail.com>
2025-06-18 11:48:21 +0200

    ♻️ add environment tag #2906

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -2,14 +2,15 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://8c4f308da7deed9aea83c76daa1938c0@o4509004923797504.ingest.de.sentry.io/4509010392121425",
+  dsn: 'https://8c4f308da7deed9aea83c76daa1938c0@o4509004923797504.ingest.de.sentry.io/4509010392121425',
+  environment: process.env.NEXT_PUBLIC_SANITY_DATASET || 'global-development',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
--- web/sentry.edge.config.ts
+++ web/sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://8c4f308da7deed9aea83c76daa1938c0@o4509004923797504.ingest.de.sentry.io/4509010392121425",
-
+  dsn: 'https://8c4f308da7deed9aea83c76daa1938c0@o4509004923797504.ingest.de.sentry.io/4509010392121425',
+  environment: process.env.NEXT_PUBLIC_SANITY_DATASET || 'global-development',
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
