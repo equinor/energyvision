5b3dc673d8165d67e3128363223bd7330b3ace64
Malin J. <millianapia@gmail.com>
2025-07-18 11:11:51 +0200

    ♿️ Svg-icons shouldn’t have aria-hidden #2840

--- web/components/src/Card/Arrow.tsx
+++ web/components/src/Card/Arrow.tsx
@@ -53,18 +53,6 @@ const AnimatedArrow = styled.span`
 
 export const Arrow = (): JSX.Element => {
   return (
-    /*   <AnimatedSVG
-      aria-hidden={true}
-      width="30"
-      height="19"
-      viewBox="0 0 30 19"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M18.7383 16L25.9997 9L18.7383 2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M1 9H25.6369" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-    </AnimatedSVG> */
-
     <AnimatedArrow />
   )
 }
