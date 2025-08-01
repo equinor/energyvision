5c4dce3b356049af69f5555ab292f49811edcbca
Borghild Selle <borghild.vikoyr@miles.no>
2025-08-01 12:47:40 +0200

    :art: comment alt

--- sanityv3/migrations/issue-3048/index.ts
+++ sanityv3/migrations/issue-3048/index.ts
@@ -15,7 +15,7 @@ export default defineMigration({
             //return at('aspectRatio', set(mappedValue))
 
             //Set the title which has 10:3 or 16:9 as the value (before 0.3 number and such)
-            const titleAsStringValue = item.title.toString()
+            //const titleAsStringValue = item.title.toString()
             return at([{ _key: item._key }, 'aspectRatio'], set(mappedValue))
             //return patch(item._id, at(['aspectRatio'], set(titleAsStringValue)))
             //return at([{ _key: item._key }, 'aspectRatio'], set(titleAsStringValue)),
