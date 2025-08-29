7c397f2b155814f2c7c3eb893cf8cc301dcf49ee
Padmaja <52911293+padms@users.noreply.github.com>
2025-08-29 14:46:55 +0530

    Use this groq version as it breaks after this

--- web/sanity/lib/api.ts
+++ web/sanity/lib/api.ts
@@ -24,7 +24,7 @@ export const projectId = assertValue(
 /**
  * see https://www.sanity.io/docs/api-versioning for how versioning works
  */
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-06-23'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-01'
 
 /**
  * Used to configure edit intent links, for Presentation Mode, as well as to configure where the Studio is mounted in the router.
