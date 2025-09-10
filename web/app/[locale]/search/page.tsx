08089d172ec6c30bb9438603d60fa5c838d12b4d
Padmaja <52911293+padms@users.noreply.github.com>
2025-09-10 18:57:25 +0530

    Fix back button #3250

--- web/app/[locale]/search/page.tsx
+++ web/app/[locale]/search/page.tsx
@@ -6,11 +6,13 @@ import { TopbarDropdown } from '@/sections/SiteMenu/TopbarDropdown'
 import { Icon } from '@equinor/eds-core-react'
 import { FloatingOverlay } from '@floating-ui/react'
 import { close } from '@equinor/eds-icons'
+import { useRouter } from 'next/router'
 
 export const dynamic = 'force-dynamic'
 // Multiple versions of this page will be statically generated
 // using the `params` returned by `generateStaticParams`
 export default function Page() {
+  const router = useRouter()
   return (
     <div className="dark fixed inset-0 overflow-auto bg-slate-blue-95">
       <FloatingOverlay lockScroll>
@@ -22,7 +24,7 @@ export default function Page() {
               aria-expanded={true}
               aria-label="Close search"
               onClick={() => {
-                //router.back()
+                router.back()
               }}
               className={`focus-visible:envis-outline-invert rounded-full p-3 text-white-100 hover:bg-moss-green-50 hover:text-slate-blue-95 focus:outline-none active:scale-99 active:bg-white-100/20`}
             >
