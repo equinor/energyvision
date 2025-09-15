882d3c3dc178055b5e41e4cdaf97eb54520840c8
Malin J. <millianapia@gmail.com>
2025-09-15 11:01:46 +0200

    WIP: boilerplate files for magazine

---
+++ web/app/[locale]/(magazine)/magasin/page.tsx
@@ -0,0 +1,112 @@
+import { getIsoFromLocale, getNameFromLocale } from '../../../../lib/localization'
+import { Flags } from '../../../../common/helpers/datasetHelpers'
+import { algolia } from '../../../../lib/config'
+import { getPageData } from '../../../../sanity/lib/fetchData'
+import { notFound } from 'next/navigation'
+import { unstable_cache } from 'next/cache'
+import MagazineRoom from '@/templates/magazine/Magazineroom'
+import { MagazineIndexPageType } from '../../../../types'
+import { setRequestLocale } from 'next-intl/server'
+import { magazineQuery } from '@/sanity/queries/magazine'
+import { algoliasearch } from 'algoliasearch'
+import { toPlainText } from 'next-sanity'
+import { isDateAfter } from '@/common/helpers/dateUtilities'
+import getOpenGraphImages from '@/common/helpers/getOpenGraphImages'
+import { metaTitleSuffix } from '@/languages'
+import { Metadata, ResolvingMetadata } from 'next'
+
+export function generateStaticParams() {
+  return Flags.HAS_MAGAZINE ? [{ locale: 'no' }] : []
+}
+
+export async function generateMetadata(
+  { params }: { params: Promise<{ locale: string }> },
+  _: ResolvingMetadata,
+): Promise<Metadata> {
+  const { locale } = await params
+  const lang = getNameFromLocale(locale)
+
+  const queryParams = {
+    lang,
+  }
+  const { pageData } = await getPageData({
+    query: magazineQuery,
+    queryParams,
+  })
+
+  const { publishDateTime, updatedAt, documentTitle, title, metaDescription, openGraphImage, heroImage } = pageData
+  const plainTitle = Array.isArray(title) ? toPlainText(title) : title
+
+  const modifiedDate = isDateAfter(publishDateTime, updatedAt) ? publishDateTime : updatedAt
+  const openGraphImages = getOpenGraphImages((openGraphImage?.asset ? openGraphImage : null) || heroImage?.image)
+
+  return {
+    title: `${documentTitle || plainTitle} - ${metaTitleSuffix}`,
+    description: metaDescription,
+    openGraph: {
+      title: plainTitle,
+      description: metaDescription,
+      url: 'https://www.equinor.com/no/magasin',
+      locale,
+      type: 'article',
+      siteName: 'Equinor',
+      publishedTime: publishDateTime,
+      modifiedTime: modifiedDate,
+      images: openGraphImages,
+    },
+    alternates: {
+      canonical: 'https://www.equinor.com/no/magasin',
+      languages: {
+        en: 'https://www.equinor.com/magazine',
+        'x-default': 'https://www.equinor.com/magazine',
+      },
+    },
+  }
+}
+
+const getInitialResponse = unstable_cache(
+  async (locale: string) => {
+    const envPrefix = Flags.IS_GLOBAL_PROD ? 'prod' : 'dev'
+    const indexName = `${envPrefix}_MAGAZINES_${locale}`
+
+    const searchClient = algoliasearch(algolia.applicationId, algolia.searchApiKey)
+    const response = await searchClient.searchSingleIndex({
+      indexName: indexName,
+      searchParams: {
+        hitsPerPage: 50,
+        facetFilters: ['type:magazine', 'topicTags:-Crude Oil Assays'],
+        facetingAfterDistinct: true,
+        facets: ['countryTags', 'topicTags', 'year'],
+      },
+    })
+    return response
+  },
+  ['magazine'],
+  { revalidate: 3600, tags: ['magazine'] },
+)
+
+export default async function MagazinePage({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+
+  if (!Flags.HAS_MAGAZINE || locale !== 'no') {
+    notFound()
+  }
+
+  setRequestLocale(locale)
+
+  const lang = getNameFromLocale(locale)
+  const isoLocale = getIsoFromLocale(locale)
+
+  const queryParams = {
+    lang,
+  }
+
+  const { pageData } = await getPageData({
+    query: magazineQuery,
+    queryParams,
+  })
+
+  const response = await getInitialResponse(isoLocale)
+
+  return <MagazineRoom pageData={pageData as MagazineIndexPageType} />
+}
---
+++ web/app/[locale]/(magazine)/magazine/page.tsx
@@ -0,0 +1,90 @@
+import { getIsoFromLocale, getNameFromLocale } from '../../../../lib/localization'
+import { Flags } from '../../../../common/helpers/datasetHelpers'
+import { getPageData } from '../../../../sanity/lib/fetchData'
+import { notFound } from 'next/navigation'
+import { unstable_cache } from 'next/cache'
+import MagazineRoom from '@/templates/magazine/Magazineroom'
+import { MagazineIndexPageType } from '../../../../types'
+import { setRequestLocale } from 'next-intl/server'
+import { allMagazineDocuments } from '@/sanity/queries/magazine'
+import { toPlainText } from 'next-sanity'
+import { isDateAfter } from '@/common/helpers/dateUtilities'
+import getOpenGraphImages from '@/common/helpers/getOpenGraphImages'
+import { metaTitleSuffix } from '@/languages'
+import { Metadata, ResolvingMetadata } from 'next'
+
+export function generateStaticParams() {
+  return Flags.HAS_MAGAZINE ? [{ locale: 'en' }] : []
+}
+
+export async function generateMetadata(
+  { params }: { params: Promise<{ locale: string }> },
+  _: ResolvingMetadata,
+): Promise<Metadata> {
+  const { locale } = await params
+  const lang = getNameFromLocale(locale)
+
+  const queryParams = {
+    lang,
+  }
+  const { pageData } = await getPageData({
+    query: allMagazineDocuments,
+    queryParams,
+  })
+
+  const { publishDateTime, updatedAt, documentTitle, title, metaDescription, openGraphImage, heroImage } = pageData
+  const plainTitle = Array.isArray(title) ? toPlainText(title) : title
+
+  const modifiedDate = isDateAfter(publishDateTime, updatedAt) ? publishDateTime : updatedAt
+  const openGraphImages = getOpenGraphImages((openGraphImage?.asset ? openGraphImage : null) || heroImage?.image)
+
+  return {
+    title: `${documentTitle || plainTitle} - ${metaTitleSuffix}`,
+    description: metaDescription,
+    openGraph: {
+      title: plainTitle,
+      description: metaDescription,
+      url: 'https://www.equinor.com/magazine',
+      locale,
+      type: 'article',
+      siteName: 'Equinor',
+      publishedTime: publishDateTime,
+      modifiedTime: modifiedDate,
+      images: openGraphImages,
+    },
+    alternates: {
+      canonical: 'https://www.equinor.com/magazine',
+      languages: {
+        no: 'https://www.equinor.com/no/magasin',
+        'x-default': 'https://www.equinor.com/magazine',
+      },
+    },
+  }
+}
+
+
+
+export default async function MagazinePage({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+
+  if (!Flags.HAS_MAGAZINE_INDEX || locale !== 'en') {
+    notFound()
+  }
+
+  setRequestLocale(locale)
+
+  const lang = getNameFromLocale(locale)
+
+  const queryParams = {
+    lang,
+  }
+
+  // Use magazine index query, not magazineQuery (which expects slug)
+  const { pageData } = await getPageData({
+    query: allMagazineDocuments,
+    queryParams,
+  })
+
+
+  return <MagazineRoom pageData={pageData as MagazineIndexPageType} />
+}
