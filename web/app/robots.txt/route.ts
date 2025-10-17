a551fab275efd7ad5be3d5da8a67fb9c617115f6
equinorpiyush <piyd@equinor.com>
2025-10-17 16:31:57 +0530

    robots.txt file changes

---
+++ web/app/robots.txt/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server';
+import { Flags } from '@/common/helpers/datasetHelpers'; // adjust path if needed
+
+const crawlableDomains = [
+  'www.equinor.com',
+  'www.equinor.ar',
+  'www.equinor.pl',
+  'www.equinorstorage.de',
+  'www.equinorfondene.no',
+  'www.equinor.jp',
+  'www.equinor.com.br',
+  'www.equinor.de',
+  'www.equinor.co.kr',
+  'www.equinorcelticsea.co.uk',
+  'www.sponsorship.equinor.com',
+];
+
+const generateRobotsTxt = (domain: string) => `User-agent: *
+${crawlableDomains.includes(domain) ? 'Allow' : 'Disallow'}: /
+${
+  domain === 'www.equinor.com'
+    ? `Disallow: /search
+Disallow: /search.html
+Disallow: /?*topic
+Disallow: /?*country
+Disallow: /?*year
+Disallow: /?*refinementList
+Disallow: /?*sortBy`
+    : ''
+}
+Sitemap: ${domain.startsWith('www') ? `https://${domain}` : domain}/sitemap.xml
+`;
+
+export async function GET(request: Request) {
+  const domain = new URL(request.url).host;
+
+  if (!crawlableDomains.includes(domain) && !Flags.IS_DEV) {
+    return new NextResponse('Not Found', { status: 404 });
+  }
+
+  const content = generateRobotsTxt(domain);
+
+  return new NextResponse(content, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
