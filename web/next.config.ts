366499ee629673601531321573ee7415e29f4287
Padmaja <52911293+padms@users.noreply.github.com>
2025-10-31 12:30:47 +0530

    Try this fix

--- web/next.config.ts
+++ web/next.config.ts
@@ -129,9 +129,6 @@ const nextConfig: NextConfig = {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: 'false',
   },
-  outputFileTracingIncludes: {
-    '/app/web/.next/standalone/.next/server/proxy.js': ['**/*.js'],
-  },
   experimental: {
     optimizePackageImports: ['@equinor/eds-core-react', '@equinor/eds-icons', 'video.js'],
   },
