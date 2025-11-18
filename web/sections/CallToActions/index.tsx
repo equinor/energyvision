7c6033a2159f81298d52458dcecd2c24c555d12d
Padmaja <52911293+padms@users.noreply.github.com>
2025-11-18 15:40:54 +0530

    Fix download label #3368

--- web/sections/CallToActions/index.tsx
+++ web/sections/CallToActions/index.tsx
@@ -3,6 +3,7 @@ import type { LinkData } from '../../types/index'
 import { getUrlFromAction } from '../../common/helpers'
 import { getLocaleFromName } from '../../lib/localization'
 import { twMerge } from 'tailwind-merge'
+import { call } from '@equinor/eds-icons'
 
 type CallToActionsProps = {
   callToActions: LinkData[]
@@ -13,17 +14,17 @@ type CallToActionsProps = {
 
 const CallToActions = ({ callToActions = [], splitList, linkVariant, className = '' }: CallToActionsProps) => {
   if (!callToActions) return null
-
   const getSingleAction = () => {
     const { label, extension, type, link } = callToActions[0]
     const url = getUrlFromAction(callToActions[0])
-    if (!url) {
+    if (!url && type !== 'downloadableFile') {
       console.warn(`CallToActions: Missing URL on Call to action link with type: '${type}' and label: '${label}'`)
       return null
     }
 
     return (
       <ResourceLink
+        {...callToActions[0]}
         href={url}
         extension={extension}
         showExtensionIcon={true}
@@ -47,10 +48,12 @@ const CallToActions = ({ callToActions = [], splitList, linkVariant, className =
     >
       {callToActions.map((callToAction: LinkData) => {
         const url = getUrlFromAction(callToAction)
+
         return url ? (
           <li key={callToAction.id}>
             {/*  If the URL is a static AEM page it should behave as an internal link in the web */}
             <ResourceLink
+              {...callToAction}
               href={url}
               {...(callToAction.link?.lang && { hrefLang: getLocaleFromName(callToAction.link?.lang) })}
               type={callToAction.type}
