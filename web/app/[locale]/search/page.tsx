84c36e39d95707030c30078215807bf1f2a22256
Borghild Selle <borghild.vikoyr@miles.no>
2025-12-03 13:20:28 +0100

    :art: search dont need meta?

--- web/app/[locale]/search/page.tsx
+++ web/app/[locale]/search/page.tsx
@@ -2,18 +2,15 @@
 import { Icon } from '@equinor/eds-core-react'
 import { close } from '@equinor/eds-icons'
 import { FloatingOverlay } from '@floating-ui/react'
-import type { Metadata } from 'next'
 import { useRouter } from 'next/navigation'
-import { getTranslations } from 'next-intl/server'
 import { LogoLink } from '@/core/Link/LogoLink'
-import { metaTitleSuffix } from '@/languageConfig'
 import { Search } from '@/sections/Search/Search'
 import { NavTopbar } from '@/sections/SiteMenu/NavTopbar'
 import { TopbarDropdown } from '@/sections/SiteMenu/TopbarDropdown'
 
 export const dynamic = 'force-dynamic'
 
-export async function generateMetadata({
+/* export async function generateMetadata({
   params,
 }: {
   params: Promise<{ locale: string }>
@@ -41,7 +38,7 @@ export async function generateMetadata({
       },
     },
   }
-}
+} */
 
 // Multiple versions of this page will be statically generated
 // using the `params` returned by `generateStaticParams`
