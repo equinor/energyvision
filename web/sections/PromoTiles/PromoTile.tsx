3b121f6098d98eb8973bb7d7faf5ab65216417cc
Borghild Selle <borghild.vikoyr@miles.no>
2025-12-03 09:53:57 +0100

    :art: set dark false on light colors

--- web/sections/PromoTiles/PromoTile.tsx
+++ web/sections/PromoTiles/PromoTile.tsx
@@ -1,57 +1,75 @@
-import Card from '@/sections/cards/Card'
-import { getUrlFromAction } from '@/lib/helpers/getUrlFromAction'
-import { ColorKeyTokens, colorKeyToUtilityMap } from '@/styles/colorKeyToUtilityMap'
-import { PromoTileData } from '@/types/index'
-import { forwardRef } from 'react'
-import { Variants } from '../cards/Card/Card'
-import { getLocaleFromName } from '../../sanity/localization'
 import { useLocale } from 'next-intl'
+import { forwardRef } from 'react'
+import { getUrlFromAction } from '@/lib/helpers/getUrlFromAction'
+import Card from '@/sections/cards/Card'
+import {
+  type ColorKeyTokens,
+  colorKeyToUtilityMap,
+} from '@/styles/colorKeyToUtilityMap'
+import type { PromoTileData } from '@/types/index'
+import { getLocaleFromName } from '../../sanity/localization'
+import type { Variants } from '../cards/Card/Card'
 
 type PromoTileProps = {
   hasSectionTitle?: boolean
   variant?: Variants
 } & PromoTileData
 
-export const PromoTile = forwardRef<HTMLAnchorElement, PromoTileProps>(function PromoTile(
-  { id, designOptions, image, title, action, linkLabelAsTitle, hasSectionTitle, variant = 'secondary' },
-  ref,
-) {
-  const url = getUrlFromAction(action)
-  const intlLocale = useLocale()
-  if (!url) {
-    return null
-  }
+export const PromoTile = forwardRef<HTMLAnchorElement, PromoTileProps>(
+  function PromoTile(
+    {
+      id,
+      designOptions,
+      image,
+      title,
+      action,
+      linkLabelAsTitle,
+      hasSectionTitle,
+      variant = 'secondary',
+    },
+    ref,
+  ) {
+    const url = getUrlFromAction(action)
+    const intlLocale = useLocale()
+    if (!url) {
+      return null
+    }
 
-  const locale = action.link?.lang ? getLocaleFromName(action.link?.lang) : getLocaleFromName(intlLocale)
-  const { background } = designOptions
+    const locale = action.link?.lang
+      ? getLocaleFromName(action.link?.lang)
+      : getLocaleFromName(intlLocale)
+    const { background } = designOptions
 
-  const colorName =
-    Object.keys(colorKeyToUtilityMap).find(
-      (key) => colorKeyToUtilityMap[key as keyof ColorKeyTokens]?.backgroundName === background?.backgroundColor,
-    ) ?? 'white-100'
+    const colorName =
+      Object.keys(colorKeyToUtilityMap).find(
+        key =>
+          colorKeyToUtilityMap[key as keyof ColorKeyTokens]?.backgroundName ===
+          background?.backgroundColor,
+      ) ?? 'white-100'
 
-  const theme = background?.backgroundUtility
-    ? colorKeyToUtilityMap[background.backgroundUtility]
-    : colorKeyToUtilityMap[colorName as keyof ColorKeyTokens]
+    const theme = background?.backgroundUtility
+      ? colorKeyToUtilityMap[background.backgroundUtility]
+      : colorKeyToUtilityMap[colorName as keyof ColorKeyTokens]
 
-  return (
-    <Card
-      {...(id && { id: id })}
-      href={url}
-      type={action.type}
-      hrefLang={locale}
-      ref={ref}
-      image={image}
-      variant={variant}
-      className={`${theme?.dark || background?.dark ? 'dark' : ''} `}
-    >
-      <Card.Content variant={variant} className={`${theme?.background}`}>
-        <Card.Header
-          titleLevel={hasSectionTitle ? 'h3' : 'h2'}
-          {...(!linkLabelAsTitle && { titleBlock: title })}
-          {...(linkLabelAsTitle && { title: action.label })}
-        />
-      </Card.Content>
-    </Card>
-  )
-})
+    return (
+      <Card
+        {...(id && { id: id })}
+        href={url}
+        type={action.type}
+        hrefLang={locale}
+        ref={ref}
+        image={image}
+        variant={variant}
+        className={`${theme?.dark || background?.dark ? 'dark' : ''} `}
+      >
+        <Card.Content variant={variant} className={`${theme?.background}`}>
+          <Card.Header
+            titleLevel={hasSectionTitle ? 'h3' : 'h2'}
+            {...(!linkLabelAsTitle && { titleBlock: title })}
+            {...(linkLabelAsTitle && { title: action.label })}
+          />
+        </Card.Content>
+      </Card>
+    )
+  },
+)
--- web/styles/colorKeyToUtilityMap.ts
+++ web/styles/colorKeyToUtilityMap.ts
@@ -25,41 +25,49 @@ export const colorKeyToUtilityMap = {
     backgroundName: 'White',
     background: 'bg-white-100',
     text: 'text-white-100',
+    dark: false,
   },
   'moss-green-50': {
     backgroundName: 'Moss Green Light',
     background: 'bg-moss-green-50',
     text: 'text-moss-green-50',
+    dark: false,
   },
   'spruce-wood-90': {
     backgroundName: 'Spruce Wood',
     background: 'bg-spruce-wood-90',
     text: 'text-spruce-wood-90',
+    dark: false,
   },
   'mist-blue-100': {
     backgroundName: 'Mist Blue',
     background: 'bg-mist-blue-100',
     text: 'text-mist-blue-100',
+    dark: false,
   },
   'energy-red-100': {
     backgroundName: 'Energy Red',
     background: 'bg-energy-red-100',
     text: 'text-energy-red-100',
+    dark: false,
   },
   'green-50': {
     backgroundName: 'Mid Green',
     background: 'bg-green-50',
     text: 'text-green-50',
+    dark: false,
   },
   'yellow-50': {
     backgroundName: 'Mid Yellow',
     background: 'bg-yellow-50',
     text: 'text-yellow-50',
+    dark: false,
   },
   'orange-50': {
     backgroundName: 'Mid Orange',
     background: 'bg-orange-50',
     text: 'text-orange-50',
+    dark: false,
   },
   'slate-blue-95': {
     backgroundName: 'Slate Blue 95',
@@ -71,37 +79,44 @@ export const colorKeyToUtilityMap = {
     backgroundName: 'Light green',
     background: 'bg-norwegin-woods-40',
     text: 'text-norwegin-woods-40',
+    dark: false,
   },
   'sand-and-summer-50': {
     backgroundName: 'Sand',
     background: 'bg-sand-and-summer-50',
     text: 'text-sand-and-summer-50',
+    dark: false,
   },
   'norwegian-woods-40': {
     backgroundName: 'Light green',
     background: 'bg-norwegian-woods-40',
     text: 'text-norwegian-woods-40',
+    dark: false,
   },
 
   'spruce-wood-20': {
     backgroundName: 'Light Spruce wood',
     background: 'bg-spruce-wood-20',
     text: 'text-spruce-wood-20',
+    dark: false,
   },
   'yellow-20': {
     backgroundName: 'Light yellow',
     background: 'bg-yellow-20',
     text: 'text-yellow-20',
+    dark: false,
   },
   'blue-20': {
     backgroundName: 'Light blue',
     background: 'bg-blue-20',
     text: 'text-blue-20',
+    dark: false,
   },
   'gray-20': {
     backgroundName: 'Light gray',
     background: 'bg-gray-20',
     text: 'text-gray-20',
+    dark: false,
   },
 }
 export type ColorKeys = keyof typeof colorKeyToUtilityMap
