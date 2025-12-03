79317c9da75b9b32cb7c7f9d93732ae341aeb543
Borghild Selle <borghild.vikoyr@miles.no>
2025-12-03 14:03:08 +0100

    :art: revalidate tag with route

--- web/app/api/revalidate-tag/route.ts
+++ web/app/api/revalidate-tag/route.ts
@@ -1,21 +1,8 @@
-/* import { revalidateTag } from 'next/cache'
-import { NextRequest } from 'next/server'
-
-export const POST = async (request: NextRequest) => {
-  const { tags, isValid } = await validateRequest(request)
-  if (!isValid) return new Response('No no no', { status: 400 })
-  for (const _tag of tags) {
-    const tag = `sanity:${_tag}`
-    revalidateTag(tag)
-    // eslint-disable-next-line no-console
-    console.log(`revalidated tag: ${tag}`)
-  }
-}
- */
-
 import { revalidateTag } from 'next/cache'
 import { type NextRequest, NextResponse } from 'next/server'
+import { groq } from 'next-sanity'
 import { parseBody } from 'next-sanity/webhook'
+import { sanityClientWithEquinorCDN } from '@/sanity/lib/equinorCdnClient'
 
 type WebhookPayload = {
   tags: string[]
@@ -41,17 +28,44 @@ export async function POST(req: NextRequest) {
         status: 401,
       })
     }
+    const docId = body?.id
+    const docType = body?.tags?.[0]
+
     if (!Array.isArray(body?.tags) || !body.tags.length) {
       const message = 'Bad Request'
       return new Response(JSON.stringify({ message, body }), { status: 400 })
     }
     console.log('tagbased revalidation tags', body.tags)
 
-    body.tags.forEach(_tag => {
-      const tag = `sanity:${_tag}`
-      revalidateTag(tag, 'max')
-      console.log(`revalidated tag: ${tag}`)
-    })
+    const docsWithoutSlugCurrent = [
+      'page',
+      'magazineIndex',
+      'newsroom',
+      'event',
+      'homePage',
+      'landingPage',
+    ]
+    //@ts-ignore
+    if (docsWithoutSlugCurrent.includes(docType)) {
+      const { data } = await sanityClientWithEquinorCDN.fetch(
+        groq`*[_id==$id][0]{"slugs": *[_type match "route_*" && references(^._id)]}`,
+        {
+          id: docId,
+        },
+      )
+
+      data.slugs.forEach((slug: any) => {
+        console.log(`fetched route on id ${docId}, slug`, slug)
+        const tag = `sanity:${docType}:${slug}`
+        revalidateTag(tag, 'max')
+      })
+    } else {
+      body.tags.forEach(_tag => {
+        const tag = `sanity:${_tag}`
+        revalidateTag(tag, 'max')
+        console.log(`revalidated tag: ${tag}`)
+      })
+    }
 
     return NextResponse.json({ body })
   } catch (err) {
