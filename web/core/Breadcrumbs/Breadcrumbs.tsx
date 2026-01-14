e46153cbe9bf8550e0a2d9886dc569c712e6789d
Padmaja <52911293+padms@users.noreply.github.com>
2026-01-14 20:08:37 +0530

    Fix bug #3442

--- web/core/Breadcrumbs/Breadcrumbs.tsx
+++ web/core/Breadcrumbs/Breadcrumbs.tsx
@@ -76,7 +76,7 @@ export const Breadcrumbs = forwardRef<HTMLElement, BreadcrumbsProps>(
       <nav
         ref={ref}
         aria-label='Breadcrumbs'
-        className={`px-layout-sm lg:px-layout-lg ${background ?? ''}`}
+        className={`px-layout-sm lg:px-layout-lg py-10 ${background ?? ''}`}
       >
         <BreadcrumbsList className={twMerge(`py-2 lg:py-4`, className)}>
           {crumbs.map(item => {
