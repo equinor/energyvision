97f0df989c09a59a95caa191b2ae5f6180826c4b
Padmaja <52911293+padms@users.noreply.github.com>
2026-01-28 16:48:51 +0530

    Temporary build fix

--- web/app/instrumentation.ts
+++ web/app/instrumentation.ts
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    // @ts-ignore
     await import("./sentry.server.config");
   }
   if (process.env.NEXT_RUNTIME === "edge") {
+    // @ts-ignore
     await import("./sentry.edge.config");
   }
 }
