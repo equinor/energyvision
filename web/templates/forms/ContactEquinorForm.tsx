e0ad0b45cbec06a06bfd5391785c1f1737945f97
Piyush Deshmukh <piyd@equinor.com>
2026-02-11 13:30:36 +0530

    minor

--- web/templates/forms/ContactEquinorForm.tsx
+++ web/templates/forms/ContactEquinorForm.tsx
@@ -79,7 +79,7 @@ const ContactEquinorForm = () => {
       }
       
       // Call the server action directly
-      // CAT0012836 is CAT ID for Contact Equinor Form
+      // CAT0012836 is CAT ID for Contact Equinor Form //
       const result = await submitFormServerAction(JSON.stringify(finalFormData), 'CAT0012836')
 
       setServerError(result.status !== 200)
