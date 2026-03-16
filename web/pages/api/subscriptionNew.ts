7ef159c3290e392d027788e805cf92af87fe9aa8
Padmaja <52911293+padms@users.noreply.github.com>
2026-03-16 05:04:05 -0700

    Padms/2839 Make to main (#3536)
    * WIP: testing newsletter #2839
    
    * WIP: Adding new date
    
    * WIP: Log request
    
    * WIP: Adding id and hashing
    
    * ✨ Make newsletter #2839
    
    * Revert captcha removal #2839
    
    * Revert friendlycaptcha removal in form #2839
    
    * add code from staging #2839
    
    * Remove requestbody #2839
    
    * Add new field for formatted date #2839
    
    * Remove category from extra field #2839
    
    * Reintroduce extra field correctly #2839
    
    * Small cleanup #2839
    
    * Remove exclude of crude oil news #2839
    
    * Only Make for crude oil #2839
    
    * Revert "Only Make for crude oil #2839"
    
    This reverts commit d1ba216f966aa874ec8092f77eb782104e5f29cd.
    
    * :art: add support for norwegian tags and use tag id
    
    * :art: adjustment to category mapping
    
    * :art: try with title
    
    * :art: add magazine and change mapping
    
    * :art: wrong field
    
    * :art: try merge tag
    
    * :art: add query to rss for lang
    
    * :art: formatting
    
    * :art: format rss feed
    
    * :art: escape url
    
    * :art: logging
    
    * :art: update pubdate groq
    
    * :art: move types to dev dep
    
    * :art: add functions to groq
    
    * :art: set up old parallel distribution
    
    ---------
    
    Co-authored-by: Malin J. <millianapia@gmail.com>
    Co-authored-by: Borghild Selle <borghild.vikoyr@miles.no>

---
+++ web/pages/api/subscriptionNew.ts
@@ -0,0 +1,154 @@
+import axios from 'axios'
+
+export type SubscribeFormParameters = {
+  firstName: string
+  email: string
+  crudeOilAssays?: boolean
+  generalNews?: boolean
+  magazineStories?: boolean
+  stockMarketAnnouncements?: boolean
+  languageCode: string
+}
+
+const MAKE_SUBSCRIBER_API_BASE_URL = process.env.MAKE_SUBSCRIBER_API_BASE_URL
+const MAKE_NEWSLETTER_API_BASE_URL = process.env.MAKE_NEWSLETTER_API_BASE_URL
+const MAKE_API_KEY = process.env.MAKE_API_KEY || ''
+const SUBSCRIBER_LIST_ID_EN = process.env.MAKE_SUBSCRIBER_LIST_ID_EN
+const SUBSCRIBER_LIST_ID_NO = process.env.MAKE_SUBSCRIBER_LIST_ID_NO
+const MAKE_API_USER = process.env.MAKE_API_USERID || ''
+const MAKE_NEWSLETTER_ID_EN = process.env.MAKE_NEWSLETTER_ID_EN
+const MAKE_NEWSLETTER_ID_NO = process.env.MAKE_NEWSLETTER_ID_NO
+
+export type NewsDistributionParameters = {
+  languageCode?: string
+}
+
+const subscriberApi = axios.create({
+  baseURL: MAKE_SUBSCRIBER_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Basic ${Buffer.from(`${MAKE_API_USER}:${MAKE_API_KEY}`).toString('base64')}`,
+  },
+})
+
+/* Used when sign up and distribution */
+export const mapCategoryToId = (category: string, locale: 'no' | 'en') => {
+  if (locale === 'no') {
+    switch (category) {
+      case 'generalNews':
+        return 'generelle nyheter' 
+      case 'Company':
+        return 'generelle nyheter' 
+      case 'crudeOilAssays':
+        return 'crude oil assays' 
+      case 'Crude':
+        return 'crude oil assays'
+      case 'magazineStories':
+        return 'magasinsaker'
+      case 'stockMarketAnnouncements':
+        return 'børsmeldinger'
+      case 'Stock':
+        return 'børsmeldinger'
+    }
+  } else {
+    switch (category) {
+      case 'generalNews':
+        return 'general news' 
+      case 'Company':
+        return 'general news'
+      case 'crudeOilAssays':
+        return 'crude oil assays'
+      case 'Crude':
+        return 'crude oil assays'
+      case 'magazineStories':
+        return 'magazine stories'
+      case 'stockMarketAnnouncements':
+        return 'stock market announcements'
+      case 'Stock':
+        return 'stock market announcements'
+    }
+  }
+  return ''
+}
+
+/**
+ *  Subscribe a user using subscriber_list_id and tags
+ */
+export const signUp = async (formParameters: SubscribeFormParameters) => {
+  try {
+    const requestedTags: string[] = []
+    if (formParameters.stockMarketAnnouncements) {
+      formParameters.languageCode === 'no'
+        ? requestedTags.push(mapCategoryToId('stockMarketAnnouncements', 'no'))
+        : requestedTags.push(mapCategoryToId('stockMarketAnnouncements', 'en'))
+    }
+    if (formParameters.generalNews) {
+      formParameters.languageCode === 'no'
+        ? requestedTags.push(mapCategoryToId('generalNews', 'no'))
+        : requestedTags.push(mapCategoryToId('generalNews', 'en'))
+    }
+    if (formParameters.crudeOilAssays) {
+      formParameters.languageCode === 'no'
+        ? requestedTags.push(mapCategoryToId('crudeOilAssays', 'no'))
+        : requestedTags.push(mapCategoryToId('crudeOilAssays', 'en'))
+    }
+    if (formParameters.magazineStories) {
+      formParameters.languageCode === 'no'
+        ? requestedTags.push(mapCategoryToId('magazineStories', 'no'))
+        : requestedTags.push(mapCategoryToId('magazineStories', 'en'))
+    }
+
+    const requestBody = {
+      email: formParameters.email,
+      tags: requestedTags,
+    }
+
+    const response = await subscriberApi.post(
+      `/subscribers?subscriber_list_id=${
+        formParameters.languageCode === 'no' ? SUBSCRIBER_LIST_ID_NO : SUBSCRIBER_LIST_ID_EN
+      }&tag=merge`,
+      requestBody,
+    )
+
+    return response.status === 200
+  } catch (error: any) {
+    console.error('❌ Error in signUp:', {
+      message: error.message,
+      responseData: error.response?.data,
+      responseStatus: error.response?.status,
+      requestHeaders: error.config?.headers,
+    })
+    return false
+  }
+}
+
+const newsletterApi = axios.create({
+  baseURL: MAKE_NEWSLETTER_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Basic ${Buffer.from(`${MAKE_API_USER}:${MAKE_API_KEY}`).toString('base64')}`,
+  },
+})
+
+/**
+ *  Distribute a newsletter
+ */
+export const distribute = async (newsDistributionParameters: NewsDistributionParameters) => {
+  try {
+    const url = `${MAKE_NEWSLETTER_API_BASE_URL}/recurring_actions/${
+      newsDistributionParameters.languageCode === 'no' ? MAKE_NEWSLETTER_ID_NO : MAKE_NEWSLETTER_ID_EN
+    }/trigger`
+
+    const response = await newsletterApi.post(url)
+    return response.status === 200
+  } catch (error: any) {
+    console.error('❌ Error in distribute:', {
+      message: error.message,
+      responseData: error.response?.data,
+      responseStatus: error.response?.status,
+      requestHeaders: error.config?.headers,
+    })
+
+    return false
+  }
+}
