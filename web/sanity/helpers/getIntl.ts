66568ed97396eb58b1a3d1ddef43265ebb1effa7
Padmaja <52911293+padms@users.noreply.github.com>
2026-03-18 16:45:12 +0530

    Fix the text snippets

--- web/sanity/helpers/getIntl.ts
+++ web/sanity/helpers/getIntl.ts
@@ -2,14 +2,14 @@
 import { defaultLanguage } from '@/languageConfig'
 import { sanityFetch } from '@/sanity/lib/live'
 import formatTextSnippets from './formatTextSnippets'
-import { getNameFromLocale } from './localization'
+import { getNameFromIso } from './localization'
 
 export default async (locale: string) => {
   const { data: textSnippetsArray } = await sanityFetch({
     query: `*[_type == "textSnippet"]`,
   })
   const textSnippetsData = formatTextSnippets(textSnippetsArray ?? [])
-  const lang = getNameFromLocale(locale)
+  const lang = getNameFromIso(locale)
 
   return {
     locale: locale,
