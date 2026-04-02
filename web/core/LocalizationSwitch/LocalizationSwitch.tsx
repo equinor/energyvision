55609b4af3ad9376539d98feaa07354fdf3f8127
Padmaja <52911293+padms@users.noreply.github.com>
2026-04-02 14:53:59 +0530

    Sort by default locale in the language switch

--- web/core/LocalizationSwitch/LocalizationSwitch.tsx
+++ web/core/LocalizationSwitch/LocalizationSwitch.tsx
@@ -18,34 +18,40 @@ export const LocalizationSwitch = (_props: LocalizationSwitchProps) => {
 
   return (
     <ul className='flex items-center md:divide-x md:divide-dashed md:divide-gray-400'>
-      {slugs.map(obj => {
-        const language = languages.find(lang => lang.iso === obj.lang)
-        return (
-          <li
-            className={`flex items-center ${activeLocale === String(language?.locale) ? 'hidden md:block' : ''} `}
-            key={obj.lang}
-          >
-            <ButtonLink
-              variant='ghost'
-              href={obj.slug}
-              hrefLang={`${language?.iso}`}
-              className={` ${activeLocale === String(language?.iso) ? 'hidden md:block' : ''} flex flex-col items-stretch gap-0 px-2 text-xs`}
-            >
-              <span className='sr-only'>
-                {activeLocale === String(language?.iso)
-                  ? `${intl('current_language') ?? 'current language'}: ${language?.title}`
-                  : `${intl('switch_to')} ${language?.title}`}
-              </span>
-              <span
-                aria-hidden
-                className={`uppercase ${activeLocale === String(language?.iso) ? 'font-semibold' : 'font-normal'}`}
-              >
-                {language?.iso === 'en-GB' ? 'en' : language?.locale}
-              </span>
-            </ButtonLink>
-          </li>
+      {slugs
+        .sort(
+          (a, b) =>
+            Number(b.lang === defaultLanguage.iso) -
+            Number(a.lang === defaultLanguage.iso),
         )
-      })}
+        .map(obj => {
+          const language = languages.find(lang => lang.iso === obj.lang)
+          return (
+            <li
+              className={`flex items-center ${activeLocale === String(language?.locale) ? 'hidden md:block' : ''} `}
+              key={obj.lang}
+            >
+              <ButtonLink
+                variant='ghost'
+                href={obj.slug}
+                hrefLang={`${language?.iso}`}
+                className={` ${activeLocale === String(language?.iso) ? 'hidden md:block' : ''} flex flex-col items-stretch gap-0 px-2 text-xs`}
+              >
+                <span className='sr-only'>
+                  {activeLocale === String(language?.iso)
+                    ? `${intl('current_language') ?? 'current language'}: ${language?.title}`
+                    : `${intl('switch_to')} ${language?.title}`}
+                </span>
+                <span
+                  aria-hidden
+                  className={`uppercase ${activeLocale === String(language?.iso) ? 'font-semibold' : 'font-normal'}`}
+                >
+                  {language?.iso === 'en-GB' ? 'en' : language?.locale}
+                </span>
+              </ButtonLink>
+            </li>
+          )
+        })}
     </ul>
   )
 }
