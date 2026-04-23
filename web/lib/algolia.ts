0fcdb8e79513d8aecd6bbfb8a5082ab3782f9da7
Borghild Selle <borghild.vikoyr@miles.no>
2026-04-23 13:20:54 +0200

    :art: remove warning

--- web/lib/algolia.ts
+++ web/lib/algolia.ts
@@ -1,8 +1,8 @@
-import { liteClient } from 'algoliasearch/lite'
-import { createInMemoryCache } from '@algolia/cache-in-memory'
-import { createFallbackableCache } from '@algolia/cache-common'
 import { createBrowserLocalStorageCache } from '@algolia/cache-browser-local-storage'
-import { SearchOptions } from 'instantsearch.js'
+import { createFallbackableCache } from '@algolia/cache-common'
+import { createInMemoryCache } from '@algolia/cache-in-memory'
+import { liteClient } from 'algoliasearch/lite'
+import type { SearchOptions } from 'instantsearch.js'
 import { algolia } from './config'
 
 export const searchClient = (options?: SearchOptions) =>
