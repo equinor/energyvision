feb876a0a781631e8aae8ee09c195d15d7333617
Padmaja <52911293+padms@users.noreply.github.com>
2026-05-15 18:04:43 +0530

    Fix links in magasin

--- web/templates/magazine/Magazineroom.tsx
+++ web/templates/magazine/Magazineroom.tsx
@@ -1,10 +1,13 @@
 'use client'
+import { useLocale } from 'next-intl'
 import type { PortableTextBlock } from 'next-sanity'
 import { useMemo, useRef, useState } from 'react'
 import { Promotion } from '@/core/Promotion/Promotion'
 import { SimplePagination } from '@/core/SimplePagination/SimplePagination'
+import { defaultLanguage } from '@/languageConfig'
 import { chunkArray } from '@/lib/helpers/chunkArray'
 import Blocks from '@/portableText/Blocks'
+import { getLocaleFromIso } from '@/sanity/helpers/localization'
 import CardSkeleton from '@/sections/cards/CardSkeleton/CardSkeleton'
 import { HeroBlock, type HeroData, HeroTypes } from '@/sections/Hero/HeroBlock'
 import MagazineTagBar from '@/sections/MagazineTags/MagazineTagBar'
@@ -66,6 +69,8 @@ const MagazineRoom = ({
     setIsLoading(false)
   }
 
+  const iso = useLocale()
+
   return (
     <main className='mx-auto flex w-full max-w-fullwidth flex-col pt-topbar'>
       {/*@ts-ignore*/}
@@ -87,18 +92,23 @@ const MagazineRoom = ({
               </li>
             ))}
           {!isLoading &&
-            pagedList?.[pageIdx]?.map(article => (
-              <li key={article.id}>
-                <Promotion
-                  href={article?.slug}
-                  image={article?.hero?.figure?.image}
-                  title={article?.title}
-                  ingress={article?.ingress}
-                  type='extended'
-                />
-                {/* <MagazineCard data={article} /> */}
-              </li>
-            ))}
+            pagedList?.[pageIdx]?.map(article => {
+              const locale =
+                iso !== defaultLanguage.iso ? getLocaleFromIso(iso) : ''
+              const href = (article?.slug && '/' + locale + article?.slug) || ''
+              return (
+                <li key={article.id}>
+                  <Promotion
+                    href={href}
+                    image={article?.hero?.figure?.image}
+                    title={article?.title}
+                    ingress={article?.ingress}
+                    type='extended'
+                  />
+                  {/* <MagazineCard data={article} /> */}
+                </li>
+              )
+            })}
         </ul>
         {magazineArticles?.length > 12 && (
           <SimplePagination
