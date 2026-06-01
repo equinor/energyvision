7236b53af78d5591f98202ae7af1cac665d5fb0f
Borghild Selle <borghild.vikoyr@miles.no>
2026-06-01 13:44:55 +0200

    :art: fix footerlinks

--- web/sections/Footer/Footer.tsx
+++ web/sections/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import type { HTMLAttributes } from 'react'
 import FooterLink, { type FooterLinkProps } from '@/core/Link/FooterLink'
 import { Typography } from '@/core/Typography'
-
+import { getUrlFromAction } from '@/lib/helpers/getUrlFromAction'
 import type { FooterColumns } from '../../types/index'
 
 type FooterProps = {
@@ -9,6 +9,7 @@ type FooterProps = {
 } & HTMLAttributes<HTMLElement>
 
 const Footer = ({ footerColumns = [] }: FooterProps) => {
+  console.log('footerColumns', footerColumns)
   return (
     <footer
       className={`dark mx-auto min-h-12 w-full max-w-fullwidth bg-slate-blue-95 px-0 py-6`}
@@ -29,7 +30,14 @@ const Footer = ({ footerColumns = [] }: FooterProps) => {
               </Typography>
               <div className='md:grid-y-0 grid grid-cols-2 items-start gap-x-8 gap-y-2 md:flex md:flex-col'>
                 {linkList?.map((footerLink: FooterLinkProps) => {
-                  return <FooterLink {...footerLink} key={footerLink?.id} />
+                  const footerLinkProps = {
+                    ...footerLink,
+                    href:
+                      footerLink?.href ?? getUrlFromAction(footerLink) ?? '/',
+                  }
+                  return (
+                    <FooterLink {...footerLinkProps} key={footerLink?.id} />
+                  )
                 })}
               </div>
             </section>
