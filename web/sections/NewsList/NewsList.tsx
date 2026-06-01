3d8a4f2b64ca6445b9783cd3906e03a60942a5fd
Padmaja <52911293+padms@users.noreply.github.com>
2026-06-01 13:15:47 +0530

    Fix dot in the list #3306

--- web/sections/NewsList/NewsList.tsx
+++ web/sections/NewsList/NewsList.tsx
@@ -57,7 +57,7 @@ const NewsList = ({
           {title && <Blocks value={title} variant='h2' />}
         </div>
 
-        <div className='grid auto-rows-fr gap-x-6 gap-y-8 3xl:px-layout-md px-layout-sm sm:grid-cols-2 xl:grid-cols-3'>
+        <ul className='grid auto-rows-fr gap-x-6 gap-y-8 3xl:px-layout-md px-layout-sm sm:grid-cols-2 xl:grid-cols-3'>
           {pagedArticles.map(newsItem => {
             const locale =
               iso !== defaultLanguage.name ? getLocaleFromIso(iso) : ''
@@ -87,7 +87,7 @@ const NewsList = ({
               </li>
             )
           })}
-        </div>
+        </ul>
         <Pagination
           totalPages={totalPages}
           onPageChange={(pageNumber: number) => handlePageChange(pageNumber)}
