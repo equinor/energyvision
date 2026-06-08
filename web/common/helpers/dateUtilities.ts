ddc6d47b3bf6368b79012d2f1884518659049431
Borghild Selle <borghild.vikoyr@miles.no>
2026-06-08 12:07:40 +0200

    :art: clean up

--- web/common/helpers/dateUtilities.ts
+++ web/common/helpers/dateUtilities.ts
@@ -24,27 +24,6 @@ export const toUTCDateParts = (datetime: Date): number[] => {
   ]
 }
 
-/* export const cestToUtcConverter = (date: string, time: string = '00:00') => {
-  // 1. Combine into a clean local ISO format (YYYY-MM-DDTHH:mm:ss)
-  const localIsoString = `${date}T${time.padEnd(5, ':00')}`
-
-  // 2. Create a standard Date object from the string
-  const baseDate = new Date(localIsoString)
-
-  // 3. Use toLocaleString to find the precise offset difference for Oslo
-  const tzString = baseDate.toLocaleString('en-GB', {
-    timeZone: 'Europe/Oslo',
-  })
-  const tzDate = new Date(tzString)
-
-  // 4. Calculate the time difference in milliseconds and apply it
-  const offset = baseDate.getTime() - tzDate.getTime()
-  const utcDate = new Date(baseDate.getTime() + offset)
-  console.log('utcDate', utcDate)
-
-  return utcDate?.toISOString()
-} */
-
 export const getEventDates = (eventDate: EventDateType | undefined) => {
   let start: string | null = null
   let end: string | null = null
