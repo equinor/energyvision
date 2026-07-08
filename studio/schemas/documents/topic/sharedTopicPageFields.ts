d442e7b66fd85d2768bdd39f0eaa4e80ee90c11b
Borghild Selle <borghild.vikoyr@miles.no>
2026-07-08 12:37:53 +0200

    :art: merge issue

--- studio/schemas/documents/topic/sharedTopicPageFields.ts
+++ studio/schemas/documents/topic/sharedTopicPageFields.ts
@@ -74,7 +74,6 @@ export const content = {
     Flags.IS_DEV && { type: 'anchorSearch' },
     { type: 'promoteTopicsV2' },
     { type: 'promoteExternalLinkV2' },
-    Flags.IS_DEV && { type: 'anchorSearch' },
   ].filter(e => e),
   components: {
     input: RolesBasedArrayInput,
