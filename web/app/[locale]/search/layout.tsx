506cf0a399d3e7243ba825b9f35067e9d10b7f1d
Padmaja <52911293+padms@users.noreply.github.com>
2026-07-28 12:50:09 +0530

    Make search page static #3842

---
+++ web/app/[locale]/search/layout.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { Icon } from '@equinor/eds-core-react'
+import { close } from '@equinor/eds-icons'
+import { useRouter } from 'next/router'
+import { LogoLink } from '@/core/Link/LogoLink'
+import { NavTopbar } from '@/sections/SiteMenu/NavTopbar'
+import { TopbarDropdown } from '@/sections/SiteMenu/TopbarDropdown'
+
+export const dynamic = 'force-static'
+
+export function SearchLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
+  return (
+    <div
+      data-search-page='true'
+      className='dark w-full overflow-auto bg-slate-blue-95'
+    >
+      <TopbarDropdown variant='dark' className='relative'>
+        <NavTopbar>
+          <LogoLink />
+          <button
+            type='button'
+            aria-expanded={true}
+            aria-label='Close search'
+            onClick={() => {
+              router.back()
+            }}
+            className={`focus-visible:envis-outline-invert rounded-full p-3 text-white-100 hover:bg-moss-green-50 hover:text-slate-blue-95 focus:outline-none active:scale-99 active:bg-white-100/20`}
+          >
+            <Icon size={24} data={close} />
+          </button>
+        </NavTopbar>
+      </TopbarDropdown>
+      {children}
+    </div>
+  )
+}
