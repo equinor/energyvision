afc019491ae972d7e5cbff301d9f9e86c81b35b7
Borghild Selle <borghild.vikoyr@miles.no>
2026-08-27 14:42:40 +0200

    :art: dont use tunnel

--- web/next.config.ts
+++ web/next.config.ts
@@ -24,7 +24,6 @@ const sentryConfig = {
   silent: true,
   //disableLogger: true,
   hideSourceMaps: true,
-  tunnelRoute: '/sentry-tunnel',
 }
 
 //TODO: Find the Redirect type from config that is not in /dist.
