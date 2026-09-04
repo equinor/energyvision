c41824bf90bb5c919a3eba8b3ba65d43c0857754
Padmaja <52911293+padms@users.noreply.github.com>
2026-09-04 15:41:53 +0530

    No sentry on preprod #3976 (#3977)
    * Check by url #3976
    
    * Conditionally init sentry #3976

--- web/sentry.server.config.ts
+++ web/sentry.server.config.ts
@@ -2,33 +2,36 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs'
-import { dataset } from './languageConfig'
+import * as Sentry from '@sentry/nextjs';
+import { dataset } from './languageConfig';
 import {
   allowUrlPattern,
   sentryBeforeSend,
   sentryDenyUrls,
   sentryIgnoreErrors,
-} from './sentry.shared'
+} from './sentry.shared';
 
-const isProd = process.env.NODE_ENV === 'production' && dataset === 'global'
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  enabled: isProd,
-  tracesSampleRate: 0.01,
-  profilesSampleRate: 0,
-  replaysSessionSampleRate: 0,
-  replaysOnErrorSampleRate: 0,
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-  includeLocalVariables: false,
-  ignoreErrors: sentryIgnoreErrors,
-  allowUrls: [allowUrlPattern],
-  denyUrls: sentryDenyUrls,
-  beforeBreadcrumb(breadcrumb, hint) {
-    return breadcrumb.category === 'ui.click' ? null : breadcrumb
-  },
-  beforeSend: sentryBeforeSend,
-})
+const isProd =
+  process.env.NODE_ENV === 'production' &&
+  dataset === 'global' &&
+  !process.env.RADIX_PUBLIC_DOMAIN_NAME?.includes('preprod');
+isProd &&
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    environment: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    enabled: isProd,
+    tracesSampleRate: 0.01,
+    profilesSampleRate: 0,
+    replaysSessionSampleRate: 0,
+    replaysOnErrorSampleRate: 0,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+    includeLocalVariables: false,
+    ignoreErrors: sentryIgnoreErrors,
+    allowUrls: [allowUrlPattern],
+    denyUrls: sentryDenyUrls,
+    beforeBreadcrumb(breadcrumb, hint) {
+      return breadcrumb.category === 'ui.click' ? null : breadcrumb;
+    },
+    beforeSend: sentryBeforeSend,
+  });
