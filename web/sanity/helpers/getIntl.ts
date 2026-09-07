208cc7b55093487f5758c642d2a0e1797bd40227
Padmaja <52911293+padms@users.noreply.github.com>
2026-09-07 12:39:11 +0530

    Redis cache #3850

--- web/sanity/helpers/getIntl.ts
+++ web/sanity/helpers/getIntl.ts
@@ -1,24 +1,24 @@
 //import translations from '../interface/translations.json'
 
-import { defaultLanguage } from '@/languageConfig'
-import { routeSanityFetch } from '@/sanity/lib/fetch'
-import formatTextSnippets from './formatTextSnippets'
-import { getNameFromIso } from './localization'
+import { defaultLanguage } from '@/languageConfig';
+import { routeSanityFetch } from '@/sanity/lib/fetch';
+import formatTextSnippets from './formatTextSnippets';
+import { getNameFromIso } from './localization';
 
 export default async (locale: string) => {
-  'use cache'
+  'use cache: remote';
   const { data: textSnippetsArray } = await routeSanityFetch({
     query: `*[_type == "textSnippet"]`,
     tags: ['textSnippet'],
     requestTag: 'text-snippets',
-  })
-  const textSnippetsData = formatTextSnippets(textSnippetsArray ?? [])
-  const lang = getNameFromIso(locale)
+  });
+  const textSnippetsData = formatTextSnippets(textSnippetsArray ?? []);
+  const lang = getNameFromIso(locale);
 
   return {
     locale: locale,
     //@ts-ignore:config file, dunno why undefined possible
     defaultLocale: defaultLanguage.locale,
     messages: textSnippetsData[lang],
-  }
-}
+  };
+};
