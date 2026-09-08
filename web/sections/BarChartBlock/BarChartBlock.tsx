834ae00f9cff224100c1455549f4b1fe906c4cc6
Borghild Selle <borghild.vikoyr@miles.no>
2026-09-08 08:11:05 +0200

    :art: update

--- web/sections/BarChartBlock/BarChartBlock.tsx
+++ web/sections/BarChartBlock/BarChartBlock.tsx
@@ -26,21 +26,19 @@ const BarChartBlock = ({
   useTextWidth,
 }: BarChartBlockProps) => {
   return (
-    <section
-      className={twMerge(
-        `px-layout-sm ${useLayoutMd && !useTextWidth ? 'lg:px-layout-md' : 'lg:px-layout-lg'} ${useTextWidth ? 'max-w-text' : ''} pb-page-content`,
-        className,
-      )}
-      id={anchor}
-    >
-      {title && (
-        <Blocks
-          variant="h2"
-          value={title}
-          className={`${hideTitle ? 'sr-only' : ''}`}
-        />
-      )}
-      <div className="flex">
+    <section className={twMerge(`pb-page-content`, className)} id={anchor}>
+      <div className="px-layout-sm lg:px-layout-lg">
+        {title && (
+          <Blocks
+            variant="h2"
+            value={title}
+            className={`${hideTitle ? 'sr-only' : ''}`}
+          />
+        )}
+      </div>
+      <div
+        className={`flex px-layout-sm ${useLayoutMd && !useTextWidth ? 'lg:px-layout-md' : 'lg:px-layout-lg'} ${useTextWidth ? 'max-w-text' : ''}`}
+      >
         {charts &&
           charts?.length > 0 &&
           charts?.map((chart: BarChartProps) => (
