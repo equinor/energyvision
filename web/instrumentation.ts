c9c69cf74bc5d8fec1c96e854ba457726715c190
Padmaja <52911293+padms@users.noreply.github.com>
2026-09-09 16:28:20 +0530

    Increase event listener limit #4001 (#4002)

--- web/instrumentation.ts
+++ web/instrumentation.ts
@@ -1,11 +1,15 @@
-import * as Sentry from '@sentry/nextjs'
+import events from 'node:events';
+import * as Sentry from '@sentry/nextjs';
+
+// Increase global limit from 10 to 30 to accommodate both SDKs final try for avoiding max event listeners warning
+events.EventEmitter.defaultMaxListeners = 30;
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config')
+    await import('./sentry.server.config');
   }
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config')
+    await import('./sentry.edge.config');
   }
 }
-export const onRequestError = Sentry.captureRequestError
+export const onRequestError = Sentry.captureRequestError;
