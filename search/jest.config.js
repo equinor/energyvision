d502b46208e526e7c1d3159b3b8362efe82a1ec5
Borghild Selle <borghild.vikoyr@miles.no>
2026-09-18 11:12:12 +0200

    :art: fix tests

--- search/jest.config.js
+++ search/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['./dist'],
-}
+  moduleNameMapper: {
+    '^@sanity/client$': '<rootDir>/test/mocks/sanity-client.ts',
+  },
+};
